7078d988fdb30201048f9933d417609c2b78ffab
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2011-02-12 09:21:11 +0000

    [GEOT-3429] PropertyIsNotEqualTo does not behave like Not(PropertyIsEqualTo) like it should
    git-svn-id: http://svn.osgeo.org/geotools/branches/2.7.x@36581 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/main/src/main/java/org/geotools/filter/IsNotEqualToImpl.java
+++ modules/library/main/src/main/java/org/geotools/filter/IsNotEqualToImpl.java
@@ -16,13 +16,14 @@
  */
 package org.geotools.filter;
 
-import org.geotools.util.Converters;
 import org.opengis.filter.FilterVisitor;
 import org.opengis.filter.PropertyIsNotEqualTo;
 import org.opengis.filter.expression.Expression;
 
 public class IsNotEqualToImpl extends CompareFilterImpl
 	implements PropertyIsNotEqualTo {
+    
+    IsEqualsToImpl delegate;
 
 	protected IsNotEqualToImpl(org.opengis.filter.FilterFactory factory) {
 		this(factory,null,null);
@@ -34,6 +35,7 @@ public class IsNotEqualToImpl extends CompareFilterImpl
 	
 	protected IsNotEqualToImpl(org.opengis.filter.FilterFactory factory, Expression expression1, Expression expression2, boolean matchCase ) {
 		super(factory, expression1, expression2, matchCase);
+		delegate = new IsEqualsToImpl(factory, expression1, expression2, matchCase);
 		
 		//backwards compat with old type system
 		this.filterType = COMPARE_NOT_EQUALS;
@@ -41,28 +43,7 @@ public class IsNotEqualToImpl extends CompareFilterImpl
 
 	//@Override
 	public boolean evaluate(Object feature) {
-		Object[] values = eval( feature );
-		Object value1 = values[ 0 ];
-		Object value2 = values[ 1 ];
-		
-		boolean notEqual = (value1 == null && value2 != null) ||
-			(value1 != null && value2 == null) ||
-		    (value1 != null && !value1.equals( value2 ));
-		
-		if ( !notEqual ) {
-			return false;
-		}
-		
-		//not equal, case insensitive?
-		if ( !isMatchingCase() && value1 != null && value2 != null ) {
-			//fall back to string and check the case insensitive flag
-			String s1 = Converters.convert(value1, String.class );
-			String s2 = Converters.convert(value2, String.class );
-			
-			return !s1.equalsIgnoreCase( s2 );
-		}
-		
-		return true;
+		return !delegate.evaluate(feature);
 	}
 	
 	public Object accept(FilterVisitor visitor, Object extraData) {

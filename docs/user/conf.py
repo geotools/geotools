6ac516c8f715e20e7fea74491614de2c60ce96b0
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2011-02-20 12:18:17 +0000

    fix up initial user documentation index page to direct hiether and yon
    git-svn-id: http://svn.osgeo.org/geotools/trunk@36640 e5c1c795-43da-0310-a71f-fac65c449510

--- docs/user/conf.py
+++ docs/user/conf.py
@@ -2,4 +2,13 @@ import sys, os
 sys.path.append(os.path.abspath('..'))
 from common import *
 
+# The suffix of source filenames.
+source_suffix = '.txt'
+
+# The encoding of source files.
+#source_encoding = 'utf-8'
+
+# The master toctree document.
+master_doc = 'index'
+
 html_title='GeoTools %s User Guide' % release

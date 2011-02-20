5e9b4d9b54066b2edc83faad40e88ea79320b27d
moovida <moovida@e5c1c795-43da-0310-a71f-fac65c449510>
2011-02-20 15:15:50 +0000

    remove unneeded stuff
    git-svn-id: http://svn.osgeo.org/geotools/trunk@36645 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/gt-swt/src/main/java/test/FileTreeLabelProvider.java
+++
@@ -1,9 +0,0 @@
-package test;
-import java.io.*;
-import org.eclipse.jface.viewers.*;
-
-public class FileTreeLabelProvider extends LabelProvider {
-    public String getText( Object element ) {
-        return ((File) element).getName();
-    }
-}

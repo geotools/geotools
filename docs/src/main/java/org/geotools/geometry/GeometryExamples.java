333d8a25292224e07384b2cb5b1f3fd6d4bad071
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2011-03-16 10:21:11 +0000

    outline of jts geometry creation for mbedward to check
    git-svn-id: http://svn.osgeo.org/geotools/trunk@36755 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ docs/src/main/java/org/geotools/geometry/GeometryExamples.java
@@ -0,0 +1,57 @@
+package org.geotools.geometry;
+
+import org.geotools.geometry.jts.Geometries;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
+
+public class GeometryExamples {
+
+    private static final double MAX_DISTANCE = 0.001;
+    
+    public void createPoint(){
+        // createPoint start
+        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory( null );
+        
+        Coordinate coord = new Coordinate( 1, 1 );
+        Point point = geometryFactory.createPoint( coord );
+        // createPoint end
+    }
+    
+    public void createPointWKT() throws ParseException{
+        // createPointWKT start
+        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory( null );
+        
+        WKTReader reader = new WKTReader( geometryFactory );
+        Point point = (Point) reader.read("POINT (1 1)");
+        // createPointWKT end
+    }
+    // geometries start
+    public boolean hit(Point point, Geometry geometry) {
+        switch (Geometries.get(geometry)) {
+            case POINT:
+            case MULTIPOINT:
+            case LINESTRING:
+            case MULTILINESTRING:
+                // Test if p is within a threshold distance
+                return geometry.isWithinDistance(point, MAX_DISTANCE);
+                
+            case POLYGON:
+            case MULTIPOLYGON:
+                // Test if the polygonal geometry contains p
+                return geometry.contains(point);
+                
+            default:
+                // For simplicity we just assume distance check will work for other
+                // types (e.g. GeometryCollection) in this example
+                return geometry.isWithinDistance(point, MAX_DISTANCE);
+        }
+    }
+    // geometries end
+
+}

fb51dd4a122b7b4ead7349e6347c6e11d8e9792c
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2011-03-19 08:15:08 +0000

    Drag over mebdwards javadoc and deprecations from trunk
    git-svn-id: http://svn.osgeo.org/geotools/branches/2.7.x@36768 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/main/src/test/java/org/geotools/data/FeatureLockTest.java
+++ modules/library/main/src/test/java/org/geotools/data/FeatureLockTest.java
@@ -16,7 +16,9 @@
  */
 package org.geotools.data;
 
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Test of LockingAPI FeatureLock data object.
@@ -27,69 +29,25 @@ import junit.framework.TestCase;
  * @source $URL$
  * @version CVS Version
  */
-public class FeatureLockTest extends TestCase {
+public class FeatureLockTest {
     String lockName;
     long lockDuration;
     FeatureLock lock;
-    /**
-     * Constructor for LockTest.
-     * @param arg0
-     */
-    public FeatureLockTest(String arg0) {
-        super(arg0);
-    }
-    /**
-     * Sets up lock objects for use.
-     * </p>
-     * @throws java.lang.Exception
-     * 
-     * @see junit.framework.TestCase#setUp()
-     */
-    protected void setUp() throws Exception {
-        lockDuration = 4*60*60*60; // 4 hours/60 min/60 sec/60 milliseconds
+
+    @Before
+    public void setUp() throws Exception {
+        lockDuration = 240; // 240 minutes
         lockName = "TestLock";
-        lock = DefaultFeatureLockFactory.createTestLock( lockName, lockDuration ); 
-    }
-    /**
-     * Clears lock objects.
-     * 
-     * @throws java.lang.Exception
-     * 
-     * @see junit.framework.TestCase#tearDown()
-     */
-    protected void tearDown() throws Exception {
-        super.tearDown();
-        lockName = null;
-        lock = null;
+        lock = new FeatureLock(lockName, lockDuration);
     }
 
+    @Test
     public void testGetID() {        
         assertEquals( "lockName", lockName, lock.getAuthorization() );
     }
+
+    @Test
     public void testGetExpire() {        
         assertEquals( "lockDate", lockDuration, lock.getDuration() ); 
     }
-    /*
-     * Test for FeatureLock generate(int)
-     */
-    public void testGenerateint() {
-        FeatureLock lock = FeatureLockFactory.generate( "Test", 3600 );        
-    }
-    public void testNextIDNumberDate(){
-        DefaultFeatureLockFactory.seedIdNumber( 0 );
-        long number1 = DefaultFeatureLockFactory.nextIdNumber( lockDuration );
-        long number2 = DefaultFeatureLockFactory.nextIdNumber( lockDuration );
-        long number3 = DefaultFeatureLockFactory.nextIdNumber( lockDuration );        
-        assertFalse( "lockDate:"+number1, number1 == number2 );
-        assertFalse( "lockDate:"+number1, number1 == number3 );
-        assertFalse( "lockDate:"+number1, number2 == number3 );
-                
-    }
-    /*
-     * Test for FeatureLock generate(String, long)
-     */
-    public void testGenerateStringLong() {
-        FeatureLock lock = FeatureLockFactory.generate( "Test", lockDuration  );
-        assertTrue( lock.getAuthorization().startsWith("Test") );
-    }    
 }

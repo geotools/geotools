bdd12998257b1fa6e9e07c819443cdd7b629dc45
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2011-03-22 08:00:40 +0000

    [GEOT-3481] Function lookup is not thread safe
    git-svn-id: http://svn.osgeo.org/geotools/trunk@36782 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/main/src/test/java/org/geotools/filter/function/FunctionFactoryTest.java
+++ modules/library/main/src/test/java/org/geotools/filter/function/FunctionFactoryTest.java
@@ -16,13 +16,17 @@
  */
 package org.geotools.filter.function;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.factory.FactoryIteratorProvider;
@@ -33,6 +37,7 @@ import org.geotools.filter.capability.FunctionNameImpl;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.opengis.filter.FilterFactory;
 import org.opengis.filter.capability.FunctionName;
 import org.opengis.filter.expression.Expression;
 import org.opengis.filter.expression.Function;
@@ -102,5 +107,38 @@ public class FunctionFactoryTest {
         assertNotNull(factory);
         Function f = factory.function("foo", null, null);
         assertNotNull(f);
+    }
+    
+    /**
+     * GEOT-3841
+     * @throws Exception
+     */
+    @Test
+    public void testThreadedFunctionLookup() throws Exception {
+        final FilterFactory ff = CommonFactoryFinder.getFilterFactory(null);
+        ExecutorService es = Executors.newCachedThreadPool();
+        List<Future<Exception>> tests = new ArrayList<Future<Exception>>();
+        for (int i = 0; i < 100; i++) {
+            Future<Exception> f = es.submit(new Callable<Exception>() {
+
+                public Exception call() throws Exception {
+                    try {
+                        ff.function("Length", ff.property("."));
+                        return null;
+                    } catch (Exception e) {
+                        return e;
+                    }
+                }
+            });
+            tests.add(f);
+        }
+        for (Future<Exception> future : tests) {
+            Exception e = future.get();
+            if(e != null) {
+                e.printStackTrace();
+            }
+            assertNull("No exception was expected", e);
+        }
+        es.shutdown();
     }
 }

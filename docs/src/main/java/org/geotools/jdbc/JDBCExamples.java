a8240b795dc1bf904f4406192e1a4dd982c771e6
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2011-04-06 15:22:18 +0000

    finished up gt-jdbc docs; and started on rendering/style
    git-svn-id: http://svn.osgeo.org/geotools/trunk@36857 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ docs/src/main/java/org/geotools/jdbc/JDBCExamples.java
@@ -0,0 +1,36 @@
+package org.geotools.jdbc;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+
+public class JDBCExamples {
+
+void h2Example() throws IOException {
+    // h2Example start
+    Map<String,Object> params = new HashMap<String,Object>();
+    params.put("dbtype", "h2");
+    params.put("database", "geotools");
+    
+    DataStore datastore = DataStoreFinder.getDataStore(params);
+    // h2Example end
+}
+
+void postgisExample() throws IOException {
+    // postgisExample start
+    Map<String,Object> params = new HashMap<String,Object>();
+    params.put( "dbtype", "postgis");
+    params.put( "host", "localhost");
+    params.put( "port", 5432);
+    params.put( "schema", "public");
+    params.put( "database", "database");
+    params.put( "user", "postgres");
+    params.put( "passwd", "postgres");
+    
+    DataStore dataStore=DataStoreFinder.getDataStore(params);
+    // postgisExample end
+}
+}

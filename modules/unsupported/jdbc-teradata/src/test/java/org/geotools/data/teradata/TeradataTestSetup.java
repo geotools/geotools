b32f22ddafe9cc9c4b2e2395a7b92633db87423f
sbrunner <sbrunner@e5c1c795-43da-0310-a71f-fac65c449510>
2011-04-11 13:17:44 +0000

    add index
    git-svn-id: http://svn.osgeo.org/geotools/trunk@36890 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/jdbc-teradata/src/test/java/org/geotools/data/teradata/TeradataTestSetup.java
+++ modules/unsupported/jdbc-teradata/src/test/java/org/geotools/data/teradata/TeradataTestSetup.java
@@ -72,18 +72,51 @@ public class TeradataTestSetup extends JDBCTestSetup {
 
         runSafe("DELETE FROM SYSSPATIAL.GEOMETRY_COLUMNS WHERE F_TABLE_NAME = 'ft1'");
         runSafe("DELETE FROM SYSSPATIAL.GEOMETRY_COLUMNS WHERE F_TABLE_NAME = 'ft2'");
+		runSafe("DROP TRIGGER \"ft1_geometry_mi\"");
+		runSafe("DROP TRIGGER \"ft1_geometry_mu\"");
+		runSafe("DROP TRIGGER \"ft1_geometry_md\"");
+        runSafe("DROP TABLE \"ft1_geometry_idx\"");
         runSafe("DROP TABLE \"ft1\"");
+		runSafe("DROP TRIGGER \"ft2_geometry_mi\"");
+		runSafe("DROP TRIGGER \"ft2_geometry_mu\"");
+		runSafe("DROP TRIGGER \"ft2_geometry_md\"");
+        runSafe("DROP TABLE \"ft2_geometry_idx\"");
         runSafe("DROP TABLE \"ft2\"");
 
         run("CREATE TABLE \"ft1\"(" //
-                + "\"id\" PRIMARY KEY not null generated always as identity (start with 0) integer, " //
+                + "\"id\" PRIMARY KEY not null integer, " //
                 + "\"geometry\" ST_GEOMETRY, " //
                 + "\"intProperty\" int," //
                 + "\"doubleProperty\" double precision, " //
                 + "\"stringProperty\" varchar(200) casespecific)");
         run("INSERT INTO SYSSPATIAL.GEOMETRY_COLUMNS (F_TABLE_CATALOG, F_TABLE_SCHEMA, F_TABLE_NAME, F_GEOMETRY_COLUMN, COORD_DIMENSION, SRID, GEOM_TYPE) VALUES ('" + fixture.getProperty("database") + "', '" + fixture.getProperty("schema") + "', 'ft1', 'geometry', 2, 1619, 'POINT')");
-//        run("CREATE INDEX FT1_GEOMETRY_INDEX ON \"ft1\" USING GIST (\"geometry\") ");
-
+		run("CREATE MULTISET TABLE \"ft1_geometry_idx\""
+				+ " (id INTEGER NOT NULL, cellid INTEGER NOT NULL) PRIMARY INDEX (cellid)");
+		run("CREATE TRIGGER \"ft1_geometry_mi\" AFTER INSERT ON ft1"
+				+ "  REFERENCING NEW TABLE AS nt"
+				+ "  FOR EACH STATEMENT"
+				+ "  BEGIN ATOMIC"
+				+ "  ("
+				+ "    INSERT INTO \"ft1_geometry_idx\""
+				+ "    SELECT"
+				+ "    id,"
+				+ "    sysspatial.tessellate_index ("
+				+ "      \"geom\".ST_Envelope().ST_ExteriorRing().ST_PointN(1).ST_X(),"
+				+ "      \"geom\".ST_Envelope().ST_ExteriorRing().ST_PointN(1).ST_Y(),"
+				+ "      \"geom\".ST_Envelope().ST_ExteriorRing().ST_PointN(3).ST_X(),"
+				+ "      \"geom\".ST_Envelope().ST_ExteriorRing().ST_PointN(3).ST_Y(),"
+				+ "      -180, -90, 180, 90, 100, 100, 1, 0.01, 0)"
+				+ "    FROM nt;" 
+				+ "  ) " 
+				+ "END");
+		run("CREATE TRIGGER \"ft1_geometry_md\" AFTER DELETE ON \"ft1\""
+				+ "  REFERENCING OLD TABLE AS ot"
+				+ "  FOR EACH STATEMENT"
+				+ "  BEGIN ATOMIC"
+				+ "  ("
+				+ "    DELETE FROM \"ft1_geometry_idx\" WHERE ID IN (SELECT ID from ot);"
+				+ "  )" 
+				+ "END");
         run("INSERT INTO \"ft1\" VALUES(0, 'POINT(0 0)', 0, 0.0, 'zero')");
         run("INSERT INTO \"ft1\" VALUES(1, 'POINT(1 1)', 1, 1.1, 'one')");
         run("INSERT INTO \"ft1\" VALUES(2, 'POINT(2 2)', 2, 2.2, 'two')");

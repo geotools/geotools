2de2a2180a405b89e8c80863079556b83e092ccd
sbrunner <sbrunner@e5c1c795-43da-0310-a71f-fac65c449510>
2011-04-12 10:07:36 +0000

    fix date
    git-svn-id: http://svn.osgeo.org/geotools/trunk@36909 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/jdbc-teradata/src/test/java/org/geotools/data/teradata/TeradataDateTestSetup.java
+++ modules/unsupported/jdbc-teradata/src/test/java/org/geotools/data/teradata/TeradataDateTestSetup.java
@@ -15,10 +15,10 @@ public class TeradataDateTestSetup extends JDBCDateTestSetup {
         run("CREATE TABLE dates (d DATE, dt TIMESTAMP, t TIME)");
 
         //_date('1998/05/31:12:00:00AM', 'yyyy/mm/dd:hh:mi:ssam'));
-
-        run("INSERT INTO DATES VALUES (DATE '1969-12-23' (format 'yyyy-MM-dd'), '2009-06-28 15:12:41' (TIMESTAMP, format 'YYYY-MM-ddBHH:mi:ss'), '15:12:41' (TIMESTAMP, format 'HH:mi:ss')  );");
-        run("INSERT INTO DATES VALUES (DATE '2009-01-15' (format 'yyyy-MM-dd'), '2009-01-15 13:10:12' (TIMESTAMP, format 'YYYY-MM-ddBHH:mi:ss'), '13:10:12' (TIMESTAMP, format 'HH:mi:ss')  );");
-        run("INSERT INTO DATES VALUES (DATE '2009-09-29' (format 'yyyy-MM-dd'), '2009-09-29 17:54:23' (TIMESTAMP, format 'YYYY-MM-ddBHH:mi:ss'), '17:54:23' (TIMESTAMP, format 'HH:mi:ss')  );");
+        
+        run("INSERT INTO DATES VALUES ('1969-12-23' (DATE, format 'yyyy-MM-dd'), '2009-06-28 15:12:41' (TIMESTAMP, format 'YYYY-MM-ddBHH:mi:ss'), '15:12:41' (TIME, format 'HH:mi:ss') );");
+        run("INSERT INTO DATES VALUES ('2009-01-15' (DATE, format 'yyyy-MM-dd'), '2009-01-15 13:10:12' (TIMESTAMP, format 'YYYY-MM-ddBHH:mi:ss'), '13:10:12' (TIME, format 'HH:mi:ss') );");
+        run("INSERT INTO DATES VALUES ('2009-09-29' (DATE, format 'yyyy-MM-dd'), '2009-09-29 17:54:23' (TIMESTAMP, format 'YYYY-MM-ddBHH:mi:ss'), '17:54:23' (TIME, format 'HH:mi:ss') );");
 
     }
 

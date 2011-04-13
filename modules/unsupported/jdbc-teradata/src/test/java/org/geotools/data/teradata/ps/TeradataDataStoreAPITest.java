56dd0b683f7973b63e95a86838cd7a188770b2e4
sbrunner <sbrunner@e5c1c795-43da-0310-a71f-fac65c449510>
2011-04-13 17:02:48 +0000

    add big geom test
    git-svn-id: http://svn.osgeo.org/geotools/trunk@36926 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/jdbc-teradata/src/test/java/org/geotools/data/teradata/ps/TeradataDataStoreAPITest.java
+++ modules/unsupported/jdbc-teradata/src/test/java/org/geotools/data/teradata/ps/TeradataDataStoreAPITest.java
@@ -16,9 +16,26 @@
  */
 package org.geotools.data.teradata.ps;
 
+import java.io.IOException;
+import java.util.Collections;
+
+import org.geotools.data.DataUtilities;
+import org.geotools.data.FeatureReader;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureStore;
 import org.geotools.data.teradata.TeradataDataStoreAPITestSetup;
+import org.geotools.feature.IllegalAttributeException;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.jdbc.JDBCDataStoreAPITest;
 import org.geotools.jdbc.JDBCDataStoreAPITestSetup;
+import org.geotools.jdbc.TestData;
+import org.hsqldb.lib.HashSet;
+import org.hsqldb.lib.Set;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+import com.vividsolutions.jts.geom.Geometry;
 
 public class TeradataDataStoreAPITest extends JDBCDataStoreAPITest {
 
@@ -27,6 +44,55 @@ public class TeradataDataStoreAPITest extends JDBCDataStoreAPITest {
         return new TeradataDataStoreAPITestSetup(new TeradataPSTestSetup());
     }
 
+    
+    public void testBigLine() throws Exception {
+    	try {
+    	TestData td = new TestData(10);
+    	td.build();
+    	
+/*    	SimpleFeatureBuilder.build(roadType,
+                new Object[] { new Integer(1), line(new int[] { 1, 1, 2, 2, 4, 2, 5, 1 }), "r1", },
+                ROAD + "." + (initialFidValue));
+  */  	
+//        FeatureReader<SimpleFeatureType, SimpleFeature> reader = DataUtilities.reader(new SimpleFeature[] { td.newRoad, });
+        SimpleFeatureStore road = (SimpleFeatureStore) dataStore.getFeatureSource(tname("road"));
+
+        int[] points = new int[100 * 2 * 4];
+        for (int i = 0 ; i < 100 ; i++) {
+        	points[i * 2 * 4] = 1;
+        	points[i * 2 * 4 + 1] = 1;
+        	points[i * 2 * 4 + 2] = 2;
+        	points[i * 2 * 4 + 3] = 2;
+        	points[i * 2 * 4 + 4] = 4;
+        	points[i * 2 * 4 + 5] = 2;
+        	points[i * 2 * 4 + 6] = 5;
+        	points[i * 2 * 4 + 7] = 1;
+        }
+        SimpleFeature newRoad = SimpleFeatureBuilder.build(td.roadType,
+        		new Object[] { new Integer(20), td.line(points), "r20" }, "20");
+        FeatureReader<SimpleFeatureType, SimpleFeature> reader = DataUtilities.reader(new SimpleFeature[] { newRoad });
+        road.addFeatures(DataUtilities.collection(reader));
+
+
+        road = (SimpleFeatureStore) dataStore.getFeatureSource(tname("road"));
+        SimpleFeatureCollection results = road.getFeatures();
+        SimpleFeatureIterator features = results.features();
+        assertTrue(features.hasNext());
+        assertEquals(100 * 4, ((Geometry)features.next().getDefaultGeometry()).getNumPoints());
+        results.close(features);
+    	}
+        catch (Exception e) {
+        	e.printStackTrace();
+        }
+    }
+    
+    
+    @Override
+    public void testGetFeaturesWriterAdd() throws IOException, IllegalAttributeException {
+    	// TODO Auto-generated method stub
+    	super.testGetFeaturesWriterAdd();
+    }
+    
     @Override
     public void testGetFeatureWriterConcurrency() throws Exception {
         // Teradata will lock indefinitely, won't throw an exception

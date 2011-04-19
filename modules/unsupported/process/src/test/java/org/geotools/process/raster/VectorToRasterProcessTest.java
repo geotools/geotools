8c6b21e7e607a1cd57fd35784788b79e44ebe701
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2011-04-19 17:18:41 +0000

    Force using US locale to avoid . vs , formatting issues when the system has a different locale (like mine italian one)
    git-svn-id: http://svn.osgeo.org/geotools/branches/2.7.x@36973 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/process/src/test/java/org/geotools/process/raster/VectorToRasterProcessTest.java
+++ modules/unsupported/process/src/test/java/org/geotools/process/raster/VectorToRasterProcessTest.java
@@ -21,6 +21,7 @@ import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.awt.image.RenderedImage;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Random;
 
@@ -254,7 +255,7 @@ public class VectorToRasterProcessTest {
                     gridPos.setLocation(x, y);
                     DirectPosition worldPos = gridGeom.gridToWorld(gridPos);
                     
-                    String wkt = String.format("MULTIPOINT((%f %f))",
+                    String wkt = String.format(Locale.US, "MULTIPOINT((%f %f))",
                             worldPos.getOrdinate(0), worldPos.getOrdinate(1));
                     fc.add( buildFeature(builder, reader, wkt, "p" + i, i ) );
                     i++ ;

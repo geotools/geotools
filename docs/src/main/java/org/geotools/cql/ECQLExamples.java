3a0ecd21b6171f8ec2d37b1f0d784b1885522765
mpazos <mpazos@e5c1c795-43da-0310-a71f-fac65c449510>
2011-04-20 13:42:10 +0000

    sphinx examples for cql in progress
    git-svn-id: http://svn.osgeo.org/geotools/trunk@36979 e5c1c795-43da-0310-a71f-fac65c449510

--- docs/src/main/java/org/geotools/cql/ECQLExamples.java
+++ docs/src/main/java/org/geotools/cql/ECQLExamples.java
@@ -2,18 +2,21 @@ package org.geotools.cql;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.logging.Logger;
 
 import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.filter.text.cql2.CQL;
 import org.geotools.filter.text.ecql.ECQL;
+import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.filter.Filter;
 
 /**
  * This class gathers up the ECQL examples shown in the sphinx documentation.
  * 
- * WORKING IN PROGESS 
+ * WORK IN PROGESS 
  * 
  * @author Mauricio Pazos
  * 
@@ -30,10 +33,10 @@ public class ECQLExamples {
                     System.in));
             for (;;) {
 
-                System.out.println("Select the Example:");
+                System.out.println("Select the ECQL Example:");
                 System.out.println("1 - CQL Compatibility");
-                System.out.println("2 - ECQL Comparison");
-                System.out.println("...");
+                System.out.println("2 - Comparison: 1000 >= POPULTATION");
+                System.out.println("3 - like pattern using String 'aabbcc' LIKE '%bb%'");
                 System.out.println("0 - quite");
                 System.out.print(">");
 
@@ -48,6 +51,9 @@ public class ECQLExamples {
                     break;
                 case 2:
                     comparisonPredicateECQL();
+                    break;
+                case 3:
+                    likePredicateInString();
                     break;
                     
                 default:
@@ -69,8 +75,15 @@ public class ECQLExamples {
      * @throws Exception
      */
     static private void retrieveFeatures(Filter filter) throws Exception {
-        SimpleFeatureSource featureSource = null;
+        SimpleFeatureSource featureSource = null; // TODO open a feature store
+        
         SimpleFeatureCollection features = featureSource.getFeatures(filter);
+        SimpleFeatureIterator iter = features.features();
+        while(iter.hasNext() ) {
+            SimpleFeature feature = iter.next();
+            System.out.println(feature.toString());
+        }
+        iter.close();
     }
 
     /**
@@ -90,8 +103,13 @@ public class ECQLExamples {
         if (!filter.equals(filterFromCQL)) {
             throw new Exception("uncompatible ECQL Syntax");
         } else {
-            System.out.println("the filter for");
+            System.out.println("CQL and ECQL have produced the same filter for the predicate \"POPULTATION >= 1000\"");
+            prittyPrintFilter( filter );
         }
+    }
+
+    private static void prittyPrintFilter(Filter filter) {
+        System.out.println("The filter result is:\n" + filter.toString());         
     }
 
     /**
@@ -101,6 +119,7 @@ public class ECQLExamples {
      */
     static private void comparisonPredicateECQL() throws Exception {
 
+        
         // comparisonPredicateECQL start
         Filter filter = ECQL.toFilter("1000 >= POPULTATION");
         // comparisonPredicateECQL end
@@ -108,19 +127,38 @@ public class ECQLExamples {
         retrieveFeatures(filter);
     }
 
+    /**
+     *  Matching a property with a text pattern (case sensitive)
+     * 
+     * @throws Exception
+     */
     static private void likePredicate() throws Exception {
-        // ecql comparison start
+        // ecql likePredicate start
         Filter filter = ECQL.toFilter("CITY_NAME LIKE 'Ar%'");
-        // ecql comparison end
+        // ecql likePredicate end
         retrieveFeatures(filter);
     }
 
+    /**
+     *  Matching a property with a text pattern (case insensitive)
+     * 
+     * @throws Exception
+     */
     static private void ilikePredicate() throws Exception {
 
-        // ecql ILIKE start
+        // ecql ilikePredicate start
         Filter filter = ECQL.toFilter("CITY_NAME ILIKE 'Ar%'");
-        // ecql ILIKE end
+        // ecql ilikePredicate end
         retrieveFeatures(filter);
+    }
+
+    
+    static private void likePredicateInString() throws Exception {
+        // ecql likePredicateInString start
+        Filter filter = ECQL.toFilter("'aabbcc' LIKE '%bb%'");
+        // ecql likePredicateInString end
+        
+        prittyPrintFilter(filter);
     }
 
     static void inPredicate() throws Exception {
@@ -130,4 +168,6 @@ public class ECQLExamples {
 
         retrieveFeatures(filter);
     }
+    
+    
 }

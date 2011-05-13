1af3bc80891b857130bca15e546d41de86506592
mpazos <mpazos@e5c1c795-43da-0310-a71f-fac65c449510>
2011-05-13 07:48:33 +0000

    cql examples in progress
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37184 e5c1c795-43da-0310-a71f-fac65c449510

--- docs/src/main/java/org/geotools/cql/ECQLExamples.java
+++ docs/src/main/java/org/geotools/cql/ECQLExamples.java
@@ -2,16 +2,30 @@ package org.geotools.cql;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Iterator;
 import java.util.logging.Logger;
 
+import org.geotools.data.DataUtilities;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.feature.FeatureCollections;
+import org.geotools.feature.SchemaException;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.filter.text.cql2.CQL;
+import org.geotools.filter.text.cql2.CQLException;
 import org.geotools.filter.text.ecql.ECQL;
+import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.filter.Filter;
+import org.opengis.filter.PropertyIsLessThan;
+import org.opengis.filter.expression.Expression;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.WKTReader;
 
 /**
  * This class gathers up the ECQL examples shown in the sphinx documentation.
@@ -35,8 +49,11 @@ public class ECQLExamples {
 
                 System.out.println("Select the ECQL Example:");
                 System.out.println("1 - CQL Compatibility");
-                System.out.println("2 - Comparison: 1000 >= POPULTATION");
+                System.out.println("2 - Comparison: 1000 <= POPULTATION");
                 System.out.println("3 - like pattern using String 'aabbcc' LIKE '%bb%'");
+                System.out.println("4 - Comparison: (under18YearsOld * 19541453 / 100 ) < (over65YearsOld * 19541453 / 100 )");
+                System.out.println("5 - population Between 10000 and 20000");
+                
                 System.out.println("0 - quite");
                 System.out.print(">");
 
@@ -50,10 +67,16 @@ public class ECQLExamples {
                     comparisonPredicateCQLCompatibility();
                     break;
                 case 2:
-                    comparisonPredicateECQL();
+                    expressionLessThanOrEqualToProperty();
                     break;
                 case 3:
                     likePredicateInString();
+                    break;
+                case 4:
+                    comparisonUsingExpressions();
+                    break;
+                case 5:
+                    betweenPredicate();
                     break;
                     
                 default:
@@ -67,6 +90,7 @@ public class ECQLExamples {
             LOGGER.severe(e.getMessage());
         }
     }
+
 
     /**
      * Retrieves the features from a data store using the filter
@@ -113,20 +137,125 @@ public class ECQLExamples {
     }
 
     /**
-     * ECQL allow property names in the right hand of comparison predicate.
+     * ECQL allows expressions in the left hand of comparison predicate.
      * 
      * @throws Exception
      */
-    static private void comparisonPredicateECQL() throws Exception {
+    static private void expressionLessThanOrEqualToProperty() throws Exception {
 
         
-        // comparisonPredicateECQL start
-        Filter filter = ECQL.toFilter("1000 >= POPULTATION");
-        // comparisonPredicateECQL end
-
+        // expressionLessThanOrEqualToProperty start
+        Filter filter = ECQL.toFilter("1000 <= POPULTATION");
+        // expressionLessThanOrEqualToProperty end
+        
         retrieveFeatures(filter);
     }
 
+    /**
+     * Comparison between persons under 18 years old and over 65 years old
+     * @throws Exception
+     */
+    static private void comparisonUsingExpressions() throws Exception {
+        
+        // comparisonUsingExpressions start
+        Filter filter = ECQL.toFilter("(under18YearsOld * 19541453 / 100 ) < (over65YearsOld * 19541453 / 100 )");
+        // comparisonUsingExpressions end
+        SimpleFeature city = createCity();
+        
+        prittyPrintFilter(filter);
+
+        PropertyIsLessThan lessThan =(PropertyIsLessThan) filter; 
+        Expression leftExpr = lessThan.getExpression1();
+        Expression rightExpr = lessThan.getExpression2();
+        System.out.println("left expression value: " + leftExpr.evaluate(city));
+        System.out.println("right expression value: " + rightExpr.evaluate(city));
+        
+        Boolean result = lessThan.evaluate(city);
+        System.out.println("Result of filter evaluation: " + result);
+    }
+    
+    private static void betweenPredicate() throws Exception {
+        
+        // betweenPredicate start
+        Filter filter = ECQL.toFilter("population BETWEEN 10000000 and 20000000");
+        // betweenPredicate end
+        prittyPrintFilter(filter);
+
+        SimpleFeature city = createCity();
+
+        System.out.println("City population: " + city.getProperty("population").getValue());
+        
+        Boolean result = filter.evaluate(city);
+        System.out.println("Result of filter evaluation: " + result);
+    }
+
+    private static void betweenUsingExpression() throws Exception {
+        
+        // betweenUsingExpression start
+        Filter filter = ECQL.toFilter("area( shape ) BETWEEN 10000 AND 30000");
+        // betweenUsingExpression end
+        prittyPrintFilter(filter);
+
+        SimpleFeature country = createCountry();
+
+        Boolean result = filter.evaluate(country);
+        System.out.println("Result of filter evaluation: " + result);
+    }
+    
+    private static SimpleFeature createCountry() throws Exception {
+
+        final SimpleFeatureType type = DataUtilities.createType("Location",
+                "geometry:Polygon:srid=4326," + 
+                "countryName:String," + 
+                "population:Integer" 
+        );
+        SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(type);
+
+        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory(null);
+        WKTReader reader = new WKTReader();
+        Polygon geometry = (Polygon) reader.read(""); // FIXME implement me
+
+        featureBuilder.add(geometry);
+        featureBuilder.add("USA");
+        featureBuilder.add(307006550);
+        
+        SimpleFeature feature = featureBuilder.buildFeature(null);
+
+        return feature;
+    }
+
+
+    /**
+     * Creates a feature that represent New York city
+     * @return a Feature
+     * @throws Exception
+     */
+    static private SimpleFeature createCity() throws Exception {
+
+        final SimpleFeatureType type = DataUtilities.createType("Location",
+                "geometry:Point:srid=4326," + 
+                "cityName:String," + 
+                "over65YearsOld:Double,"+ 
+                "under18YearsOld:Double," +
+                "population:Integer" 
+        );
+        SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(type);
+
+        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory(null);
+        
+        Point point = geometryFactory.createPoint(new Coordinate(-17.2053, 11.9517));
+
+        featureBuilder.add(point);
+        featureBuilder.add("New York");
+        featureBuilder.add(22.6);
+        featureBuilder.add(13.4);
+        featureBuilder.add(19541453);
+        
+        SimpleFeature feature = featureBuilder.buildFeature(null);
+
+        return feature;
+}
+    
     /**
      *  Matching a property with a text pattern (case sensitive)
      * 
@@ -152,7 +281,11 @@ public class ECQLExamples {
         retrieveFeatures(filter);
     }
 
-    
+    /**
+     * Matching a literal string
+     * 
+     * @throws Exception
+     */
     static private void likePredicateInString() throws Exception {
         // ecql likePredicateInString start
         Filter filter = ECQL.toFilter("'aabbcc' LIKE '%bb%'");

fd43cbb0d7edcf891a575085e97262aeb949ae0f
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2011-06-22 05:17:08 +0000

    Update details for 2.7.2
    git-svn-id: http://svn.osgeo.org/geotools/tags/2.7.2@37473 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/metadata/src/main/java/org/geotools/factory/GeoTools.java
+++ modules/library/metadata/src/main/java/org/geotools/factory/GeoTools.java
@@ -83,7 +83,7 @@ public final class GeoTools {
     /**
      * The current GeoTools version. The separator character must be the dot.
      */
-    private static final Version VERSION = new Version(PROPS.getProperty("version", "2.7-SNAPSHOT"));
+    private static final Version VERSION = new Version(PROPS.getProperty("version", "2.7.2"));
 
     /**
      * The version control (svn) revision at which this version of geotools was built.

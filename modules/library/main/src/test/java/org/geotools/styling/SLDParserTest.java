77c737afa21c40e01c47ba3e71c1f0544c014cb5
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2011-06-25 14:11:18 +0000

    Previous commit did break default mark handling
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37491 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/main/src/test/java/org/geotools/styling/SLDParserTest.java
+++ modules/library/main/src/test/java/org/geotools/styling/SLDParserTest.java
@@ -16,14 +16,18 @@
  */
 package org.geotools.styling;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.List;
 
 import org.geotools.factory.CommonFactoryFinder;
+import org.junit.Test;
+import org.opengis.style.GraphicalSymbol;
 
-import junit.framework.TestCase;
-
-public class SLDParserTest extends TestCase {
+public class SLDParserTest {
 
     public static String SLD = 
         "<StyledLayerDescriptor xmlns=\"http://www.opengis.net/sld\" version=\"1.0.0\">"+
@@ -44,16 +48,41 @@ public class SLDParserTest extends TestCase {
         " </NamedLayer>"+
         "</StyledLayerDescriptor>";
     
+    static String SLD_DEFAULT_POINT = 
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+        "<StyledLayerDescriptor version=\"1.0.0\" \n" + 
+        "        xsi:schemaLocation=\"http://www.opengis.net/sld StyledLayerDescriptor.xsd\" \n" + 
+        "        xmlns=\"http://www.opengis.net/sld\" xmlns:ogc=\"http://www.opengis.net/ogc\" \n" + 
+        "        xmlns:xlink=\"http://www.w3.org/1999/xlink\" \n" + 
+        "        xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" + 
+        "    <UserStyle>\n" + 
+        "        <Name>Default Styler</Name>\n" + 
+        "        <Title>Default Styler</Title>\n" + 
+        "        <Abstract></Abstract>\n" + 
+        "        <FeatureTypeStyle>\n" + 
+        "            <FeatureTypeName>Feature</FeatureTypeName>\n" + 
+        "            <Rule>\n" + 
+        "                <PointSymbolizer>\n" + 
+        "                    <Graphic>\n" + 
+        "                    </Graphic>\n" + 
+        "                </PointSymbolizer>\n" + 
+        "            </Rule>\n" + 
+        "        </FeatureTypeStyle>\n" + 
+        "    </UserStyle>\n" + 
+        "</StyledLayerDescriptor>";
+    
     static StyleFactory styleFactory = CommonFactoryFinder.getStyleFactory(null);
     
+    @Test
     public void testBasic() throws Exception {
-        SLDParser parser = new SLDParser(styleFactory, input());
+        SLDParser parser = new SLDParser(styleFactory, input(SLD));
         Style[] styles = parser.readXML();
         assertStyles(styles);
     }
-    
+
+    @Test
     public void testMultipleParse() throws Exception {
-        SLDParser parser = new SLDParser(styleFactory, input());
+        SLDParser parser = new SLDParser(styleFactory, input(SLD));
         Style[] styles = parser.readXML();
         assertStyles(styles);
         
@@ -67,12 +96,33 @@ public class SLDParserTest extends TestCase {
         catch(Exception e) {}
     }
     
+    @Test
+    public void testDefaultPoint() throws Exception {
+        // fixes for GEOS-3111 broke default point symbsolizer handling
+        SLDParser parser = new SLDParser(styleFactory, input(SLD_DEFAULT_POINT));
+        Style[] styles = parser.readXML();
+        
+        assertEquals(1, styles.length);
+        List<FeatureTypeStyle> fts = styles[0].featureTypeStyles();
+        assertEquals(1, fts.size());
+        List<Rule> rules = fts.get(0).rules();
+        assertEquals(1, rules.size());
+        List<Symbolizer> symbolizers = rules.get(0).symbolizers();
+        assertEquals(1, symbolizers.size());
+        PointSymbolizer ps = (PointSymbolizer) symbolizers.get(0);
+        // here we would have had two instead of one
+        List<GraphicalSymbol> graphicalSymbols = ps.getGraphic().graphicalSymbols();
+        assertEquals(1, graphicalSymbols.size());
+        Mark mark = (Mark) graphicalSymbols.get(0);
+        assertEquals(mark, CommonFactoryFinder.getStyleFactory(null).createMark());
+    }
+    
     void assertStyles(Style[] styles) {
         assertEquals(1, styles.length);
         assertEquals("style", styles[0].getName());
     }
     
-    InputStream input() {
-        return new ByteArrayInputStream(SLD.getBytes());
+    InputStream input(String sld) {
+        return new ByteArrayInputStream(sld.getBytes());
     }
 }

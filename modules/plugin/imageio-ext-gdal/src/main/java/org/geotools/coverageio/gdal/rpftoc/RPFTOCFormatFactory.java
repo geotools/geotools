9c02855754e627b7b9d933a5a085aad808d2e114
danieleromagnoli <danieleromagnoli@e5c1c795-43da-0310-a71f-fac65c449510>
2011-06-28 14:50:06 +0000

    GEOT-3642: CADRG RPFTOC support
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37532 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ modules/plugin/imageio-ext-gdal/src/main/java/org/geotools/coverageio/gdal/rpftoc/RPFTOCFormatFactory.java
@@ -0,0 +1,86 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2007-2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.coverageio.gdal.rpftoc;
+	   
+import it.geosolutions.imageio.plugins.rpftoc.RPFTOCImageReaderSpi;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.io.GridFormatFactorySpi;
+import org.geotools.coverageio.BaseGridFormatFactorySPI;
+import org.opengis.coverage.grid.Format;
+
+/**
+ * Implementation of the {@link Format} service provider interface for RPFTOC
+ * files.
+ * 
+ * @author Daniele Romagnoli, GeoSolutions
+ * @author Simone Giannecchini (simboss), GeoSolutions
+ * @since 2.5.x
+ *
+ * @source $URL $
+ */
+public final class RPFTOCFormatFactory extends BaseGridFormatFactorySPI  implements GridFormatFactorySpi {
+	/** Logger. */
+	private final static Logger LOGGER = org.geotools.util.logging.Logging
+			.getLogger("org.geotools.coverageio.gdal.rpftoc");
+
+	/**
+	 * Tells me if the coverage plugin to access Erdas imagine is available or
+	 * not.
+	 * 
+	 * @return <code>true</code> if the plugin is available,
+	 *         <code>false</code> otherwise.
+	 */
+	public boolean isAvailable() {
+		boolean available = true;
+
+		// if these classes are here, then the runtime environment has
+		// access to JAI and the JAI ImageI/O toolbox.
+		try {
+			Class
+					.forName("it.geosolutions.imageio.plugins.rpftoc.RPFTOCImageReaderSpi");
+			available = new RPFTOCImageReaderSpi().isAvailable();
+
+			if (LOGGER.isLoggable(Level.FINE)) {
+				if (available) {
+					LOGGER.fine("RPFTOCFormatFactory is availaible.");
+				} else {
+					LOGGER.fine("RPFTOCFormatFactory is not availaible.");
+				}
+			}
+		} catch (ClassNotFoundException cnf) {
+			if (LOGGER.isLoggable(Level.FINE)) {
+				LOGGER.fine("RPFTOCFormatFactory is not availaible.");
+			}
+
+			available = false;
+		}
+
+		return available;
+	}
+
+	/**
+	 * Creating a {@link RPFTOCFormat}
+	 * 
+	 * @return A {@link RPFTOCFormat}
+	 */
+	public RPFTOCFormat createFormat() {
+		return new RPFTOCFormat();
+	}
+}
---
+++ modules/plugin/imageio-ext-gdal/src/main/java/org/geotools/coverageio/gdal/rpftoc/RPFTOCReader.java
@@ -0,0 +1,113 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2007-2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.coverageio.gdal.rpftoc;
+
+import java.io.File;
+
+import it.geosolutions.imageio.plugins.rpftoc.RPFTOCImageReaderSpi;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.coverageio.gdal.BaseGDALGridCoverage2DReader;
+import org.geotools.data.DataSourceException;
+import org.geotools.factory.Hints;
+import org.opengis.coverage.grid.Format;
+import org.opengis.coverage.grid.GridCoverageReader;
+
+
+/**
+ * This class can read a RPFTOC data source and create a {@link GridCoverage2D}
+ * from the data.
+ *
+ * @author Daniele Romagnoli, GeoSolutions.
+ * @author Simone Giannecchini (simboss), GeoSolutions
+ * @since 2.5.x
+ *
+ * @source $URL:  $
+ */
+public final class RPFTOCReader extends BaseGDALGridCoverage2DReader implements GridCoverageReader {
+    private final static String worldFileExt = "";
+
+    /**
+     * Creates a new instance of a {@link RPFTOCReader}. I assume nothing about
+     * file extension.
+     *
+     * @param input
+     *            Source object for which we want to build an {@link RPFTOCReader}.
+     * @throws DataSourceException
+     */
+    public RPFTOCReader(Object input) throws DataSourceException {
+        this(input, null);
+    }
+
+    /**
+     * Creates a new instance of a {@link RPFTOCReader}. I assume nothing about
+     * file extension.
+     *
+     * @param input
+     *            Source object for which we want to build an {@link RPFTOCReader}.
+     * @param hints
+     *            Hints to be used by this reader throughout his life.
+     * @throws DataSourceException
+     */
+    public RPFTOCReader(Object input, Hints hints) throws DataSourceException {
+        super(input, hints, worldFileExt, new RPFTOCImageReaderSpi());
+    }
+
+    /**
+     * @see org.opengis.coverage.grid.GridCoverageReader#getFormat()
+     */
+    public Format getFormat() {
+        return new RPFTOCFormat();
+    }
+    
+    /**
+     * Override coverage name because of the default structure of the folders. Very often XXX/RPF/A.TOC
+     * The parent directory is a better alternative
+     * @return the coverage name
+     */
+    public String getCoverageName() {
+    	/* 
+    	* Override coverage name because of the default structure of the folders. Very often XXX/RPF/A.TOC
+        * The coverage name would be always A. The parent directory is a better alternative.
+        */
+    	String retVal = "RPFTOC Coverage";
+    	if (!super.getCoverageName().equals("A")){
+    		retVal = super.getCoverageName();
+    	}
+    	else
+    	{
+    		retVal = "RPFTOC Coverage";
+    	    File oFile = super.getInputFile().getParentFile();
+    		while (oFile != null)
+    		{
+    			String sName = oFile.getName();
+    			if (!sName.toUpperCase().equals("RPF"))
+    			{
+    				retVal = sName;
+    				break;
+    			}
+    			else
+    			{
+    				oFile = oFile.getParentFile();
+    			}
+    		} 
+    	}
+        return retVal;
+    }
+    
+    
+}

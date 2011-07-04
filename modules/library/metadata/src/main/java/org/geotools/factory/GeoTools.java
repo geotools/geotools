8a0b40f61bfcb9fdfa477279fa5bcf5d6580afcb
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2011-07-04 15:30:44 +0000

    GeoTools class should record the current 8-SNAPSHOT (hopefully parsing the version number does not break)
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37560 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/metadata/src/main/java/org/geotools/factory/GeoTools.java
+++ modules/library/metadata/src/main/java/org/geotools/factory/GeoTools.java
@@ -86,7 +86,7 @@ public final class GeoTools {
     /**
      * The current GeoTools version. The separator character must be the dot.
      */
-    private static final Version VERSION = new Version(PROPS.getProperty("version", "2.7-SNAPSHOT"));
+    private static final Version VERSION = new Version(PROPS.getProperty("version", "8-SNAPSHOT"));
 
     /**
      * The version control (svn) revision at which this version of geotools was built.

e3f5b008dc15dadb87e5537886c63c4a9357d2e3
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2011-07-08 05:12:11 +0000

    GEOT-3700 fire correct event type for layer selection change
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37577 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/render/src/main/java/org/geotools/map/Layer.java
+++ modules/library/render/src/main/java/org/geotools/map/Layer.java
@@ -148,7 +148,7 @@ public abstract class Layer {
         if (listenerList == null) {
             return;
         }
-        MapLayerEvent event = new MapLayerEvent(this, MapLayerEvent.VISIBILITY_CHANGED);
+        MapLayerEvent event = new MapLayerEvent(this, MapLayerEvent.SELECTION_CHANGED);
         for (MapLayerListener mapListener : listenerList) {
             try {
                 mapListener.layerSelected(event);
@@ -170,7 +170,7 @@ public abstract class Layer {
         if (listenerList == null) {
             return;
         }
-        MapLayerEvent event = new MapLayerEvent(this, MapLayerEvent.VISIBILITY_CHANGED);
+        MapLayerEvent event = new MapLayerEvent(this, MapLayerEvent.SELECTION_CHANGED);
         for (MapLayerListener mapListener : listenerList) {
             try {
                 mapListener.layerDeselected(event);

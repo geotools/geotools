79786c415f65bbbc4ffa1dc0f4faa6ff2a7109ad
simonegiannecchini <simonegiannecchini@e5c1c795-43da-0310-a71f-fac65c449510>
2011-07-15 13:35:29 +0000

    GEOT-3718
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37646 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/coveragetools/src/test/java/org/geotools/utils/imageoverviews/OverviewEmbedderTest.java
+++ modules/unsupported/coveragetools/src/test/java/org/geotools/utils/imageoverviews/OverviewEmbedderTest.java
@@ -3,12 +3,22 @@
  */
 package org.geotools.utils.imageoverviews;
 
+import it.geosolutions.imageioimpl.plugins.tiff.TIFFImageReaderSpi;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
 import javax.media.jai.JAI;
 
+import junit.framework.Assert;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.geotools.test.TestData;
 import org.geotools.util.logging.Logging;
 import org.geotools.utils.progress.ExceptionEvent;
 import org.geotools.utils.progress.ProcessingEvent;
@@ -17,6 +27,7 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -25,7 +36,7 @@ import org.junit.Test;
  * @author Simone Giannecchini, GeoSolutions SAS
  *
  */
-public class OverviewEmbedderTest {
+public class OverviewEmbedderTest extends Assert{
     
     private final static Logger LOGGER = Logging.getLogger(OverviewEmbedderTest.class);
 
@@ -48,6 +59,8 @@ public class OverviewEmbedderTest {
      */
     @Before
     public void setUp() throws Exception {
+        File inputFile=TestData.file(this, "DEM.tiff");
+        FileUtils.copyFile(inputFile, new File(inputFile.getParent(), "DEM_.tiff"));
     }
 
     /**
@@ -55,20 +68,25 @@ public class OverviewEmbedderTest {
      */
     @After
     public void tearDown() throws Exception {
+        FileUtils.deleteQuietly(TestData.temp(this, "DEM_.tiff"));
     }
     
-
     @Test
-    public void firstTest() throws FileNotFoundException, IOException{
+   
+    public void nearest() throws FileNotFoundException, IOException{
+//        JAI.getDefaultInstance().getTileCache().setMemoryCapacity(512*1024*1024);
+//        JAI.getDefaultInstance().getTileScheduler().setParallelism(10);
+//        JAI.getDefaultInstance().getTileScheduler().setPrefetchParallelism(10);
+        
         final OverviewsEmbedder oe= new OverviewsEmbedder();
         oe.setDownsampleStep(2);
         oe.setNumSteps(5);
-        oe.setScaleAlgorithm(OverviewsEmbedder.SubsampleAlgorithm.Average.toString());
+        oe.setScaleAlgorithm(OverviewsEmbedder.SubsampleAlgorithm.Nearest.toString());
         // use default 
         oe.setTileCache(JAI.getDefaultInstance().getTileCache());
         oe.setTileWidth(256);
         oe.setTileHeight(256);
-        oe.setSourcePath(org.geotools.test.TestData.file(this, "DEM.tiff").getAbsolutePath());
+        oe.setSourcePath(TestData.file(this, "DEM_.tiff").getAbsolutePath());
         oe.addProcessingEventListener(new ProcessingEventListener() {
             
             @Override
@@ -81,10 +99,204 @@ public class OverviewEmbedderTest {
             public void exceptionOccurred(ExceptionEvent event) {
                 LOGGER.warning(event.toString());                
             }
+            
         });
         oe.run();
         
+        // now red it back and check that things are coherent
+        final ImageReader reader= new TIFFImageReaderSpi().createReaderInstance();
+        reader.setInput(ImageIO.createImageInputStream(org.geotools.test.TestData.file(this, "DEM_.tiff")));
+        assertTrue(reader.getNumImages(true)==6);
         
     }
 
+    @Test
+   
+        public void average() throws FileNotFoundException, IOException{
+    //        JAI.getDefaultInstance().getTileCache().setMemoryCapacity(512*1024*1024);
+    //        JAI.getDefaultInstance().getTileScheduler().setParallelism(10);
+    //        JAI.getDefaultInstance().getTileScheduler().setPrefetchParallelism(10);
+            
+            final OverviewsEmbedder oe= new OverviewsEmbedder();
+            oe.setDownsampleStep(2);
+            oe.setNumSteps(5);
+            oe.setScaleAlgorithm(OverviewsEmbedder.SubsampleAlgorithm.Average.toString());
+            // use default 
+            oe.setTileCache(JAI.getDefaultInstance().getTileCache());
+            oe.setTileWidth(256);
+            oe.setTileHeight(256);
+            oe.setSourcePath(TestData.file(this, "DEM_.tiff").getAbsolutePath());
+            oe.addProcessingEventListener(new ProcessingEventListener() {
+                
+                @Override
+                public void getNotification(ProcessingEvent event) {
+                    LOGGER.info(event.toString());
+                    
+                }
+                
+                @Override
+                public void exceptionOccurred(ExceptionEvent event) {
+                    LOGGER.warning(event.toString());                
+                }
+                
+            });
+            oe.run();
+            
+            // now red it back and check that things are coherent
+            final ImageReader reader= new TIFFImageReaderSpi().createReaderInstance();
+            reader.setInput(ImageIO.createImageInputStream(org.geotools.test.TestData.file(this, "DEM_.tiff")));
+            assertTrue(reader.getNumImages(true)==6);
+            
+        }
+
+    @Test
+   
+        public void bicubic() throws FileNotFoundException, IOException{
+    //        JAI.getDefaultInstance().getTileCache().setMemoryCapacity(512*1024*1024);
+    //        JAI.getDefaultInstance().getTileScheduler().setParallelism(10);
+    //        JAI.getDefaultInstance().getTileScheduler().setPrefetchParallelism(10);
+            
+            final OverviewsEmbedder oe= new OverviewsEmbedder();
+            oe.setDownsampleStep(2);
+            oe.setNumSteps(5);
+            oe.setScaleAlgorithm(OverviewsEmbedder.SubsampleAlgorithm.Bicubic.toString());
+            // use default 
+            oe.setTileCache(JAI.getDefaultInstance().getTileCache());
+            oe.setTileWidth(256);
+            oe.setTileHeight(256);
+            oe.setSourcePath(TestData.file(this, "DEM_.tiff").getAbsolutePath());
+            oe.addProcessingEventListener(new ProcessingEventListener() {
+                
+                @Override
+                public void getNotification(ProcessingEvent event) {
+                    LOGGER.info(event.toString());
+                    
+                }
+                
+                @Override
+                public void exceptionOccurred(ExceptionEvent event) {
+                    LOGGER.warning(event.toString());                
+                }
+                
+            });
+            oe.run();
+            
+            // now red it back and check that things are coherent
+            final ImageReader reader= new TIFFImageReaderSpi().createReaderInstance();
+            reader.setInput(ImageIO.createImageInputStream(org.geotools.test.TestData.file(this, "DEM_.tiff")));
+            assertTrue(reader.getNumImages(true)==6);
+            
+        }
+
+    @Test
+   
+        public void bilinear() throws FileNotFoundException, IOException{
+    //        JAI.getDefaultInstance().getTileCache().setMemoryCapacity(512*1024*1024);
+    //        JAI.getDefaultInstance().getTileScheduler().setParallelism(10);
+    //        JAI.getDefaultInstance().getTileScheduler().setPrefetchParallelism(10);
+            
+            final OverviewsEmbedder oe= new OverviewsEmbedder();
+            oe.setDownsampleStep(2);
+            oe.setNumSteps(5);
+            oe.setScaleAlgorithm(OverviewsEmbedder.SubsampleAlgorithm.Bilinear.toString());
+            // use default 
+            oe.setTileCache(JAI.getDefaultInstance().getTileCache());
+            oe.setTileWidth(256);
+            oe.setTileHeight(256);
+            oe.setSourcePath(TestData.file(this, "DEM_.tiff").getAbsolutePath());
+            oe.addProcessingEventListener(new ProcessingEventListener() {
+                
+                @Override
+                public void getNotification(ProcessingEvent event) {
+                    LOGGER.info(event.toString());
+                    
+                }
+                
+                @Override
+                public void exceptionOccurred(ExceptionEvent event) {
+                    LOGGER.warning(event.toString());                
+                }
+                
+            });
+            oe.run();
+            
+            // now red it back and check that things are coherent
+            final ImageReader reader= new TIFFImageReaderSpi().createReaderInstance();
+            reader.setInput(ImageIO.createImageInputStream(org.geotools.test.TestData.file(this, "DEM_.tiff")));
+            assertTrue(reader.getNumImages(true)==6);
+            
+        }
+
+    @Test
+        public void filtered() throws FileNotFoundException, IOException{
+    //        JAI.getDefaultInstance().getTileCache().setMemoryCapacity(512*1024*1024);
+    //        JAI.getDefaultInstance().getTileScheduler().setParallelism(10);
+    //        JAI.getDefaultInstance().getTileScheduler().setPrefetchParallelism(10);
+            
+            final OverviewsEmbedder oe= new OverviewsEmbedder();
+            oe.setDownsampleStep(2);
+            oe.setNumSteps(5);
+            oe.setScaleAlgorithm(OverviewsEmbedder.SubsampleAlgorithm.Filtered.toString());
+            // use default 
+            oe.setTileCache(JAI.getDefaultInstance().getTileCache());
+            oe.setTileWidth(256);
+            oe.setTileHeight(256);
+            oe.setSourcePath(TestData.file(this, "DEM_.tiff").getAbsolutePath());
+            oe.addProcessingEventListener(new ProcessingEventListener() {
+                
+                @Override
+                public void getNotification(ProcessingEvent event) {
+                    LOGGER.info(event.toString());
+                    
+                }
+                
+                @Override
+                public void exceptionOccurred(ExceptionEvent event) {
+                    LOGGER.warning(event.toString());                
+                }
+                
+            });
+            oe.run();
+            
+            // now red it back and check that things are coherent
+            final ImageReader reader= new TIFFImageReaderSpi().createReaderInstance();
+            reader.setInput(ImageIO.createImageInputStream(org.geotools.test.TestData.file(this, "DEM_.tiff")));
+            assertTrue(reader.getNumImages(true)==2);
+            
+        }
+
+//    @Test
+//        public void wrong() throws FileNotFoundException, IOException{
+//    //        JAI.getDefaultInstance().getTileCache().setMemoryCapacity(512*1024*1024);
+//    //        JAI.getDefaultInstance().getTileScheduler().setParallelism(10);
+//    //        JAI.getDefaultInstance().getTileScheduler().setPrefetchParallelism(10);
+//            
+//            final OverviewsEmbedder oe= new OverviewsEmbedder();
+//            oe.setDownsampleStep(0);
+//            oe.setNumSteps(-3);
+//            oe.setScaleAlgorithm("geosolutionsrocks!");
+//            // use default 
+//            oe.setTileCache(JAI.getDefaultInstance().getTileCache());
+//            oe.setTileWidth(0);
+//            oe.setTileHeight(256);
+//            oe.setSourcePath(TestData.file(this, "DEM_.tiff").getAbsolutePath());
+//            oe.addProcessingEventListener(new ProcessingEventListener() {
+//                
+//                @Override
+//                public void getNotification(ProcessingEvent event) {
+//                    LOGGER.info(event.toString());
+//                    
+//                }
+//                
+//                @Override
+//                public void exceptionOccurred(ExceptionEvent event) {
+//                    LOGGER.warning(event.toString());                
+//                }
+//                
+//            });
+//            oe.run();
+//            
+//
+//            
+//        }
 }

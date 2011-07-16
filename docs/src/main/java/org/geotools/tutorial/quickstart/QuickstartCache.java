28844d92b7463247069b14c83a58e4805d3adae6
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2011-07-16 10:06:01 +0000

    Make this example work, now it just NPE's due to the lack of a style
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37650 e5c1c795-43da-0310-a71f-fac65c449510

--- docs/src/main/java/org/geotools/tutorial/quickstart/QuickstartCache.java
+++ docs/src/main/java/org/geotools/tutorial/quickstart/QuickstartCache.java
@@ -9,6 +9,8 @@ import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.Layer;
 import org.geotools.map.MapContent;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
 import org.geotools.swing.JMapFrame;
 import org.geotools.swing.data.JFileDataStoreChooser;
 
@@ -42,7 +44,8 @@ public class QuickstartCache {
         // Create a map content and add our shapefile to it
         MapContent map = new MapContent();
         map.setTitle("Using cached features");
-        Layer layer = new FeatureLayer(cache, null);
+        Style style = SLD.createSimpleStyle(featureSource.getSchema());
+        Layer layer = new FeatureLayer(cache, style);
         map.addLayer(layer);
 
         // Now display the map

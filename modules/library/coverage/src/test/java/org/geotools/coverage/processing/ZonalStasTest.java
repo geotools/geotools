626112575119bdf6f313ec3746133018052ca209
simonegiannecchini <simonegiannecchini@e5c1c795-43da-0310-a71f-fac65c449510>
2011-07-20 16:54:58 +0000

    merged processing packages
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37693 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/coverage/src/test/java/org/geotools/coverage/processing/ZonalStasTest.java
+++ modules/library/coverage/src/test/java/org/geotools/coverage/processing/ZonalStasTest.java
@@ -14,7 +14,7 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
-package org.geotools.coverage.processing.operation;
+package org.geotools.coverage.processing;
 
 import static java.lang.Math.round;
 import it.geosolutions.imageioimpl.plugins.tiff.TIFFImageReader;

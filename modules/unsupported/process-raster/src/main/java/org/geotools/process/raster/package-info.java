ea22093ac0c9f3a2a392e075069fa5573bddb556
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2011-07-25 05:42:48 +0000

    set up process plugins
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37712 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ modules/unsupported/process-raster/src/main/java/org/geotools/process/raster/package-info.java
@@ -0,0 +1,26 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2011, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+/**
+ * Raster processing support for the GeoTools library.
+ * <p>
+ * These raster operations are made available as:
+ * <ul>
+ * <li>static methods for direct use in your code</li>
+ * <li>ProcessFactory for use in dynamic systems</li>
+ * </ul>
+ */
+package org.geotools.process.raster;

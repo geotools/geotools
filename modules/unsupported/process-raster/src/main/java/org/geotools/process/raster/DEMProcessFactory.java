b2a0c598d41c0d39b6c998638250653960cdcd7f
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2011-07-25 05:43:41 +0000

    moving DEMTools out into process-raster
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37714 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ modules/unsupported/process-raster/src/main/java/org/geotools/process/raster/DEMProcessFactory.java
@@ -0,0 +1,38 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.raster;
+
+import org.geotools.process.factory.StaticMethodsProcessFactory;
+import org.geotools.util.SimpleInternationalString;
+
+/**
+ * ProcessFactory for several digital elevation model processes.
+ * 
+ * @since 2.7
+ * @version 8.0
+ *
+ * @source $URL: http://svn.osgeo.org/geotools/trunk/modules/unsupported/process/src/main/java/org/geotools/process/dem/DEMProcessFactory.java $
+ *         http://svn.osgeo.org/geotools/trunk/modules/unsupported/process/src/main/java/org/geotools
+ *         /process/raster/RasterToVectorFactory.java $
+ */
+public class DEMProcessFactory extends StaticMethodsProcessFactory<DEMTools> {
+    public DEMProcessFactory() {
+        super(new SimpleInternationalString("Simple JTS based spatial analysis methods"), "JTS",
+                DEMTools.class);
+    }
+
+}

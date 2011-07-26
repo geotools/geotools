5ed37db8a7269d10d08d642af7bf336f3794231d
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2011-07-26 14:23:55 +0000

    licence and header
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37725 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/swing/src/main/java/org/geotools/swing/control/PopupMenuProvider.java
+++ modules/unsupported/swing/src/main/java/org/geotools/swing/control/PopupMenuProvider.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2011, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 
 package org.geotools.swing.control;
@@ -8,8 +20,13 @@ package org.geotools.swing.control;
 import javax.swing.JPopupMenu;
 
 /**
+ * Used with {@linkplain MenuStatusBarItem} to pass a (possibly dynamic)
+ * pop-up menu.
  *
- * @author michael
+ * @author Michael Bedward
+ * @since 8.0
+ * @source $URL$
+ * @version $Id$
  */
 public interface PopupMenuProvider {
 

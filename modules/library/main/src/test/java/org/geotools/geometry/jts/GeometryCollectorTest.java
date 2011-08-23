a7c10b388de91e201955a0fa1cefa258c4af0ee3
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2011-08-23 11:12:11 +0000

    [GEOT-3816] GeometryCollector does not preserve geometry CRS (often available in the userdata)
    git-svn-id: http://svn.osgeo.org/geotools/branches/2.7.x@37897 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/main/src/test/java/org/geotools/geometry/jts/GeometryCollectorTest.java
+++ modules/library/main/src/test/java/org/geotools/geometry/jts/GeometryCollectorTest.java
@@ -3,6 +3,8 @@ package org.geotools.geometry.jts;
 import static org.junit.Assert.*;
 
 import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.referencing.crs.DefaultGeocentricCRS;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.junit.Test;
 import org.opengis.filter.FilterFactory2;
 
@@ -63,6 +65,45 @@ public class GeometryCollectorTest {
         } catch (IllegalStateException e) {
             // fine
         }
+    }
+    
+    @Test
+    public void testCRSSimple() throws Exception {
+        WKTReader reader = new WKTReader();
+
+        GeometryCollector collector = new GeometryCollector();
+        collector.setFactory(null);
+        final Geometry p0 = reader.read("POINT(0 0)");
+        p0.setUserData(DefaultGeographicCRS.WGS84);
+        collector.add(p0);
+        final Geometry p1 = reader.read("POINT(1 1)");
+        collector.add(p1);
+        p1.setUserData(DefaultGeographicCRS.WGS84);
+
+        GeometryCollection result = collector.collect();
+        assertEquals(2, result.getNumGeometries());
+        assertSame(DefaultGeographicCRS.WGS84, result.getUserData());
+        assertSame(p0, result.getGeometryN(0));
+        assertSame(p1, result.getGeometryN(1));
+    }
+    
+    @Test
+    public void testCRSNested() throws Exception {
+        WKTReader reader = new WKTReader();
+
+        GeometryCollector collector = new GeometryCollector();
+        collector.setFactory(null);
+        final Geometry p0 = reader.read("MULTIPOINT(0 0, 1 1)");
+        p0.setUserData(DefaultGeographicCRS.WGS84);
+        collector.add(p0);
+        final Geometry p1 = reader.read("MULTIPOINT(2 2, 3 3)");
+        collector.add(p1);
+        p1.setUserData(DefaultGeographicCRS.WGS84);
+
+        GeometryCollection result = collector.collect();
+        assertEquals(4, result.getNumGeometries());
+        assertSame(DefaultGeographicCRS.WGS84, result.getUserData());
+        assertTrue(reader.read("POINT(0 0)").equals(result.getGeometryN(0)));
     }
 
 }

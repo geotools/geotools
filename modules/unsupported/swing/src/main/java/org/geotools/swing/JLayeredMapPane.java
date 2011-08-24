f5180d1c5cd1f6c8f01e9b921e06c085421ae080
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2011-08-24 04:42:13 +0000

    Don't paint the background in layer graphics
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37911 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/swing/src/main/java/org/geotools/swing/JLayeredMapPane.java
+++ modules/unsupported/swing/src/main/java/org/geotools/swing/JLayeredMapPane.java
@@ -131,11 +131,11 @@ public class JLayeredMapPane extends AbstractMapPane {
             }
 
             op.graphics = op.image.createGraphics();
-            op.graphics.setBackground(getBackground());
+            // op.graphics.setBackground(getBackground());
 
         } else {
-            op.graphics.setBackground(getBackground());
-            op.graphics.clearRect(0, 0, r.width, r.height);
+            // op.graphics.setBackground(getBackground());
+            // op.graphics.clearRect(0, 0, r.width, r.height);
         }
         
         if (op.renderer == null) {

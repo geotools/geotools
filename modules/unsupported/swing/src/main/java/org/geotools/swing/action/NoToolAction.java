848ec8eeecbfbb38874ebd120066c8dc0f0c20d1
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2011-09-09 04:52:02 +0000

    Licence, javadocs and minor edits
    git-svn-id: http://svn.osgeo.org/geotools/trunk@37978 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/swing/src/main/java/org/geotools/swing/action/NoToolAction.java
+++ modules/unsupported/swing/src/main/java/org/geotools/swing/action/NoToolAction.java
@@ -1,7 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2011, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
+
 package org.geotools.swing.action;
 
 import java.awt.event.ActionEvent;
@@ -9,17 +22,21 @@ import java.util.ResourceBundle;
 import org.geotools.swing.MapPane;
 
 /**
- *
- * @author michael
+ * An action to de-select any active map cursor tool.
+ * 
+ * @author Michael Bedward
+ * @since 8.0
+ * @source $URL: http://svn.osgeo.org/geotools/trunk/modules/unsupported/swing/src/main/java/org/geotools/swing/action/PanAction.java $
+ * @version $Id: PanAction.java 37635 2011-07-13 13:39:39Z mbedward $
  */
 public class NoToolAction extends MapAction {
     /** Name for this tool */
     public static final String TOOL_NAME = 
-            ResourceBundle.getBundle("org/geotools/swing/Text").getString("tool_name_reset");
+            ResourceBundle.getBundle("org/geotools/swing/Text").getString("tool_name_no_tool");
     
     /** Tool tip text */
     public static final String TOOL_TIP = 
-            ResourceBundle.getBundle("org/geotools/swing/Text").getString("tool_tip_reset");
+            ResourceBundle.getBundle("org/geotools/swing/Text").getString("tool_tip_no_tool");
     
     /** Icon for the control */
     public static final String ICON_IMAGE = "/org/geotools/swing/icons/pointer.png";

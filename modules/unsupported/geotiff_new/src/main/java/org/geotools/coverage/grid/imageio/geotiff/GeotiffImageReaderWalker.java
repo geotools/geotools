c232d2032a5851ca3f68d062b0983768f04c410b
simonegiannecchini <simonegiannecchini@e5c1c795-43da-0310-a71f-fac65c449510>
2011-09-17 02:51:30 +0000

    more experiments
    git-svn-id: http://svn.osgeo.org/geotools/trunk@38032 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/geotiff_new/src/main/java/org/geotools/coverage/grid/imageio/geotiff/GeotiffImageReaderWalker.java
+++ modules/unsupported/geotiff_new/src/main/java/org/geotools/coverage/grid/imageio/geotiff/GeotiffImageReaderWalker.java
@@ -19,11 +19,15 @@ import org.geotools.resources.i18n.Errors;
 public class GeotiffImageReaderWalker extends FileBasedImageReaderWalker {
 
     /** Logger. */
-    private final static Logger LOGGER = org.geotools.util.logging.Logging.getLogger(GeotiffImageReaderWalker.class);
-    
+    private final static Logger LOGGER = org.geotools.util.logging.Logging
+            .getLogger(GeotiffImageReaderWalker.class);
+
     private ImageReader mainImageReader;
+
     private ImageReader externalOvrImageReader;
+
     private int mainFileNumImages;
+
     private int extOvrFileNumImages;
 
     private ImageInputStream mainImageInputStream;
@@ -33,7 +37,6 @@ public class GeotiffImageReaderWalker extends FileBasedImageReaderWalker {
     public GeotiffImageReaderWalker(URL sourceURL, Hints hints) throws IOException {
         super(sourceURL, hints, GeoTiffUtils.TIFFREADERFACTORY);
 
-        
     }
 
     @Override
@@ -42,127 +45,129 @@ public class GeotiffImageReaderWalker extends FileBasedImageReaderWalker {
     }
 
     @Override
-    protected ImageReader acquireReader(int i) throws IOException{
-        if(mainImageReader==null)
+    protected ImageReader acquireReader(int i) throws IOException {
+        if (mainImageReader == null)
             throw new IOException(Errors.format(ErrorKeys.DISPOSED_FACTORY));
-        if(!hasExternalOverviews)
+        if (!hasExternalOverviews)
             return mainImageReader;
-        if(i<mainFileNumImages)
+        if (i < mainFileNumImages)
             return mainImageReader;
-        if(i<mainFileNumImages+extOvrFileNumImages)
+        if (i < mainFileNumImages + extOvrFileNumImages)
             return externalOvrImageReader;
         throw new IOException(Errors.format(ErrorKeys.INDEX_OUT_OF_BOUNDS_$1));
     }
 
     @Override
-    protected void releaseReader(int i)throws IOException {
-//        do nothing
-    }
-
-    @Override
-    protected void endWalk() throws IOException{
-        dispose();
-        
-    }
-
-    @Override
-    protected void prepareElement(int i)throws IOException {
+    protected void releaseReader(int i) throws IOException {
         // do nothing
     }
 
     @Override
-    protected int getNumElemements()throws IOException {
-        if(mainImageReader==null)
+    protected void endWalk() throws IOException {
+        dispose();
+
+    }
+
+    @Override
+    protected void prepareElement(int i) throws IOException {
+        // do nothing
+    }
+
+    @Override
+    protected int getNumElemements() throws IOException {
+        if (mainImageReader == null)
             return -1;
-        return mainFileNumImages+extOvrFileNumImages;
+        return mainFileNumImages + extOvrFileNumImages;
     }
 
     @Override
     protected void startWalk() throws IOException {
-        
+
         // check if we have ext ovr then open needed readers
         // MAIN FILE
         // create a stream
-        mainImageInputStream=this.inStreamSPI.createInputStreamInstance(sourceFile,ImageIO.getUseCache(),ImageIO.getCacheDirectory());
+        mainImageInputStream = this.inStreamSPI.createInputStreamInstance(sourceFile,
+                ImageIO.getUseCache(), ImageIO.getCacheDirectory());
         // create the main reader
-        mainImageReader=this.readerSPI.createReaderInstance();
+        mainImageReader = this.readerSPI.createReaderInstance();
         mainImageReader.setInput(mainImageInputStream);
-        
+
         // OVERVIEWS FILE
         // create a stream
-        if(hasExternalOverviews){
-            externalOvrImageInputStream=this.inStreamSPI.createInputStreamInstance(externalOverviewsFile,ImageIO.getUseCache(),ImageIO.getCacheDirectory());
+        if (hasExternalOverviews) {
+            externalOvrImageInputStream = this.inStreamSPI.createInputStreamInstance(
+                    externalOverviewsFile, ImageIO.getUseCache(), ImageIO.getCacheDirectory());
             // create the main reader
-            externalOvrImageReader=GeoTiffUtils.TIFFREADERFACTORY.createReaderInstance();
+            externalOvrImageReader = GeoTiffUtils.TIFFREADERFACTORY.createReaderInstance();
             externalOvrImageReader.setInput(externalOvrImageInputStream);
         }
-        
+
         // now check the num image
-        mainFileNumImages=mainImageReader.getNumImages(true);
-        if(hasExternalOverviews){
-            extOvrFileNumImages=externalOvrImageReader.getNumImages(true);
-            
-            // TODO if we have ext ovr we should somehow ignore additional pages that might bring levels themselves
-            }
-            
-        
+        mainFileNumImages = mainImageReader.getNumImages(true);
+        if (hasExternalOverviews) {
+            extOvrFileNumImages = externalOvrImageReader.getNumImages(true);
+
+            // TODO if we have ext ovr we should somehow ignore additional pages that might bring
+            // levels themselves
+        }
+
     }
 
     private void dispose() {
-        mainFileNumImages=-1;
-        extOvrFileNumImages=-1;
+        mainFileNumImages = -1;
+        extOvrFileNumImages = -1;
         try {
-            if(mainImageInputStream!=null)
+            if (mainImageInputStream != null)
                 mainImageInputStream.close();
         } catch (Exception e) {
             // TODO: handle exception
         }
-        
+
         try {
-            if(externalOvrImageInputStream!=null)
+            if (externalOvrImageInputStream != null)
                 externalOvrImageInputStream.close();
         } catch (Exception e) {
             // TODO: handle exception
         }
-        
-        
+
         try {
-            if(mainImageReader!=null)
+            if (mainImageReader != null)
                 mainImageReader.dispose();
         } catch (Exception e) {
             // TODO: handle exception
         } finally {
-            mainImageReader=null;
+            mainImageReader = null;
         }
-        
+
         try {
-            if(externalOvrImageReader!=null)
+            if (externalOvrImageReader != null)
                 externalOvrImageReader.dispose();
         } catch (Exception e) {
             // TODO: handle exception
         } finally {
-            externalOvrImageReader=null;
+            externalOvrImageReader = null;
         }
-        
+
     }
 
     @Override
     protected IIOMetadata getStreamMetadata() throws IOException {
-        if(mainImageReader==null)
+        if (mainImageReader == null)
             return null;
         return mainImageReader.getStreamMetadata();
     }
 
     @Override
     protected ImageReaderSource<?> getSource(int imageIndex) throws IOException {
-        if(mainImageReader==null)
+        if (mainImageReader == null)
             throw new IOException(Errors.format(ErrorKeys.DISPOSED_FACTORY));
-        if(!hasExternalOverviews)
+        if (!hasExternalOverviews)
             return super.getSource(imageIndex);
-        if(imageIndex<mainFileNumImages)
+        if (imageIndex < mainFileNumImages)
             return super.getSource(imageIndex);
-        if(imageIndex<mainFileNumImages+extOvrFileNumImages)
-            return ImageReaderSource.wrapFile(imageIndex,externalOverviewsFile, super.inStreamSPI, GeoTiffUtils.TIFFREADERFACTORY);
+        if (imageIndex < mainFileNumImages + extOvrFileNumImages)
+            return ImageReaderSource.wrapFile(imageIndex, externalOverviewsFile, super.inStreamSPI,
+                    GeoTiffUtils.TIFFREADERFACTORY);
         throw new IOException(Errors.format(ErrorKeys.INDEX_OUT_OF_BOUNDS_$1));
     }
 

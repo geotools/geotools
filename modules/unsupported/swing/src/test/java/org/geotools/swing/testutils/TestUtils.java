84b47e693a368d8aae3e764164b2f659c3c50de2
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2011-10-09 11:49:12 +0000

    Added class to hold general helper methods for swing unit tests
    git-svn-id: http://svn.osgeo.org/geotools/trunk@38157 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ modules/unsupported/swing/src/test/java/org/geotools/swing/testutils/TestUtils.java
@@ -0,0 +1,128 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2011, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotools.swing.testutils;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.RenderedImage;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.CountDownLatch;
+
+import javax.swing.SwingUtilities;
+
+import org.jaitools.swing.ImageFrame;
+
+
+/**
+ * Provides static utility methods for unit testing Swing components.
+ * 
+ * @author Michael Bedward
+ * @since 8.0
+ * @source $URL$
+ * @version $Id$
+ */
+public class TestUtils {
+    
+    /**
+     * Displays the given image in an {@linkplain ImageFrame} and counts down the
+     * returned latch when the frame is closed. This allows test methods to display 
+     * an image and then wait until the user dismisses the frame.
+     * <pre><code>
+     * // In test method
+     * RenderedImage img = ...
+     * CountDownLatch latch = TestUtils.showImage(img, "Look at this image");
+     * 
+     * // Wait for the user to close the frame
+     * latch.await();
+     * </code></pre>
+     * 
+     * This method can be safely called from any thread.
+     * 
+     * @param image image to display
+     * @param title frame title (may be {@code null}
+     * 
+     * @return the latch which will be set to zero when the frame is closed
+     * 
+     * @throws InterruptedException if the thread is interrupted while the image
+     *     frame is being created and shown
+     * 
+     * @throws InvocationTargetException on internal error
+     */
+    public static CountDownLatch showImage(final RenderedImage image, final String title) 
+            throws InterruptedException, InvocationTargetException {
+        
+        if (image == null) {
+            throw new IllegalArgumentException("image must not be null");
+        }
+        
+        final String frameTitle;
+        if (title == null || title.trim().length() == 0) {
+            frameTitle = "Image";
+        } else {
+            frameTitle = title;
+        }
+        
+        final CountDownLatch latch = new CountDownLatch(1);
+        
+        if (SwingUtilities.isEventDispatchThread()) {
+            doShowImage(image, frameTitle, latch);
+        } else {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    doShowImage(image, frameTitle, latch);
+                }
+            });
+        }
+        
+        return latch;
+    }
+
+    /**
+     * Helper method which always runs on the event dispatch thread.
+     * 
+     * @param image image to display
+     * @param title frame title
+     * @param latch latch to count down when the frame is closed
+     */
+    private static void doShowImage(final RenderedImage image, 
+            final String title,
+            final CountDownLatch latch) {
+        
+        ImageFrameWithLatch frame = new ImageFrameWithLatch(image, title, latch);
+        frame.setVisible(true);
+    }
+    
+    private static class ImageFrameWithLatch extends ImageFrame {
+        private final CountDownLatch closeLatch;
+
+        public ImageFrameWithLatch(RenderedImage img, String title, CountDownLatch latch) {
+            super(img, title);
+            this.closeLatch = latch;
+            
+            addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    closeLatch.countDown();
+                }
+            });
+        }
+        
+    }
+
+}

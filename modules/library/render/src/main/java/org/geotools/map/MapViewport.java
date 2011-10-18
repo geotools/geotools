9c6962a49ef105f8de7b68b95b531f2b308e4900
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2011-10-18 03:39:36 +0000

    licence header
    git-svn-id: http://svn.osgeo.org/geotools/branches/2.7.x@38238 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/render/src/main/java/org/geotools/map/MapViewport.java
+++ modules/library/render/src/main/java/org/geotools/map/MapViewport.java
@@ -1,15 +1,30 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ * 
+ *    (C) 2010-2011, Open Source Geospatial Foundation (OSGeo)
+ *    
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package org.geotools.map;
 
 import java.awt.Rectangle;
-import java.awt.geom.AffineTransform;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.map.event.MapBoundsEvent;
-import org.geotools.map.event.MapBoundsListener;
 import org.geotools.map.event.MapBoundsEvent.Type;
+import org.geotools.map.event.MapBoundsListener;
 import org.geotools.util.logging.Logging;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;

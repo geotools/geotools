72845a70e03659b0b0f11b994b3b507576444289
jdeolive <jdeolive@e5c1c795-43da-0310-a71f-fac65c449510>
2011-12-21 15:17:43 +0000

    updating version numbers to 2.7.4
    git-svn-id: http://svn.osgeo.org/geotools/tags/2.7.4@38443 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/metadata/src/main/java/org/geotools/factory/GeoTools.java
+++ modules/library/metadata/src/main/java/org/geotools/factory/GeoTools.java
@@ -93,7 +93,7 @@ public final class GeoTools {
     /**
      * The current GeoTools version. The separator character must be the dot.
      */
-    private static final Version VERSION = new Version(PROPS.getProperty("version", "2.7-SNAPSHOT"));
+    private static final Version VERSION = new Version(PROPS.getProperty("version", "2.7.4"));
 
     /**
      * The version control (svn) revision at which this version of geotools was built.

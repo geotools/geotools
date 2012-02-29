f95fc6e2063c05ff92aca6345ca3e61cc491496b
groldan <groldan@e5c1c795-43da-0310-a71f-fac65c449510>
2012-02-29 15:35:47 +0000

    add missing dummy constant in SeDBMSInfo
    git-svn-id: http://svn.osgeo.org/geotools/branches/2.7.x@38585 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/plugin/arcsde/sde-dummy/src/main/java/com/esri/sde/sdk/client/SeDBMSInfo.java
+++ modules/plugin/arcsde/sde-dummy/src/main/java/com/esri/sde/sdk/client/SeDBMSInfo.java
@@ -3,6 +3,7 @@ package com.esri.sde.sdk.client;
 public class SeDBMSInfo {
 
     public static int SE_DBMS_IS_SQLSERVER = 0;
+    public static int SE_DBMS_IS_ORACLE = 1;
     public int dbmsId;
 
 }

515323f3fde24ff243715e59ffcb29025e0ad3b7
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2012-03-25 09:54:07 +0000

    [GEOT-4087] Make connection validation on by default
    git-svn-id: http://svn.osgeo.org/geotools/branches/2.7.x@38645 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/jdbc/src/main/java/org/geotools/jdbc/JDBCDataStoreFactory.java
+++ modules/library/jdbc/src/main/java/org/geotools/jdbc/JDBCDataStoreFactory.java
@@ -90,7 +90,7 @@ public abstract class JDBCDataStoreFactory extends AbstractDataStoreFactory {
     
     /** If connections should be validated before using them */
     public static final Param VALIDATECONN = new Param("validate connections", Boolean .class,
-            "check connection is alive before using it", false, Boolean.FALSE);
+            "check connection is alive before using it", false, Boolean.TRUE);
     
     /** If connections should be validated before using them */
     public static final Param FETCHSIZE = new Param("fetch size", Integer.class,

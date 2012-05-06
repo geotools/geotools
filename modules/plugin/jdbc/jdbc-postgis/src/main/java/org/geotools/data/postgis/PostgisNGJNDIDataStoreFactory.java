adb3d717ea7a311a0097fdffd2618d08a28f99a2
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2012-05-06 10:24:36 +0000

    Remove extra unexpected param which makes sense only for factories building their own connection pool
    git-svn-id: http://svn.osgeo.org/geotools/trunk@38704 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/plugin/jdbc/jdbc-postgis/src/main/java/org/geotools/data/postgis/PostgisNGJNDIDataStoreFactory.java
+++ modules/plugin/jdbc/jdbc-postgis/src/main/java/org/geotools/data/postgis/PostgisNGJNDIDataStoreFactory.java
@@ -44,7 +44,6 @@ public class PostgisNGJNDIDataStoreFactory extends JDBCJNDIDataStoreFactory {
         parameters.put(LOOSEBBOX.key, LOOSEBBOX);
         parameters.put(ESTIMATED_EXTENTS.key, ESTIMATED_EXTENTS);
         parameters.put(PREPARED_STATEMENTS.key, PREPARED_STATEMENTS);
-        parameters.put(MAX_OPEN_PREPARED_STATEMENTS.key, MAX_OPEN_PREPARED_STATEMENTS);
         parameters.put(ENCODE_FUNCTIONS.key, ENCODE_FUNCTIONS);
     }
 }

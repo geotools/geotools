9af48078e85131e72b7861e3819fd96a8315437f
bencaradocdavies <bencaradocdavies@e5c1c795-43da-0310-a71f-fac65c449510>
2012-05-14 04:52:03 +0000

    Removed unused imports
    git-svn-id: http://svn.osgeo.org/geotools/trunk@38722 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/extension/app-schema/app-schema-resolver/src/main/java/org/geotools/xml/AppSchemaValidator.java
+++ modules/extension/app-schema/app-schema-resolver/src/main/java/org/geotools/xml/AppSchemaValidator.java
@@ -21,8 +21,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;

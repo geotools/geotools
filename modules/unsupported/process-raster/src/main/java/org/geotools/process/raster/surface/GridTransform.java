a57c4b6f571da97b51001260ec41b6c5e165fb8c
mdavis <mdavis@e5c1c795-43da-0310-a71f-fac65c449510>
2012-06-18 18:28:10 +0000

    Added BarnesSurfaceProcess and supporting classes
    git-svn-id: http://svn.osgeo.org/geotools/trunk@38813 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ modules/unsupported/process-raster/src/main/java/org/geotools/process/raster/surface/GridTransform.java
@@ -0,0 +1,115 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2011, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2008-2011 TOPP - www.openplans.org.
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.raster.surface;
+
+import com.vividsolutions.jts.geom.Envelope;
+
+/**
+ * An affine transformation between two parallel 
+ * coordinate systems, one defined by an {@link Envelope}
+ * and one defined by a discrete zero-based grid
+ * representing the same area as the envelope.
+ * The transformation incorporates an isotropic scaling and a translation.
+ * 
+ * @author Martin Davis - OpenGeo
+ *
+ */
+class GridTransform {
+
+    private Envelope env;
+
+    private int xSize;
+
+    private int ySize;
+
+    private double dx;
+
+    private double dy;
+
+    /**
+     * Creates a new transform.
+     * 
+     * @param env the envelope defining one coordinate system
+     * @param xSize the number of cells along the X axis of the grid
+     * @param ySize the number of cells along the Y axis of the grid
+     */
+    public GridTransform(Envelope env, int xSize, int ySize) {
+        this.env = env;
+        this.xSize = xSize;
+        this.ySize = ySize;
+        dx = env.getWidth() / (xSize - 1);
+        dy = env.getHeight() / (ySize - 1);
+    }
+
+    /**
+     * Computes the X ordinate of the i'th grid column.
+     * @param i the index of a grid column
+     * @return the X ordinate of the column
+     */
+    public double x(int i) {
+        if (i >= xSize - 1)
+            return env.getMaxX();
+        return env.getMinX() + i * dx;
+    }
+
+    /**
+     * Computes the Y ordinate of the i'th grid row.
+     * @param j the index of a grid row
+     * @return the Y ordinate of the row
+     */
+    public double y(int j) {
+        if (j >= ySize - 1)
+            return env.getMaxY();
+        return env.getMinY() + j * dy;
+    }
+
+    /**
+     * Computes the column index of an X ordinate.
+     * @param x the X ordinate
+     * @return the column index
+     */
+    public int i(double x) {
+        if (x > env.getMaxX())
+            return xSize;
+        if (x < env.getMinX())
+            return -1;
+        int i = (int) ((x - env.getMinX()) / dx);
+        // have already check x is in bounds, so ensure returning a valid value
+        if (i >= xSize)
+            i = xSize - 1;
+        return i;
+    }
+
+    /**
+     * Computes the column index of an Y ordinate.
+     * @param y the Y ordinate
+     * @return the column index
+     */
+    public int j(double y) {
+        if (y > env.getMaxY())
+            return ySize;
+        if (y < env.getMinY())
+            return -1;
+        int j = (int) ((y - env.getMinY()) / dy);
+        // have already check x is in bounds, so ensure returning a valid value
+        if (j >= ySize)
+            j = ySize - 1;
+        return j;
+    }
+
+}
---
+++ modules/unsupported/process-raster/src/test/java/org/geotools/process/raster/surface/BarnesSurfaceProcessTest.java
@@ -0,0 +1,128 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2002-2011, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.raster.surface;
+
+import static org.junit.Assert.assertTrue;
+
+import java.awt.geom.Point2D;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.FeatureCollections;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.junit.Test;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.util.ProgressListener;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.impl.PackedCoordinateSequenceFactory;
+
+/**
+ * @author Martin Davis - OpenGeo
+ * 
+ */
+public class BarnesSurfaceProcessTest {
+    
+    /**
+     * A test of a simple surface, validating that the process
+     * can be invoked and return a reasonable result in a simple situation.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testSimpleSurface() {
+
+        ReferencedEnvelope bounds = new ReferencedEnvelope(0, 30, 0, 30, DefaultGeographicCRS.WGS84);
+        Coordinate[] data = new Coordinate[] { 
+                new Coordinate(10, 10, 100),
+                new Coordinate(10, 20, 20), 
+                new Coordinate(20, 10, 0), 
+                new Coordinate(20, 20, 80) };
+        SimpleFeatureCollection fc = createPoints(data, bounds);
+
+        ProgressListener monitor = null;
+
+        BarnesSurfaceProcess process = new BarnesSurfaceProcess();
+        GridCoverage2D cov = process.execute(fc, // data
+                "value", // valueAttr
+                1000, // dataLimit
+                10.0, // scale
+                (Double) null, // convergence
+                (Integer) 2, // passes
+                (Integer) null, // minObservations
+                (Double) null, // maxObservationDistance
+                -999.0, // noDataValue
+                1, // pixelsPerCell
+                0.0, // queryBuffer
+                bounds, // outputEnv
+                100, // outputWidth
+                100, // outputHeight
+                monitor // monitor)
+        );
+        
+//      System.out.println(coverageValue(cov, 20, 20));
+
+        double ERROR_TOL = 10;
+        
+        for (Coordinate p : data) {
+            float covval = coverageValue(cov, p.x, p.y);
+            double error = Math.abs(p.z - covval);
+            assertTrue(error < ERROR_TOL);
+        }
+
+    }
+
+    private float coverageValue(GridCoverage2D cov, double x, double y)
+    {
+        float[] covVal = new float[1];
+        Point2D worldPos = new Point2D.Double(x, y);
+        cov.evaluate(worldPos, covVal);
+        return covVal[0];
+    }
+    
+    private SimpleFeatureCollection createPoints(Coordinate[] pts, ReferencedEnvelope bounds)
+    {
+
+        SimpleFeatureTypeBuilder tb = new SimpleFeatureTypeBuilder();
+        tb.setName("obsType");
+        tb.setCRS(bounds.getCoordinateReferenceSystem());
+        tb.add("shape", MultiPoint.class);
+        tb.add("value", Double.class);
+
+        SimpleFeatureType type = tb.buildFeatureType();
+        SimpleFeatureBuilder fb = new SimpleFeatureBuilder(type);
+        SimpleFeatureCollection fc = FeatureCollections.newCollection();
+
+        GeometryFactory factory = new GeometryFactory(new PackedCoordinateSequenceFactory());
+
+        for (Coordinate p : pts) {
+            Geometry point = factory.createPoint(p);
+            fb.add(point);
+            fb.add(p.z);
+            fc.add(fb.buildFeature(null));
+        }
+
+        return fc;
+    }
+
+}

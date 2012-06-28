b901f22bdb79a3c019dc55e01b159a75f66e75bc
victortey <victortey@e5c1c795-43da-0310-a71f-fac65c449510>
2012-06-28 06:06:53 +0000

    GEOT-4185:Docs and Readme not updated when using build script for release, update the copyright year to 2012
    git-svn-id: http://svn.osgeo.org/geotools/trunk@38842 e5c1c795-43da-0310-a71f-fac65c449510

--- docs/common.py
+++ docs/common.py
@@ -42,7 +42,7 @@ exclude_patterns = ['**/.svn']
 
 # General information about the project.
 project = u'GeoTools'
-copyright = u'2011, GeoTools'
+copyright = u'2012, GeoTools'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

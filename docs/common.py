85b5b3f4f8170d500f357c374232f5f83915cc01
jdeolive <jdeolive@opengeo.org>
2012-07-02 23:11:16 +0000

    updating version numbers and README for 8.0-RC2

--- docs/common.py
+++ docs/common.py
@@ -49,7 +49,7 @@ copyright = u'2012, GeoTools'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '8-SNAPSHOT'
+release = '8.0-RC2'
 
 # The short X.Y version.
 version = release

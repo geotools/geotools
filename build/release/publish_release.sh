3fb7834a9747789863853e59cb7a1972cf899d22
Andrea Aime <andrea.aime@gmail.com>
2012-09-01 15:31:33 +0200

    Make the publish script create the parent directory if necessary (rsynch just creates one target directory, not its parents), have one parent directory per release series like in the past

--- build/release/publish_release.sh
+++ build/release/publish_release.sh
@@ -4,23 +4,19 @@
 set -e
 
 function usage() {
-  echo "$0 [options] <tag> <branch> <user> <email>"
+  echo "$0 [options] <tag> <branch>"
   echo
   echo " tag : Release tag (eg: 2.1.4, 2.2-beta1, ...)"
-  echo " user:  Git username"
-  echo " email: Git email"
   echo 
 }
 
-if [ -z $4 ]; then
+if [ -z $2 ]; then
   usage
   exit
 fi
 
 tag=$1
 branch=$2
-git_user=$3
-git_email=$4
 
 # load properties + functions
 . "$( cd "$( dirname "$0" )" && pwd )"/properties
@@ -36,8 +32,6 @@ if [ `is_primary_branch_num $tag` == "1" ]; then
 fi
 
 pushd ../../ > /dev/null
-
-init_git $git_user $git_email
 
 # switch to the release branch
 git checkout rel_$tag
@@ -60,17 +54,18 @@ if [ "$( echo $str | egrep "[0-9]+\.[0-9]+((\.|-).*)?" )" != "$str" ]; then
   echo "$tag is not a valid release version number"
   exit 1
 fi
-dir=`echo $tag | sed 's/\([0-9]*\.[0-9]*\).*/\1/g'`
+dir=`echo $tag | sed 's/\([0-9]*\)\.\([0-9]*\).*/\1/g'
 
 pushd $DIST_PATH/$tag > /dev/null
 
+ssh -i $SF_PK $SF_USER@$SF_HOST mkdir -p "/home/pfs/project/g/ge/geotools/GeoTools\ $dir\ Releases"
 rsync -ave "ssh -i $SF_PK" *.zip $SF_USER@$SF_HOST:"/home/pfs/project/g/ge/geotools/GeoTools\ $dir\ Releases/$tag/"
 
 popd > /dev/null
 
 # merge the tag release branch into main release branch and tag it
 git checkout rel_$branch
-git merge -Xtheirs -m "Merging rel_$tag into rel_$branch" rel_$tag
+git merge -m "Merging rel_$tag into rel_$branch" rel_$tag
 git tag $tag
 
 # push them up

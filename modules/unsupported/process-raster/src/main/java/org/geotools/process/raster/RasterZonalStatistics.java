e818da02b5f81defa470b994532f59d9336b3631
Jody Garnett <jody.garnett@gmail.com>
2012-10-30 21:53:52 +1000

    grind through process-raster - no surprises here

--- modules/unsupported/process-raster/src/main/java/org/geotools/process/raster/RasterZonalStatistics.java
+++ modules/unsupported/process-raster/src/main/java/org/geotools/process/raster/RasterZonalStatistics.java
@@ -169,18 +169,6 @@ public class RasterZonalStatistics implements RasterProcess {
             return new RasterZonalStatisticsIterator(delegate.features(), coverage, band,
                     targetSchema, classification);
         }
-
-        @Override
-        public Iterator<SimpleFeature> iterator() {
-            return new WrappingIterator(features());
-        }
-
-        @Override
-        public void close(Iterator<SimpleFeature> close) {
-            if (close instanceof WrappingIterator) {
-                ((WrappingIterator) close).close();
-            }
-        }
     }
 
     /**

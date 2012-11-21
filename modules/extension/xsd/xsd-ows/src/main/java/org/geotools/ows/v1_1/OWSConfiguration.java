98239d5d8d8737dad766576a1d59218f75af8bd2
jdeolive <jdeolive@opengeo.org>
2012-11-20 21:47:20 -0700

    updated OWSConfiguration (1.1) to include DomainTypeBinding, and added test case

--- modules/extension/xsd/xsd-ows/src/main/java/org/geotools/ows/v1_1/OWSConfiguration.java
+++ modules/extension/xsd/xsd-ows/src/main/java/org/geotools/ows/v1_1/OWSConfiguration.java
@@ -78,6 +78,7 @@ public class OWSConfiguration extends Configuration {
         bindings.put(OWS.CodeType,new SimpleContentComplexEMFBinding(Ows11Factory.eINSTANCE, OWS.CodeType));
         bindings.put(OWS.ContactType,new ComplexEMFBinding(Ows11Factory.eINSTANCE, OWS.ContactType));
         bindings.put(OWS.DomainMetadataType,new SimpleContentComplexEMFBinding(Ows11Factory.eINSTANCE,OWS.DomainMetadataType));
+        bindings.put(OWS.DomainType,new ComplexEMFBinding(Ows11Factory.eINSTANCE,OWS.DomainType));
         
         bindings.put(OWS.ExceptionType,new ComplexEMFBinding(Ows11Factory.eINSTANCE, OWS.ExceptionType));
         bindings.put(OWS.KeywordsType, new ComplexEMFBinding(Ows11Factory.eINSTANCE, OWS.KeywordsType));

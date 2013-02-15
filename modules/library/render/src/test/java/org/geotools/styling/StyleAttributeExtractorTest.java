8638ff8935eb8f592c50005ee8f0f7a65b44fa00
Andrea Aime <andrea.aime@gmail.com>
2013-02-15 12:19:27 +0100

    [GEOT-4393] 'property' filter function does not work anymore after style and filter attribute extractor got updated to support complex features

--- modules/library/render/src/test/java/org/geotools/styling/StyleAttributeExtractorTest.java
+++ modules/library/render/src/test/java/org/geotools/styling/StyleAttributeExtractorTest.java
@@ -1,15 +1,17 @@
 package org.geotools.styling;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.Set;
 
 import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.filter.function.EnvFunction;
 import org.junit.Test;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory;
+import org.opengis.filter.PropertyIsEqualTo;
+import org.opengis.filter.expression.Function;
+import org.opengis.filter.expression.PropertyName;
 
 /**
  * 
@@ -49,6 +51,41 @@ public class StyleAttributeExtractorTest {
         assertTrue(atts.contains("offx"));
         assertTrue(atts.contains("offy"));
         assertFalse(extractor.getDefaultGeometryUsed());
+    }
+    
+    public void testPropertyFucntion() {
+        PointSymbolizer ps = sb.createPointSymbolizer();
+        ps.setGeometry(ff.function("offset", ff.property("the_geom"), ff.property("offx"), ff.property("offy")));
+        Function func = ff.function("property", ff.function("env", ff.literal("pname")));
+        PropertyIsEqualTo filter = ff.equals(func, ff.literal("test"));
+        Rule r = sb.createRule(ps);
+        r.setFilter(filter);
+        
+        try  {
+            EnvFunction.setLocalValue("pname", "name");
+            StyleAttributeExtractor extractor = new StyleAttributeExtractor();
+            r.accept(extractor);
+            
+            // check the plain names
+            Set<String> atts = extractor.getAttributeNameSet();
+            assertEquals(4, atts.size());
+            assertTrue(atts.contains("the_geom"));
+            assertTrue(atts.contains("offx"));
+            assertTrue(atts.contains("offy"));
+            assertTrue(atts.contains("name"));
+            assertFalse(extractor.getDefaultGeometryUsed());
+            
+            // checks also the property names, see they are consistent
+            Set<PropertyName> propNames = extractor.getAttributes();
+            assertNotNull(propNames);
+            assertEquals(atts.size(), propNames.size());
+            
+            for (PropertyName pn : propNames) {
+                assertTrue(atts.contains(pn.getPropertyName()));
+            }
+        } finally {
+            EnvFunction.clearLocalValues();
+        }
     }
 
 }

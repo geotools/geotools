61e547fd65db31f42a2913299032f452cc9d1c28
Christian Mueller <christian.mueller@os-solutions.at>
2013-06-13 14:38:02 +0200

    GEOT-4561 Incorrect image creation for images with type BufferedImage.TYPE_CUSTOM

--- modules/plugin/imagemosaic-jdbc/src/main/java/org/geotools/gce/imagemosaic/jdbc/AbstractThread.java
+++ modules/plugin/imagemosaic-jdbc/src/main/java/org/geotools/gce/imagemosaic/jdbc/AbstractThread.java
@@ -19,10 +19,12 @@ package org.geotools.gce.imagemosaic.jdbc;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.awt.image.IndexColorModel;
+import java.awt.image.Raster;
+import java.awt.image.SampleModel;
+import java.awt.image.WritableRaster;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -85,58 +87,61 @@ abstract class AbstractThread extends Thread {
 	}
 
 	/**
-	 * @return a Map containing a rendereing hint for the interpolation as
-	 *         specified in the config
-	 */
-	Map<RenderingHints.Key, Object> getRenderingHints() {
-		final Map<RenderingHints.Key, Object> hints = new HashMap<RenderingHints.Key, Object>();
-		Object interpolation = null;
-
-		if (config.getInterpolation().intValue() == 1) {
-			interpolation = RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR;
-		}
-
-		if (config.getInterpolation().intValue() == 2) {
-			interpolation = RenderingHints.VALUE_INTERPOLATION_BILINEAR;
-		}
-
-		if (config.getInterpolation().intValue() == 3) {
-			interpolation = RenderingHints.VALUE_INTERPOLATION_BICUBIC;
-		}
-
-		hints.put(RenderingHints.KEY_INTERPOLATION, interpolation);
-
-		return hints;
-	}
-
-	/**
 	 * @param image
 	 *            to scale
 	 * @return rescaled image fitting into the requested pixel dimension
 	 */
 	protected BufferedImage rescaleImage(BufferedImage image) {
+	    	    
+		
+		Map<String,Object> properties = null;
+	                
+	        if (image.getPropertyNames()!=null) {
+	            properties = new HashMap<String, Object>();
+	            for (String name : image.getPropertyNames()) {
+	                 properties.put(name, image.getProperty(name));
+	            }
+	        }
+	                            
+	        int newWidth = (int) Math.floor(image.getWidth()* rescaleX);
+	        int newHeight= (int) Math.floor(image.getHeight() * rescaleY);
+	        SampleModel sm = image.getSampleModel().createCompatibleSampleModel(newWidth,newHeight);
+	        WritableRaster raster = Raster.createWritableRaster(sm, null);
 
-		BufferedImage scaledImage = null;
-		int imageType = image.getType();
+	        BufferedImage scaledImage = new BufferedImage(image.getColorModel(), 
+	                raster, image.isAlphaPremultiplied(), (Hashtable<?, ?>) properties); 
 
-		if ((imageType == BufferedImage.TYPE_BYTE_BINARY || imageType == BufferedImage.TYPE_BYTE_INDEXED)
-				&& (image.getColorModel() instanceof IndexColorModel))
-			scaledImage = new BufferedImage((int) Math.floor(image.getWidth()
-					* rescaleX),
-					(int) Math.floor(image.getHeight() * rescaleY), image
-							.getType(), (IndexColorModel) image.getColorModel());
-		else
-			scaledImage = new BufferedImage((int) Math.floor(image.getWidth()
-					* rescaleX),
-					(int) Math.floor(image.getHeight() * rescaleY), image
-							.getType());
 
-		final Graphics2D g2D = (Graphics2D) scaledImage.getGraphics();
+                Object interpolation=RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR;
+                
+                if (config.getInterpolation().intValue() == 2) 
+                           interpolation = RenderingHints.VALUE_INTERPOLATION_BILINEAR;
 
-		g2D.addRenderingHints(getRenderingHints());
-		g2D.drawImage(image, AffineTransform.getScaleInstance(rescaleX,
-				rescaleY), null);
+                if (config.getInterpolation().intValue() == 3) 
+                           interpolation = RenderingHints.VALUE_INTERPOLATION_BICUBIC;
+
+                
+                Graphics2D g = scaledImage.createGraphics();
+                g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, interpolation);
+                g.drawImage(image, 0, 0, newWidth, newHeight, 0, 0, image.getWidth(), image.getHeight(), null);
+                g.dispose();
+                return scaledImage;
+	        
+	       /* 
+	       int interpolation=AffineTransformOp.TYPE_NEAREST_NEIGHBOR;
+               
+	       if (config.getInterpolation().intValue() == 2) 
+	                   interpolation = AffineTransformOp.TYPE_BILINEAR;
+
+                if (config.getInterpolation().intValue() == 3) 
+	                     interpolation = AffineTransformOp.TYPE_BICUBIC;
+                
+		AffineTransformOp op= new AffineTransformOp(AffineTransform.getScaleInstance(rescaleX,
+		        rescaleY), interpolation);
+		op.filter(image.getRaster(),raster);
 
 		return scaledImage;
+		*/
+		
 	}
 }

cb7b26f1be1eefcb945e5dab6c8ae40fb850ae72
Andrea Aime <andrea.aime@gmail.com>
2013-06-29 12:11:59 +0200

    GEOT-4429, updating tests for mysql and sql server

--- modules/plugin/jdbc/jdbc-mysql/src/test/java/org/geotools/data/mysql/MySQLJoinTestSetup.java
+++ modules/plugin/jdbc/jdbc-mysql/src/test/java/org/geotools/data/mysql/MySQLJoinTestSetup.java
@@ -27,36 +27,43 @@ public class MySQLJoinTestSetup extends JDBCJoinTestSetup {
 
     @Override
     protected void createJoinTable() throws Exception {
-      //create some data
+      //create some data  
         StringBuffer sb = new StringBuffer();
         sb.append("CREATE TABLE ftjoin ").append("(id int, ")
-          .append("name VARCHAR(255) COLLATE latin1_general_cs, geom POLYGON) ENGINE=InnoDB;");
+          .append("name VARCHAR(255) COLLATE latin1_general_cs, geom POLYGON, join1intProperty int) ENGINE=InnoDB;");
         run(sb.toString());
 
         sb = new StringBuffer();
         sb.append("INSERT INTO ftjoin VALUES (")
-          .append("0, 'zero', GeometryFromText('POLYGON ((-0.1 -0.1, -0.1 0.1, 0.1 0.1, 0.1 -0.1, -0.1 -0.1))',4326));");
+          .append("0, 'zero', GeometryFromText('POLYGON ((-0.1 -0.1, -0.1 0.1, 0.1 0.1, 0.1 -0.1, -0.1 -0.1))',4326), 0);");
         run(sb.toString());
 
         sb = new StringBuffer();
         sb.append("INSERT INTO ftjoin VALUES (")
-        .append("1, 'one', GeometryFromText('POLYGON ((-1.1 -1.1, -1.1 1.1, 1.1 1.1, 1.1 -1.1, -1.1 -1.1))',4326));");
+        .append("1, 'one', GeometryFromText('POLYGON ((-1.1 -1.1, -1.1 1.1, 1.1 1.1, 1.1 -1.1, -1.1 -1.1))',4326), 1);");
         run(sb.toString());
       
         sb = new StringBuffer();
         sb.append("INSERT INTO ftjoin VALUES (")
-        .append("2, 'two', GeometryFromText('POLYGON ((-10 -10, -10 10, 10 10, 10 -10, -10 -10))',4326));");
+        .append("2, 'two', GeometryFromText('POLYGON ((-10 -10, -10 10, 10 10, 10 -10, -10 -10))',4326), 2);");
         run(sb.toString());
         
         sb = new StringBuffer();
         sb.append("INSERT INTO ftjoin VALUES (")
-        .append("3, 'three', NULL);");
+        .append("3, 'three', NULL, 3);");
         run(sb.toString());
+        
+        run( "CREATE TABLE ftjoin2(id int, join2intProperty int, stringProperty2 varchar(255))");
+        run( "INSERT INTO ftjoin2 VALUES (0, 0, '2nd zero')");
+        run( "INSERT INTO ftjoin2 VALUES (1, 1, '2nd one')");
+        run( "INSERT INTO ftjoin2 VALUES (2, 2, '2nd two')");
+        run( "INSERT INTO ftjoin2 VALUES (3, 3, '2nd three')");
     }
 
     @Override
     protected void dropJoinTable() throws Exception {
         runSafe("DROP TABLE ftjoin");
+        runSafe("DROP TABLE ftjoin2");
     }
 
     @Override
--- modules/plugin/jdbc/jdbc-sqlserver/src/test/java/org/geotools/data/sqlserver/SQLServerJoinTestSetup.java
+++ modules/plugin/jdbc/jdbc-sqlserver/src/test/java/org/geotools/data/sqlserver/SQLServerJoinTestSetup.java
@@ -27,20 +27,27 @@ public class SQLServerJoinTestSetup extends JDBCJoinTestSetup {
     @Override
     protected void createJoinTable() throws Exception {
         
-        run( "CREATE TABLE ftjoin ( id int, name VARCHAR(10), geom GEOMETRY)" );
+        run( "CREATE TABLE ftjoin ( id int, name VARCHAR(10), geom GEOMETRY, join1intProperty int)" );
         run("ALTER TABLE ftjoin ALTER COLUMN name VARCHAR(255) COLLATE Latin1_General_CS_AS");
-        run( "INSERT INTO ftjoin VALUES (0, 'zero', geometry::STGeomFromText('POLYGON ((-0.1 -0.1, -0.1 0.1, 0.1 0.1, 0.1 -0.1, -0.1 -0.1))', 4326))");
-        run( "INSERT INTO ftjoin VALUES (1, 'one', geometry::STGeomFromText('POLYGON ((-1.1 -1.1, -1.1 1.1, 1.1 1.1, 1.1 -1.1, -1.1 -1.1))', 4326))");
-        run( "INSERT INTO ftjoin VALUES (2, 'two', geometry::STGeomFromText('POLYGON ((-10 -10, -10 10, 10 10, 10 -10, -10 -10))', 4326))");
-        run( "INSERT INTO ftjoin VALUES (3, 'three', NULL)");
+        run( "INSERT INTO ftjoin VALUES (0, 'zero', geometry::STGeomFromText('POLYGON ((-0.1 -0.1, -0.1 0.1, 0.1 0.1, 0.1 -0.1, -0.1 -0.1))', 4326), 0)");
+        run( "INSERT INTO ftjoin VALUES (1, 'one', geometry::STGeomFromText('POLYGON ((-1.1 -1.1, -1.1 1.1, 1.1 1.1, 1.1 -1.1, -1.1 -1.1))', 4326), 1)");
+        run( "INSERT INTO ftjoin VALUES (2, 'two', geometry::STGeomFromText('POLYGON ((-10 -10, -10 10, 10 10, 10 -10, -10 -10))', 4326), 2)");
+        run( "INSERT INTO ftjoin VALUES (3, 'three', NULL, 3)");
         
         // won't work in sql server since the table has no primary key
         // run("CREATE SPATIAL INDEX _ftjoin_geometry_index on ftjoin(geom) WITH (BOUNDING_BOX = (-10, -10, 10, 10))");
+        
+        run( "CREATE TABLE ftjoin2(id int, join2intProperty int, stringProperty2 varchar(255))");
+        run( "INSERT INTO ftjoin2 VALUES (0, 0, '2nd zero')");
+        run( "INSERT INTO ftjoin2 VALUES (1, 1, '2nd one')");
+        run( "INSERT INTO ftjoin2 VALUES (2, 2, '2nd two')");
+        run( "INSERT INTO ftjoin2 VALUES (3, 3, '2nd three')");
     }
 
     @Override
     protected void dropJoinTable() throws Exception {
         runSafe("DROP TABLE ftjoin");
+        runSafe("DROP TABLE ftjoin2");
     }
 
 }

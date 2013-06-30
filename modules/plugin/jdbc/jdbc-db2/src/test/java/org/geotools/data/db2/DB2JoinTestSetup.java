cd6eeea579a27f04ee5dc44a6db4d993d0d37ccf
Christian Mueller <christian.mueller@os-solutions.at>
2013-06-30 11:46:40 +0200

    GEOT-4429, updating tests DB2

--- modules/plugin/jdbc/jdbc-db2/src/test/java/org/geotools/data/db2/DB2JoinTestSetup.java
+++ modules/plugin/jdbc/jdbc-db2/src/test/java/org/geotools/data/db2/DB2JoinTestSetup.java
@@ -35,30 +35,66 @@ public class DB2JoinTestSetup extends JDBCJoinTestSetup {
         String stmt = "create table "+DB2TestUtil.SCHEMA_QUOTED+
                         ".\"ftjoin\" (\"id\" int ," +
                         "\"name\" varchar(255), " +
-                        " \"geom\" DB2GSE.ST_GEOMETRY ) ";
+                        " \"geom\" DB2GSE.ST_GEOMETRY, \"join1intProperty\" INT ) ";
                         
         con.prepareStatement(stmt    ).execute();    
         DB2Util.executeRegister(DB2TestUtil.SCHEMA, "ftjoin", "geom", DB2TestUtil.SRSNAME, con);
         
         con.prepareStatement( 
-                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin\" (\"id\",\"name\", \"geom\")  " +                        
-                "VALUES (0, 'zero',db2gse.st_GeomFromText('POLYGON ((-0.1 -0.1, -0.1 0.1, 0.1 0.1, 0.1 -0.1, -0.1 -0.1))', "+DB2TestUtil.SRID+"))" )
+                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin\" (\"id\",\"name\", \"geom\" , \"join1intProperty\")  " +                        
+                "VALUES (0, 'zero',db2gse.st_GeomFromText('POLYGON ((-0.1 -0.1, -0.1 0.1, 0.1 0.1, 0.1 -0.1, -0.1 -0.1))', "+DB2TestUtil.SRID+"),0)" )
                 .execute();
         
         con.prepareStatement( 
-                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin\" (\"id\",\"name\", \"geom\")  " +                        
-                "VALUES (1, 'one',db2gse.st_GeomFromText('POLYGON ((-1.1 -1.1, -1.1 1.1, 1.1 1.1, 1.1 -1.1, -1.1 -1.1))', "+DB2TestUtil.SRID+"))" )
+                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin\" (\"id\",\"name\", \"geom\", \"join1intProperty\")  " +                        
+                "VALUES (1, 'one',db2gse.st_GeomFromText('POLYGON ((-1.1 -1.1, -1.1 1.1, 1.1 1.1, 1.1 -1.1, -1.1 -1.1))', "+DB2TestUtil.SRID+"),1)" )
                 .execute();
 
         con.prepareStatement( 
-                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin\" (\"id\",\"name\", \"geom\")  " +                        
-                "VALUES (2, 'two',db2gse.st_GeomFromText('POLYGON ((-10 -10, -10 10, 10 10, 10 -10, -10 -10))', "+DB2TestUtil.SRID+"))" )
+                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin\" (\"id\",\"name\", \"geom\", \"join1intProperty\")  " +                        
+                "VALUES (2, 'two',db2gse.st_GeomFromText('POLYGON ((-10 -10, -10 10, 10 10, 10 -10, -10 -10))', "+DB2TestUtil.SRID+"),2)" )
                 .execute();
 
         con.prepareStatement( 
-                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin\" (\"id\",\"name\", \"geom\")  " +                        
-                "VALUES (3, 'three',NULL)" )
+                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin\" (\"id\",\"name\", \"geom\", \"join1intProperty\")  " +                        
+                "VALUES (3, 'three',NULL,3)" )
                 .execute();
+
+        stmt = "create table "+DB2TestUtil.SCHEMA_QUOTED+
+                ".\"ftjoin2\" (\"id\" int ," +
+                "\"join2intProperty\" int, " +
+                "\"stringProperty2\" VARCHAR(255)) ";
+                
+                
+        con.prepareStatement(stmt    ).execute();
+        
+        con.prepareStatement( 
+                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin2\" (\"id\",\"join2intProperty\", \"stringProperty2\")  " +                        
+                "VALUES (0, 0, '2nd zero')" )
+                .execute();
+        
+        con.prepareStatement( 
+                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin2\" (\"id\",\"join2intProperty\", \"stringProperty2\")  " +                        
+                "VALUES (1, 1, '2nd one')" )
+                .execute();
+        
+        con.prepareStatement( 
+                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin2\" (\"id\",\"join2intProperty\", \"stringProperty2\")  " +                        
+                "VALUES (2, 2, '2nd two')" )
+                .execute();
+
+        con.prepareStatement( 
+                "INSERT INTO "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin2\" (\"id\",\"join2intProperty\", \"stringProperty2\")  " +                        
+                "VALUES (3, 3, '2nd three')" )
+                .execute();
+
+
+        
+//        run( "CREATE TABLE ftjoin2( id INT, join2intProperty INT, stringProperty2 VARCHAR(255))");
+//        run( "INSERT INTO ftjoin2 VALUES (0, 0, '2nd zero')");
+//        run( "INSERT INTO ftjoin2 VALUES (1, 1, '2nd one')");
+//        run( "INSERT INTO ftjoin2 VALUES (2, 2, '2nd two')");
+//        run( "INSERT INTO ftjoin2 VALUES (3, 3, '2nd three')");
 
         
         con.close();
@@ -72,7 +108,11 @@ public class DB2JoinTestSetup extends JDBCJoinTestSetup {
         Connection con = getDataSource().getConnection();
         DB2Util.executeUnRegister(DB2TestUtil.SCHEMA, "ftjoin", "geom",  con);
         con.prepareStatement("DROP TABLE "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin\"").execute();
+        con.prepareStatement("DROP TABLE "+DB2TestUtil.SCHEMA_QUOTED+".\"ftjoin2\"").execute();
+        
         con.close();
+        
+        
         
     }
 

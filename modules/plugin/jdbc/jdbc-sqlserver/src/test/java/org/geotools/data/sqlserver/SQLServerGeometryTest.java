02ddcb35ea551b69f362afe73ea7af9053d9a9ae
Jody Garnett <jody.garnett@gmail.com>
2013-07-17 16:30:10 +1000

    Fix headers after confirming (c) 2012
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- modules/plugin/jdbc/jdbc-sqlserver/src/test/java/org/geotools/data/sqlserver/SQLServerGeometryTest.java
+++ modules/plugin/jdbc/jdbc-sqlserver/src/test/java/org/geotools/data/sqlserver/SQLServerGeometryTest.java
@@ -1,23 +1,18 @@
 /*
- *  GeoBatch - Open Source geospatial batch processing system
- *  https://github.com/nfms4redd/nfms-geobatch
- *  Copyright (C) 2007-2012 GeoSolutions S.A.S.
- *  http://www.geo-solutions.it
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
  *
- *  GPLv3 + Classpath exception
+ *    (C) 2012, Open Source Geospatial Foundation (OSGeo)
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 package org.geotools.data.sqlserver;
 

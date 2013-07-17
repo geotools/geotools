6dbaf9bae1a0799a2cafc6e2433847259c3326fd
Jody Garnett <jody.garnett@gmail.com>
2013-07-17 16:27:04 +1000

    Fix headers for gt-swt (checked github history to confirm (c) 2011
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- modules/unsupported/swt/src/main/java/org/geotools/swt/action/OpenGeotiffAction.java
+++ modules/unsupported/swt/src/main/java/org/geotools/swt/action/OpenGeotiffAction.java
@@ -1,19 +1,18 @@
 /*
- * JGrass - Free Open Source Java GIS http://www.jgrass.org 
- * (C) HydroloGIS - www.hydrologis.com 
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *    (C) 2011, Open Source Geospatial Foundation (OSGeo)
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 package org.geotools.swt.action;
 

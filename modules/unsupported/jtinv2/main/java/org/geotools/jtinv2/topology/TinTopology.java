fa58a1e86fd151475a4bc6d2abb230308ec4500a
The Sunburned Surveyor <sunburned.surveyor@gmail.com>
2013-08-09 15:37:42 -0700

    JTIN V2: Commit GeoTools source code header to all source code files.

--- modules/unsupported/jtinv2/main/java/org/geotools/jtinv2/topology/TinTopology.java
+++ modules/unsupported/jtinv2/main/java/org/geotools/jtinv2/topology/TinTopology.java
@@ -19,52 +19,12 @@ package org.geotools.jtinv2.topology;
 
 import java.util.List;
 
-import org.geotools.jtinv2.main.ThreeDimensionalLine;
 import org.geotools.jtinv2.main.TinFace;
 import org.geotools.jtinv2.main.TriangulatedIrregularNetwork;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
-public class TinTopologyFactory 
+public interface TinTopology 
 {
-
-	public Coordinate findFirstNode()
-	{
-		
-	}
-	
-	public Coordinate findSecondNode()
-	{
-		
-	}
-	
-	public Coordinate findThirdNode()
-	{
-		
-	}
-	
-	public List<ThreeDimensionalLine> FindConnectedEdges(ThreeDimensionalLine argEdge, 
-			TriangulatedIrregularNetwork argTin)
-	{
-		
-	}
-	
-	public List<TinFace> FindConnectedEdges(ThreeDimensionalLine argEdge, 
-			TriangulatedIrregularNetwork argTin)
-	{
-		
-	}
-	
-	public List<TinFace> FindConnectedFaces(ThreeDimensionalLine argEdge, 
-			TriangulatedIrregularNetwork argTin)
-	{
-			
-	}
-	
-	public List<TinFace> FindConnectedFaces(TinFace argFace, 
-			TriangulatedIrregularNetwork argTin)
-	{
-			
-	}
-
+	public abstract TinFace[] getAdjacentFaces(TinFace argTarget);
 }

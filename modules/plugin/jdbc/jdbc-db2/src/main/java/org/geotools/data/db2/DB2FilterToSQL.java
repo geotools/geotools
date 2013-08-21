f6b94aef9084b7ae6edd2644758d827bfcf1aa0c
Christian Mueller <christian.mueller@os-solutions.at>
2013-08-21 12:20:58 +0200

    Correct DB2 headers

--- modules/plugin/jdbc/jdbc-db2/src/main/java/org/geotools/data/db2/DB2FilterToSQL.java
+++ modules/plugin/jdbc/jdbc-db2/src/main/java/org/geotools/data/db2/DB2FilterToSQL.java
@@ -1,7 +1,8 @@
 /*
  *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
- *    (C) Copyright IBM Corporation, 2005-2007. All rights reserved.
+ *
+ *    (C) 2011-2012, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -14,6 +15,7 @@
  *    Lesser General Public License for more details.
  *
  */
+
 package org.geotools.data.db2;
 
 import java.io.IOException;
--- modules/plugin/jdbc/jdbc-db2/src/main/java/org/geotools/data/db2/DB2WKBReader.java
+++ modules/plugin/jdbc/jdbc-db2/src/main/java/org/geotools/data/db2/DB2WKBReader.java
@@ -1,7 +1,8 @@
 /*
  *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
- *    (C) Copyright IBM Corporation, 2005-2007. All rights reserved.
+ *
+ *    (C) 2011-2012, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -14,15 +15,8 @@
  *    Lesser General Public License for more details.
  *
  */
-
 package org.geotools.data.db2;
 
-/**
- * This is a modified JTS WKBReader suiting for DB2
- * 
- * @author Christian Mueller
- *
- */
 
 import java.io.IOException;
 import com.vividsolutions.jts.geom.*;

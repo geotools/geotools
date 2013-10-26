5111bbfd970990e53bf48608c53e5a45ecfd7ab1
Andrea Aime <andrea.aime@gmail.com>
2013-10-26 12:36:27 +0200

    Fixing test expectations after GEOT-4600

--- modules/library/jdbc/src/test/java/org/geotools/jdbc/EscapeSqlTest.java
+++ modules/library/jdbc/src/test/java/org/geotools/jdbc/EscapeSqlTest.java
@@ -31,15 +31,15 @@ public class EscapeSqlTest extends TestCase  {
         vt.addParameter(new VirtualTableParameter("param1", "default_value", new RegexpValidator(".*")));
         String singleQuote = vt.expandParameters(new Hints(
                 Hints.VIRTUAL_TABLE_PARAMETERS, Collections.singletonMap("param1", "o'shea")));
-        assertEquals("single quotes should be doubled", "o''shea", singleQuote);
+        assertEquals("single quotes should be doubled", "o''shea\n", singleQuote);
         
         String doubleQuote = vt.expandParameters(new Hints(
                 Hints.VIRTUAL_TABLE_PARAMETERS, Collections.singletonMap("param1", "If you hear a voice within you say \"you cannot paint,\" then by all means paint, and that voice will be silenced.")));
-        assertEquals("double quotes should be doubled", "If you hear a voice within you say \"\"you cannot paint,\"\" then by all means paint, and that voice will be silenced.", 
+        assertEquals("double quotes should be doubled", "If you hear a voice within you say \"\"you cannot paint,\"\" then by all means paint, and that voice will be silenced.\n", 
                 doubleQuote);
         
         String backslash = vt.expandParameters(new Hints(
                 Hints.VIRTUAL_TABLE_PARAMETERS, Collections.singletonMap("param1", "abc\\n")));
-        assertEquals("backslashes should be removed", "abcn", backslash);
+        assertEquals("backslashes should be removed", "abcn\n", backslash);
     }
 }

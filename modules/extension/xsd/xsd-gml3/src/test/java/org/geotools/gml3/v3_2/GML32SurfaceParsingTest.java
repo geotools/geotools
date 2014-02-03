040d8b446a333c3acd19b0169b53261a76701859
Tobias Warneke <t.warneke@gmx.net>
2014-02-03 14:26:55 +0100

    simple composite curve type parsing support included, GEOT-4683
    Backort of #357

--- modules/extension/xsd/xsd-gml3/src/test/java/org/geotools/gml3/v3_2/GML32SurfaceParsingTest.java
+++ modules/extension/xsd/xsd-gml3/src/test/java/org/geotools/gml3/v3_2/GML32SurfaceParsingTest.java
@@ -1,15 +1,17 @@
 package org.geotools.gml3.v3_2;
 
+import com.vividsolutions.jts.geom.Geometry;
 import org.geotools.xml.Parser;
 
-import com.vividsolutions.jts.geom.LineString;
 
 public class GML32SurfaceParsingTest extends GML32TestSupport {
 
     public void testMultiSurface() throws Exception {
         GMLConfiguration gml = new GMLConfiguration(true);
         Parser p = new Parser(gml);
-        System.out.println(p.parse(getClass().getResourceAsStream("multisurface.xml")));
+        Object multiSurface = p.parse(getClass().getResourceAsStream("multisurface.xml"));
+        System.out.println(multiSurface);
+        assertTrue(multiSurface instanceof Geometry);
         //assertNotNull(l);
     }
 }

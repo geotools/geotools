5a4d8262a6fd7c9fb2dfd5453ed0af2ec2bd1716
Sampo Savolainen <sampo.savolainen@spatineo.com>
2014-04-11 14:33:45 +0300

    Clean up unused code

--- modules/unsupported/wfs-ng/src/main/java/org/geotools/data/wfs/internal/DescribeStoredQueriesResponse.java
+++ modules/unsupported/wfs-ng/src/main/java/org/geotools/data/wfs/internal/DescribeStoredQueriesResponse.java
@@ -8,48 +8,29 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 
-import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import net.opengis.wfs20.DescribeStoredQueriesResponseType;
-import net.opengis.wfs20.QueryExpressionTextType;
 import net.opengis.wfs20.StoredQueryDescriptionType;
 
 import org.apache.commons.io.IOUtils;
 import org.geotools.data.DataSourceException;
 import org.geotools.data.ows.HTTPResponse;
-import org.geotools.data.wfs.internal.parsers.EmfAppSchemaParser;
 import org.geotools.ows.ServiceException;
 import org.geotools.xml.Configuration;
 import org.geotools.xml.DOMParser;
-import org.opengis.feature.type.FeatureType;
 import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class DescribeStoredQueriesResponse extends WFSResponse {
 
 	private DescribeStoredQueriesResponseType describeStoredQueriesResponse;
 	
-	private Map<String, List<FeatureType>> featureTypesPerStoredQueryId;
-	
 	public DescribeStoredQueriesResponse(WFSRequest originatingRequest, HTTPResponse response) throws IOException, ServiceException {
 		super(originatingRequest, response);
-		
-		final WFSStrategy strategy = originatingRequest.getStrategy();
-		final Configuration wfsConfiguration = strategy.getWfsConfiguration();
 		
 		MODULE.finer("Parsing DescribeStoredQueries response");
 		try {
@@ -82,34 +63,11 @@ public class DescribeStoredQueriesResponse extends WFSResponse {
 						+ e.getMessage(), e);
 			}
 
-			
 			describeStoredQueriesResponse = parseStoredQueries(rawDocument, WFS_2_0_CONFIGURATION);
-
 			
 			if (null == describeStoredQueriesResponse) {
 				throw new IllegalStateException(
 						"Unable to parse DescribeStoredQueriesResponse document");
-			}
-
-			featureTypesPerStoredQueryId = new HashMap<String, List<FeatureType>>();
-			
-
-			for (StoredQueryDescriptionType sqdt : describeStoredQueriesResponse.getStoredQueryDescription()) {
-				String id = sqdt.getId();
-				
-				List<FeatureType> featureTypesForId = new ArrayList<FeatureType>();
-				featureTypesPerStoredQueryId.put(id, featureTypesForId);
-				
-				for (QueryExpressionTextType qett : sqdt.getQueryExpressionText()) {
-					for (QName remoteTypeName : qett.getReturnFeatureTypes()) {
-						URL schemaLocation = findSchemaLocation(rawDocument, remoteTypeName.getNamespaceURI());
-						
-						FeatureType tmp = EmfAppSchemaParser.parse(wfsConfiguration, remoteTypeName,
-								schemaLocation, null);
-						
-						featureTypesForId.add(tmp);
-					}
-				}
 			}
 			
 		} finally {
@@ -118,42 +76,6 @@ public class DescribeStoredQueriesResponse extends WFSResponse {
 		
 	}
 	
-
-	private URL findSchemaLocation(Document rawDocument, String namespaceURI) {
-		return findSchemaLocation(rawDocument.getChildNodes(), namespaceURI);
-	}
-
-	private URL findSchemaLocation(NodeList nodes, String namespaceURI) {
-		URL ret = null;
-		for (int i = 0 ; i < nodes.getLength(); i++) {
-			ret = findSchemaLocation(nodes.item(i), namespaceURI);
-			if (ret != null) return ret;
-		}
-
-		return ret;
-	}
-
-	private URL findSchemaLocation(Node node, String namespaceURI)
-	{
-		NamedNodeMap attributes = node.getAttributes();
-		Node attr = attributes.getNamedItemNS("http://www.w3.org/2001/XMLSchema-instance", "schemaLocation");
-		if (attr == null) return null;
-		
-		URL ret = null;
-		
-		String [] parts = attr.getTextContent().split("\\s+");
-		for (int i = 0; i < parts.length; i += 2) {
-			if (namespaceURI.equals(parts[i])) {
-				try {
-					ret = new URL(parts[i+1]);
-				} catch(MalformedURLException mue) {
-					MODULE.log(Level.WARNING, "SchemaLocation for "+namespaceURI+" ("+parts[i+1]+") is malformed", mue);
-				}
-			}
-		}
-		
-		return ret;
-	}
 
 	private DescribeStoredQueriesResponseType parseStoredQueries(Document document,
 			Configuration wfsConfig) throws DataSourceException {
@@ -165,19 +87,14 @@ public class DescribeStoredQueriesResponse extends WFSResponse {
             throw new DataSourceException("Exception parsing DescribeStoredQueriesResponse", e);
         }
         
+        if (!(parsed instanceof DescribeStoredQueriesResponseType)) {
+        	throw new DataSourceException("DescribedStoredQueries response is not a DescribeStoredQueriesResponseType but a "+parsed.getClass());
+        }
+        
         return (DescribeStoredQueriesResponseType)parsed;
 	}
 	
 	public List<StoredQueryDescriptionType> getStoredQueryDescriptions() {
 		return describeStoredQueriesResponse.getStoredQueryDescription();
 	}
-	
-	public List<FeatureType> getFeatureTypes(String id) {
-		List<FeatureType> ret = featureTypesPerStoredQueryId.get(id);
-		if (ret == null) {
-			ret = Collections.emptyList();
-		}
-		return ret;
-	}
-
 }

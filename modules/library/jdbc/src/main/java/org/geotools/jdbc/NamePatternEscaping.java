3ccbaf09b2129db0176e496411a98b512f5a9bd9
Sebastian Graca <sgraca@ispik.pl>
2014-10-05 09:56:57 +0200

    Added missing copyrights and javadocs

--- modules/library/jdbc/src/main/java/org/geotools/jdbc/NamePatternEscaping.java
+++ modules/library/jdbc/src/main/java/org/geotools/jdbc/NamePatternEscaping.java
@@ -1,9 +1,30 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package org.geotools.jdbc;
 
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * A helper class for escaping object name patterns in calls to {@link java.sql.DatabaseMetaData}.
+ *
+ * @see org.geotools.jdbc.JDBCDataStore#escapeNamePattern(java.sql.DatabaseMetaData, String)
+ */
 class NamePatternEscaping {
     private final String escape;
     private final String quotedReplacement;

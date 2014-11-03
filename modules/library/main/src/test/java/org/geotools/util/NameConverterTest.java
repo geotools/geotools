7148f0153a4df4fbce917fa30032e1e857945870
aaime <andrea.aime@gmail.com>
2014-11-03 12:19:10 +0100

    GEOT-4929] Add a converterFactory for org.opengis.feature.type.Name

---
+++ modules/library/main/src/test/java/org/geotools/util/NameConverterTest.java
@@ -0,0 +1,26 @@
+package org.geotools.util;
+
+import static org.junit.Assert.*;
+
+import org.geotools.feature.NameImpl;
+import org.junit.Test;
+import org.opengis.feature.type.Name;
+
+public class NameConverterTest {
+    
+
+    @Test
+    public void testToName() {
+        assertEquals(new NameImpl("test"), Converters.convert("test", Name.class));
+        assertEquals(new NameImpl("a", "b"), Converters.convert("a:b", Name.class));
+        assertNull(Converters.convert("a:b:c", Name.class));
+    }
+    
+    @Test
+    public void testNameToString() {
+        assertEquals("test", Converters.convert(new NameImpl("test"), String.class));
+        assertEquals("a:b", Converters.convert(new NameImpl("a", "b"), String.class));
+    }
+    
+    
+}

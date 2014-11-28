5ce60559c8c482b9e616ccc94c4364b7180cf538
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2014-11-28 12:19:29 +0100

    [GEOT-4948]: Adding DrawableBitSet

---
+++ modules/library/coverage/src/main/java/org/geotools/image/DrawableBitSet.java
@@ -0,0 +1,80 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
+package org.geotools.image;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.util.BitSet;
+
+import com.vividsolutions.jts.awt.ShapeWriter;
+import com.vividsolutions.jts.geom.Geometry;
+
+
+/**
+ * An analogous of {@link BitSet} which is explicitly 2 dimensional and allows
+ * drawing a shape to quickly turn on a large area of the map.
+ * This class is not thread safe.
+ * 
+ * @author Andrea Aime - GeoSolutions
+ *
+ */
+public class DrawableBitSet {
+
+    BufferedImage image;
+    WritableRaster raster;
+    int[] pixel = new int[1];
+
+    public DrawableBitSet(int width, int height) {
+        image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
+        raster = image.getRaster();
+    }
+
+    public boolean get(int x, int y) {
+        raster.getPixel(x, y, pixel);
+
+        return pixel[0] > 0;
+    }
+
+    public void set(int x, int y) {
+        pixel[0] = 1;
+        raster.setPixel(x, y, pixel);
+    }
+
+    public void set(Geometry geom) {
+        if (geom == null) {
+            return;
+        }
+
+        set(new ShapeWriter().toShape(geom));
+    }
+
+    public void set(Shape shape) {
+        Graphics2D g2d = image.createGraphics();
+        g2d.setColor(Color.WHITE);
+        g2d.draw(shape);
+        g2d.fill(shape);
+        g2d.dispose();
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+}
---
+++ modules/library/coverage/src/test/java/org/geotools/image/DrawableBitSetTest.java
@@ -0,0 +1,126 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ * 
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.image;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+
+import org.junit.Test;
+
+public class DrawableBitSetTest {
+
+    private static final int WIDTH = 50;
+
+    private static final int HEIGHT = 50;
+
+    private static final int MIN = 10;
+
+    private static final int SIZE = 30;
+
+    private static final int MAX = MIN + SIZE;
+
+    private static final int SET_PIXELS = SIZE * SIZE + SIZE * 2 + 1;
+
+    @Test
+    public void testPointSet() {
+        DrawableBitSet bitset = new DrawableBitSet(WIDTH, HEIGHT);
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                assertFalse(bitset.get(i, j));
+            }
+        }
+
+        bitset.set(20, 20);
+        assertTrue(bitset.get(20, 20));
+    }
+
+    @Test
+    public void testPointOutside() {
+        DrawableBitSet bitset = new DrawableBitSet(WIDTH, HEIGHT);
+        boolean success = true;
+        try {
+            bitset.set(60, 60);
+        } catch (ArrayIndexOutOfBoundsException aie) {
+            success = false;
+        }
+        assertFalse(success);
+    }
+
+    @Test
+    public void testGetImage() {
+        DrawableBitSet bitset = new DrawableBitSet(WIDTH, HEIGHT);
+        // Draw a rectangle
+        Shape rectangle = new Rectangle(MIN, MIN, SIZE, SIZE);
+        bitset.set(rectangle);
+        BufferedImage image = bitset.getImage();
+        assertNotNull(image);
+        Raster raster = image.getData();
+        assertNotNull(raster);
+        int sample;
+        int setPixel = 0;
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                sample = raster.getSample(i, j, 0);
+                if ((i >= MIN && j >= MIN && i <= MAX && j <= MAX)) {
+                    // Check pixels within the rectangle have been set
+                    assertEquals(1, sample);
+                    setPixel++;
+                } else {
+                    assertEquals(0, sample);
+                }
+            }
+        }
+        assertEquals(SET_PIXELS, setPixel);
+    }
+
+    @Test
+    public void testDrawShape() {
+        DrawableBitSet bitset = new DrawableBitSet(WIDTH, HEIGHT);
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                assertFalse(bitset.get(i, j));
+            }
+        }
+
+        int setPixel = 0;
+        // Draw a rectangle
+        Shape rectangle = new Rectangle(MIN, MIN, SIZE, SIZE);
+        bitset.set(rectangle);
+        boolean value = false;
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                value = bitset.get(i, j);
+                if ((i >= MIN && j >= MIN && i <= MAX && j <= MAX)) {
+                    // Check pixels within the rectangle have been set
+                    assertTrue(value);
+                    setPixel++;
+                } else {
+                    assertFalse(value);
+                }
+            }
+        }
+        assertEquals(SET_PIXELS, setPixel);
+    }
+
+}

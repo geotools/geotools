24763f604a669294122c23df5e56ef4631e8b2e4
travis <travislbrundage@gmail.com>
2014-12-22 17:35:20 -0800

    Backporting geoserver's changes to CSVDataStore

--- modules/unsupported/csv/src/main/java/org/geotools/data/csv/CSVFeatureReader.java
+++ modules/unsupported/csv/src/main/java/org/geotools/data/csv/CSVFeatureReader.java
@@ -1,10 +1,7 @@
-/* GeoTools - The Open Source Java GIS Toolkit
- * http://geotools.org
- *
- * (C) 2010-2014, Open Source Geospatial Foundation (OSGeo)
- *
- * This file is hereby placed into the Public Domain. This means anyone is
- * free to do whatever they wish with this file. Use it well and enjoy!
+/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+ * (c) 2001 - 2013 OpenPlans
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
  */
 package org.geotools.data.csv;
 
@@ -13,153 +10,46 @@ import java.util.NoSuchElementException;
 
 import org.geotools.data.FeatureReader;
 import org.geotools.data.Query;
-import org.geotools.data.store.ContentState;
-import org.geotools.feature.simple.SimpleFeatureBuilder;
-import org.geotools.geometry.jts.JTSFactoryFinder;
-import org.opengis.feature.IllegalAttributeException;
+import org.geotools.data.csv.parse.CSVIterator;
+import org.geotools.data.csv.parse.CSVStrategy;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 
-import com.csvreader.CsvReader;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
-
-/**
- * 
- *
- * @source $URL$
- */
 public class CSVFeatureReader implements FeatureReader<SimpleFeatureType, SimpleFeature> {
-    
-    /** State used when reading file */
-    protected ContentState state;
-    
-    /**
-     * Current row number - used in the generation of FeatureId.
-     * TODO: Subclass ContentState to track row
-     */
-    private int row;
 
-    protected CsvReader reader;
-    
-    /** Utility class used to build features */
-    protected SimpleFeatureBuilder builder;
+    private SimpleFeatureType featureType;
 
-    /** Factory class for goemetry creation */
-    private GeometryFactory geometryFactory;
+    private CSVIterator iterator;
 
-    public CSVFeatureReader(ContentState contentState, Query query) throws IOException {
-        this.state = contentState;
-        CSVDataStore csv = (CSVDataStore) contentState.getEntry().getDataStore();
-        reader = csv.read(); // this may throw an IOException if it could not connect
-        boolean header = reader.readHeaders();
-        if (! header ){
-            throw new IOException("Unable to read csv header");
-        }
-        builder = new SimpleFeatureBuilder( state.getFeatureType() );
-        geometryFactory = JTSFactoryFinder.getGeometryFactory(null);
-        row = 0;
+    public CSVFeatureReader(CSVStrategy csvStrategy) throws IOException {
+        this(csvStrategy, Query.ALL);
     }
-    /** Access FeatureType (documenting avaiable attributes) */
+
+    public CSVFeatureReader(CSVStrategy csvStrategy, Query query)
+            throws IOException {
+        this.featureType = csvStrategy.getFeatureType();
+        this.iterator = csvStrategy.iterator();
+    }
+
+    @Override
     public SimpleFeatureType getFeatureType() {
-        return (SimpleFeatureType) state.getFeatureType();
+        return featureType;
     }
-    // class definition end
-    
-    // read start
-    /** The next feature */
-    private SimpleFeature next;
-    
-    /**
-     * Access the next feature (if available).
-     * 
-     * @return SimpleFeature read from property file
-     * @throws IOException If problem encountered reading file
-     * @throws IllegalAttributeException for invalid data
-     * @throws NoSuchElementException If hasNext() indicates no more features are available
-     */
+
+    @Override
+    public void close() throws IOException {
+        iterator.close();
+    }
+
+    @Override
     public SimpleFeature next() throws IOException, IllegalArgumentException,
             NoSuchElementException {
-        SimpleFeature feature;
-        if( next != null ){
-            feature = next;
-            next = null;
-        }
-        else {
-            feature = readFeature();
-        }
-        return feature;
+        return iterator.next();
     }
-    /**
-     * Check if additional content is available.
-     * 
-     * @return <code>true</code> if additional content is available
-     * @throws IOException
-     */
-    public boolean hasNext() throws IOException {
-        if( next != null ){
-            return true;
-        }
-        else if (reader == null ){
-            return false;
-        }
-        else {
-            next = readFeature(); // read next feature so we can check
-            return next != null;
-        }
-    }
-    // read end
-    
-    // parse start
-    /** Read a line of content from CSVReader and parse into values */
-    SimpleFeature readFeature() throws IOException {
-        if( reader == null ){
-            throw new IOException("FeatureReader is closed; no additional features can be read");
-        }
-        boolean read = reader.readRecord(); // read the "next" record
-        if( read == false ){
-            close(); // automatic close to be nice
-            return null; // no additional features are available
-        }
-        Coordinate coordinate = new Coordinate();
-        for( String column : reader.getHeaders() ){
-            String value = reader.get(column);
-            if( "lat".equalsIgnoreCase(column)){
-                coordinate.y = Double.valueOf( value.trim() );
-            }
-            else if( "lon".equalsIgnoreCase(column)){
-                coordinate.x = Double.valueOf( value.trim() );
-            }
-            else {
-                builder.set(column, value );
-            }
-        }
-        builder.set("Location", geometryFactory.createPoint( coordinate ) );
-        
-        return this.buildFeature();
-    }
-    
-    /** Build feature using the current row number to generate FeatureId */
-    protected SimpleFeature buildFeature() {
-        row += 1;
-        return builder.buildFeature( state.getEntry().getTypeName()+"."+row );
-    }
-    // parse end
 
-    // close start
-    /**
-     * Close the FeatureReader when not in use.
-     * 
-     * @throws IOException
-     */
-    public void close() throws IOException {
-        if( reader != null ){
-            reader.close();
-            reader = null;
-        }
-        builder = null;
-        geometryFactory = null;
-        next = null;
+    @Override
+    public boolean hasNext() throws IOException {
+        return iterator.hasNext();
     }
-    // close start
+
 }

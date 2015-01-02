c762295147b8732feed403fba574078fd8ae7041
travis <travislbrundage@gmail.com>
2015-01-02 13:57:28 -0800

    Fixed licensing

--- modules/unsupported/csv/src/main/java/org/geotools/data/csv/CSVFileState.java
+++ modules/unsupported/csv/src/main/java/org/geotools/data/csv/CSVFileState.java
@@ -1,7 +1,19 @@
-/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
- * (c) 2001 - 2013 OpenPlans
- * This code is licensed under the GPL 2.0 license, available at the root
- * application directory.
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *    
+ * 	  (c) 2014 Open Source Geospatial Foundation - all rights reserved
+ * 	  (c) 2012 - 2014 OpenPlans
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 package org.geotools.data.csv;
 

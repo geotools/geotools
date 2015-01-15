60d6c7543ee599f02c8b21954349dd599dd77246
Niels Charlier <niels@scitus.be>
2015-01-15 16:50:59 +0100

    WFS-NG: enable parsing GML3.2, cleanup unused parser code

--- modules/unsupported/wfs-ng/src/main/java/org/geotools/data/wfs/internal/parsers/ListStoredQueriesResponseFactory.java
+++ modules/unsupported/wfs-ng/src/main/java/org/geotools/data/wfs/internal/parsers/ListStoredQueriesResponseFactory.java
@@ -55,8 +55,7 @@ public class ListStoredQueriesResponseFactory implements WFSResponseFactory {
     }
 
     @Override
-    public WFSResponse createResponse(WFSRequest request,
-            HTTPResponse response, String axisOrder) throws IOException {
+    public WFSResponse createResponse(WFSRequest request, HTTPResponse response) throws IOException {
         try {
             return new ListStoredQueriesResponse((ListStoredQueriesRequest)request, response);
         } catch (ServiceException e) {

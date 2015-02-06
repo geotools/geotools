3d7aad1b8c4c04c915952bc2736d1f423ab7d316
jdeolive <jdeolive@gmail.com>
2015-02-06 10:04:38 -0700

    Supporting previously pending version of BBox type.

--- modules/unsupported/solr/src/main/java/org/geotools/data/solr/SolrUtils.java
+++ modules/unsupported/solr/src/main/java/org/geotools/data/solr/SolrUtils.java
@@ -45,7 +45,8 @@ public class SolrUtils {
             return Boolean.class;
         if (className.equals("org.apache.solr.schema.SpatialRecursivePrefixTreeFieldType")
                 || className.equals("org.apache.solr.schema.LatLonType")
-                || className.equals("org.apache.solr.schema.BBoxField"))
+                || className.equals("org.apache.solr.schema.BBoxField")
+                || className.equals("org.apache.solr.spatial.pending.BBoxFieldType"))
             return Geometry.class;
         if (className.equals("org.apache.solr.schema.DateField")
                 || className.equals("org.apache.solr.schema.TrieDateField"))

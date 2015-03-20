3e400364d95c06a3e449c34dbc3c587a12bbdfb6
travislbrundage <travislbrundage@gmail.com>
2015-03-20 00:42:56 +0000

    updating version numbers and README for 13.0

--- docs/common.py
+++ docs/common.py
@@ -59,7 +59,7 @@ copyright = u'2015, GeoTools'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '13-SNAPSHOT'
+release = '13.0'
 
 # The short X.Y version.
 version = release

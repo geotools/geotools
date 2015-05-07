fabe41f6a3e0a8ee7d01985cdcb9ab6612133219
Jody Garnett <jody.garnett@gmail.com>
2015-05-06 19:28:31 -0700

    Add test case for transform differences with vecmath

--- modules/plugin/epsg-hsql/src/test/java/org/geotools/referencing/factory/epsg/OperationFactoryTest.java
+++ modules/plugin/epsg-hsql/src/test/java/org/geotools/referencing/factory/epsg/OperationFactoryTest.java
@@ -23,6 +23,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.opengis.geometry.DirectPosition;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.IdentifiedObject;
 import org.opengis.referencing.crs.CRSAuthorityFactory;
@@ -32,12 +33,14 @@ import org.opengis.referencing.operation.Transformation;
 import org.opengis.referencing.operation.CoordinateOperation;
 import org.opengis.referencing.operation.CoordinateOperationFactory;
 import org.opengis.referencing.operation.ConcatenatedOperation;
-
 import org.geotools.referencing.operation.BufferedCoordinateOperationFactory;
 import org.geotools.referencing.operation.AbstractCoordinateOperation;
 import org.geotools.referencing.operation.AuthorityBackedFactory;
+import org.geotools.referencing.CRS;
 import org.geotools.referencing.ReferencingFactoryFinder;
 import org.geotools.factory.Hints;
+import org.geotools.geometry.DirectPosition2D;
+import org.geotools.geometry.TransformedDirectPosition;
 import org.geotools.resources.Arguments;
 import org.geotools.resources.Classes;
 
@@ -46,9 +49,7 @@ import org.geotools.resources.Classes;
  * Tests the usage of {@link CoordinateOperationFactory} with the help of the
  * EPSG database. Any EPSG plugin should fit. However, this test live in the
  * {@code plugin/epsg-hsql} module since the HSQL plugin is the only one which
- * is garantee to work on any machine running Maven.
- *
- *
+ * is guaranteed to work on any machine running Maven.
  *
  * @source $URL$
  * @version $Id$
@@ -141,5 +142,69 @@ public class OperationFactoryTest extends TestCase {
         }
         assertEquals("The coordinate operation should contains exactly 1 transformation", 1, count);
         assertTrue(AbstractCoordinateOperation.getAccuracy(operation) <= 25);
+    }
+    
+    /**
+     * The TransformedDirectPosition class is a thin wrapper around
+     * CoordinateOperationFactory used for time critical functions.
+     * 
+     * This test isolates a regression introduced during GEOT-22 change
+     * of vector library.
+     */
+    public void testTransformedDirectPosition() throws Exception {
+        CoordinateReferenceSystem requestedCRS = CRS.parseWKT(
+                "PROJCS[\"WGS84 / Google Mercator\","+ 
+                "  GEOGCS[\"WGS 84\", "+
+                "   DATUM[\"World Geodetic System 1984\", "+
+                "     SPHEROID[\"WGS 84\", 6378137.0, 298.257223563, AUTHORITY[\"EPSG\",\"7030\"]],"+ 
+                "     AUTHORITY[\"EPSG\",\"6326\"]], "+
+                "   PRIMEM[\"Greenwich\", 0.0, AUTHORITY[\"EPSG\",\"8901\"]],"+ 
+                "   UNIT[\"degree\", 0.017453292519943295],"+ 
+                "   AXIS[\"Longitude\", EAST], "+
+                "   AXIS[\"Latitude\", NORTH], "+
+                "   AUTHORITY[\"EPSG\",\"4326\"]],"+ 
+                " PROJECTION[\"Mercator_1SP\"], "+
+                " PARAMETER[\"semi_minor\", 6378137.0], "+
+                " PARAMETER[\"latitude_of_origin\", 0.0],"+ 
+                " PARAMETER[\"central_meridian\", 0.0],"+ 
+                " PARAMETER[\"scale_factor\", 1.0], "+
+                " PARAMETER[\"false_easting\", 0.0], "+
+                " PARAMETER[\"false_northing\", 0.0],"+ 
+                " UNIT[\"m\", 1.0], "+
+                " AXIS[\"x\", EAST], "+
+                " AXIS[\"y\", NORTH], "+
+                " AUTHORITY[\"EPSG\",\"900913\"]]");
+        CoordinateReferenceSystem targetCRS = CRS. parseWKT(
+                "PROJCS[\"Lisboa_Hayford_Gauss_IGeoE\", "+
+                "  GEOGCS[\"GCS_Datum_Lisboa_Hayford\", "+
+                "    DATUM[\"D_Datum_Lisboa_Hayford\", "+
+                "      SPHEROID[\"International_1924\", 6378388.0, 297.0], "+
+                "      TOWGS84[-288.885, -91.744, 126.244, -1.691, 0.41, -0.211, -4.598]],"+ 
+                "    PRIMEM[\"Greenwich\", 0.0], "+
+                "    UNIT[\"degree\", 0.017453292519943295],"+ 
+                "    AXIS[\"Longitude\", EAST], "+
+                "    AXIS[\"Latitude\", NORTH]], "+
+                "  PROJECTION[\"Transverse_Mercator\"], "+
+                "  PARAMETER[\"central_meridian\", -8.131906111111114], "+
+                "  PARAMETER[\"latitude_of_origin\", 39.666666666666664],"+ 
+                "  PARAMETER[\"scale_factor\", 1.0], "+
+                "  PARAMETER[\"false_easting\", 200000.0], "+
+                "  PARAMETER[\"false_northing\", 300000.0],"+ 
+                "  UNIT[\"m\", 1.0], "+
+                "  AXIS[\"x\", EAST], "+
+                "  AXIS[\"y\", NORTH]]");
+
+        DirectPosition position = new DirectPosition2D(requestedCRS, -886885.0962724264, 4468200.416916506);
+        final TransformedDirectPosition arbitraryToInternal = new TransformedDirectPosition(
+                requestedCRS, targetCRS, new Hints(Hints.LENIENT_DATUM_SHIFT,
+                        Boolean.TRUE));
+        arbitraryToInternal.transform(position);
+        
+        DirectPosition expected = new DirectPosition2D(targetCRS, 214636.7447572897,  27218.077500249085);
+        
+        // geoserver vecmath:  TransformedDirectPosition[214741.10238960697, 26957.60506898933]
+        // geoserver    ejml:  TransformedDirectPosition[214636.7447572897,  27218.077500249085]
+        // geotools     ejml:  TransformedDirectPosition[214636.7447572897,  27218.077500249085]
+        assertEquals( expected, arbitraryToInternal);
     }
 }

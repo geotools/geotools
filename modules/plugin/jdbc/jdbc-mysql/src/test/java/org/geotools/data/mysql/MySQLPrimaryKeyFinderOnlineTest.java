a2a400d7b8c6685c649330b5abdf54ab2f4e39a6
Andrea Aime <andrea.aime@gmail.com>
2015-06-13 17:30:45 +0200

    Fixing mysql MySQLPrimaryKeyFinderOnlineTest failure

--- modules/plugin/jdbc/jdbc-mysql/src/test/java/org/geotools/data/mysql/MySQLPrimaryKeyFinderOnlineTest.java
+++ modules/plugin/jdbc/jdbc-mysql/src/test/java/org/geotools/data/mysql/MySQLPrimaryKeyFinderOnlineTest.java
@@ -16,6 +16,9 @@
  */
 package org.geotools.data.mysql;
 
+import java.util.HashMap;
+
+import org.geotools.jdbc.JDBCDataStoreFactory;
 import org.geotools.jdbc.JDBCPrimaryKeyFinderOnlineTest;
 import org.geotools.jdbc.JDBCPrimaryKeyFinderTestSetup;
 
@@ -34,5 +37,12 @@ public class MySQLPrimaryKeyFinderOnlineTest extends JDBCPrimaryKeyFinderOnlineT
     @Override
     public void testSequencedPrimaryKey() throws Exception {
         //MySQL does not do sequences
+    }
+
+    @Override
+    protected HashMap createDataStoreFactoryParams() throws Exception {
+        HashMap params = super.createDataStoreFactoryParams();
+        params.put(JDBCDataStoreFactory.PK_METADATA_TABLE.key, "gt_pk_metadata");
+        return params;
     }
 }

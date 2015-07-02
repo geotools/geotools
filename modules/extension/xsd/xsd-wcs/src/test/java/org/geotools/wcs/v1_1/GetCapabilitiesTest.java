73a1402e02f49ae5acafb13042970a5e30e04eda
Torben Barsbsballe <tbarsballe@boundlessgeo.com>
2015-07-02 15:47:40 -0700

    Added WCS 1.1 schema from GeoServer, added test cases, use LGPL License

---
+++ modules/extension/xsd/xsd-wcs/src/test/java/org/geotools/wcs/v1_1/GetCapabilitiesTest.java
@@ -0,0 +1,34 @@
+package org.geotools.wcs.v1_1;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import net.opengis.wcs11.GetCapabilitiesType;
+
+import org.geotools.xml.Parser;
+import org.junit.Test;
+
+public class GetCapabilitiesTest {
+
+    Parser parser = new Parser(new WCSConfiguration());
+
+    @Test
+    public void testParseCapabilitiesRequest() throws Exception {
+        String capRequestPath = "requestGetCapabilities.xml";
+        HashMap<String, Object> caps = (HashMap<String, Object>) parser.parse(getClass()
+                .getResourceAsStream(capRequestPath));
+        assertEquals("WCS", caps.get("service"));
+        
+        List versions = (List)((HashMap)caps.get("AcceptVersions")).get("Version");
+        assertEquals("1.1.1", versions.get(0));
+        assertEquals("1.1.0", versions.get(1));
+        assertEquals("1.0.0", versions.get(2));
+        
+        assertEquals("OperationsMetadata", caps.get("Sections"));
+        assertEquals("application/xml", caps.get("AcceptFormats"));
+    }
+
+}

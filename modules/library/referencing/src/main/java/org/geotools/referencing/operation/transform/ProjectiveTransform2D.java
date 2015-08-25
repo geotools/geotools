189628b1ce26efef5b35d59337bd17a87fad4f0f
highsource <aleksei.valikov@gmail.com>
2015-08-25 14:31:24 +0200

    Fixes GEOT-5198.

--- modules/library/referencing/src/main/java/org/geotools/referencing/operation/transform/ProjectiveTransform2D.java
+++ modules/library/referencing/src/main/java/org/geotools/referencing/operation/transform/ProjectiveTransform2D.java
@@ -16,10 +16,9 @@
  */
 package org.geotools.referencing.operation.transform;
 
-import org.opengis.referencing.operation.Matrix;
 import org.opengis.referencing.operation.MathTransform2D;
+import org.opengis.referencing.operation.Matrix;
 import org.opengis.referencing.operation.NoninvertibleTransformException;
-
 
 /**
  * Projective transform in 2D case.
@@ -28,32 +27,33 @@ import org.opengis.referencing.operation.NoninvertibleTransformException;
  * @version $Id$
  * @author Jan Jezek
  */
-final class ProjectiveTransform2D extends ProjectiveTransform implements MathTransform2D {
-    /**
-     * For cross-version compatibility.
-     */
-    private static final long serialVersionUID = -3101392684596817045L;
+final class ProjectiveTransform2D extends ProjectiveTransform implements
+		MathTransform2D {
+	/**
+	 * For cross-version compatibility.
+	 */
+	private static final long serialVersionUID = -3101392684596817045L;
 
-    /**
-     * Creates projective transform from a matrix.
-     */
-    public ProjectiveTransform2D(final Matrix matrix) {
-        super(matrix);
-    }
+	/**
+	 * Creates projective transform from a matrix.
+	 */
+	public ProjectiveTransform2D(final Matrix matrix) {
+		super(matrix);
+	}
 
-    /**
-     * Creates the inverse transform of this object.
-     */
-    @Override
-    public MathTransform2D inverse() throws NoninvertibleTransformException {
-        return (MathTransform2D) super.inverse();
-    }
+	/**
+	 * Creates the inverse transform of this object.
+	 */
+	@Override
+	public MathTransform2D inverse() throws NoninvertibleTransformException {
+		return (MathTransform2D) super.inverse();
+	}
 
-    /**
-     * Creates an inverse transform using the specified matrix.
-     */
-    @Override
-    MathTransform2D createInverse(final Matrix matrix) {
-        return new ProjectiveTransform2D(matrix);
-    }
+	/**
+	 * Creates an inverse transform using the specified matrix.
+	 */
+	@Override
+	ProjectiveTransform2D createInverse(final Matrix matrix) {
+		return new ProjectiveTransform2D(matrix);
+	}
 }

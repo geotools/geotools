2a32bedd8762a5667e452145f7c52c799a432ccb
highsource <aleksei.valikov@gmail.com>
2015-08-25 14:49:38 +0200

    Fixed formatting.

--- modules/library/referencing/src/test/java/org/geotools/referencing/operation/transform/ProjectiveTransform2DTest.java
+++ modules/library/referencing/src/test/java/org/geotools/referencing/operation/transform/ProjectiveTransform2DTest.java
@@ -1,8 +1,8 @@
 /*
  *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
- * 
- *    (C) 2005-2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    (C) 2001-2015, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -13,6 +13,9 @@
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
+ *
+ *    This package contains documentation from OpenGIS specifications.
+ *    OpenGIS consortium's work is fully acknowledged here.
  */
 package org.geotools.referencing.operation.transform;
 
@@ -30,13 +33,13 @@ import org.opengis.referencing.operation.NoninvertibleTransformException;
  * @author Alexey Valikov
  */
 public final class ProjectiveTransform2DTest {
-	// Tests that ProjectiveTransform2D is invertible
-	@Test
-	public void testInvertible() throws NoninvertibleTransformException {
-		final GeneralMatrix matrix = new GeneralMatrix(3);
-		matrix.setElement(0, 1, -1);
-		matrix.setElement(1, 0, 1);
-		final MathTransform transform = new ProjectiveTransform2D(matrix);
-		Assert.assertNotNull(transform.inverse());
-	}
+    // Tests that ProjectiveTransform2D is invertible
+    @Test
+    public void testInvertible() throws NoninvertibleTransformException {
+        final GeneralMatrix matrix = new GeneralMatrix(3);
+        matrix.setElement(0, 1, -1);
+        matrix.setElement(1, 0, 1);
+        final MathTransform transform = new ProjectiveTransform2D(matrix);
+        Assert.assertNotNull(transform.inverse());
+    }
 }

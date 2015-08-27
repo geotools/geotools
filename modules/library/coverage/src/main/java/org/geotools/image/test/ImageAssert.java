43e6c444b486818ce1919b6dd28d9ca5fe42ce4e
Andrea Aime <andrea.aime@gmail.com>
2015-08-27 15:00:53 +0200

    Be more explicit about the flag to get the image comparator

--- modules/library/coverage/src/main/java/org/geotools/image/test/ImageAssert.java
+++ modules/library/coverage/src/main/java/org/geotools/image/test/ImageAssert.java
@@ -142,7 +142,8 @@ public class ImageAssert {
                 } else {
                     throw new AssertionError("Images are visibly different, found "
                             + comparator.getMismatchCount()
-                            + " different pixels, against a threshold of " + threshold);
+                            + " different pixels, against a threshold of " + threshold
+                            + "\nYou can add -Dorg.geotools.image.test.interactive=true to show a dialog comparing them (requires GUI support)");
                 }
             } else {
                 LOGGER.fine("Images are not visibly different, found "

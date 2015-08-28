5b314e4951f29931937bd4fdaa8eee1ffe97b479
Jody Garnett <jody.garnett@gmail.com>
2015-08-27 17:38:06 -0700

    Fix header and remove compile warnings and log messages.

--- modules/library/render/src/main/java/org/geotools/renderer/label/GlyphVectorProcessor.java
+++ modules/library/render/src/main/java/org/geotools/renderer/label/GlyphVectorProcessor.java
@@ -1,3 +1,19 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2015, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package org.geotools.renderer.label;
 
 import com.vividsolutions.jts.geom.Coordinate;
@@ -9,7 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * An abstract class to process all the glyphs of label.
+ * Process all the glyphs of label.
  */
 public abstract class GlyphVectorProcessor {
 

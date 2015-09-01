8845ab342113bf238955744f474189fd4c0cd4a0
Christian Mueller <christian.mueller@os-solutions.at>
2015-09-01 12:39:55 +0200

    Adjusting H2 tests for GEOT-5197

--- modules/plugin/jdbc/jdbc-h2/src/main/java/org/geotools/data/h2/H2DialectBasic.java
+++ modules/plugin/jdbc/jdbc-h2/src/main/java/org/geotools/data/h2/H2DialectBasic.java
@@ -2,7 +2,7 @@
  *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
  *
- *    (C) 2002-2008, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2002-2015, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -250,4 +250,10 @@ public class H2DialectBasic extends BasicSQLDialect {
     public FilterToSQL createFilterToSQL() {
         return new H2FilterToSQL();
     }
+    
+    @Override
+    protected boolean supportsSchemaForIndex() {
+        return delegate.supportsSchemaForIndex();
+    }
+
 }

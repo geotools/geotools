4c367628f7ca13d43cebccb4a3ee50cd2d72bb3c
Brad Hards <bradh@frogmouth.net>
2015-09-27 20:25:33 +1000

    Additional GeoPackage schema updates.
    I'm using https://github.com/opengeospatial/geopackage/blob/master/spec/annexes/ddl.adoc
    as the truth reference for these.

--- modules/unsupported/geopkg/src/main/resources/org/geotools/geopkg/gpkg_metadata_reference.sql
+++ modules/unsupported/geopkg/src/main/resources/org/geotools/geopkg/gpkg_metadata_reference.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS gpkg_metadata_reference (
   table_name TEXT,
   column_name TEXT,
   row_id_value INTEGER,
-  timestamp DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ',CURRENT_TIMESTAMP)),
+  timestamp DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
   md_file_id INTEGER NOT NULL,
   md_parent_id INTEGER,
   CONSTRAINT crmr_mfi_fk FOREIGN KEY (md_file_id) REFERENCES gpkg_metadata(id),

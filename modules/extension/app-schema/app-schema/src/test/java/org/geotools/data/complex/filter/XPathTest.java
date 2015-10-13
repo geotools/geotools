98e8c0fbc0c7e358f04dc63ef505723206d4dc1a
Stefano Costa <ridethepenguin@gmail.com>
2015-10-13 15:05:24 +0200

    [GEOT-5251] App-schema can't set string value to complex type extending anyType

--- modules/extension/app-schema/app-schema/src/test/java/org/geotools/data/complex/filter/XPathTest.java
+++ modules/extension/app-schema/app-schema/src/test/java/org/geotools/data/complex/filter/XPathTest.java
@@ -2,7 +2,7 @@
  *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
  *
- *    (C) 2005-2011, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2005-2015, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -17,13 +17,25 @@
 
 package org.geotools.data.complex.filter;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.xml.namespace.QName;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xsd.XSDElementDeclaration;
+import org.eclipse.xsd.util.XSDResourceFactoryImpl;
+import org.geotools.data.complex.config.AppSchemaFeatureTypeRegistry;
+import org.geotools.data.complex.config.EmfComplexFeatureReader;
 import org.geotools.data.complex.config.Types;
 import org.geotools.gml3.GMLSchema;
 import org.geotools.test.AppSchemaTestSupport;
+import org.geotools.xml.SchemaIndex;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.opengis.feature.type.ComplexType;
+import org.opengis.feature.type.Name;
 
 /**
  * 
@@ -36,6 +48,20 @@ import static org.junit.Assert.assertTrue;
  * @since 2.4
  */
 public class XPathTest extends AppSchemaTestSupport {
+
+    private static final String COMPLEX_WITH_TEXT_CONTENT_SCHEMA_LOCATION = "/test-data/complexWithTextContent.xsd";
+    private static final String URI = "http://www.geotools.org/appschema/test";
+
+    private static EmfComplexFeatureReader reader;
+
+    @BeforeClass
+    public static void oneTimeSetUp() {
+        reader = EmfComplexFeatureReader.newInstance();
+
+        //need to register custom factory to load schema resources
+        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap()
+                                          .put("xsd", new XSDResourceFactoryImpl());
+    }
 
     /**
      * Test that some simple-content and non-simple-content types are correctly detected.
@@ -50,4 +76,35 @@ public class XPathTest extends AppSchemaTestSupport {
         assertFalse(Types.isSimpleContentType(GMLSchema.ABSTRACTFEATURECOLLECTIONTYPE_TYPE));
     }
 
+    /**
+     * Test that complex elements that can hold text content are correctly detected.
+     * @throws Exception
+     */
+    @Test
+    public void testCanHaveTextContent() throws Exception {
+        SchemaIndex schemaIndex = reader.parse(getClass().getResource(
+                COMPLEX_WITH_TEXT_CONTENT_SCHEMA_LOCATION));
+
+        XSDElementDeclaration unrestrictedElDecl = schemaIndex.getElementDeclaration(new QName(URI,
+                "unrestrictedEl"));
+        assertNotNull(unrestrictedElDecl);
+        XSDElementDeclaration restrictedElDecl = schemaIndex.getElementDeclaration(new QName(URI,
+                "restrictedEl"));
+        assertNotNull(restrictedElDecl);
+
+        AppSchemaFeatureTypeRegistry typeRegistry = new AppSchemaFeatureTypeRegistry();
+        typeRegistry.addSchemas(schemaIndex);
+
+        Name unrestrictedTypeName = Types.typeName(URI, "UnrestrictedType");
+        ComplexType unrestrictedType = (ComplexType) typeRegistry
+                .getAttributeType(unrestrictedTypeName);
+        assertNotNull(unrestrictedType);
+        assertTrue(Types.canHaveTextContent(unrestrictedType));
+
+        Name restrictedTypeName = Types.typeName(URI, "RestrictedType");
+        ComplexType restrictedType = (ComplexType) typeRegistry
+                .getAttributeType(restrictedTypeName);
+        assertNotNull(restrictedType);
+        assertFalse(Types.canHaveTextContent(restrictedType));
+    }
 }

945b1758e7315393e003736f11fb329f71683d30
Ben Caradoc-Davies <ben@transient.nz>
2016-01-31 10:19:31 +1300

    Remove unicode case folding flag from case sensitive LikeFilter matches as it has no effect [GEOT-5346]

--- modules/library/main/src/main/java/org/geotools/filter/LikeFilterImpl.java
+++ modules/library/main/src/main/java/org/geotools/filter/LikeFilterImpl.java
@@ -197,8 +197,8 @@ public class LikeFilterImpl extends AbstractFilter implements PropertyIsLike {
     private Matcher getMatcher(String string) {
         if (compPattern == null) {
             String pattern = new LikeToRegexConverter(this).getPattern();
-            compPattern = isMatchingCase() ? Pattern.compile(pattern, Pattern.UNICODE_CASE) : Pattern.compile(pattern,
-                    Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+            compPattern = isMatchingCase() ? Pattern.compile(pattern)
+                    : Pattern.compile(pattern, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
         }
         return compPattern.matcher(string);
     }

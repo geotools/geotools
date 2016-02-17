607ada69c225e1baff763c9c4a8e6611448a84a9
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2016-02-17 16:15:26 +0100

    [GEOT-5366]: ClassCastException fix

--- modules/library/coverage/src/main/java/org/geotools/coverage/processing/OperationJAI.java
+++ modules/library/coverage/src/main/java/org/geotools/coverage/processing/OperationJAI.java
@@ -2,7 +2,7 @@
  *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
  *
- *    (C) 2001-2015, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2001 - 2016, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -784,7 +784,7 @@ public class OperationJAI extends Operation2D {
                     result[numBands] = sampleDim;
                     continue;
                 }
-                categoryArray       = (Category[]) categories.toArray();
+                categoryArray = (Category[]) categories.toArray(new Category[categories.size()]);
                 indexOfQuantitative = getQuantitative(categoryArray);
                 if (indexOfQuantitative < 0) {
                     return null;

03bc240ba338dee7807a8f3edb8afee45c543f80
HendrikPeilke <Hendrik.Peilke@ibykus.de>
2016-02-18 09:52:43 +0100

    [GEOT-4317] corrected headers and tests
    Signed-off-by: HendrikPeilke <Hendrik.Peilke@ibykus.de>

--- modules/extension/wms/src/main/java/org/geotools/data/wms/request/AbstractGetMapRequest.java
+++ modules/extension/wms/src/main/java/org/geotools/data/wms/request/AbstractGetMapRequest.java
@@ -2,7 +2,7 @@
  *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
  *
- *    (C) 2004-2008, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2004-2016, Open Source Geospatial Foundation (OSGeo)
  *    
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -79,14 +79,14 @@ public abstract class AbstractGetMapRequest extends AbstractWMSRequest implement
                 try {
                     // spaces are converted to plus signs, but must be %20 for url calls [GEOT-4317]
                     layerString = layerString + URLEncoder.encode(layerName, "UTF-8").replaceAll("\\+", "%20");
-                } catch (UnsupportedEncodingException e) {
+                } catch (UnsupportedEncodingException | NullPointerException e) {
                     layerString = layerString + layerName;
                 }
                 styleName = styleName == null ? "" : styleName;
                 try {
 
-                    styleString = styleString + URLEncoder.encode(styleName, "UTF-8");
-                } catch (UnsupportedEncodingException e1) {
+                    styleString = styleString + URLEncoder.encode(styleName, "UTF-8").replaceAll("\\+", "%20");
+                } catch (UnsupportedEncodingException | NullPointerException e1) {
                     styleString = styleString + styleName;
                 }
                 

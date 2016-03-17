65a9cfd704e83a07f33cb502f4c28a7b84c55738
Shane StClair <srstclair@gmail.com>
2016-03-17 02:24:45 -0700

    [GEOT-5079] PostGISDialect Error with PostGIS 2.0

--- modules/plugin/jdbc/jdbc-postgis/src/test/java/org/geotools/data/postgis/PostgisGeometryOnlineTest.java
+++ modules/plugin/jdbc/jdbc-postgis/src/test/java/org/geotools/data/postgis/PostgisGeometryOnlineTest.java
@@ -2,7 +2,7 @@
  *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
  *
- *    (C) 2002-2009, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2002-2016, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -15,6 +15,8 @@
  *    Lesser General Public License for more details.
  */
 package org.geotools.data.postgis;
+
+import java.sql.Connection;
 
 import org.geotools.jdbc.JDBCGeometryOnlineTest;
 import org.geotools.jdbc.JDBCGeometryTestSetup;
@@ -36,4 +38,17 @@ public class PostgisGeometryOnlineTest extends JDBCGeometryOnlineTest {
         // linear ring type is not a supported type in postgis
     }
 
+    public void testDimensionFromFirstGeometry() throws Exception {
+        Connection cx = dataStore.getDataSource().getConnection();
+        PostGISDialect dialect = ((PostGISDialect) dataStore.getSQLDialect());
+        assertEquals((Integer) 0, dialect.getDimensionFromFirstGeo("public",
+                "dim_point", "geom", cx));
+        assertEquals((Integer) 1, dialect.getDimensionFromFirstGeo("public",
+                "dim_line", "geom", cx));
+        assertEquals((Integer) 2, dialect.getDimensionFromFirstGeo("public",
+                "dim_polygon", "geom", cx));
+        assertEquals((Integer) 1, dialect.getDimensionFromFirstGeo("public",
+                "dim_collection", "geom", cx));
+        dataStore.closeSafe(cx);
+    }
 }

d5a8834b8362d08795cd8a13aa09fcbafbba5abe
Ian Turton <ijturton@gmail.com>
2016-04-14 16:00:51 +0100

    back port of https://osgeo-org.atlassian.net/projects/GEOT/issues/GEOT-5403 fix

--- modules/library/main/src/test/java/org/geotools/data/store/FilteringSimpleFeatureCollectionTest.java
+++ modules/library/main/src/test/java/org/geotools/data/store/FilteringSimpleFeatureCollectionTest.java
@@ -1,3 +1,20 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ * 
+ *    (C) 2002-2016, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+ 
 package org.geotools.data.store;
 
 import java.io.IOException;
@@ -17,7 +34,12 @@ import org.opengis.filter.FilterFactory;
  */
 public class FilteringSimpleFeatureCollectionTest extends FeatureCollectionWrapperTestSupport {
     FilterFactory ff = CommonFactoryFinder.getFilterFactory(null);
-
+    
+    public void testNext() {
+        Filter filter = ff.equal(ff.property("someAtt"), ff.literal("1"), false);
+        SimpleFeatureCollection collection = new FilteringSimpleFeatureCollection(delegate, filter);
+        assertNotNull(collection.features().next());
+    }
     public void testCount() {
         Filter filter = ff.equal(ff.property("someAtt"), ff.literal("1"), false);
         SimpleFeatureCollection collection = new FilteringSimpleFeatureCollection(delegate, filter);

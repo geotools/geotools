51cf0476108134fbbb6ebd6c7aae17c1396ac848
Andrea Aime <andrea.aime@gmail.com>
2016-04-27 15:18:34 +0200

    Removing unused import to fix the build

--- modules/library/main/src/main/java/org/geotools/feature/visitor/GroupByVisitor.java
+++ modules/library/main/src/main/java/org/geotools/feature/visitor/GroupByVisitor.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.geotools.feature.DefaultFeatureCollection;
 import org.opengis.feature.Feature;

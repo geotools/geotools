8753cb4592e0c7d10d6825c060e30ef41c9125ad
tbarsballe <tbarsballe@boundlessgeo.com>
2016-05-02 19:09:00 +0000

    updating version numbers and README for 15-RC1

--- docs/common.py
+++ docs/common.py
@@ -58,7 +58,7 @@ copyright = u'2015, GeoTools'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '15-SNAPSHOT'
+release = '15-RC1'
 
 # The short X.Y version.
 version = release

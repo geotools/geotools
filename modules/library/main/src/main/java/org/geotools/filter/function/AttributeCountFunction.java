f8aa9db8169273ea70f243f0c9fd9ba14b4f0de9
ang05a <rini.angreani@csiro.au>
2016-10-12 13:14:07 +0800

    GEOT-5541: Create a function to return number of complex attributes

---
+++ modules/library/main/src/main/java/org/geotools/filter/function/AttributeCountFunction.java
@@ -0,0 +1,57 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2016, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.filter.function;
+
+import java.util.Collection;
+
+import org.geotools.filter.FunctionExpressionImpl;
+import org.geotools.filter.capability.FunctionNameImpl;
+import org.opengis.filter.capability.FunctionName;
+import org.opengis.filter.expression.Expression;
+
+/**
+ * Takes an AttributeExpression, and computes the node count for the attribute.
+ * 
+ * @author Rini Angreani (CSIRO Mineral Resources)
+ * @source $URL$
+ */
+public class AttributeCountFunction extends FunctionExpressionImpl {
+
+    public static FunctionName NAME = new FunctionNameImpl("attributeCount", "count");
+
+    public AttributeCountFunction() {
+        super(NAME);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.geotools.filter.Expression#getValue(org.geotools.feature.Feature)
+     */
+    public Object evaluate(Object feature) {
+        Expression ae = (Expression) getParameters().get(0);
+        Object value = ae.evaluate(feature);
+        if (value == null) {
+            return 0;
+        } else if (value instanceof Collection) {
+            return ((Collection<?>) value).size();
+        } else {
+            return 1;
+        }
+    }
+
+}

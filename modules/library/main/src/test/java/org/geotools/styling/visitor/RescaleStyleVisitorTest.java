870ba1d7645ba3f1ce401756fd5d131fb16b7811
Rob Ward <robert.ward@scisys.co.uk>
2016-11-16 12:10:08 +0000

    RescaleStyleVisior visit(Graphic) method now copies the anchor point data.

--- modules/library/main/src/test/java/org/geotools/styling/visitor/RescaleStyleVisitorTest.java
+++ modules/library/main/src/test/java/org/geotools/styling/visitor/RescaleStyleVisitorTest.java
@@ -25,9 +25,11 @@ import javax.measure.unit.SI;
 
 import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.filter.IllegalFilterException;
+import org.geotools.styling.AnchorPoint;
 import org.geotools.styling.FeatureTypeStyle;
 import org.geotools.styling.Fill;
 import org.geotools.styling.Font;
+import org.geotools.styling.Graphic;
 import org.geotools.styling.LineSymbolizer;
 import org.geotools.styling.Mark;
 import org.geotools.styling.PolygonSymbolizer;
@@ -171,7 +173,11 @@ public class RescaleStyleVisitorTest {
         // a graphic stroke
         Stroke stroke = sb.createStroke();
         stroke.setColor(null);
-        stroke.setGraphicStroke(sb.createGraphic(null, sb.createMark("square", null, sb.createStroke(1)), null));
+        Graphic graphic = sb.createGraphic(null, sb.createMark("square", null, sb.createStroke(1)), null);
+        double expectedAnchorPointX = 0.25;
+        double expectedAnchorPointY = 0.75;
+        graphic.setAnchorPoint(sb.createAnchorPoint(expectedAnchorPointX, expectedAnchorPointY));
+        stroke.setGraphicStroke(graphic);
         
         // a graphic fill
         Fill fill = sb.createFill();
@@ -189,7 +195,11 @@ public class RescaleStyleVisitorTest {
         rm = (Mark) rps.getFill().getGraphicFill().graphicalSymbols().get(0);
         assertEquals(4.0, rm.getStroke().getWidth().evaluate(null, Double.class), 0d);
 
-        
+        AnchorPoint actualAnchorPoint = rps.getStroke().getGraphicStroke().getAnchorPoint();
+        assertNotNull(actualAnchorPoint);
+        assertEquals(expectedAnchorPointX, actualAnchorPoint.getAnchorPointX().evaluate(null, Double.class), 0d);
+        assertEquals(expectedAnchorPointY, actualAnchorPoint.getAnchorPointY().evaluate(null, Double.class), 0d);
+
         // a line symbolizer that uses a graphic stroke
         LineSymbolizer ls = sb.createLineSymbolizer(stroke);
         

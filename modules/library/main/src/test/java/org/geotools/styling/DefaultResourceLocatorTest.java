5a7c7580bfb7dd09e890fe461c24cce647deed9b
Jody Garnett <jody.garnett@gmail.com>
2016-11-17 23:57:58 -0800

    Allow DefaultResourceLocator to preserver query if provided (GEOS-7861)

--- modules/library/main/src/test/java/org/geotools/styling/DefaultResourceLocatorTest.java
+++ modules/library/main/src/test/java/org/geotools/styling/DefaultResourceLocatorTest.java
@@ -4,11 +4,12 @@ import java.io.File;
 import java.net.URL;
 
 import org.geotools.data.DataUtilities;
+import org.junit.Test;
 
 import junit.framework.TestCase;
 
 public class DefaultResourceLocatorTest extends TestCase {
-
+    @Test
     public void testRelativeFileURL() throws Exception {
         DefaultResourceLocator locator = new DefaultResourceLocator();
         locator.setSourceUrl(getClass().getResource("test-data/blob.gif"));
@@ -18,10 +19,21 @@ public class DefaultResourceLocatorTest extends TestCase {
         checkURL(locator.locateResource("file://blob.gif"));
         checkURL(locator.locateResource("file://./blob.gif"));
     }
+    
+    public void testPreserveURLQuery() throws Exception{
+        DefaultResourceLocator locator = new DefaultResourceLocator();
+        locator.setSourceUrl(getClass().getResource("test-data/blob.gif"));
+        
+        // Confirm still able to resolve to a File
+        URL url = locator.locateResource("blob.gif?query=parameter");
+        assertEquals("query=parameter",url.getQuery());
+        File file = DataUtilities.urlToFile(url);
+        assertTrue( file.exists() );
+    }
 
     void checkURL(URL url) {
-        assertNotNull(url);
         File f = DataUtilities.urlToFile(url);
         assertTrue(f.exists());
     }
+    
 }

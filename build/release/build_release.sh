eefefc4f710e778d88b24479aac3f918b2fa12e8
Ian Turton <ijturton@gmail.com>
2017-01-20 13:36:26 +0000

    increase memory for build

--- build/release/build_release.sh
+++ build/release/build_release.sh
@@ -125,6 +125,7 @@ popd > /dev/null
 # build the release
 if [ -z $SKIP_BUILD ]; then
   echo "building release"
+  export MAVEN_OPTS="-Xmx512m -XX:MaxPermSoze=128m"
   mvn $MAVEN_FLAGS -DskipTests -Dall clean -Pcollect install
   mvn $MAVEN_FLAGS -DskipTests assembly:assembly
 fi

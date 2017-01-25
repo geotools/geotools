5331f134c11ef4221989287e7264914585b2f7ba
vickdw <dvick@boundlessgeo.com>
2017-01-25 14:27:39 -0600

    adding header information

--- docs/src/main/java/org/geotools/po/bindings/POConfiguration.java
+++ docs/src/main/java/org/geotools/po/bindings/POConfiguration.java
@@ -1,3 +1,12 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2017, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This file is hereby placed into the Public Domain. This means anyone is
+ *    free to do whatever they wish with this file. Use it well and enjoy!
+ */
 package org.geotools.po.bindings;
 
 import org.geotools.xml.Configuration;

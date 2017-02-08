81ab0c80e9bb5b77feb09e15cc4197db78992052
Ben Caradoc-Davies <ben@transient.nz>
2017-02-08 15:06:50 +1300

    [GEOT-5611] Invalid data for ImageMosaic of multi-coverage NetCDF/GRIB files

--- modules/plugin/coverage-multidim/netcdf/src/main/java/org/geotools/coverage/io/netcdf/NetCDFReader.java
+++ modules/plugin/coverage-multidim/netcdf/src/main/java/org/geotools/coverage/io/netcdf/NetCDFReader.java
@@ -16,9 +16,6 @@
  */
 package org.geotools.coverage.io.netcdf;
 
-import it.geosolutions.imageio.utilities.ImageIOUtilities;
-import ucar.nc2.dataset.NetcdfDataset;
-
 import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BandedSampleModel;
@@ -31,7 +28,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -108,6 +104,8 @@ import org.opengis.referencing.datum.PixelInCell;
 import org.opengis.referencing.operation.MathTransform;
 import org.opengis.referencing.operation.MathTransform2D;
 import org.opengis.referencing.operation.TransformException;
+
+import it.geosolutions.imageio.utilities.ImageIOUtilities;
 
 /**
  * A NetCDF Reader implementation
@@ -729,18 +727,17 @@ public class NetCDFReader extends AbstractGridCoverage2DReader implements Struct
 
     private String checkUnspecifiedCoverage(String coverageName) {
         if (coverageName.equalsIgnoreCase(UNSPECIFIED)) {
-            if (getGridCoverageCount() > 1) {
+            if (defaultName == null) {
                 throw new IllegalArgumentException(
-                        "Need to specify the coverageName for a reader related to multiple coverages");
+                        "coverageName not specified and no defaultName for " + sourceURL);
             } else {
                 return defaultName;
             }
+        } else if (!setNames.contains(coverageName)) {
+            throw new IllegalArgumentException(
+                    "coverageName " + coverageName + " not found for " + sourceURL);
         } else {
-            if (!setNames.contains(coverageName)) {
-                throw new IllegalArgumentException("The specified coverageName is unavailable");
-            } else {
-                return coverageName;
-            }
+            return coverageName;
         }
     }
 

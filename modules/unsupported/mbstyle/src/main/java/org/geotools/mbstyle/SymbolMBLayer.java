944aa8c02fe959f37e936a523efeb28d12ef82cf
Jody Garnett <jody.garnett@gmail.com>
2017-03-09 00:24:37 -0800

    please double check initial commits have both a header and the correct geotoosl formatting

--- modules/unsupported/mbstyle/src/main/java/org/geotools/mbstyle/SymbolMBLayer.java
+++ modules/unsupported/mbstyle/src/main/java/org/geotools/mbstyle/SymbolMBLayer.java
@@ -1,3 +1,19 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2017, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package org.geotools.mbstyle;
 
 import java.awt.Color;
@@ -6,15 +22,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.geotools.mbstyle.CircleMBLayer.CirclePitchScale;
-import org.geotools.mbstyle.CircleMBLayer.CircleTranslateAnchor;
-import org.geotools.mbstyle.FillMBLayer.FillTranslateAnchor;
 import org.geotools.mbstyle.parse.MBFormatException;
 import org.geotools.mbstyle.parse.MBObjectParser;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.opengis.filter.expression.Expression;
 
+/**
+ * A filled circle.
+ * <p>
+ * MBLayer wrapper around a {@link JSONObject} representation of a "circle" type latyer. All
+ * methods act as accessors on provided JSON layer, no other state is maintained. This allows
+ * modifications to be made cleanly with out chance of side-effect.
+ * 
+ * <ul>
+ * <li>get methods: access the json directly</li>
+ * <li>query methods: provide logic / transforms to GeoTools classes as required.</li>
+ * </ul>
+ */
 public class SymbolMBLayer extends MBLayer {
 
     private JSONObject layout;
@@ -80,53 +105,133 @@ public class SymbolMBLayer extends MBLayer {
          */
         RIGHT
     }
-
+    /**
+     * Text justification options.
+     */
     public static enum TextAnchor {
         /**
          * The center of the text is placed closest to the anchor.
          */
-        CENTER,
+        CENTER(0.5, 0.5),
 
         /**
          * The left side of the text is placed closest to the anchor.
          */
-        LEFT,
+        LEFT(0.0, 0.5),
 
         /**
          * The right side of the text is placed closest to the anchor.
          */
-        RIGHT,
+        RIGHT(1.0, 0.5),
 
         /**
          * The top of the text is placed closest to the anchor.
          */
-        TOP,
+        TOP(0.5, 1.0),
 
         /**
          * The bottom of the text is placed closest to the anchor.
          */
-        BOTTOM,
+        BOTTOM(0.5, 0.0),
 
         /**
          * The top left corner of the text is placed closest to the anchor.
          */
-        TOP_LEFT,
+        TOP_LEFT(0.0, 1.0),
 
         /**
          * The top right corner of the text is placed closest to the anchor.
          */
-        TOP_RIGHT,
+        TOP_RIGHT(1.0, 1.0),
 
         /**
          * The bottom left corner of the text is placed closest to the anchor.
          */
-        BOTTOM_LEFT,
+        BOTTOM_LEFT(0.0, 0.0),
 
         /**
          * The bottom right corner of the text is placed closest to the anchor.
          */
-        BOTTOM_RIGHT,
+        BOTTOM_RIGHT(1.0, 0.0);
 
+        /** horizontal justification */
+        final private double x;
+
+        /** vertical justification */
+        final private double y;
+
+        private TextAnchor(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        /**
+         * Horizontal justification.
+         * 
+         * @return horizontal alignment between 0.0 and 1.0.
+         */
+        public double getX() {
+            return x;
+        }
+
+        /**
+         * Vertical justification.
+         * 
+         * @return vertical alignment between 0.0 and 1.0.
+         */
+        public double getY() {
+            return y;
+        }
+
+        /**
+         * Parse provided jsonString as a TextAnchor.
+         * <p>
+         * One of center, left, right, top, bottom, top-left, top-right, bottom-left, bottom-right. Defaults to center.</p>
+         *  
+         * @param jsonString text anchor definition
+         * @return TextAnchor, defaults TextAnchor#CENTER if undefined
+         */
+        public static TextAnchor parse(String jsonString){
+            if( jsonString == null ){
+                return CENTER;
+            }
+            String name = jsonString.toUpperCase().trim().replace('-', '_');
+            try {
+                return TextAnchor.valueOf(name);
+            }
+            catch (IllegalArgumentException invalid){
+                throw new MBFormatException("Invalid text-alginment '"+jsonString+"' expected one of"
+                        + "center, left, right, top, bottom, top-left, top-right, bottom-left, bottom-right");
+            }
+        }
+        
+        /**
+         * The json represetnation of this TextAnchor.
+         * 
+         * @return json representation
+         */
+        public String json(){
+            return name().toLowerCase().replace('_', '-');
+        }
+        
+        /**
+         * Quickly grab y justification for jsonString.
+         * 
+         * @param jsonString
+         * @return vertical anchor, defaults to 0.5
+         */
+        public static double getAnchorY(String jsonString){
+            return TextAnchor.parse(jsonString).getY();
+        }
+        /**
+         * Quickly grab x justification for jsonString.
+         * 
+         * @param jsonString
+         * @return horizontal anchor, defaults to 0.5
+         */
+        public static double getAnchorX(String jsonString){
+            return TextAnchor.parse(jsonString).getX();
+        }
     }
 
     public static enum TextTransform {

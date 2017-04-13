ae479905b2e060a08065ab65eb8cc4fb169ef40c
jdeolive <jdeolive@gmail.com>
2017-04-13 10:06:58 -0600

    Factoring out some centroid code from label cache to RendererUtiliites.
    - Adds getGeometryCentroid method for “safe” centroid calculation for polygon
    - Adds sampleForCentralPoint for finding “centroid” that lies within
    the polygon

--- modules/library/render/src/main/java/org/geotools/renderer/lite/RendererUtilities.java
+++ modules/library/render/src/main/java/org/geotools/renderer/lite/RendererUtilities.java
@@ -29,6 +29,8 @@ import javax.measure.unit.SI;
 import javax.measure.unit.Unit;
 import javax.swing.Icon;
 
+import com.vividsolutions.jts.geom.prep.PreparedGeometry;
+import com.vividsolutions.jts.geom.prep.PreparedGeometryFactory;
 import org.geotools.coverage.grid.GridEnvelope2D;
 import org.geotools.data.crs.ForceCoordinateSystemFeatureResults;
 import org.geotools.data.simple.SimpleFeatureCollection;
@@ -873,5 +875,106 @@ public final class RendererUtilities {
             }
         }
         return features;
+    }
+
+    /**
+     * Finds a centroid for a polygon catching any exceptions resulting from generalization or 
+     * other polygon irregularities.
+     * 
+     * @param geom The polygon.
+     * @return The polygon centroid, or null if it can't be found.
+     */
+    public static Point getPolygonCentroid(Polygon geom) {
+        Point centroid;
+        try {
+            centroid = geom.getCentroid();
+        } catch (Exception e) {
+            // generalized polygons causes problems - this
+            // tries to hide them.
+            try {
+                centroid = geom.getExteriorRing().getCentroid();
+            } catch (Exception ee) {
+                try {
+                    centroid = geom.getFactory().createPoint(geom.getCoordinate());
+                } catch (Exception eee) {
+                    return null; // we're hooped
+                }
+            }
+        }
+        return centroid;
+    }
+
+    /**
+     * Uses a sampling technique to obtain a central point that lies inside the specified polygon.
+     * <p>
+     * Sampling occurs horizontally along the middle of the polygon obtained from the y coordinate of the polygon 
+     * centroid.
+     * </p>
+     * 
+     * @param geom The polygon.
+     * @param centroid The centroid of the polygon, can be null in which case it will be computed from {@link #getPolygonCentroid(Polygon)}.
+     * @param pg The prepared version of geom, can be null in which case it will be computed on demand.
+     * @param gf The geometry factory, can be null in which case the polygons factory will be used.
+     * 
+     * @return A central point that lies inside of the polygon, or null if one could not be found.
+     */
+    public static Point sampleForCentralPoint(Polygon geom, Point centroid, PreparedGeometry pg, GeometryFactory gf, 
+        double stepSize, int numSamples) {
+
+        if (centroid == null) {
+            centroid = getPolygonCentroid(geom);
+        }
+        if (pg == null) {
+            pg = PreparedGeometryFactory.prepare(geom);
+        }
+        if (gf == null) {
+            gf = geom.getFactory();
+        }
+
+        if (pg.contains(centroid)) {
+            return centroid;
+        }
+
+        Envelope env = geom.getEnvelopeInternal();
+        if (stepSize > 0) {
+            numSamples = (int) Math.round(env.getWidth() / stepSize);
+        }
+        else if (numSamples > 0) {
+            stepSize = env.getWidth() / numSamples;
+        }
+        else {
+            throw new IllegalArgumentException("One of stepSize or numSamples must be greater than zero");
+        }
+
+        Coordinate c = new Coordinate();
+        Point pp = gf.createPoint(c);
+        c.y = centroid.getY();
+        int max = -1;
+        int maxIdx = -1;
+        int containCounter = -1;
+        for (int i = 0; i < numSamples; i++) {
+            c.x = env.getMinX() + stepSize * i;
+            pp.geometryChanged();
+            if(!pg.contains(pp)) {
+                containCounter = 0;
+            } else if(i == 0) {
+                containCounter = 1;
+            } else {
+                containCounter++;
+                if(containCounter > max) {
+                    max = containCounter;
+                    maxIdx = i;
+                }
+            }
+        }
+
+        if(maxIdx != -1) {
+            int midIdx = max > 1 ? maxIdx - max / 2 : maxIdx;
+            c.x = env.getMinX() + stepSize * midIdx;
+            pp.geometryChanged();
+            return pp;
+        } else {
+            return null;
+        }
     }
 }

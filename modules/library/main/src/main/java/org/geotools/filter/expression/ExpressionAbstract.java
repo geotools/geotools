ba2ba7e6fb391ff4b1677ead1c74cc8a5a648927
Jody Garnett <jody.garnett@gmail.com>
2017-04-19 23:37:17 -0500

    remove developer notes javadoc
    Expression chaining was not implemented, filter factory provides terse expresion creation

--- modules/library/main/src/main/java/org/geotools/filter/expression/ExpressionAbstract.java
+++ modules/library/main/src/main/java/org/geotools/filter/expression/ExpressionAbstract.java
@@ -23,21 +23,6 @@ import org.opengis.filter.expression.ExpressionVisitor;
 //
 /**
  * Abstract superclass of these Expression implementations.
- * <p>
- * Contains additional support for "Expression chaining". This allows
- * Expressions to be constructed as a chain of Java commands similar to the use
- * of the java collections api.
- * </p>
- * <p>
- * Note: Expression chaining is a simple developer convience, it has no effect
- * on the data model exposed by the GeoAPI interfaces.
- * </p>
- * <p>
- * Idea: We may also be able to teach this implementation to make use of
- * JXPath to extract "attribute values" from Java Beans, DOM, JDOM in addition
- * to the geotools & geoapi FeatureType models. It is a cunning plan - any
- * implementation will make use of this abstract base class.
- * </p>
  * 
  * @author Jody Garnett
  *
@@ -55,10 +40,10 @@ public abstract class ExpressionAbstract implements Expression {
     }
     /**
      * Default implementation delegates handling of context
-     * conversion to Value utility class.
+     * conversion to Converters utility class.
      * <p>
-     * Subclasses are expected to make use of the Value utility class
-     * (as the easiest way to provide value morphing in confirmance with
+     * Subclasses are expected to make use of the {@linkplain Converters} utility class
+     * (as the easiest way to provide value morphing in conformance with
      *  the Filter specification).
      */
     public <T> T evaluate(Object object, Class<T> context) {

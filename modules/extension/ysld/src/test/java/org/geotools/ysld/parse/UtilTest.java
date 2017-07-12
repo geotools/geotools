8adae77c24387c49f67df3c63c19f026b42b6c91
Andreas Schmitz <schmitz@terrestris.de>
2017-07-12 08:36:00 +0200

    [GEOS-8034] Fixed ysld color conversion

--- modules/extension/ysld/src/test/java/org/geotools/ysld/parse/UtilTest.java
+++ modules/extension/ysld/src/test/java/org/geotools/ysld/parse/UtilTest.java
@@ -19,8 +19,6 @@ package org.geotools.ysld.parse;
 
 import static org.junit.Assert.assertEquals;
 
-import java.awt.Color;
-
 import org.junit.Test;
 import org.opengis.filter.expression.Literal;
 
@@ -29,12 +27,12 @@ public class UtilTest {
     public void testColor() {
         Factory factory = new Factory();
 
-        assertEquals(new Color(0xFF0000), ((Literal) Util.color("FF0000", factory)).getValue());
-        assertEquals(new Color(0x00FF00), ((Literal) Util.color("00ff00", factory)).getValue());
-        assertEquals(new Color(0x0000FF), ((Literal) Util.color("#0000FF", factory)).getValue());
-        assertEquals(new Color(0xFFFF00), ((Literal) Util.color("0xFFFF00", factory)).getValue());
-        assertEquals(new Color(0x00FFFF),
+        assertEquals("#FF0000", ((Literal) Util.color("FF0000", factory)).getValue());
+        assertEquals("#00FF00", ((Literal) Util.color("00ff00", factory)).getValue());
+        assertEquals("#0000FF", ((Literal) Util.color("#0000FF", factory)).getValue());
+        assertEquals("#FFFF00", ((Literal) Util.color("0xFFFF00", factory)).getValue());
+        assertEquals("#00FFFF",
                 ((Literal) Util.color("rgb(0, 255, 255)", factory)).getValue());
-        assertEquals(new Color(0xFF00FF), ((Literal) Util.color("f0f", factory)).getValue());
+        assertEquals("#FF00FF", ((Literal) Util.color("f0f", factory)).getValue());
     }
 }

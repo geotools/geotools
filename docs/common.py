f307e5eaa65bc4bae0e37fac46b27151566ade37
aaime <andrea.aime@gmail.com>
2017-07-31 11:23:49 +0000

    updating version numbers and README for 17.2

--- docs/common.py
+++ docs/common.py
@@ -58,7 +58,7 @@ copyright = u'2016, GeoTools'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '17-SNAPSHOT'
+release = '17.2'
 
 # The short X.Y version.
 version = release

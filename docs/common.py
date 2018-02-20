d58fcc7e56bb280b2844615bb0397a043f6639fc
Torben Barsballe <tbarsballe@boundlessgeo.com>
2018-02-20 09:15:49 -0800

    Update doc links to stable

--- docs/common.py
+++ docs/common.py
@@ -28,9 +28,9 @@ extensions = ['sphinx.ext.todo','sphinx.ext.extlinks']
 extlinks = { 
     'wiki': ('https://github.com/geotools/geotools/wiki/%s',''),
     'website': ('http://geotools.org/%s',''),
-    'geoserver': ('http://docs.geoserver.org/latest/en/user/%s',''),
-    'developer': ('http://docs.geotools.org/latest/developer/%s',''),
-    'user': ('http://docs.geotools.org/latest/userguide/%s',''),
+    'geoserver': ('http://docs.geoserver.org/stable/en/user/%s',''),
+    'developer': ('http://docs.geotools.org/stable/developer/%s',''),
+    'user': ('http://docs.geotools.org/stable/userguide/%s',''),
     'geot': ('https://osgeo-org.atlassian.net/browse/GEOT-%s','GEOT-')
 }
 

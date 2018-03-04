edaf783f3b40fbe3f3712e6f72d92f531e032cf1
Andrea Aime <andrea.aime@gmail.com>
2018-03-04 10:36:22 +0100

    Fixing OpenJDK build

--- modules/unsupported/mbstyle/src/test/java/org/geotools/mbstyle/transform/VisualTransformerTest.java
+++ modules/unsupported/mbstyle/src/test/java/org/geotools/mbstyle/transform/VisualTransformerTest.java
@@ -711,7 +711,7 @@ public class VisualTransformerTest {
         renderer.setJava2DHints(new RenderingHints(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON));
         BufferedImage image = MapboxTestUtils.showRender(renderTitle, renderer, DISPLAY_TIME,
                 new ReferencedEnvelope[] { bounds }, null);
-        ImageAssert.assertEquals(file(renderComparisonFileName), image, 1800);
+        ImageAssert.assertEquals(file(renderComparisonFileName), image, 2000);
         mc.dispose();
     }
     

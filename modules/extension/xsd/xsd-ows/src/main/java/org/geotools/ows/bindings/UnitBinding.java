e05f1be8b9d374041a66aea77467882c3f05e603
Jody Garnett <jody.garnett@gmail.com>
2018-03-21 13:50:49 +0100

    api conversion

--- modules/extension/xsd/xsd-ows/src/main/java/org/geotools/ows/bindings/UnitBinding.java
+++ modules/extension/xsd/xsd-ows/src/main/java/org/geotools/ows/bindings/UnitBinding.java
@@ -18,10 +18,12 @@ package org.geotools.ows.bindings;
 
 import java.lang.reflect.Field;
 
-import javax.measure.unit.BaseUnit;
-import javax.measure.unit.DerivedUnit;
 import si.uom.NonSI;
 import si.uom.SI;
+import tec.uom.se.AbstractUnit;
+import tec.uom.se.unit.AlternateUnit;
+import tec.uom.se.unit.BaseUnit;
+
 import javax.measure.Unit;
 import javax.measure.format.UnitFormat;
 import javax.xml.namespace.QName;
@@ -87,18 +89,18 @@ public class UnitBinding extends AbstractSimpleBinding {
         return null;
     }
 
-	private Unit lookup(Class class1, String name) {
-		Unit unit = null;
+	private Unit<?> lookup(Class<?> class1, String name) {
+		Unit<?> unit = null;
 		Field[] fields = class1.getDeclaredFields();
 		for (int i=0; i<fields.length; i++) {
 			Field field = fields[i];
 			String name2 = field.getName();
 			if ( (field.getType().isAssignableFrom(BaseUnit.class) ||
-					field.getType().isAssignableFrom(DerivedUnit.class)) &&
+					field.getType().isAssignableFrom(AlternateUnit.class)) &&
 					name2.equalsIgnoreCase(name) ) {
 
 				try {
-					unit = (Unit) field.get(unit);
+					unit = (Unit<?>) field.get(unit);
 					return unit;
 				} catch (Exception e) {
 					// continue searching

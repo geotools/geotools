24689bd2e11164a5b607cfde5a94d0f5098dab24
Ian Turton <ijturton@gmail.com>
2018-05-07 15:13:23 +0100

    reversed WFS test

--- modules/unsupported/wfs-ng/src/test/java/org/geotools/data/wfs/online/v1_0/GeoServerOnlineTest.java
+++ modules/unsupported/wfs-ng/src/test/java/org/geotools/data/wfs/online/v1_0/GeoServerOnlineTest.java
@@ -18,23 +18,16 @@
 package org.geotools.data.wfs.online.v1_0;
 
 import static org.geotools.data.wfs.WFSTestData.GEOS_STATES_11;
-import static org.junit.Assert.*;
 
-import java.io.IOException;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Polygon;
 import java.util.Collections;
-
-import org.geotools.data.Query;
-import org.geotools.data.simple.SimpleFeatureCollection;
-import org.geotools.data.simple.SimpleFeatureIterator;
-import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.data.wfs.WFSDataStoreFactory;
 import org.geotools.data.wfs.online.AbstractWfsDataStoreOnlineTest;
 import org.geotools.factory.CommonFactoryFinder;
-import org.geotools.filter.text.cql2.CQLException;
-import org.geotools.filter.text.ecql.ECQL;
-import org.junit.Test;
-import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
 
@@ -70,71 +63,4 @@ public class GeoServerOnlineTest extends AbstractWfsDataStoreOnlineTest {
         FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
         return ff.intersects(ff.property("the_geom"), ff.literal(polygon));
     }
-
-    
-    /**
-     * Test to demonstrate GEOT-5921
-     * WFSClient passes ILIKE to 1.0.0 servers that can't handle it
-     * https://osgeo-org.atlassian.net/browse/GEOT-5921
-     */
-    @Test
-    public void testFeatureSourceGetFeaturesILikeFilter() throws IOException, CQLException {
-        if (Boolean.FALSE.equals(serviceAvailable)) {
-            return;
-        }
-        
-        
-        SimpleFeatureSource featureSource;
-        featureSource = wfs.getFeatureSource(testType.FEATURETYPENAME);
-        assertNotNull(featureSource);
-
-        Query query = new Query(testType.FEATURETYPENAME);
-        
-        Filter filter = ECQL.toFilter("STATE_NAME ILIKE 'north%'");
-        
-        //ILIKE (or matchCase) is not supported in WFS 1.0.0 and the client should know this!
-        
-        query.setFilter(filter);
-        System.out.println(query);
-        System.out.println(((org.geotools.filter.LikeFilterImpl)query.getFilter()).isMatchingCase());
-        SimpleFeatureCollection features;
-        features = featureSource.getFeatures(query);
-        assertNotNull(features);
-        assertEquals(2,features.size());
-        SimpleFeatureType schema = features.getSchema();
-        assertNotNull(schema);
-
-    }
-    
-    /**
-     * Test to demonstrate GEOT-5920
-     * WFSClient fails when a function is used in a query
-     * https://osgeo-org.atlassian.net/browse/GEOT-5920
-     */
-    @Test
-    public void testFeatureSourceGetFeaturesFunctionFilter() throws IOException, CQLException {
-        if (Boolean.FALSE.equals(serviceAvailable)) {
-            return;
-        }
-        
-        
-        SimpleFeatureSource featureSource;
-        featureSource = wfs.getFeatureSource(testType.FEATURETYPENAME);
-        assertNotNull(featureSource);
-
-        Query query = new Query(testType.FEATURETYPENAME);
-        Filter filter = ECQL.toFilter("strToLowerCase(STATE_NAME) LIKE 'north%'");
-        query.setFilter(filter);
-        System.out.println(query);
-        System.out.println(((org.geotools.filter.LikeFilterImpl)query.getFilter()).isMatchingCase());
-        SimpleFeatureCollection features;
-        features = featureSource.getFeatures(query);
-        assertNotNull(features);
-        assertEquals(2,features.size());
-        SimpleFeatureType schema = features.getSchema();
-        assertNotNull(schema);
-
-    }
-
-
 }

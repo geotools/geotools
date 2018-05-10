85d61a0544a81c2e60d114a798e8e5e90966a131
Andrea Aime <andrea.aime@gmail.com>
2018-05-10 10:00:59 +0200

    [GEOT-6013] Support GML in JP2K georeferentiation in Kakadu based reader

--- modules/plugin/jp2k/src/test/java/org/geotools/coverageio/jp2k/JP2KTest.java
+++ modules/plugin/jp2k/src/test/java/org/geotools/coverageio/jp2k/JP2KTest.java
@@ -17,10 +17,12 @@
  */
 package org.geotools.coverageio.jp2k;
 
+import it.geosolutions.util.KakaduUtilities;
 import java.awt.Color;
 import java.awt.geom.AffineTransform;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.logging.Logger;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.GridGeometry2D;
@@ -32,9 +34,11 @@ import org.geotools.referencing.CRS;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.geotools.test.TestData;
 import org.junit.Test;
+import org.junit.internal.AssumptionViolatedException;
 import org.opengis.geometry.Envelope;
 import org.opengis.parameter.GeneralParameterValue;
 import org.opengis.parameter.ParameterValue;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.referencing.operation.MathTransform;
 
 /**
@@ -61,16 +65,7 @@ public final class JP2KTest extends BaseJP2K {
             return;
         }
 
-        File file = null;
-        try {
-            file = TestData.file(this, "sample.jp2");
-        } catch (FileNotFoundException fnfe) {
-            LOGGER.warning("test-data not found: sample.jp2 \nTests are skipped");
-            return;
-        }
-
-        final JP2KFormat format = factorySpi.createFormat();
-        assertTrue(format.accepts(file));
+        File file = getInputFile("sample.jp2");
 
         // //
         //
@@ -123,15 +118,7 @@ public final class JP2KTest extends BaseJP2K {
             return;
         }
 
-        File file = null;
-        try {
-            file = TestData.file(this, "bogota.jp2");
-        } catch (FileNotFoundException fnfe) {
-            LOGGER.warning("test-data not found: bogota.jp2 \nTests are skipped");
-            return;
-        }
-        final JP2KFormat format = factorySpi.createFormat();
-        assertTrue(format.accepts(file));
+        File file = getInputFile("bogota.jp2");
 
         // //
         //
@@ -165,5 +152,64 @@ public final class JP2KTest extends BaseJP2K {
         gc = (GridCoverage2D) reader.read(new GeneralParameterValue[] {gg, useJAI});
         assertNotNull(gc);
         forceDataLoading(gc);
+    }
+
+    @Test
+    public void testGMLJP2Projected() throws Exception {
+        // GMLJP2 parsing is available only when Kakadu is enabled
+        if (!testingEnabled() || !KakaduUtilities.isKakaduAvailable()) {
+            throw new AssumptionViolatedException("Testing not enabled or Kakadu not available");
+        }
+
+        File file = getInputFile("bogota_gml.jp2");
+
+        // check georeferencing has been loaded properly
+        final AbstractGridCoverage2DReader reader = new JP2KReader(file);
+        final int nCov = reader.getGridCoverageCount();
+        assertEquals(nCov, 1);
+        CoordinateReferenceSystem expected = CRS.decode("EPSG:21892");
+        assertTrue(CRS.equalsIgnoreMetadata(expected, reader.getCoordinateReferenceSystem()));
+        GeneralEnvelope envelope = reader.getOriginalEnvelope();
+        final double EPS = 1e-6;
+        assertEquals(440720, envelope.getMinimum(0), EPS);
+        assertEquals(471440, envelope.getMaximum(0), EPS);
+        assertEquals(69280, envelope.getMinimum(1), EPS);
+        assertEquals(100000, envelope.getMaximum(1), EPS);
+    }
+
+    @Test
+    public void testGMLJP2Geographic() throws Exception {
+        // GMLJP2 parsing is available only when Kakadu is enabled
+        if (!testingEnabled() || !KakaduUtilities.isKakaduAvailable()) {
+            throw new AssumptionViolatedException("Testing not enabled or Kakadu not available");
+        }
+
+        File file = getInputFile("sample_gml.jp2");
+
+        // check georeferencing has been loaded properly
+        final AbstractGridCoverage2DReader reader = new JP2KReader(file);
+        final int nCov = reader.getGridCoverageCount();
+        assertEquals(nCov, 1);
+        CoordinateReferenceSystem expected = CRS.decode("EPSG:4326", true);
+        assertTrue(CRS.equalsIgnoreMetadata(expected, reader.getCoordinateReferenceSystem()));
+        GeneralEnvelope envelope = reader.getOriginalEnvelope();
+        final double EPS = 1e-6;
+        assertEquals(-180, envelope.getMinimum(0), EPS);
+        assertEquals(180, envelope.getMaximum(0), EPS);
+        assertEquals(-90, envelope.getMinimum(1), EPS);
+        assertEquals(90, envelope.getMaximum(1), EPS);
+    }
+
+    private File getInputFile(String path) throws IOException {
+        File file = null;
+        try {
+            file = TestData.file(this, path);
+        } catch (FileNotFoundException fnfe) {
+            throw new AssumptionViolatedException(
+                    "test-data not found: " + file + "\nTests are skipped");
+        }
+        final JP2KFormat format = factorySpi.createFormat();
+        assertTrue(format.accepts(file));
+        return file;
     }
 }

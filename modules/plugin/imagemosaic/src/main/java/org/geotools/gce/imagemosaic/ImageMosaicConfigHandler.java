c30e1cd1c2dfe938b4ec7d15721099041b6a73f9
Andrea Aime <andrea.aime@gmail.com>
2018-06-13 11:42:02 +0200

    [GEOT-6039] Found evidence of un-disposed granule catalogs in the geoserver build logs

--- modules/plugin/imagemosaic/src/main/java/org/geotools/gce/imagemosaic/ImageMosaicConfigHandler.java
+++ modules/plugin/imagemosaic/src/main/java/org/geotools/gce/imagemosaic/ImageMosaicConfigHandler.java
@@ -1034,6 +1034,8 @@ public class ImageMosaicConfigHandler {
 
         // fileIndex = 0;
         runConfiguration = null;
+
+        this.catalog.dispose();
     }
 
     public boolean getStop() {

7d6c60a5355550aa73b8a6957d9942d377244e3a
Brad Hards <bradh@frogmouth.net>
2018-07-16 08:23:52 +1000

    [GEOT-6064] Upgrade apache commons-lang to 3.7.
    The upgrade is mostly mechanical.

--- modules/unsupported/arcgis-rest/src/main/java/org/geotools/data/arcgisrest/schema/catalog/Dataset.java
+++ modules/unsupported/arcgis-rest/src/main/java/org/geotools/data/arcgisrest/schema/catalog/Dataset.java
@@ -8,9 +8,9 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
  * Project Open Data Dataset
--- modules/unsupported/solr/src/main/java/org/geotools/data/solr/SolrFeatureReader.java
+++ modules/unsupported/solr/src/main/java/org/geotools/data/solr/SolrFeatureReader.java
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.logging.Level;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;

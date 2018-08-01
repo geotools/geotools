c8c1d3c310780f48c43384129b27d73eb3c9e07f
Lombardi <steve.lombardi@connexta.com>
2018-07-31 18:15:30 -0700

    [GEOT-6073] When loading or saving a fes:DWithin filter, the units of measure attribute is ignored

--- modules/extension/xsd/xsd-fes/src/main/java/org/geotools/filter/v2_0/bindings/DWithinBinding.java
+++ modules/extension/xsd/xsd-fes/src/main/java/org/geotools/filter/v2_0/bindings/DWithinBinding.java
@@ -18,9 +18,13 @@ package org.geotools.filter.v2_0.bindings;
 
 import javax.xml.namespace.QName;
 import org.geotools.filter.v1_0.OGCDWithinBinding;
+import org.geotools.filter.v1_0.OGCUtils;
 import org.geotools.filter.v2_0.FES;
+import org.geotools.xml.ElementInstance;
+import org.geotools.xml.Node;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.expression.Expression;
 
 /**
  * Binding object for the element http://www.opengis.net/ogc:DWithin.
@@ -39,11 +43,26 @@ import org.opengis.filter.FilterFactory2;
  */
 public class DWithinBinding extends OGCDWithinBinding {
 
+    private final FilterFactory2 localFilterFactory;
+
+    private final GeometryFactory localGeometryFactory;
+
     public DWithinBinding(FilterFactory2 filterFactory, GeometryFactory geometryFactory) {
         super(filterFactory, geometryFactory);
+        this.localFilterFactory = filterFactory;
+        this.localGeometryFactory = geometryFactory;
     }
 
     public QName getTarget() {
         return FES.DWithin;
+    }
+
+    @Override
+    public Object parse(ElementInstance instance, Node node, Object value) throws Exception {
+        Expression[] operands = OGCUtils.spatial(node, localFilterFactory, localGeometryFactory);
+        double distance = ((Double) node.getChildValue("Distance")).doubleValue();
+        Object units = node.getChild("Distance").getAttributeValue("uom");
+        return localFilterFactory.dwithin(
+                operands[0], operands[1], distance, units == null ? null : units.toString());
     }
 }

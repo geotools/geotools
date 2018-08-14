4bb66701ce82a7c0b0df97475d5de0e79c605861
Cesar Martinez Izquierdo <cesar.izq@gmail.com>
2018-08-14 18:24:35 +0200

    [GEOT-6081] ESRI NamedIdentifier is wrong for CassiniSolder and EquidistantCylindrical projections
    * Fix ESRI NamedIdentifier for Cassini and Equidistant Cylindrical projections
    
    * Fix formatting
    
    * Add test case
    
    * Include a test case which provides ESRI authority, which may influence wkt parsing
    
    * Adapt EsriNamedIdentifierAliasesTest to wkt parsing limitations

--- modules/library/referencing/src/main/java/org/geotools/referencing/operation/projection/EquidistantCylindrical.java
+++ modules/library/referencing/src/main/java/org/geotools/referencing/operation/projection/EquidistantCylindrical.java
@@ -197,7 +197,8 @@ public class EquidistantCylindrical extends MapProjection {
                         new NamedIdentifier[] {
                             new NamedIdentifier(Citations.OGC, "Equidistant_Cylindrical"),
                             new NamedIdentifier(Citations.EPSG, "Equidistant Cylindrical"),
-                            new NamedIdentifier(Citations.ESRI, "Equidistant_Cylindrical"),
+                            new NamedIdentifier(
+                                    Citations.ESRI, "Equidistant_Cylindrical_Ellipsoidal"),
                             new NamedIdentifier(Citations.EPSG, "9823"),
                             new NamedIdentifier(
                                     Citations.GEOTOOLS,
@@ -268,6 +269,7 @@ public class EquidistantCylindrical extends MapProjection {
                         new NamedIdentifier[] {
                             new NamedIdentifier(
                                     Citations.EPSG, "Equidistant Cylindrical (Spherical)"),
+                            new NamedIdentifier(Citations.ESRI, "Equidistant_Cylindrical"),
                             new NamedIdentifier(
                                     Citations.GEOTOOLS,
                                     Vocabulary.formatInternational(

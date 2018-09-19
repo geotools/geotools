124f478d09cb8d37ae97e2cb08c5445b9f3ad085
rfecher <rfecher@gmail.com>
2018-09-19 14:25:37 -0400

    [GEOS-8503] geopackage mosaics should use tile matrix set bounds (#1892)
    * geopackage mosaics should use tile matrix set bounds [GEOS-8503]
    
    * fixed error in jts conflict mitigation
    
    * fixed formatting

--- modules/plugin/geopkg/src/main/java/org/geotools/geopkg/mosaic/GeoPackageReader.java
+++ modules/plugin/geopkg/src/main/java/org/geotools/geopkg/mosaic/GeoPackageReader.java
@@ -124,7 +124,7 @@ public class GeoPackageReader extends AbstractGridCoverage2DReader {
                     "The specified coverageName " + coverageName + "is not supported");
         }
 
-        return new GeneralEnvelope(tiles.get(coverageName).getBounds());
+        return new GeneralEnvelope(tiles.get(coverageName).getTileMatrixSetBounds());
     }
 
     @Override

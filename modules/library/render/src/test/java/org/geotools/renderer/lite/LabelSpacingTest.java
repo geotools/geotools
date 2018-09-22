032fc7ca52dbc07f80a21cfd24c4eb3e5a097826
Andrea Aime <andrea.aime@gmail.com>
2018-09-22 16:09:23 +0200

    Use BitStreamVera everywhere, relax tolerances (Linux Mint 19 renders these fonts bolder)

--- modules/library/render/src/test/java/org/geotools/renderer/lite/LabelSpacingTest.java
+++ modules/library/render/src/test/java/org/geotools/renderer/lite/LabelSpacingTest.java
@@ -16,6 +16,7 @@
  */
 package org.geotools.renderer.lite;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import org.geotools.image.test.ImageAssert;
@@ -82,6 +83,6 @@ public class LabelSpacingTest extends AbstractLabelLineTest {
         File reference =
                 new File(
                         "./src/test/resources/org/geotools/renderer/lite/test-data/lineLabelsIncreaseWordSpacing.png");
-        ImageAssert.assertEquals(reference, image, 3500);
+        ImageAssert.assertEquals(reference, image, 3800);
     }
 }
--- modules/library/render/src/test/resources/org/geotools/renderer/lite/test-data/obstacles/hatch.sld
+++ modules/library/render/src/test/resources/org/geotools/renderer/lite/test-data/obstacles/hatch.sld
@@ -35,7 +35,7 @@
           <ogc:PropertyName>name</ogc:PropertyName>
         </sld:Label>
         <sld:Font>
-          <sld:CssParameter name="font-family">Dialog</sld:CssParameter>
+          <sld:CssParameter name="font-family">Bitstream Vera Sans</sld:CssParameter>
           <sld:CssParameter name="font-size">10.0</sld:CssParameter>
           <sld:CssParameter name="font-style">normal</sld:CssParameter>
           <sld:CssParameter name="font-weight">normal</sld:CssParameter>

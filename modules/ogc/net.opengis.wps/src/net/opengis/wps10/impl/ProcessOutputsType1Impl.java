baf43139ae9edba2955149eca68f6a48e531ec96
Ian Turton <ijturton@gmail.com>
2018-10-23 16:11:32 +0100

    EMF Upgrade, wps working

--- modules/ogc/net.opengis.wps/src/net/opengis/wps10/impl/ProcessOutputsType1Impl.java
+++ modules/ogc/net.opengis.wps/src/net/opengis/wps10/impl/ProcessOutputsType1Impl.java
@@ -30,120 +30,120 @@ import org.eclipse.emf.ecore.util.InternalEList;
  * <!-- end-user-doc -->
  * <p>
  * The following features are implemented:
+ * </p>
  * <ul>
  *   <li>{@link net.opengis.wps10.impl.ProcessOutputsType1Impl#getOutput <em>Output</em>}</li>
  * </ul>
- * </p>
  *
  * @generated
  */
 public class ProcessOutputsType1Impl extends EObjectImpl implements ProcessOutputsType1 {
     /**
-     * The cached value of the '{@link #getOutput() <em>Output</em>}' containment reference list.
-     * <!-- begin-user-doc -->
+   * The cached value of the '{@link #getOutput() <em>Output</em>}' containment reference list.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getOutput()
-     * @generated
-     * @ordered
-     */
+   * @see #getOutput()
+   * @generated
+   * @ordered
+   */
     protected EList output;
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     protected ProcessOutputsType1Impl() {
-        super();
-    }
+    super();
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     protected EClass eStaticClass() {
-        return Wps10Package.Literals.PROCESS_OUTPUTS_TYPE1;
-    }
+    return Wps10Package.Literals.PROCESS_OUTPUTS_TYPE1;
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public EList getOutput() {
-        if (output == null) {
-            output = new EObjectContainmentEList(OutputDataType.class, this, Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT);
-        }
-        return output;
+    if (output == null) {
+      output = new EObjectContainmentEList(OutputDataType.class, this, Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT);
     }
+    return output;
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
-        switch (featureID) {
-            case Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT:
-                return ((InternalEList)getOutput()).basicRemove(otherEnd, msgs);
-        }
-        return super.eInverseRemove(otherEnd, featureID, msgs);
+    switch (featureID) {
+      case Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT:
+        return ((InternalEList)getOutput()).basicRemove(otherEnd, msgs);
     }
+    return super.eInverseRemove(otherEnd, featureID, msgs);
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public Object eGet(int featureID, boolean resolve, boolean coreType) {
-        switch (featureID) {
-            case Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT:
-                return getOutput();
-        }
-        return super.eGet(featureID, resolve, coreType);
+    switch (featureID) {
+      case Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT:
+        return getOutput();
     }
+    return super.eGet(featureID, resolve, coreType);
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public void eSet(int featureID, Object newValue) {
-        switch (featureID) {
-            case Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT:
-                getOutput().clear();
-                getOutput().addAll((Collection)newValue);
-                return;
-        }
-        super.eSet(featureID, newValue);
+    switch (featureID) {
+      case Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT:
+        getOutput().clear();
+        getOutput().addAll((Collection)newValue);
+        return;
     }
+    super.eSet(featureID, newValue);
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public void eUnset(int featureID) {
-        switch (featureID) {
-            case Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT:
-                getOutput().clear();
-                return;
-        }
-        super.eUnset(featureID);
+    switch (featureID) {
+      case Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT:
+        getOutput().clear();
+        return;
     }
+    super.eUnset(featureID);
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public boolean eIsSet(int featureID) {
-        switch (featureID) {
-            case Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT:
-                return output != null && !output.isEmpty();
-        }
-        return super.eIsSet(featureID);
+    switch (featureID) {
+      case Wps10Package.PROCESS_OUTPUTS_TYPE1__OUTPUT:
+        return output != null && !output.isEmpty();
     }
+    return super.eIsSet(featureID);
+  }
 
 } //ProcessOutputsType1Impl

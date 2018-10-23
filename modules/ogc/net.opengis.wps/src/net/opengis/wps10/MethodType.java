d1a6b5487611d0adbc116f9e4190150645d406c7
Ian Turton <ijturton@gmail.com>
2018-10-23 16:11:32 +0100

    EMF Upgrade, wps working

--- modules/ogc/net.opengis.wps/src/net/opengis/wps10/MethodType.java
+++ modules/ogc/net.opengis.wps/src/net/opengis/wps10/MethodType.java
@@ -23,129 +23,135 @@ import org.eclipse.emf.common.util.AbstractEnumerator;
  */
 public final class MethodType extends AbstractEnumerator {
     /**
-     * The '<em><b>GET</b></em>' literal value.
-     * <!-- begin-user-doc -->
+   * The '<em><b>GET</b></em>' literal value.
+   * <!-- begin-user-doc -->
      * <p>
      * If the meaning of '<em><b>GET</b></em>' literal object isn't clear,
      * there really should be more of a description here...
      * </p>
      * <!-- end-user-doc -->
-     * @see #GET_LITERAL
-     * @model
-     * @generated
-     * @ordered
-     */
+   * @see #GET_LITERAL
+   * @model
+   * @generated
+   * @ordered
+   */
     public static final int GET = 0;
 
     /**
-     * The '<em><b>POST</b></em>' literal value.
-     * <!-- begin-user-doc -->
+   * The '<em><b>POST</b></em>' literal value.
+   * <!-- begin-user-doc -->
      * <p>
      * If the meaning of '<em><b>POST</b></em>' literal object isn't clear,
      * there really should be more of a description here...
      * </p>
      * <!-- end-user-doc -->
-     * @see #POST_LITERAL
-     * @model
-     * @generated
-     * @ordered
-     */
+   * @see #POST_LITERAL
+   * @model
+   * @generated
+   * @ordered
+   */
     public static final int POST = 1;
 
     /**
-     * The '<em><b>GET</b></em>' literal object.
-     * <!-- begin-user-doc -->
+   * The '<em><b>GET</b></em>' literal object.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #GET
-     * @generated
-     * @ordered
-     */
+   * @see #GET
+   * @generated
+   * @ordered
+   */
     public static final MethodType GET_LITERAL = new MethodType(GET, "GET", "GET");
 
     /**
-     * The '<em><b>POST</b></em>' literal object.
-     * <!-- begin-user-doc -->
+   * The '<em><b>POST</b></em>' literal object.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #POST
-     * @generated
-     * @ordered
-     */
+   * @see #POST
+   * @generated
+   * @ordered
+   */
     public static final MethodType POST_LITERAL = new MethodType(POST, "POST", "POST");
 
     /**
-     * An array of all the '<em><b>Method Type</b></em>' enumerators.
-     * <!-- begin-user-doc -->
+   * An array of all the '<em><b>Method Type</b></em>' enumerators.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     private static final MethodType[] VALUES_ARRAY =
         new MethodType[] {
-            GET_LITERAL,
-            POST_LITERAL,
-        };
+      GET_LITERAL,
+      POST_LITERAL,
+    };
 
     /**
-     * A public read-only list of all the '<em><b>Method Type</b></em>' enumerators.
-     * <!-- begin-user-doc -->
+   * A public read-only list of all the '<em><b>Method Type</b></em>' enumerators.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public static final List VALUES = Collections.unmodifiableList(Arrays.asList(VALUES_ARRAY));
 
     /**
-     * Returns the '<em><b>Method Type</b></em>' literal with the specified literal value.
-     * <!-- begin-user-doc -->
+   * Returns the '<em><b>Method Type</b></em>' literal with the specified literal value.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @param literal the literal.
+   * @return the matching enumerator or <code>null</code>.
+   * @generated
+   */
     public static MethodType get(String literal) {
-        for (int i = 0; i < VALUES_ARRAY.length; ++i) {
-            MethodType result = VALUES_ARRAY[i];
-            if (result.toString().equals(literal)) {
-                return result;
-            }
-        }
-        return null;
+    for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+      MethodType result = VALUES_ARRAY[i];
+      if (result.toString().equals(literal)) {
+        return result;
+      }
     }
+    return null;
+  }
 
     /**
-     * Returns the '<em><b>Method Type</b></em>' literal with the specified name.
-     * <!-- begin-user-doc -->
+   * Returns the '<em><b>Method Type</b></em>' literal with the specified name.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @param name the name.
+   * @return the matching enumerator or <code>null</code>.
+   * @generated
+   */
     public static MethodType getByName(String name) {
-        for (int i = 0; i < VALUES_ARRAY.length; ++i) {
-            MethodType result = VALUES_ARRAY[i];
-            if (result.getName().equals(name)) {
-                return result;
-            }
-        }
-        return null;
+    for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+      MethodType result = VALUES_ARRAY[i];
+      if (result.getName().equals(name)) {
+        return result;
+      }
     }
+    return null;
+  }
 
     /**
-     * Returns the '<em><b>Method Type</b></em>' literal with the specified integer value.
-     * <!-- begin-user-doc -->
+   * Returns the '<em><b>Method Type</b></em>' literal with the specified integer value.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @param value the integer value.
+   * @return the matching enumerator or <code>null</code>.
+   * @generated
+   */
     public static MethodType get(int value) {
-        switch (value) {
-            case GET: return GET_LITERAL;
-            case POST: return POST_LITERAL;
-        }
-        return null;
+    switch (value) {
+      case GET: return GET_LITERAL;
+      case POST: return POST_LITERAL;
     }
+    return null;
+  }
 
     /**
-     * Only this class can construct instances.
-     * <!-- begin-user-doc -->
+   * Only this class can construct instances.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     private MethodType(int value, String name, String literal) {
-        super(value, name, literal);
-    }
+    super(value, name, literal);
+  }
 
 } //MethodType

4d9253bd2f20c0e9c4dd2fd95b15925769ab6e86
Ian Turton <ijturton@gmail.com>
2018-10-23 16:11:32 +0100

    EMF Upgrade, wps working

--- modules/ogc/net.opengis.wps/src/net/opengis/wps10/impl/HeaderTypeImpl.java
+++ modules/ogc/net.opengis.wps/src/net/opengis/wps10/impl/HeaderTypeImpl.java
@@ -22,194 +22,194 @@ import org.eclipse.emf.ecore.impl.EObjectImpl;
  * <!-- end-user-doc -->
  * <p>
  * The following features are implemented:
+ * </p>
  * <ul>
  *   <li>{@link net.opengis.wps10.impl.HeaderTypeImpl#getKey <em>Key</em>}</li>
  *   <li>{@link net.opengis.wps10.impl.HeaderTypeImpl#getValue <em>Value</em>}</li>
  * </ul>
- * </p>
  *
  * @generated
  */
 public class HeaderTypeImpl extends EObjectImpl implements HeaderType {
     /**
-     * The default value of the '{@link #getKey() <em>Key</em>}' attribute.
-     * <!-- begin-user-doc -->
+   * The default value of the '{@link #getKey() <em>Key</em>}' attribute.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getKey()
-     * @generated
-     * @ordered
-     */
+   * @see #getKey()
+   * @generated
+   * @ordered
+   */
     protected static final String KEY_EDEFAULT = null;
 
     /**
-     * The cached value of the '{@link #getKey() <em>Key</em>}' attribute.
-     * <!-- begin-user-doc -->
+   * The cached value of the '{@link #getKey() <em>Key</em>}' attribute.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getKey()
-     * @generated
-     * @ordered
-     */
+   * @see #getKey()
+   * @generated
+   * @ordered
+   */
     protected String key = KEY_EDEFAULT;
 
     /**
-     * The default value of the '{@link #getValue() <em>Value</em>}' attribute.
-     * <!-- begin-user-doc -->
+   * The default value of the '{@link #getValue() <em>Value</em>}' attribute.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getValue()
-     * @generated
-     * @ordered
-     */
+   * @see #getValue()
+   * @generated
+   * @ordered
+   */
     protected static final String VALUE_EDEFAULT = null;
 
     /**
-     * The cached value of the '{@link #getValue() <em>Value</em>}' attribute.
-     * <!-- begin-user-doc -->
+   * The cached value of the '{@link #getValue() <em>Value</em>}' attribute.
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getValue()
-     * @generated
-     * @ordered
-     */
+   * @see #getValue()
+   * @generated
+   * @ordered
+   */
     protected String value = VALUE_EDEFAULT;
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     protected HeaderTypeImpl() {
-        super();
-    }
+    super();
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     protected EClass eStaticClass() {
-        return Wps10Package.Literals.HEADER_TYPE;
-    }
+    return Wps10Package.Literals.HEADER_TYPE;
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public String getKey() {
-        return key;
-    }
+    return key;
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public void setKey(String newKey) {
-        String oldKey = key;
-        key = newKey;
-        if (eNotificationRequired())
-            eNotify(new ENotificationImpl(this, Notification.SET, Wps10Package.HEADER_TYPE__KEY, oldKey, key));
-    }
+    String oldKey = key;
+    key = newKey;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, Wps10Package.HEADER_TYPE__KEY, oldKey, key));
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public String getValue() {
-        return value;
-    }
+    return value;
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public void setValue(String newValue) {
-        String oldValue = value;
-        value = newValue;
-        if (eNotificationRequired())
-            eNotify(new ENotificationImpl(this, Notification.SET, Wps10Package.HEADER_TYPE__VALUE, oldValue, value));
-    }
+    String oldValue = value;
+    value = newValue;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, Wps10Package.HEADER_TYPE__VALUE, oldValue, value));
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public Object eGet(int featureID, boolean resolve, boolean coreType) {
-        switch (featureID) {
-            case Wps10Package.HEADER_TYPE__KEY:
-                return getKey();
-            case Wps10Package.HEADER_TYPE__VALUE:
-                return getValue();
-        }
-        return super.eGet(featureID, resolve, coreType);
+    switch (featureID) {
+      case Wps10Package.HEADER_TYPE__KEY:
+        return getKey();
+      case Wps10Package.HEADER_TYPE__VALUE:
+        return getValue();
     }
+    return super.eGet(featureID, resolve, coreType);
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public void eSet(int featureID, Object newValue) {
-        switch (featureID) {
-            case Wps10Package.HEADER_TYPE__KEY:
-                setKey((String)newValue);
-                return;
-            case Wps10Package.HEADER_TYPE__VALUE:
-                setValue((String)newValue);
-                return;
-        }
-        super.eSet(featureID, newValue);
+    switch (featureID) {
+      case Wps10Package.HEADER_TYPE__KEY:
+        setKey((String)newValue);
+        return;
+      case Wps10Package.HEADER_TYPE__VALUE:
+        setValue((String)newValue);
+        return;
     }
+    super.eSet(featureID, newValue);
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public void eUnset(int featureID) {
-        switch (featureID) {
-            case Wps10Package.HEADER_TYPE__KEY:
-                setKey(KEY_EDEFAULT);
-                return;
-            case Wps10Package.HEADER_TYPE__VALUE:
-                setValue(VALUE_EDEFAULT);
-                return;
-        }
-        super.eUnset(featureID);
+    switch (featureID) {
+      case Wps10Package.HEADER_TYPE__KEY:
+        setKey(KEY_EDEFAULT);
+        return;
+      case Wps10Package.HEADER_TYPE__VALUE:
+        setValue(VALUE_EDEFAULT);
+        return;
     }
+    super.eUnset(featureID);
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public boolean eIsSet(int featureID) {
-        switch (featureID) {
-            case Wps10Package.HEADER_TYPE__KEY:
-                return KEY_EDEFAULT == null ? key != null : !KEY_EDEFAULT.equals(key);
-            case Wps10Package.HEADER_TYPE__VALUE:
-                return VALUE_EDEFAULT == null ? value != null : !VALUE_EDEFAULT.equals(value);
-        }
-        return super.eIsSet(featureID);
+    switch (featureID) {
+      case Wps10Package.HEADER_TYPE__KEY:
+        return KEY_EDEFAULT == null ? key != null : !KEY_EDEFAULT.equals(key);
+      case Wps10Package.HEADER_TYPE__VALUE:
+        return VALUE_EDEFAULT == null ? value != null : !VALUE_EDEFAULT.equals(value);
     }
+    return super.eIsSet(featureID);
+  }
 
     /**
-     * <!-- begin-user-doc -->
+   * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+   * @generated
+   */
     public String toString() {
-        if (eIsProxy()) return super.toString();
+    if (eIsProxy()) return super.toString();
 
-        StringBuffer result = new StringBuffer(super.toString());
-        result.append(" (key: ");
-        result.append(key);
-        result.append(", value: ");
-        result.append(value);
-        result.append(')');
-        return result.toString();
-    }
+    StringBuffer result = new StringBuffer(super.toString());
+    result.append(" (key: ");
+    result.append(key);
+    result.append(", value: ");
+    result.append(value);
+    result.append(')');
+    return result.toString();
+  }
 
 } //HeaderTypeImpl

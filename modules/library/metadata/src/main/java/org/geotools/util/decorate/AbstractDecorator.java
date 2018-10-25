e04352a97814d2abf9491c5494e582c776744679
Jody Garnett <jody.garnett@gmail.com>
2018-10-24 21:50:33 -0700

    restructure gt-api to across gt-main, gt-referencing and gt-metadata to prevent split packages

--- modules/library/metadata/src/main/java/org/geotools/util/decorate/AbstractDecorator.java
+++ modules/library/metadata/src/main/java/org/geotools/util/decorate/AbstractDecorator.java
@@ -14,7 +14,7 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
-package org.geotools.decorate;
+package org.geotools.util.decorate;
 
 import java.io.Serializable;
 

98998f3be8a65d53f67b6067edb536c4da49a15b
Jody Garnett <jody.garnett@gmail.com>
2018-10-24 21:50:33 -0700

    restructure gt-api to across gt-main, gt-referencing and gt-metadata to prevent split packages

--- modules/library/metadata/src/test/java/org/geotools/util/decorate/AbstractDecoratorTest.java
+++ modules/library/metadata/src/test/java/org/geotools/util/decorate/AbstractDecoratorTest.java
@@ -14,7 +14,7 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
-package org.geotools.decorate;
+package org.geotools.util.decorate;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;

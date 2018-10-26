35782e34f498760cdcd61e436bc1f939f5b80f23
Jim Hughes <jnh5y@ccri.com>
2018-10-26 17:12:50 -0400

    Step 4.
    Signed-off-by: Jim Hughes <jnh5y@ccri.com>

--- modules/extension/wms/src/test/java/org/geotools/ows/wms/xml/test/WMSSchema_StyleAbstractTest.java
+++ modules/extension/wms/src/test/java/org/geotools/ows/wms/xml/test/WMSSchema_StyleAbstractTest.java
@@ -14,7 +14,7 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
-package org.geotools.data.wms.xml.test;
+package org.geotools.ows.wms.xml.test;
 
 import java.io.File;
 import java.net.URL;

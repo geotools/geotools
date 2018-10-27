c4248a3c7d0dfd22b132ea69eeb3e419057da111
Jim Hughes <jnh5y@ccri.com>
2018-10-26 20:38:19 -0400

    Moved xsd-filter.
    Signed-off-by: Jim Hughes <jnh5y@ccri.com>

--- modules/extension/xsd/xsd-filter/src/main/java/org/geotools/filter/v1_0/OGCFilterTypeBinding.java
+++ modules/extension/xsd/xsd-filter/src/main/java/org/geotools/filter/v1_0/OGCFilterTypeBinding.java
@@ -18,10 +18,10 @@ package org.geotools.filter.v1_0;
 
 import java.util.HashSet;
 import javax.xml.namespace.QName;
-import org.geotools.filter.FilterParsingUtils;
 import org.geotools.xml.AbstractComplexBinding;
 import org.geotools.xml.ElementInstance;
 import org.geotools.xml.Node;
+import org.geotools.xsd.filter.FilterParsingUtils;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
 import org.picocontainer.MutablePicoContainer;
--- modules/extension/xsd/xsd-filter/src/main/java/org/geotools/filter/v1_0/OGCOrBinding.java
+++ modules/extension/xsd/xsd-filter/src/main/java/org/geotools/filter/v1_0/OGCOrBinding.java
@@ -18,10 +18,10 @@ package org.geotools.filter.v1_0;
 
 import java.util.List;
 import javax.xml.namespace.QName;
-import org.geotools.filter.FilterParsingUtils;
 import org.geotools.xml.AbstractComplexBinding;
 import org.geotools.xml.ElementInstance;
 import org.geotools.xml.Node;
+import org.geotools.xsd.filter.FilterParsingUtils;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory;
 import org.opengis.filter.Or;

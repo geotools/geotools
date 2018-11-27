7eeb42c7c06d8fc00538b6527fcdac2e383201fb
Nuno Oliveira <nuno.oliveira@geo-solutions.it>
2018-11-27 21:29:43 +0000

    [GEOT-6182] Add the possibility to specify the target CRS for the ReprojectingFilterVisitor

--- modules/library/main/src/main/java/org/geotools/filter/spatial/ReprojectingFilterVisitor.java
+++ modules/library/main/src/main/java/org/geotools/filter/spatial/ReprojectingFilterVisitor.java
@@ -63,9 +63,17 @@ import org.opengis.referencing.operation.TransformException;
 public class ReprojectingFilterVisitor extends DuplicatingFilterVisitor {
     FeatureType featureType;
 
+    private final CoordinateReferenceSystem targetCrs;
+
     public ReprojectingFilterVisitor(FilterFactory2 factory, FeatureType featureType) {
+        this(factory, featureType, null);
+    }
+
+    public ReprojectingFilterVisitor(
+            FilterFactory2 factory, FeatureType featureType, CoordinateReferenceSystem targetCrs) {
         super(factory);
         this.featureType = featureType;
+        this.targetCrs = targetCrs;
     }
 
     /**
@@ -76,6 +84,11 @@ public class ReprojectingFilterVisitor extends DuplicatingFilterVisitor {
      * @return
      */
     private CoordinateReferenceSystem findPropertyCRS(PropertyName propertyName) {
+        if (targetCrs != null) {
+            // an explicit target CRS was provided
+            return targetCrs;
+        }
+        // let's try to get the CRS from the provided property name
         Object o = propertyName.evaluate(featureType);
         if (o instanceof GeometryDescriptor) {
             GeometryDescriptor gat = (GeometryDescriptor) o;

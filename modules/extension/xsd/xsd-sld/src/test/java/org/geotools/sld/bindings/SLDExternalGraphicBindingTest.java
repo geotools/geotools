770d979c5218f988a4435b1d53f7502dddf07b34
Andrea Aime <andrea.aime@gmail.com>
2019-01-28 11:40:11 +0100

    [GEOT-6221] SLDExternalGraphicBinding does not allow external graphics references with custom protocols

--- modules/extension/xsd/xsd-sld/src/test/java/org/geotools/sld/bindings/SLDExternalGraphicBindingTest.java
+++ modules/extension/xsd/xsd-sld/src/test/java/org/geotools/sld/bindings/SLDExternalGraphicBindingTest.java
@@ -44,4 +44,23 @@ public class SLDExternalGraphicBindingTest extends SLDTestSupport {
         assertEquals(getClass().getResource("eclipse.png"), externalGraphic.getLocation());
         assertEquals("image/png", externalGraphic.getFormat());
     }
+
+    public void testUnknownProtocol() throws Exception {
+        document.appendChild(document.createElementNS(SLD.NAMESPACE, "ExternalGraphic"));
+
+        Element r = document.createElementNS(SLD.NAMESPACE, "OnlineResource");
+        r.setAttributeNS(XLINK.NAMESPACE, "href", "resource:/foo/bar/test.png");
+
+        Element f = document.createElementNS(SLD.NAMESPACE, "Format");
+        f.appendChild(document.createTextNode("image/png"));
+
+        document.getDocumentElement().appendChild(r);
+        document.getDocumentElement().appendChild(f);
+
+        ExternalGraphic externalGraphic = (ExternalGraphic) parse();
+        assertNotNull(externalGraphic);
+
+        assertEquals("resource:/foo/bar/test.png", externalGraphic.getURI());
+        assertEquals("image/png", externalGraphic.getFormat());
+    }
 }

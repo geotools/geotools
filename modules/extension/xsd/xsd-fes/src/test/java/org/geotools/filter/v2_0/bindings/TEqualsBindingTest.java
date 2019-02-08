4c3df5ae71882ebb48fb1d8e7481963d58ef4ab9
Fernando Mino <fernandomino7@gmail.com>
2019-02-08 12:35:08 -0500

    [GEOT-6092] Fix for Match action using uppercase names on encoding.

--- modules/extension/xsd/xsd-fes/src/test/java/org/geotools/filter/v2_0/bindings/TEqualsBindingTest.java
+++ modules/extension/xsd/xsd-fes/src/test/java/org/geotools/filter/v2_0/bindings/TEqualsBindingTest.java
@@ -1,10 +1,23 @@
 package org.geotools.filter.v2_0.bindings;
 
+import java.util.Iterator;
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+import org.geotools.filter.FilterFactoryImpl;
+import org.geotools.filter.v2_0.FES;
 import org.geotools.filter.v2_0.FESTestSupport;
+import org.geotools.xsd.Configuration;
+import org.geotools.xsd.Encoder;
+import org.junit.Test;
+import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.MultiValuedFilter.MatchAction;
 import org.opengis.filter.expression.Literal;
 import org.opengis.filter.expression.PropertyName;
 import org.opengis.filter.temporal.TEquals;
 import org.opengis.temporal.Period;
+import org.w3c.dom.Document;
 
 public class TEqualsBindingTest extends FESTestSupport {
 
@@ -44,5 +57,45 @@ public class TEqualsBindingTest extends FESTestSupport {
 
         assertTrue(equals.getExpression2() instanceof Literal);
         assertTrue(equals.getExpression2().evaluate(null) instanceof Period);
+    }
+
+    /**
+     * Test for checking correct "Any" match action instead "ANY". <br>
+     * See issue GEOT-6092
+     */
+    @Test
+    public void testPropertyEqualsMatchEncoding() throws Exception {
+        FilterFactory2 ff = new FilterFactoryImpl();
+        Filter filter = ff.equal(ff.property("prop"), ff.literal("abc"), true, MatchAction.ANY);
+        Configuration configuration = new org.geotools.filter.v2_0.FESConfiguration();
+        Encoder encoder = new Encoder(configuration);
+        encoder.setIndenting(true);
+        Document encodedDoc = encoder.encodeAsDOM(filter, FES.Filter);
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        defaultNamespaceContext(xpath);
+        String matchAction =
+                xpath.evaluate("/fes:Filter/fes:PropertyIsEqualTo/@matchAction", encodedDoc);
+        assertEquals("Any", matchAction);
+    }
+
+    private void defaultNamespaceContext(XPath xpath) {
+        xpath.setNamespaceContext(
+                new NamespaceContext() {
+                    @Override
+                    public Iterator getPrefixes(String namespaceURI) {
+                        return null;
+                    }
+
+                    @Override
+                    public String getPrefix(String namespaceURI) {
+                        return null;
+                    }
+
+                    @Override
+                    public String getNamespaceURI(String prefix) {
+                        if ("fes".equals(prefix)) return "http://www.opengis.net/fes/2.0";
+                        return null;
+                    }
+                });
     }
 }

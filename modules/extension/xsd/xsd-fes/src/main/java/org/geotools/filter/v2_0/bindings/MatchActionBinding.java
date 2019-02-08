21229a082ab3d110c307667c30877d497d859925
Fernando Mino <fernandomino7@gmail.com>
2019-02-08 12:35:08 -0500

    [GEOT-6092] Fix for Match action using uppercase names on encoding.

---
+++ modules/extension/xsd/xsd-fes/src/main/java/org/geotools/filter/v2_0/bindings/MatchActionBinding.java
@@ -0,0 +1,20 @@
+package org.geotools.filter.v2_0.bindings;
+
+import org.apache.commons.lang3.StringUtils;
+import org.geotools.filter.v2_0.FES;
+import org.geotools.xsd.EnumSimpleBinding;
+import org.opengis.filter.MultiValuedFilter.MatchAction;
+
+/** Binding for encoding {@link MatchAction} enum values. */
+public class MatchActionBinding extends EnumSimpleBinding {
+
+    public MatchActionBinding() {
+        super(MatchAction.class, FES.MatchActionType);
+    }
+
+    @Override
+    public String encode(Object object, String value) throws Exception {
+        if (StringUtils.isBlank(value)) return value;
+        return StringUtils.capitalize(value.toLowerCase());
+    }
+}

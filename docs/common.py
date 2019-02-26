09adca385a7952446269b2af313a091cf56cdf21
Jody Garnett <jody.garnett@gmail.com>
2019-02-25 18:59:24 -0800

    Ensure docs reference 21-SNAPSHOT
    Testing rename.xml script earlier, now reverting its success.

--- docs/common.py
+++ docs/common.py
@@ -65,7 +65,7 @@ copyright = u'{} Open Source Geospatial Foundation'.format(year)
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '21.0'
+release = '21-SNAPSHOT'
 
 # The short X.Y version.
 version = release

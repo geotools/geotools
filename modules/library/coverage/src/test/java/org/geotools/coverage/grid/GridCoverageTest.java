3142f6368228148f961640a900cace1f176e7fb2
Imran <rajjad@gmail.com>
2019-02-28 21:03:37 +0500

    [GEOT-6088] - Subsampling enabled irrespective of OverViews for grids

--- modules/library/coverage/src/test/java/org/geotools/coverage/grid/GridCoverageTest.java
+++ modules/library/coverage/src/test/java/org/geotools/coverage/grid/GridCoverageTest.java
@@ -18,11 +18,15 @@ package org.geotools.coverage.grid;
 
 import static org.junit.Assert.*;
 
+import java.awt.Rectangle;
 import java.io.IOException;
 import java.net.InetAddress;
+import javax.imageio.ImageReadParam;
+import org.geotools.geometry.GeneralEnvelope;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.junit.*;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.TransformException;
 
 /**
  * Tests the {@link GridCoverage2D} implementation.
@@ -71,5 +75,41 @@ public final class GridCoverageTest extends GridCoverageTestBase {
             assertEquals(GridCoverage2D.class, serial.getClass());
             assertRasterEquals(coverage, serial);
         }
+    }
+
+    @Test
+    public void testSubSampling() throws IllegalArgumentException, IOException, TransformException {
+
+        GridCoverage2D coverage = EXAMPLES.get(2);
+
+        MockAbstractGridCoverage2DReader reader = new MockAbstractGridCoverage2DReader();
+
+        reader.setOetOriginalGridRange(new GridEnvelope2D(new Rectangle(700, 400)));
+        reader.setHighestResolution(0.5, 0.5);
+        reader.setCRS(coverage.getCoordinateReferenceSystem());
+        ImageReadParam readP = new ImageReadParam();
+        Rectangle requestedDim = new Rectangle(700, 400);
+
+        // TEST WHEN NO SUBSAMPLING IS REQUIRED
+        GeneralEnvelope requestedEnvelope = coverage.gridGeometry.envelope;
+        // requestedEnvelope.setEnvelope(-517.2704081632651, -353.2270408163266, 697.7295918367349,
+        // 350.3571428571428);
+        reader.setReadParams("geotools_coverage", null, readP, requestedEnvelope, requestedDim);
+        assertNotNull(readP);
+        // 1 means no subsampling and no pixel skipping
+        assertTrue(readP.getSourceXSubsampling() == 1);
+        assertTrue(readP.getSourceYSubsampling() == 1);
+
+        // MOCK ZOOMOUT TO FORCE SUBSAMPLING
+        GeneralEnvelope requestedEnvelopeZoomOut = coverage.gridGeometry.envelope.clone();
+        // select much larger geographical area for same screen size
+        requestedEnvelopeZoomOut.setEnvelope(
+                -517.2704081632651, -353.2270408163266, 697.7295918367349, 350.3571428571428);
+        reader.setReadParams(
+                "geotools_coverage", null, readP, requestedEnvelopeZoomOut, requestedDim);
+        assertNotNull(readP);
+        // above 1 means do sub-sampling
+        assertTrue(readP.getSourceXSubsampling() > 1);
+        assertTrue(readP.getSourceYSubsampling() > 1);
     }
 }

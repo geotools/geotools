db04a836afe34678fe123542eff4c7dfe28b73fa
Luca Morandini <lmorandini@ieee.org>
2019-04-04 08:22:27 +1100

    [AUR-5856] Geoserver ArcGIS datastore Fails to Recognize CRS - Re-generated JSON schema classes

--- modules/unsupported/arcgis-rest/src/main/java/org/geotools/data/arcgisrest/schema/webservice/Renderer.java
+++ modules/unsupported/arcgis-rest/src/main/java/org/geotools/data/arcgisrest/schema/webservice/Renderer.java
@@ -1,3 +1,4 @@
+
 package org.geotools.data.arcgisrest.schema.webservice;
 
 import com.google.gson.annotations.Expose;
@@ -5,31 +6,55 @@ import com.google.gson.annotations.SerializedName;
 
 public class Renderer {
 
-    /** (Required) */
+    /**
+     * 
+     * (Required)
+     * 
+     */
     @SerializedName("type")
     @Expose
     private String type;
-    /** (Required) */
+    /**
+     * 
+     * (Required)
+     * 
+     */
     @SerializedName("symbol")
     @Expose
     private Symbol symbol;
 
-    /** (Required) */
+    /**
+     * 
+     * (Required)
+     * 
+     */
     public String getType() {
         return type;
     }
 
-    /** (Required) */
+    /**
+     * 
+     * (Required)
+     * 
+     */
     public void setType(String type) {
         this.type = type;
     }
 
-    /** (Required) */
+    /**
+     * 
+     * (Required)
+     * 
+     */
     public Symbol getSymbol() {
         return symbol;
     }
 
-    /** (Required) */
+    /**
+     * 
+     * (Required)
+     * 
+     */
     public void setSymbol(Symbol symbol) {
         this.symbol = symbol;
     }
@@ -37,20 +62,17 @@ public class Renderer {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(Renderer.class.getName())
-                .append('@')
-                .append(Integer.toHexString(System.identityHashCode(this)))
-                .append('[');
+        sb.append(Renderer.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
         sb.append("type");
         sb.append('=');
-        sb.append(((this.type == null) ? "<null>" : this.type));
+        sb.append(((this.type == null)?"<null>":this.type));
         sb.append(',');
         sb.append("symbol");
         sb.append('=');
-        sb.append(((this.symbol == null) ? "<null>" : this.symbol));
+        sb.append(((this.symbol == null)?"<null>":this.symbol));
         sb.append(',');
-        if (sb.charAt((sb.length() - 1)) == ',') {
-            sb.setCharAt((sb.length() - 1), ']');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
         } else {
             sb.append(']');
         }
@@ -60,8 +82,8 @@ public class Renderer {
     @Override
     public int hashCode() {
         int result = 1;
-        result = ((result * 31) + ((this.type == null) ? 0 : this.type.hashCode()));
-        result = ((result * 31) + ((this.symbol == null) ? 0 : this.symbol.hashCode()));
+        result = ((result* 31)+((this.type == null)? 0 :this.type.hashCode()));
+        result = ((result* 31)+((this.symbol == null)? 0 :this.symbol.hashCode()));
         return result;
     }
 
@@ -74,8 +96,7 @@ public class Renderer {
             return false;
         }
         Renderer rhs = ((Renderer) other);
-        return (((this.type == rhs.type) || ((this.type != null) && this.type.equals(rhs.type)))
-                && ((this.symbol == rhs.symbol)
-                        || ((this.symbol != null) && this.symbol.equals(rhs.symbol))));
+        return (((this.type == rhs.type)||((this.type!= null)&&this.type.equals(rhs.type)))&&((this.symbol == rhs.symbol)||((this.symbol!= null)&&this.symbol.equals(rhs.symbol))));
     }
+
 }

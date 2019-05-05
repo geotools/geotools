c4f363d196b5e7729f405ddac2817ce1a4907fd2
chosenegbe <milkymike45@gmail.com>
2019-05-05 09:51:05 +0200

    math filter function test cases for null value added (#2360)
    [GEOT-6079] Math filter functions are not null safe

--- modules/library/main/src/main/java/org/geotools/filter/function/math/FilterFunction_abs.java
+++ modules/library/main/src/main/java/org/geotools/filter/function/math/FilterFunction_abs.java
@@ -23,6 +23,8 @@ import static org.geotools.filter.capability.FunctionNameImpl.parameter;
 
 import org.geotools.filter.FunctionExpressionImpl;
 import org.geotools.filter.capability.FunctionNameImpl;
+import org.geotools.util.Converters;
+import org.geotools.util.factory.Hints;
 import org.opengis.filter.capability.FunctionName;
 
 /** @source $URL$ */
@@ -37,16 +39,17 @@ public class FilterFunction_abs extends FunctionExpressionImpl {
     }
 
     public Object evaluate(Object feature) {
-        int arg0;
+        Object arg0 = getExpression(0).evaluate(feature);
+        if (arg0 == null) {
+            return null;
+        }
 
-        try { // attempt to get value and perform conversion
-            arg0 = (getExpression(0).evaluate(feature, Integer.class)).intValue();
-        } catch (Exception e) {
-            // probably a type error
+        arg0 = Converters.convert(arg0, Integer.class, new Hints());
+        if (arg0 == null) {
             throw new IllegalArgumentException(
                     "Filter Function problem for function abs argument #0 - expected type int");
         }
+        return Math.abs((Integer) arg0);
 
-        return new Integer(Math.abs(arg0));
     }
 }

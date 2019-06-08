dc2d28099fc7604b37537b808eb85ca96a0f1105
Andrea Aime <andrea.aime@gmail.com>
2019-06-08 15:41:43 +0200

    [GEOT-6315] Prepared statement BBOX filter encoding fails on 22.x

--- modules/plugin/jdbc/jdbc-oracle/src/test/java/org/geotools/data/oracle/OracleFilterToSqlTest.java
+++ modules/plugin/jdbc/jdbc-oracle/src/test/java/org/geotools/data/oracle/OracleFilterToSqlTest.java
@@ -53,7 +53,7 @@ public class OracleFilterToSqlTest extends TestCase {
     }
 
     public void testBboxFilter() throws Exception {
-        BBOX bbox = ff.bbox("GEOM", -180, -90, 180, 90, "ESPG:4326");
+        BBOX bbox = ff.bbox("GEOM", -180, -90, 180, 90, "EPSG:4326");
         String encoded = encoder.encodeToString(bbox);
         assertEquals(
                 "WHERE SDO_RELATE(\"GEOM\", ?, 'mask=anyinteract querytype=WINDOW') = 'TRUE' ",
@@ -61,7 +61,7 @@ public class OracleFilterToSqlTest extends TestCase {
     }
 
     public void testLooseBboxFilter() throws Exception {
-        BBOX bbox = ff.bbox("GEOM", -180, -90, 180, 90, "ESPG:4326");
+        BBOX bbox = ff.bbox("GEOM", -180, -90, 180, 90, "EPSG:4326");
         encoder.setLooseBBOXEnabled(true);
         String encoded = encoder.encodeToString(bbox);
         assertEquals(

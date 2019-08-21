e629af821800023b13c52b5030074dd4d331013e
Torben Barsballe <tbarsballe@federal.planet.com>
2019-08-21 16:50:05 -0700

    Update doc links and README to point to stable

--- docs/common.py
+++ docs/common.py
@@ -30,8 +30,8 @@ extlinks = {
     'wiki': ('https://github.com/geotools/geotools/wiki/%s',''),
     'website': ('http://geotools.org/%s',''),
     'geoserver': ('http://docs.geoserver.org/latest/en/user/%s',''),
-    'developer': ('http://docs.geotools.org/latest/developer/%s',''),
-    'user': ('http://docs.geotools.org/latest/userguide/%s',''),
+    'developer': ('http://docs.geotools.org/stable/developer/%s',''),
+    'user': ('http://docs.geotools.org/stable/userguide/%s',''),
     'geot': ('https://osgeo-org.atlassian.net/browse/GEOT-%s','GEOT-')
 }
 

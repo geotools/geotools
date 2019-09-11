23579a3ce3a1d76f7caecd3a02c0f42349aa65c7
Ian Turton <ijturton@gmail.com>
2019-09-11 09:48:25 +0100

    Documentation fixes (#2554)
    * update function list to new format
    
    * remove spell checker until we can add it to build server
    
    * remove std err from list

--- docs/common.py
+++ docs/common.py
@@ -24,7 +24,7 @@ import datetime
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo','sphinx.ext.extlinks', 'sphinxcontrib.spelling']
+extensions = ['sphinx.ext.todo','sphinx.ext.extlinks']
 
 extlinks = { 
     'wiki': ('https://github.com/geotools/geotools/wiki/%s',''),

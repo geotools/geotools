648dda215d0ec25671eabc119de4d2be80a992fd
devops@boundlessgeo.com <Boundless Devops>
2020-01-20 16:49:15 +0000

    updating version numbers and README for 22.2

--- docs/common.py
+++ docs/common.py
@@ -65,7 +65,7 @@ copyright = u'{} Open Source Geospatial Foundation'.format(year)
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '22-SNAPSHOT'
+release = '22.2'
 
 # The short X.Y version.
 version = release

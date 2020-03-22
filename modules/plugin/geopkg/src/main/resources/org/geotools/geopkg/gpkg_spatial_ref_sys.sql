f2642212d9e17f296f7b80c00fc711c37b18ecbb
Andrea Aime <andrea.aime@gmail.com>
2020-03-22 14:44:27 +0100

    [GEOT-6519] GeoPackage store does not support generic cartesian and geographic crs (requirement 11)

--- modules/plugin/geopkg/src/main/resources/org/geotools/geopkg/gpkg_spatial_ref_sys.sql
+++ modules/plugin/geopkg/src/main/resources/org/geotools/geopkg/gpkg_spatial_ref_sys.sql
@@ -6,4 +6,6 @@ CREATE TABLE IF NOT EXISTS gpkg_spatial_ref_sys (
   definition  TEXT NOT NULL,
   description TEXT
 );
-
+INSERT OR IGNORE INTO gpkg_spatial_ref_sys VALUES('WGS84', 4326, 'EPSG', 4326, 'GEOGCS["WGS 84", DATUM["World Geodetic System 1984", SPHEROID["WGS 84", 6378137.0, 298.257223563, AUTHORITY["EPSG","7030"]], AUTHORITY["EPSG","6326"]], PRIMEM["Greenwich", 0.0, AUTHORITY["EPSG","8901"]], UNIT["degree", 0.017453292519943295], AXIS["Geodetic longitude", EAST], AXIS["Geodetic latitude", NORTH], AUTHORITY["EPSG","4326"]]', 'longitude/latitude coordinates in decimal degrees on the WGS 84 spheroid');
+INSERT OR IGNORE INTO gpkg_spatial_ref_sys VALUES('Undefined cartesian SRS', -1, 'NONE', -1, 'undefined', 'undefined cartesian coordinate reference system');
+INSERT OR IGNORE INTO gpkg_spatial_ref_sys VALUES('Undefined cartesian SRS', 0, 'NONE', 0, 'undefined', 'undefined geographic coordinate reference system');

8de0337fc973b6781ec0860d3a1981d0922d7152
Jody Garnett <jody.garnett@gmail.com>
2020-04-10 01:05:02 -0700

    fix jarCollector warning (#2870)
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- build/maven/jar-collector/src/main/java/org/geotools/maven/JarCollector.java
+++ build/maven/jar-collector/src/main/java/org/geotools/maven/JarCollector.java
@@ -48,7 +48,7 @@ public class JarCollector extends AbstractMojo {
     /**
      * Directory containing the generated JAR.
      *
-     * @parameter expression="${project.build.directory}"
+     * @parameter property="project.build.directory"
      * @required
      */
     private String outputDirectory;
@@ -56,7 +56,7 @@ public class JarCollector extends AbstractMojo {
     /**
      * Name of the generated JAR.
      *
-     * @parameter expression="${project.build.finalName}"
+     * @parameter property="project.build.finalName"
      * @required
      */
     private String jarName;
@@ -64,7 +64,7 @@ public class JarCollector extends AbstractMojo {
     /**
      * The Maven project running this plugin.
      *
-     * @parameter expression="${project}"
+     * @parameter property="project"
      * @required
      */
     private MavenProject project;

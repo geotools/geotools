d399147a43e97606029d48730dd72fa768c81d4b
taba90 <46672505+taba90@users.noreply.github.com>
2020-04-14 10:10:33 +0200

    [GEOT-6476] MongoDb store wrongly return false when supports sorting method is issued (#2865)

--- modules/plugin/mongodb/src/main/java/org/geotools/data/mongodb/MongoFeatureSource.java
+++ modules/plugin/mongodb/src/main/java/org/geotools/data/mongodb/MongoFeatureSource.java
@@ -26,11 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.geotools.data.DataUtilities;
-import org.geotools.data.FeatureReader;
-import org.geotools.data.FilteringFeatureReader;
-import org.geotools.data.Query;
-import org.geotools.data.ReTypeFeatureReader;
+import org.geotools.data.*;
 import org.geotools.data.mongodb.complex.JsonSelectAllFunction;
 import org.geotools.data.mongodb.complex.JsonSelectFunction;
 import org.geotools.data.store.ContentEntry;
@@ -387,5 +383,11 @@ public class MongoFeatureSource extends ContentFeatureSource {
                 };
         f.accept(splitter, null);
         return new Filter[] {splitter.getFilterPre(), splitter.getFilterPost()};
+    }
+
+    @Override
+    protected QueryCapabilities buildQueryCapabilities() {
+        if (queryCapabilities == null) return new MongoQueryCapabilities(this);
+        else return queryCapabilities;
     }
 }

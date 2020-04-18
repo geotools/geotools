07699ab922af5b431642308a9c560e610c6a40ad
Andrea Aime <andrea.aime@gmail.com>
2020-04-18 15:16:09 +0200

    [GEOT-6553] NPE in StreamingRenderer if dynamic densification is enabled on a projection without ProjectionHandler

--- modules/library/render/src/main/java/org/geotools/renderer/lite/StreamingRenderer.java
+++ modules/library/render/src/main/java/org/geotools/renderer/lite/StreamingRenderer.java
@@ -1387,13 +1387,17 @@ public class StreamingRenderer implements GTRenderer {
                 sourceToScreen.inverse(), screen, generalizationDistance);
     }
 
-    private ReferencedEnvelope transformEnvelope(
+    protected ReferencedEnvelope transformEnvelope(
             ReferencedEnvelope envelope, CoordinateReferenceSystem crs)
             throws TransformException, FactoryException {
         try {
             ProjectionHandler projectionHandler =
                     ProjectionHandlerFinder.getHandler(envelope, crs, isMapWrappingEnabled());
-            return projectionHandler.getProjectedEnvelope(envelope, crs);
+            if (projectionHandler != null) {
+                return projectionHandler.getProjectedEnvelope(envelope, crs);
+            } else {
+                return envelope.transform(crs, true);
+            }
         } catch (FactoryException e) {
             return envelope.transform(crs, true);
         }

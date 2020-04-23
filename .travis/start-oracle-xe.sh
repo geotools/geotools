351e238b3405beb2b5f01aa1e64ca46a3659cbc6
Mark Prins <1165786+mprins@users.noreply.github.com>
2020-04-23 17:35:03 +0200

    disable OracleAggregateFunctionOnlineTest#testStoreChecksVisitorLimits as it doesn't work, see new comments in the file

--- .travis/start-oracle-xe.sh
+++ .travis/start-oracle-xe.sh
@@ -1,29 +1,30 @@
 #!/bin/bash -e
 docker version
 
-# image has the following users/credentials (user/password)
-# system / oracle
+# this docker image has the following users/credentials (user/password = system/oracle)
 docker pull pvargacl/oracle-xe-18.4.0:latest
 
-# start the dockerized oracle-xe instance (that will be removed on stop)
-# this instance can be stopped using: docker stop geotools
+# start the dockerized oracle-xe instance (the container will be destroyed/removed on stopping)
+# this container can be stopped using: docker stop geotools
 docker run --rm -p 1521:1521 --name geotools -h geotools -d pvargacl/oracle-xe-18.4.0:latest
 
-docker logs geotools
+# print logs
+# docker logs geotools
 
-printf "\n\nWaiting for Oracle XE database to initialize.... "
+printf "\n\nStarting Oracle XE container, this could take a few minutes..."
+printf "\nWaiting for Oracle XE database to start up.... "
 _WAIT=0;
 while :
 do
     printf " $_WAIT"
     if $(docker logs geotools | grep -q 'DATABASE IS READY TO USE!'); then
-        printf "\n\nOracle XE Database initialized\n\n"
+        printf "\nOracle XE Database started\n\n"
         break
     fi
     sleep 10
     _WAIT=$(($_WAIT+10))
 done
 
-#check logs again
-docker ps -a
+# docker ps -a
+# print logs
 docker logs geotools

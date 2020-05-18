1285eb7725da5ac93b53dca480684db88be59f9e
eropartz <49937952+eropartz@users.noreply.github.com>
2020-05-18 17:00:40 +0200

    [GEOT-6588] : SortBy element not encoded with FESConfiguration (#2928)
    * [GEOT-6588] : SortBy element not encoded with FESConfiguration

--- modules/extension/xsd/xsd-wfs/src/test/java/org/geotools/wfs/v2_0/bindings/WfsQueryTypeTest.java
+++ modules/extension/xsd/xsd-wfs/src/test/java/org/geotools/wfs/v2_0/bindings/WfsQueryTypeTest.java
@@ -17,11 +17,19 @@
 
 package org.geotools.wfs.v2_0.bindings;
 
+import java.util.HashMap;
 import javax.xml.namespace.QName;
 import net.opengis.wfs20.QueryType;
 import net.opengis.wfs20.Wfs20Factory;
+import org.custommonkey.xmlunit.SimpleNamespaceContext;
+import org.custommonkey.xmlunit.XMLAssert;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.filter.v2_0.FES;
 import org.geotools.wfs.v2_0.WFS;
 import org.geotools.wfs.v2_0.WFSTestSupport;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.sort.SortOrder;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -32,7 +40,11 @@ public class WfsQueryTypeTest extends WFSTestSupport {
         QueryType query = Wfs20Factory.eINSTANCE.createQueryType();
         query.getTypeNames().add(typeName);
 
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
+        query.getSortBy().add(ff.sort("myProperty", SortOrder.ASCENDING));
+
         Document doc = encode(query, WFS.Query);
+
         Element root = doc.getDocumentElement();
         String attr = root.getAttribute("typeNames");
         assertNotNull(attr);
@@ -42,5 +54,19 @@ public class WfsQueryTypeTest extends WFSTestSupport {
         assertFalse(attr.startsWith("[{"));
         assertTrue(attr.indexOf(tmp) != -1);
         assertEquals(attr.length(), attr.indexOf(tmp) + tmp.length()); // 8 == ":theType".length
+
+        HashMap<String, String> m = new HashMap<String, String>();
+        m.put("wfs", WFS.NAMESPACE);
+        m.put("fes", FES.NAMESPACE);
+        XMLUnit.setXpathNamespaceContext(new SimpleNamespaceContext(m));
+        XMLAssert.assertXpathExists("//wfs:Query", doc);
+        XMLAssert.assertXpathExists("//wfs:Query/fes:SortBy", doc);
+        XMLAssert.assertXpathExists("//wfs:Query/fes:SortBy/fes:SortProperty", doc);
+        XMLAssert.assertXpathExists(
+                "//wfs:Query/fes:SortBy/fes:SortProperty/fes:ValueReference", doc);
+        XMLAssert.assertXpathEvaluatesTo(
+                "myProperty", "//wfs:Query/fes:SortBy/fes:SortProperty/fes:ValueReference", doc);
+        XMLAssert.assertXpathEvaluatesTo(
+                "ASC", "//wfs:Query/fes:SortBy/fes:SortProperty/fes:SortOrder", doc);
     }
 }

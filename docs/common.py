a68c2ab916bf692bcab4f598c2df9d12497d6974
randomorder <alessa.parma@gmail.com>
2020-05-19 14:11:55 +0000

    updating version numbers and README for 22.3

--- docs/common.py
+++ docs/common.py
@@ -65,7 +65,7 @@ copyright = u'{} Open Source Geospatial Foundation'.format(year)
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '22-SNAPSHOT'
+release = '22.3'
 
 # The short X.Y version.
 version = release

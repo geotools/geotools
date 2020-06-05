3f39f0faae37be3686a72d50843ccbc3fc4f84f2
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2020-06-05 18:30:11 +0200

    [GEOT-6616]: mapGet FilterFunction to get a value from a map given the mapName.key identifier

---
+++ modules/library/main/src/main/java/org/geotools/filter/function/MapGetFunction.java
@@ -0,0 +1,65 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2020, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.filter.function;
+
+import static org.geotools.filter.capability.FunctionNameImpl.parameter;
+
+import java.util.Map;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.filter.FunctionExpressionImpl;
+import org.geotools.filter.capability.FunctionNameImpl;
+import org.geotools.util.Converters;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.capability.FunctionName;
+import org.opengis.filter.expression.VolatileFunction;
+
+/** Extracts a value from a map given the map name and the needed key as parameters */
+public class MapGetFunction extends FunctionExpressionImpl implements VolatileFunction {
+
+    FilterFactory2 FF = CommonFactoryFinder.getFilterFactory2();
+
+    public static FunctionName NAME =
+            new FunctionNameImpl(
+                    "mapGet",
+                    parameter("value", Object.class),
+                    parameter("map", Map.class),
+                    parameter("key", String.class));
+
+    public MapGetFunction() {
+        super(NAME);
+    }
+
+    @Override
+    public Object evaluate(Object object, Class context) {
+        Object result = evaluate(object);
+        if (result == null) {
+            return null;
+        } else {
+            return Converters.convert(result, context);
+        }
+    }
+
+    public Object evaluate(Object feature) {
+        Map map = getExpression(0).evaluate(feature, Map.class);
+        String key = getExpression(1).evaluate(feature, String.class);
+        if (map != null) {
+            return map.get(key);
+        }
+
+        return null;
+    }
+}
---
+++ modules/library/main/src/test/java/org/geotools/filter/function/MapGetFunctionTest.java
@@ -0,0 +1,176 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2020, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.filter.function;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import junit.framework.TestCase;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.feature.NameImpl;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.feature.type.AttributeDescriptorImpl;
+import org.geotools.feature.type.AttributeTypeImpl;
+import org.junit.Test;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LineString;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.AttributeDescriptor;
+import org.opengis.feature.type.AttributeType;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.PropertyIsGreaterThan;
+import org.opengis.filter.expression.Function;
+
+public class MapGetFunctionTest extends TestCase {
+
+    static FilterFactory2 FF = CommonFactoryFinder.getFilterFactory2();
+    private SimpleFeatureType sampleDataType;
+
+    private String namespace = "ns";
+    private SimpleFeature[] features;
+    private GeometryFactory gf;
+
+    @Override
+    protected void setUp() throws Exception {
+        SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+        builder.setName("map");
+        builder.setNamespaceURI(namespace);
+
+        builder.add(createAttribute("id", String.class));
+        builder.add(createAttribute("geom", LineString.class));
+        builder.setDefaultGeometry("geom");
+        builder.add(createAttribute("name", String.class));
+        builder.add(createAttribute("attributes", Map.class));
+        sampleDataType = builder.buildFeatureType();
+
+        gf = new GeometryFactory();
+
+        features = new SimpleFeature[3];
+        features[0] =
+                SimpleFeatureBuilder.build(
+                        sampleDataType,
+                        new Object[] {
+                            Integer.valueOf(1),
+                            line(new int[] {1, 1, 2, 2, 4, 2, 5, 1}),
+                            "object1",
+                            new HashMap<String, Object>() {
+                                {
+                                    put("name", "firstObject");
+                                    put("valid", false);
+                                    put("score", 100);
+                                    put("key1", "value1");
+                                }
+                            }
+                        },
+                        "sample.s1");
+
+        features[1] =
+                SimpleFeatureBuilder.build(
+                        sampleDataType,
+                        new Object[] {
+                            Integer.valueOf(2),
+                            line(new int[] {3, 0, 3, 2, 3, 3, 3, 4}),
+                            "object2",
+                            new HashMap<String, Object>() {
+                                {
+                                    put("name", "secondObject");
+                                    put("valid", true);
+                                    put("score", 50);
+                                    put("key1", 10);
+                                }
+                            }
+                        },
+                        "sample.s2");
+
+        features[2] =
+                SimpleFeatureBuilder.build(
+                        sampleDataType,
+                        new Object[] {
+                            Integer.valueOf(2),
+                            line(new int[] {3, 2, 4, 2, 5, 3}),
+                            "object3",
+                            new HashMap<String, Object>() {
+                                {
+                                    put("name", "thirdObject");
+                                    put("score", 70);
+                                    put("key1", true);
+                                }
+                            }
+                        },
+                        "sample.s3");
+    }
+
+    private AttributeDescriptor createAttribute(String name, Class clazz) {
+        AttributeType at =
+                new AttributeTypeImpl(
+                        new NameImpl(name),
+                        clazz,
+                        false,
+                        false,
+                        Collections.emptyList(),
+                        null,
+                        null);
+        return new AttributeDescriptorImpl(at, new NameImpl(name), 0, 1, false, null);
+    }
+
+    /**
+     * Creates a line from the specified (<var>x</var>,<var>y</var>) coordinates. The coordinates
+     * are stored in a flat array.
+     */
+    public LineString line(int[] xy) {
+        Coordinate[] coords = new Coordinate[xy.length / 2];
+
+        for (int i = 0; i < xy.length; i += 2) {
+            coords[i / 2] = new Coordinate(xy[i], xy[i + 1]);
+        }
+
+        return gf.createLineString(coords);
+    }
+
+    @Test
+    public void testEvaluate() {
+        // evaluating on attributes.name
+        Function f = FF.function("mapGet", FF.property("attributes"), FF.literal("name"));
+        assertEquals("firstObject", f.evaluate(features[0], String.class));
+        assertEquals("secondObject", f.evaluate(features[1], String.class));
+        assertEquals("thirdObject", f.evaluate(features[2], String.class));
+
+        PropertyIsGreaterThan gt =
+                FF.greater(
+                        FF.function("mapGet", FF.property("attributes"), FF.literal("score")),
+                        FF.literal(80));
+        assertEquals(true, gt.evaluate(features[0]));
+        assertEquals(false, gt.evaluate(features[1]));
+        assertEquals(false, gt.evaluate(features[2]));
+
+        Function f2 = FF.function("mapGet", FF.property("attributes"), FF.literal("valid"));
+        assertFalse(f2.evaluate(features[0], Boolean.class));
+        assertTrue(f2.evaluate(features[1], Boolean.class));
+        assertNull(f2.evaluate(features[2], Boolean.class));
+
+        Function f3 = FF.function("mapGet", FF.property("attributes"), FF.literal("key1"));
+        assertEquals("value1", f3.evaluate(features[0], String.class));
+        assertEquals(Integer.valueOf(10), f3.evaluate(features[1], Integer.class));
+        assertEquals(Boolean.TRUE, f3.evaluate(features[2], Boolean.class));
+
+        Function f4 = FF.function("mapGet", FF.property("missingMap"), FF.literal("key1"));
+        assertNull(f4.evaluate(features[0], String.class));
+    }
+}

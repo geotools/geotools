78cd171e32d13988cdd4b75aca71657a4e8abc06
Niels Charlier <niels@scitus.be>
2020-06-08 14:55:24 +0200

    [GEOT-6595] CRS can't get axis order from compound CRS

--- modules/library/referencing/src/test/java/org/geotools/referencing/CrsTest.java
+++ modules/library/referencing/src/test/java/org/geotools/referencing/CrsTest.java
@@ -330,6 +330,20 @@ public final class CrsTest {
                         + "  AUTHORITY[\"EPSG\",\"23031\"]]";
         assertEquals(AxisOrder.EAST_NORTH, CRS.getAxisOrder(CRS.parseWKT(wkt)));
         assertEquals(AxisOrder.NORTH_EAST, CRS.getAxisOrder(CRS.parseWKT(wkt), true));
+
+        // test with compound axis
+        wkt =
+                "COMPD_CS[\"ETRS89 + EVRF2000 height\", GEOGCS[\"ETRS89\", "
+                        + "DATUM[\"European Terrestrial Reference System 1989\", "
+                        + "SPHEROID[\"GRS 1980\", 6378137.0, 298.257222101, AUTHORITY[\"EPSG\",\"7019\"]], "
+                        + "TOWGS84[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], AUTHORITY[\"EPSG\",\"6258\"]], "
+                        + "PRIMEM[\"Greenwich\", 0.0, AUTHORITY[\"EPSG\",\"8901\"]], UNIT[\"degree\", 0.017453292519943295], "
+                        + "AXIS[\"Geodetic latitude\", NORTH], AXIS[\"Geodetic longitude\", EAST], AUTHORITY[\"EPSG\",\"4258\"]], "
+                        + "VERT_CS[\"EVRF2000 height\", VERT_DATUM[\"European Vertical Reference Frame 2000\", 2005, "
+                        + "AUTHORITY[\"EPSG\",\"5129\"]], UNIT[\"m\", 1.0], AXIS[\"Gravity-related height\", UP], "
+                        + "AUTHORITY[\"EPSG\",\"5730\"]], AUTHORITY[\"EPSG\",\"7409\"]]";
+        assertEquals(AxisOrder.NORTH_EAST, CRS.getAxisOrder(CRS.parseWKT(wkt)));
+        assertEquals(AxisOrder.NORTH_EAST, CRS.getAxisOrder(CRS.parseWKT(wkt), true));
     }
 
     @Test

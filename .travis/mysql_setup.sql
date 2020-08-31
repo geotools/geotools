6543b95d6dcfc23bbc58ba91016bab9b7a73c3c9
Mark Prins <1165786+mprins@users.noreply.github.com>
2020-08-31 14:40:02 +0200

    Add a Travis job to run MySQL online tests

---
+++ .travis/mysql_setup.sql
@@ -0,0 +1,6 @@
+-- create a geotools database
+CREATE DATABASE geotools;
+-- set up user
+-- CREATE USER geotools@% IDENTIFIED BY geotools;
+-- GRANT ALL PRIVILEGES ON geotools.* TO geotools@%;
+-- FLUSH PRIVILEGES;

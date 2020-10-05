9202c441a8ebd4ee443948388926863b49b884bd
Andreas Watermeyer <Andreas.Watermeyer@its-digital.de>
2020-10-05 09:56:27 +0200

    [GEOT-6704] BaseRequestTypeImpl.extendedProperties must not be shared across all instances

---
+++ modules/ogc/net.opengis.wfs/test/net/opengis/wfs/impl/BaseRequestTypeImplTest.java
@@ -0,0 +1,43 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2020, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package net.opengis.wfs.impl;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import net.opengis.wfs20.TransactionType;
+import net.opengis.wfs20.Wfs20Factory;
+import net.opengis.wfs20.impl.BaseRequestTypeImpl;
+
+/**
+ * Contains tests for {@link BaseRequestTypeImpl}
+ * 
+ * @author awaterme
+ */
+public class BaseRequestTypeImplTest {
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testOwnExtendedProperties() {
+        TransactionType lTransaction1 = Wfs20Factory.eINSTANCE.createTransactionType();
+        lTransaction1.getExtendedProperties().put("foo", "bar");
+        TransactionType lTransaction2 = Wfs20Factory.eINSTANCE.createTransactionType();
+        assertFalse(lTransaction2.getExtendedProperties().containsKey("foo"));
+    }
+
+}

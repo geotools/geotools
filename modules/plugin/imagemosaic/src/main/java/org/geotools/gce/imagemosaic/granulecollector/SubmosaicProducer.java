4d2505dff2a015b910a6fa19c71db1215e991470
Andrea Aime <andrea.aime@gmail.com>
2021-02-02 15:41:40 +0100

    [GEOT-6796] Concurrent usage of image mosaic with heterogeneous CRS, against a database index, can cause deadlock

--- modules/plugin/imagemosaic/src/main/java/org/geotools/gce/imagemosaic/granulecollector/SubmosaicProducer.java
+++ modules/plugin/imagemosaic/src/main/java/org/geotools/gce/imagemosaic/granulecollector/SubmosaicProducer.java
@@ -19,6 +19,7 @@ package org.geotools.gce.imagemosaic.granulecollector;
 
 import java.io.IOException;
 import java.util.List;
+import org.geotools.data.Query;
 import org.geotools.gce.imagemosaic.GranuleDescriptor;
 import org.geotools.gce.imagemosaic.MosaicElement;
 
@@ -29,6 +30,12 @@ import org.geotools.gce.imagemosaic.MosaicElement;
  * can be handled on its own before being
  */
 public interface SubmosaicProducer {
+
+    /**
+     * Gives the producer an opportunity to pre-visit the granules, if it needs so for any reason
+     */
+    default void init(Query query) throws IOException, Exception {}
+
     boolean accept(GranuleDescriptor granuleDescriptor);
 
     List<MosaicElement> createMosaic() throws IOException;

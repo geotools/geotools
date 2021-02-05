4dbee6a50eea6fb4a79836024a8fb22bc6af3501
Andrea Aime <andrea.aime@gmail.com>
2021-02-05 17:52:48 +0100

    [GEOT-6796] Follow up, was going NPE on requests selecting no granules

--- modules/plugin/imagemosaic/src/test/java/org/geotools/gce/imagemosaic/HeterogenousCRSTest.java
+++ modules/plugin/imagemosaic/src/test/java/org/geotools/gce/imagemosaic/HeterogenousCRSTest.java
@@ -28,7 +28,7 @@ import static org.junit.Assert.assertTrue;
 
 import com.sun.media.jai.operator.ImageReadDescriptor;
 import it.geosolutions.imageio.stream.input.FileImageInputStreamExtImpl;
-import java.awt.*;
+import java.awt.Color;
 import java.awt.geom.AffineTransform;
 import java.awt.image.RenderedImage;
 import java.awt.image.renderable.ParameterBlock;
@@ -51,6 +51,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.imageio.ImageReader;
 import javax.imageio.stream.ImageInputStream;
@@ -300,7 +301,41 @@ public class HeterogenousCRSTest {
     }
 
     @Test
-    public void testConcurrentHeteroUTMH2() throws Exception {
+    public void testConcurrentHeteroUTMH2FullArea() throws Exception {
+        runConcurrentHeteroUTMH2(r -> null);
+    }
+
+    @Test
+    public void testConcurrentHeteroUTMH2Across() throws Exception {
+        runConcurrentHeteroUTMH2(
+                r -> {
+                    ParameterValue<GridGeometry2D> ggp =
+                            AbstractGridFormat.READ_GRIDGEOMETRY2D.createValue();
+                    GridEnvelope range = r.getOriginalGridRange();
+                    // move read area partially outside
+                    ReferencedEnvelope bounds =
+                            ReferencedEnvelope.reference(r.getOriginalEnvelope());
+                    bounds.translate(bounds.getWidth() / 2, 0);
+                    GridGeometry2D gg = new GridGeometry2D(range, bounds);
+                    ggp.setValue(gg);
+                    return new GeneralParameterValue[] {ggp};
+                });
+    }
+
+    @Test
+    public void testConcurrentHeteroUTMH2NoGranules() throws Exception {
+        runConcurrentHeteroUTMH2(
+                r -> {
+                    ParameterValue<Filter> filter = ImageMosaicFormat.FILTER.createValue();
+                    filter.setValue(Filter.EXCLUDE);
+                    return new GeneralParameterValue[] {filter};
+                });
+    }
+
+    private void runConcurrentHeteroUTMH2(
+            Function<ImageMosaicReader, GeneralParameterValue[]> parameters)
+            throws URISyntaxException, IOException, InterruptedException,
+                    java.util.concurrent.ExecutionException, java.util.concurrent.TimeoutException {
         String testLocation = "hetero_utm";
         URL storeUrl = TestData.url(this, testLocation);
 
@@ -330,7 +365,8 @@ public class HeterogenousCRSTest {
         try {
             List<Future> futures = new ArrayList<>();
             for (int i = 0; i < 20; i++) {
-                Future<GridCoverage2D> future = executors.submit(() -> reader.read(null));
+                Future<GridCoverage2D> future =
+                        executors.submit(() -> reader.read(parameters.apply(reader)));
                 futures.add(future);
             }
             for (Future future : futures) {

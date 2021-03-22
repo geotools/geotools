01ce13f920196c45281f730ea4afe5498eab31ba
taba90 <46672505+taba90@users.noreply.github.com>
2021-03-22 17:00:36 +0100

    [GEOT-6848] Vendor options in RasterSymbolizer are not parsed (#3404)
    * [GEOT-6848] Vendor options in RasterSymbolizer are not parsed
    
    * review feedback applied

--- modules/library/xml/src/main/java/org/geotools/xml/styling/SLDParser.java
+++ modules/library/xml/src/main/java/org/geotools/xml/styling/SLDParser.java
@@ -1311,6 +1311,8 @@ public class SLDParser {
                 symbol.setShadedRelief(parseShadedRelief(child));
             } else if (childName.equalsIgnoreCase(imageOutlineString)) {
                 symbol.setImageOutline(parseLineSymbolizer(child));
+            } else if (childName.equalsIgnoreCase(VendorOptionString)) {
+                parseVendorOption(symbol.getOptions(), child);
             }
         }
 

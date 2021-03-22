62bb68495fe17512c61ed6c2f4ff84de23d101ee
taba90 <46672505+taba90@users.noreply.github.com>
2021-03-22 17:00:36 +0100

    [GEOT-6848] Vendor options in RasterSymbolizer are not parsed (#3404)
    * [GEOT-6848] Vendor options in RasterSymbolizer are not parsed
    
    * review feedback applied

--- modules/extension/xsd/xsd-sld/src/test/java/org/geotools/sld/bindings/SLDRasterSymbolizerBindingTest.java
+++ modules/extension/xsd/xsd-sld/src/test/java/org/geotools/sld/bindings/SLDRasterSymbolizerBindingTest.java
@@ -19,6 +19,7 @@ package org.geotools.sld.bindings;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Map;
 import org.geotools.styling.RasterSymbolizer;
 import org.junit.Test;
 
@@ -42,5 +43,25 @@ public class SLDRasterSymbolizerBindingTest extends SLDTestSupport {
         assertNotNull(rs.getOverlap());
         assertNotNull(rs.getGeometryPropertyName());
         assertNotNull(rs.getShadedRelief());
+    }
+
+    @Test
+    public void testWithVendorOptions() throws Exception {
+        SLDMockData.rasterSymbolizerWithVendorOptions(document, document);
+
+        RasterSymbolizer rs = (RasterSymbolizer) parse();
+        assertNotNull(rs);
+        assertNotNull(rs.getChannelSelection());
+        assertNotNull(rs.getColorMap());
+        assertNotNull(rs.getContrastEnhancement());
+        assertNotNull(rs.getImageOutline());
+        assertNotNull(rs.getOpacity());
+        assertNotNull(rs.getOverlap());
+        assertNotNull(rs.getGeometryPropertyName());
+        assertNotNull(rs.getShadedRelief());
+        Map<String, String> options = rs.getOptions();
+        assertEquals(2, options.size());
+        assertEquals("value", options.get("name"));
+        assertEquals("value2", options.get("name2"));
     }
 }

fee181cdac33d9e8c7f00fcbb8b933e168452dbe
Ian Turton <ijturton@gmail.com>
2021-04-05 14:36:54 +0100

    [GEOT-6863] Datastores with Character parameters can't be reopened (#3440)

--- modules/library/main/src/main/java/org/geotools/data/DataAccessFactory.java
+++ modules/library/main/src/main/java/org/geotools/data/DataAccessFactory.java
@@ -488,7 +488,8 @@ public interface DataAccessFactory extends Factory {
          *
          * <ul>
          *   <li>null: If text is null
-         *   <li>origional text: if type == String.class
+         *   <li>original text: if type == String.class
+         *   <li>first character of original text: if type == Character.class
          *   <li>null: if type != String.class and text.getLength == 0
          *   <li>parse( text ): if type != String.class
          * </ul>
@@ -504,7 +505,9 @@ public interface DataAccessFactory extends Factory {
             if (type == String.class) {
                 return text;
             }
-
+            if (type == Character.class) {
+                return text.charAt(0);
+            }
             if (text.length() == 0) {
                 return null;
             }

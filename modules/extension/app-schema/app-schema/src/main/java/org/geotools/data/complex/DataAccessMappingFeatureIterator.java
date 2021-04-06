d662ce3268945b76233054dd24965438e2ba2f42
taba90 <46672505+taba90@users.noreply.github.com>
2021-04-06 10:29:55 +0200

    [GEOT-6854] AppSchema JDBCMultipleValue directive fails to encode ClientProperties if targetValue is not specified - [GEOT-6855] AppSchema JDBCMultipleValue might generate ambiguous query when ClientProperty is defined (#3420)
    * [GEOT-6854] AppSchema JDBCMultipleValue directive fails to encode ClientProperties if targetValue is not specified
    
    * [GEOT-6855] AppSchema JDBCMultipleValue might generate ambiguous query when ClientProperty is defined

--- modules/extension/app-schema/app-schema/src/main/java/org/geotools/data/complex/DataAccessMappingFeatureIterator.java
+++ modules/extension/app-schema/app-schema/src/main/java/org/geotools/data/complex/DataAccessMappingFeatureIterator.java
@@ -1579,10 +1579,6 @@ public class DataAccessMappingFeatureIterator extends AbstractMappingFeatureIter
 
             Collection collection = (Collection) property.getValue();
 
-            if (this.getClientProperties(property).containsKey(XLINK_HREF_NAME)) {
-                return true;
-            }
-
             List<Property> values = new ArrayList<>();
             for (Object o : collection) {
                 if (o instanceof Property) {
@@ -1602,6 +1598,9 @@ public class DataAccessMappingFeatureIterator extends AbstractMappingFeatureIter
                 }
             }
             property.setValue(values);
+            if (this.getClientProperties(property).containsKey(XLINK_HREF_NAME)) {
+                return true;
+            }
         } else if (property.getName().equals(ComplexFeatureConstants.FEATURE_CHAINING_LINK_NAME)) {
             // ignore fake attribute FEATURE_LINK
             result = false;

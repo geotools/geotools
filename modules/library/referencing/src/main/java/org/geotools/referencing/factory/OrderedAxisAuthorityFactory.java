4a004224ee8d8965a1da49cb6b3ed24646b23975
Andrea Aime <andrea.aime@gmail.com>
2021-06-16 11:17:53 +0200

    [GEOT-6916] "WGS 84 / UPS South (N,E)" is rendered with flipped axis

--- modules/library/referencing/src/main/java/org/geotools/referencing/factory/OrderedAxisAuthorityFactory.java
+++ modules/library/referencing/src/main/java/org/geotools/referencing/factory/OrderedAxisAuthorityFactory.java
@@ -16,13 +16,15 @@
  */
 package org.geotools.referencing.factory;
 
+import static org.geotools.referencing.cs.DefaultCoordinateSystemAxis.EASTING;
+import static org.geotools.referencing.cs.DefaultCoordinateSystemAxis.NORTHING;
+
 import java.util.Arrays;
 import java.util.Comparator;
 import javax.measure.Unit;
 import org.geotools.metadata.i18n.ErrorKeys;
 import org.geotools.metadata.i18n.Errors;
 import org.geotools.referencing.ReferencingFactoryFinder;
-import org.geotools.referencing.cs.DefaultCoordinateSystemAxis;
 import org.geotools.util.factory.FactoryRegistryException;
 import org.geotools.util.factory.Hints;
 import org.opengis.referencing.crs.CRSAuthorityFactory;
@@ -228,8 +230,7 @@ public class OrderedAxisAuthorityFactory extends TransformedAuthorityFactory
         hints.put(Hints.FORCE_STANDARD_AXIS_DIRECTIONS, forceStandardDirections);
         // The following hint has no effect on this class behaviour,
         // but tells to the user what this factory do about axis order.
-        if (compare(DefaultCoordinateSystemAxis.EASTING, DefaultCoordinateSystemAxis.NORTHING)
-                < 0) {
+        if (compare(EASTING, NORTHING) < 0) {
             hints.put(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, Boolean.TRUE);
         }
     }
@@ -303,7 +304,21 @@ public class OrderedAxisAuthorityFactory extends TransformedAuthorityFactory
      */
     @Override
     public int compare(CoordinateSystemAxis axis1, CoordinateSystemAxis axis2) {
-        return rank(axis1) - rank(axis2);
+        int r1 = rank(axis1);
+        int r2 = rank(axis2);
+        if (r1 >= directionRanks.length && r2 >= directionRanks.length) {
+            // special directions, they were not ranked. Try a heuristic based on the axis labels,
+            // works for stereographic polars
+            String abb1 = axis1.getAbbreviation();
+            String abb2 = axis2.getAbbreviation();
+            if (abb1.equals(NORTHING.getAbbreviation()) && abb2.equals(EASTING.getAbbreviation())) {
+                return 1;
+            } else if (abb1.equals(EASTING.getAbbreviation())
+                    && abb2.equals(NORTHING.getAbbreviation())) {
+                return -1;
+            }
+        }
+        return r1 - r2;
     }
 
     /**

cd63ee1815eb0dde45ba59a44bfd2b7c97fa2c06
Andrea Aime <andrea.aime@gmail.com>
2021-06-18 15:57:50 +0200

    Clarifications in javadoc

--- modules/library/main/src/main/java/org/geotools/filter/function/NowFunction.java
+++ modules/library/main/src/main/java/org/geotools/filter/function/NowFunction.java
@@ -22,7 +22,21 @@ import org.geotools.filter.FunctionExpressionImpl;
 import org.geotools.filter.capability.FunctionNameImpl;
 import org.opengis.filter.capability.FunctionName;
 
-/** Return the current time (milliseconds) as a Date */
+/**
+ * Return the current time as a Date. When evaluated directly, it returns a new value at each
+ * invocation. When evaluated as part of a filter againsta a DataStore, the value might be one, and
+ * fixed, during the data access. For example:
+ *
+ * <ul>
+ *   <li>If the datastore uses {@link org.geotools.filter.visitor.SimplifyingFilterVisitor}, the
+ *       function will be called once, and replaced with the returned value
+ *   <li>If the datastore encodes the function down in a native query language, the value could also
+ *       be the same for all processed features. For example, the PostgreSQL <a
+ *       href="https://www.postgresql.org/docs/13/functions-datetime.html#FUNCTIONS-DATETIME-CURRENT">now</a>
+ *       function always returns the time at the beginning of the current transaction, to give a
+ *       consistent notion of current time across it.
+ * </ul>
+ */
 public class NowFunction extends FunctionExpressionImpl {
     public static FunctionName NAME = new FunctionNameImpl("now", Date.class);
 

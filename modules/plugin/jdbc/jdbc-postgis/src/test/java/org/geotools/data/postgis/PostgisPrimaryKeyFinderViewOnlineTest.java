2e506cb00d32f8402951831f7e3ccdaea865e6ff
elielwaltrick <frasson.eliel@gmail.com>
2021-07-09 13:39:41 -0300

    [GEOT-6921][backport 24.x] GeoTools primary key finder always query the table gt_pk_metadata even if it doesn't exists

--- modules/plugin/jdbc/jdbc-postgis/src/test/java/org/geotools/data/postgis/PostgisPrimaryKeyFinderViewOnlineTest.java
+++ modules/plugin/jdbc/jdbc-postgis/src/test/java/org/geotools/data/postgis/PostgisPrimaryKeyFinderViewOnlineTest.java
@@ -1,6 +1,6 @@
 package org.geotools.data.postgis;
 
-import java.util.Map;
+import java.util.HashMap;
 import org.geotools.jdbc.JDBCDataStoreFactory;
 import org.geotools.jdbc.JDBCPrimaryKeyFinderViewOnlineTest;
 import org.geotools.jdbc.JDBCPrimaryKeyFinderViewTestSetup;
@@ -13,8 +13,8 @@ public class PostgisPrimaryKeyFinderViewOnlineTest extends JDBCPrimaryKeyFinderV
     }
 
     @Override
-    protected Map<String, Object> createDataStoreFactoryParams() throws Exception {
-        Map<String, Object> params = super.createDataStoreFactoryParams();
+    protected HashMap createDataStoreFactoryParams() throws Exception {
+        HashMap params = super.createDataStoreFactoryParams();
         params.put(JDBCDataStoreFactory.PK_METADATA_TABLE.key, "gt_pk_metadata");
         return params;
     }

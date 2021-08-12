f922f0bdd19e32b9648ad644af2a1eed75417964
Mark Prins <1165786+mprins@users.noreply.github.com>
2021-08-12 16:38:55 +0200

    Use a smaller Oracle docker image for the tests
    this image is about 3.4GB, while the other is 12.7GB, so this should shave off some download time, startup time seemed pretty much the same for both at ~30sec
    We need to use the full SDO_GEOMETRY construct for insert and not rely on the Java stored procedure "SDO_GEOMETRY(wkt clob, srid))" being available in the database
    
    Update workflow file to use Ubuntu 20.04 and stop deleting pre-installed images

--- modules/plugin/jdbc/jdbc-oracle/src/test/java/org/geotools/data/oracle/OracleNativeFilterTestSetup.java
+++ modules/plugin/jdbc/jdbc-oracle/src/test/java/org/geotools/data/oracle/OracleNativeFilterTestSetup.java
@@ -49,18 +49,21 @@ public final class OracleNativeFilterTestSetup extends JDBCNativeFilterTestSetup
                 "CREATE INDEX GT_JDBC_TEST_MEASUREMENTS_GEOM_INDEX ON "
                         + "GT_JDBC_TEST_MEASUREMENTS(location) INDEXTYPE IS MDSYS.SPATIAL_INDEX");
         // insert the needed records
+        // NB we could use '"INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (1, '#1', 'temperature',
+        // 15.0, SDO_GEOMETRY('POINT (1.0 2.0)', 4326))"'
+        // but that relies on a Java stored procedure being available
         run(
-                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (1, '#1', 'temperature', 15.0, SDO_GEOMETRY('POINT (1.0 2.0)', 4326))");
+                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (1, '#1', 'temperature', 15.0, SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(1.0, 2.0, NULL), NULL, NULL))");
         run(
-                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (2, '#2', 'temperature', 18.5, SDO_GEOMETRY('POINT (1.0 4.0)', 4326))");
+                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (2, '#2', 'temperature', 18.5, SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(1.0, 4.0, NULL), NULL, NULL))");
         run(
-                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (3, '#3', 'wind', 8.5, SDO_GEOMETRY('POINT (2.0 4.0)', 4326))");
+                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (3, '#3', 'wind',        8.5,  SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(2.0, 4.0, NULL), NULL, NULL))");
         run(
-                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (4, '#4', 'wind', 4.5, SDO_GEOMETRY('POINT (2.0 2.0)', 4326))");
+                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (4, '#4', 'wind',        4.5,  SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(2.0, 2.0, NULL), NULL, NULL))");
         run(
-                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (5, '#5', 'humidity', 0.7, SDO_GEOMETRY('POINT (1.0 4.0)', 4326))");
+                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (5, '#5', 'humidity',    0.7,  SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(1.0, 4.0, NULL), NULL, NULL))");
         run(
-                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (6, '#6', 'humidity', 0.5, SDO_GEOMETRY('POINT (5.0 4.0)', 4326))");
+                "INSERT INTO GT_JDBC_TEST_MEASUREMENTS VALUES (6, '#6', 'humidity',    0.5,  SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(5.0, 4.0, NULL), NULL, NULL))");
     }
 
     @Override

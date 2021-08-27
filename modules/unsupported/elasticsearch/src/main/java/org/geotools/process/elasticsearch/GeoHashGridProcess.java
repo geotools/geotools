02ecd69031c8428004ce7e648481c5ddf21e9e62
Andrea Aime <andrea.aime@gmail.com>
2021-08-27 18:40:21 +0200

    [GEOT-7031] Elasticsearch geohash process can fail with NPE in invertQuery

--- modules/unsupported/elasticsearch/src/main/java/org/geotools/process/elasticsearch/GeoHashGridProcess.java
+++ modules/unsupported/elasticsearch/src/main/java/org/geotools/process/elasticsearch/GeoHashGridProcess.java
@@ -164,31 +164,32 @@ public class GeoHashGridProcess implements VectorProcess {
 
         final BBOXRemovingFilterVisitor visitor = new BBOXRemovingFilterVisitor();
         Filter filter = (Filter) targetQuery.getFilter().accept(visitor, null);
-        final String geometryName = visitor.getGeometryPropertyName();
-        if (geometryName != null) {
-            final BBOX bbox;
-            try {
-                if (envelope.getCoordinateReferenceSystem() != null) {
-                    envelope = envelope.transform(DefaultGeographicCRS.WGS84, false);
-                }
-                bbox =
-                        FILTER_FACTORY.bbox(
-                                geometryName,
-                                envelope.getMinX(),
-                                envelope.getMinY(),
-                                envelope.getMaxX(),
-                                envelope.getMaxY(),
-                                "EPSG:4326");
-            } catch (Exception e) {
-                throw new ProcessException("Unable to create bbox filter for feature source", e);
-            }
-            filter =
-                    (Filter)
-                            FILTER_FACTORY
-                                    .and(filter, bbox)
-                                    .accept(new SimplifyingFilterVisitor(), null);
-            targetQuery.setFilter(filter);
+        String geometryName = visitor.getGeometryPropertyName();
+        if (geometryName == null) {
+            geometryName = "";
         }
+        final BBOX bbox;
+        try {
+            if (envelope.getCoordinateReferenceSystem() != null) {
+                envelope = envelope.transform(DefaultGeographicCRS.WGS84, false);
+            }
+            bbox =
+                    FILTER_FACTORY.bbox(
+                            geometryName,
+                            envelope.getMinX(),
+                            envelope.getMinY(),
+                            envelope.getMaxX(),
+                            envelope.getMaxY(),
+                            "EPSG:4326");
+        } catch (Exception e) {
+            throw new ProcessException("Unable to create bbox filter for feature source", e);
+        }
+        filter =
+                (Filter)
+                        FILTER_FACTORY
+                                .and(filter, bbox)
+                                .accept(new SimplifyingFilterVisitor(), null);
+        targetQuery.setFilter(filter);
 
         final List<PropertyName> properties = new ArrayList<>();
         properties.add(FILTER_FACTORY.property("_aggregation"));

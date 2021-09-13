a033a0c20dbde8eb08b19ae9e536659d35bde8f7
Jody Garnett <jody.garnett@gmail.com>
2021-09-12 20:20:56 -0700

    avoid masking common.py settings
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- docs/developer/conf.py
+++ docs/developer/conf.py
@@ -2,15 +2,6 @@ import sys, os
 sys.path.append(os.path.abspath('..'))
 from common import *
 
-# The suffix of source filenames.
-source_suffix = '.rst'
-
-# The encoding of source files.
-#source_encoding = 'utf-8'
-
-# The master toctree document.
-master_doc = 'index'
-
 # Options for HTML output
 # -----------------------
 

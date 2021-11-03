269585764d652105fe34a24e9c921d2fc81614ee
Roar Brænden <roar.brenden@me.com>
2021-11-03 16:00:30 +0100

    [GEOT-7001] XmlComplexFeatureParser gives wrong name for ComplexAttribute (#3654)
    * [GEOT-7001] XmlComplexFeatureParser gives wrong name for ComplexAttribute
    
    * Example code in docs

---
+++ docs/src/main/java/org/geotools/wfs/ReadWFSKommuner.java
@@ -0,0 +1,149 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2021, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
+
+package org.geotools.wfs;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.URL;
+import java.util.HashMap;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.wfs.WFSDataStoreFactory;
+import org.geotools.data.wfs.impl.WFSContentDataAccess;
+import org.geotools.data.wfs.impl.WFSDataAccessFactory;
+import org.geotools.feature.FeatureIterator;
+import org.opengis.feature.ComplexAttribute;
+import org.opengis.feature.Feature;
+import org.opengis.feature.Property;
+import org.opengis.feature.type.FeatureType;
+import org.opengis.feature.type.Name;
+
+/**
+ * Read the contents of a WFS with complex structure.
+ *
+ * @author Roar Brænden
+ */
+public class ReadWFSKommuner {
+
+    /*
+    	 *   Content of a single feature with only one AdministrativEnhetNavn
+    	 *
+    <wfs:member>
+        <app:Kommune xmlns:app="http://skjema.geonorge.no/SOSI/produktspesifikasjon/AdmEnheter/4.1" gml:id="kommune_view.357">
+          <app:identifikasjon>
+            <app:Identifikasjon>
+              <app:lokalId>173103</app:lokalId>
+              <app:navnerom>https://data.geonorge.no/sosi/administrativeenheter/fylker_kommuner</app:navnerom>
+              <app:versjonId>4.1</app:versjonId>
+            </app:Identifikasjon>
+          </app:identifikasjon>
+          <app:oppdateringsdato>2020-02-07T00:00:00</app:oppdateringsdato>
+          <app:datauttaksdato>2021-01-04T10:07:10</app:datauttaksdato>
+          <app:område>
+            <!--Inlined geometry 'kommune_view.357_APP_OMRÅDE'-->
+            <gml:Polygon gml:id="kommune_view.357_APP_OMRÅDE" srsName="urn:ogc:def:crs:EPSG::4258">
+              <gml:exterior>
+                <gml:LinearRing>
+                  <gml:posList>58.426581 6.605405 58.420867 6.599559 .......
+                </gml:LinearRing>
+              </gml:exterior>
+            </gml:Polygon>
+          </app:område>
+          <app:kommunenummer>4207</app:kommunenummer>
+          <app:kommunenavn>
+            <app:AdministrativEnhetNavn>
+              <app:navn>Flekkefjord</app:navn>
+              <app:språk>nor</app:språk>
+            </app:AdministrativEnhetNavn>
+          </app:kommunenavn>
+          <app:samiskForvaltningsområde>false</app:samiskForvaltningsområde>
+        </app:Kommune>
+      </wfs:member>
+    	 *
+    	 */
+
+    private static final String SCHEMA_CACHE = "../../schemas";
+
+    private static final String CAPABILITIES_URL =
+            "https://wfs.geonorge.no/skwms1/wfs.administrative_enheter?&service=WFS&acceptversions=2.0.0&request=GetCapabilities";
+
+    /** Calls the WFS server and prints out the content. */
+    public void read() throws IOException {
+
+        final HashMap<String, Serializable> params = new HashMap<String, Serializable>();
+        params.put(WFSDataStoreFactory.URL.key, new URL(CAPABILITIES_URL));
+        params.put(WFSDataStoreFactory.TIMEOUT.key, 60000); // 60 seconds
+        params.put(WFSDataStoreFactory.BUFFER_SIZE.key, 100);
+        params.put(WFSDataStoreFactory.PROTOCOL.key, Boolean.FALSE); // Prefer GET to support Schema
+        params.put("WFSDataStoreFactory:SCHEMA_CACHE_LOCATION", SCHEMA_CACHE);
+
+        WFSContentDataAccess complexDataAccess =
+                (WFSContentDataAccess) new WFSDataAccessFactory().createDataStore(params);
+        Name featureName = null;
+        for (Name name : complexDataAccess.getNames()) {
+            if ("Kommune".equals(name.getLocalPart())) {
+                featureName = name;
+                break;
+            }
+        }
+        if (featureName == null) {
+            throw new RuntimeException("WFS didn't return a Kommune feature.");
+        }
+
+        FeatureSource<FeatureType, Feature> kommuneSource =
+                complexDataAccess.getFeatureSource(featureName);
+
+        try (FeatureIterator<Feature> features = kommuneSource.getFeatures().features()) {
+
+            while (features.hasNext()) {
+                Feature nextFeature = features.next();
+                String kommunenr = (String) nextFeature.getProperty("kommunenummer").getValue();
+                String kommunenavn = "Uten norsk navn";
+                ComplexAttribute kommunenavnAttribute =
+                        (ComplexAttribute) nextFeature.getProperty("kommunenavn");
+                for (Property administrativEnhetNavn : kommunenavnAttribute.getProperties()) {
+                    ComplexAttribute innerElement = innerComplex(administrativEnhetNavn);
+                    if ("nor".equals(innerElement.getProperty("språk").getValue())) {
+                        kommunenavn = (String) innerElement.getProperty("navn").getValue();
+                        break;
+                    }
+                }
+
+                System.out.printf("%s : %s\n-------------\n", kommunenr, kommunenavn);
+                for (Property prop : nextFeature.getProperties()) {
+                    System.out.println(prop.toString());
+                }
+                System.out.println("-------------");
+            }
+        }
+    }
+
+    private static ComplexAttribute innerComplex(Property complex) {
+        return (ComplexAttribute) ((ComplexAttribute) complex).getProperties().iterator().next();
+    }
+
+    /** Call read */
+    public static void main(String[] args) {
+        try {
+            new ReadWFSKommuner().read();
+        } catch (Exception e) {
+            System.out.println("ReadWFSKommuner ended with an exception: " + e.getMessage());
+            e.printStackTrace(System.out);
+        }
+    }
+}
--- modules/unsupported/wfs-ng/src/test/java/org/geotools/data/wfs/online/KartverketStedsnavnDataStoreOnlineTest.java
+++ modules/unsupported/wfs-ng/src/test/java/org/geotools/data/wfs/online/KartverketStedsnavnDataStoreOnlineTest.java
@@ -18,20 +18,34 @@ package org.geotools.data.wfs.online;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import javax.xml.namespace.QName;
+import org.geotools.data.FeatureSource;
 import org.geotools.data.wfs.WFSDataStoreFactory;
+import org.geotools.data.wfs.WFSTestData;
 import org.geotools.data.wfs.WFSTestData.TestDataType;
+import org.geotools.data.wfs.impl.WFSContentDataAccess;
+import org.geotools.data.wfs.impl.WFSDataAccessFactory;
+import org.geotools.feature.FeatureIterator;
+import org.geotools.test.TestData;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.locationtech.jts.geom.Polygon;
+import org.opengis.feature.Feature;
+import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.FeatureType;
 import org.opengis.feature.type.GeometryDescriptor;
+import org.opengis.feature.type.Name;
+import org.opengis.feature.type.PropertyDescriptor;
 import org.opengis.filter.Filter;
 import org.opengis.filter.Id;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
@@ -42,13 +56,15 @@ public class KartverketStedsnavnDataStoreOnlineTest extends AbstractWfsDataStore
     static final String SERVER_URL =
             "https://wfs.geonorge.no/skwms1/wfs.stedsnavn?request=GetCapabilities&service=WFS";
 
+    static final String NAME = "Sted";
+
     static final TestDataType KARTVERKET_STEDSNAVN =
             new TestDataType(
                     "KartverketNo",
                     new QName(
                             "http://skjema.geonorge.no/SOSI/produktspesifikasjon/StedsnavnForVanligBruk/20181115",
                             "sted"),
-                    "app_Sted",
+                    "app_" + NAME,
                     "urn:ogc:def:crs:EPSG::4258");
 
     static final String defaultGeometryName = "område";
@@ -97,6 +113,52 @@ public class KartverketStedsnavnDataStoreOnlineTest extends AbstractWfsDataStore
         Assert.assertEquals(geometryType, geometryDescriptor.getType().getBinding());
         CoordinateReferenceSystem crs = geometryDescriptor.getCoordinateReferenceSystem();
         Assert.assertNotNull(crs);
+    }
+
+    /**
+     * Check that the returned feature doesn't have other property names than those specified when
+     * calling getSchema()
+     */
+    @Test
+    public void testComplexSchemaMatches() throws Exception {
+        if (Boolean.FALSE.equals(serviceAvailable)) {
+            return;
+        }
+
+        Map<String, Serializable> params = new HashMap<>();
+        params.put(WFSDataStoreFactory.URL.key, new URL(SERVER_URL));
+        params.put(WFSDataStoreFactory.PROTOCOL.key, Boolean.FALSE); // Prefer GET to support Schema
+        params.put(
+                "WFSDataStoreFactory:SCHEMA_CACHE_LOCATION",
+                TestData.file(WFSTestData.class, "KartverketNo")
+                        .getAbsolutePath()); // Must be specified when Schema is http
+
+        WFSDataAccessFactory dataStoreFactory = new WFSDataAccessFactory();
+        WFSContentDataAccess dataAccess =
+                (WFSContentDataAccess) dataStoreFactory.createDataStore(params);
+        Name featureName = null;
+        for (Name nextName : dataAccess.getNames()) {
+            if (NAME.equals(nextName.getLocalPart())) {
+                featureName = nextName;
+                break;
+            }
+        }
+        Assert.assertNotNull(String.format("We should find %s here.", NAME), featureName);
+
+        FeatureSource<FeatureType, Feature> source = dataAccess.getFeatureSource(featureName);
+        HashSet<String> properties = new HashSet<>();
+        for (PropertyDescriptor desc : source.getSchema().getDescriptors()) {
+            properties.add(desc.getName().getLocalPart());
+        }
+
+        try (FeatureIterator<Feature> iterator = source.getFeatures().features()) {
+            Feature feature = iterator.next();
+            for (Property prop : feature.getProperties()) {
+                final String name = prop.getName().getLocalPart();
+                Assert.assertTrue(
+                        "Schema doesn't contain property: " + name, properties.contains(name));
+            }
+        }
     }
 
     @Override

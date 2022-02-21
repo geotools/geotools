476ec1a753a1432de79b08ea63957748eaab0829
sergitopo <al283988@uji.es>
2022-02-21 13:06:34 +0100

    PR review modifications
    PR review modifications

--- modules/extension/complex/src/test/java/org/geotools/filter/attribute/extractor/FilterAttributeExtractorTest.java
+++ modules/extension/complex/src/test/java/org/geotools/filter/attribute/extractor/FilterAttributeExtractorTest.java
@@ -1,56 +1,55 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2022, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package org.geotools.filter.attribute.extractor;
 
-import static org.geotools.feature.FakeTypes.ANYSIMPLETYPE_TYPE;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import org.geotools.feature.NameImpl;
+import java.util.HashSet;
+import java.util.Set;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
-import org.geotools.feature.simple.SimpleFeatureTypeImpl;
-import org.geotools.feature.type.AttributeDescriptorImpl;
-import org.geotools.feature.type.AttributeTypeImpl;
-import org.geotools.feature.type.Types;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.filter.AttributeExpressionImpl;
 import org.geotools.filter.FilterAttributeExtractor;
 import org.junit.Assert;
 import org.junit.Test;
 import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.feature.type.AttributeDescriptor;
-import org.opengis.feature.type.AttributeType;
-import org.xml.sax.helpers.NamespaceSupport;
 
 public class FilterAttributeExtractorTest {
 
     @Test
     public void testNonExistingAttributeEvaluatesToAttributeDescriptor() throws Exception {
-        List<AttributeDescriptor> schema = new ArrayList<>();
-        AttributeType doubleType =
-                new AttributeTypeImpl(
-                        new NameImpl("http://www.w3.org/2001/XMLSchema", "double"),
-                        Double.class,
-                        false,
-                        false,
-                        Collections.emptyList(),
-                        ANYSIMPLETYPE_TYPE,
-                        null);
-        schema.add(
-                new AttributeDescriptorImpl(
-                        doubleType, Types.typeName("pointOne"), 0, 1, false, null));
-        schema.add(
-                new AttributeDescriptorImpl(
-                        doubleType, Types.typeName("pointTwo"), 0, 1, false, null));
 
-        SimpleFeatureType type =
-                new SimpleFeatureTypeImpl(
-                        Types.typeName("GeometryContainer"), schema, null, false, null, null, null);
+        SimpleFeatureTypeBuilder sftb = new SimpleFeatureTypeBuilder();
+        sftb.setName("twoDoublesFeatureType");
+        sftb.add("pointOne", Double.class);
+        sftb.add("pointTwo", Double.class);
+        SimpleFeatureType type = sftb.buildFeatureType();
+
         Feature feature = SimpleFeatureBuilder.build(type, new Object[] {5.0, 2.5}, null);
         FilterAttributeExtractor fae = new FilterAttributeExtractor(type);
-        NamespaceSupport nss = new NamespaceSupport();
-        nss.declarePrefix("ogc", "http://www.opengis.net/ogc");
-        nss.declarePrefix("gml", "http://www.opengis.net/gml");
-        nss.declarePrefix("sld", "http://www.opengis.net/sld");
-        Assert.assertNotNull(fae.visit(new AttributeExpressionImpl("type", nss), feature));
+
+        // the next call to fae.visit used to throw a ClassCastException, here it is ensured is not
+        // anymore thrown
+        Assert.assertNotNull(fae.visit(new AttributeExpressionImpl("type"), feature));
+
+        Set<String> attributeTypesDescriptorsNames = new HashSet<>();
+        attributeTypesDescriptorsNames.add("type");
+        // checking that there is the there is a "type" AttributeDescriptor
+        Assert.assertEquals(
+                attributeTypesDescriptorsNames,
+                fae.visit(new AttributeExpressionImpl("type"), feature));
     }
 }

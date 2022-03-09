7ad7fb09dac0b6a38a0f6fd755fe21f16de04752
Joseph Miller <millerj@pixia.com>
2022-03-08 22:18:13 -0500

    Started on new JSON model
    switched to jsonnode tree parsing, got most of the expression conversions done
    
    binary comparison predicates completed
    
    started on time queries
    
    First pass of all predicates and expressions complete
    
    comments and cleanup
    
    cleanup
    
    fixed dependency version issue
    
    switched to maintained geojson module
    
    removed unused util class

--- modules/unsupported/cql-json/src/test/java/org/geotools/filter/text/cqljson/CQLJsonParsingTest.java
+++ modules/unsupported/cql-json/src/test/java/org/geotools/filter/text/cqljson/CQLJsonParsingTest.java
@@ -14,187 +14,254 @@ import org.opengis.filter.Filter;
 
 public class CQLJsonParsingTest {
     @Test
-    public void convertJsonToPredicates() throws IOException, CQLException {
-        // {"and":[{"eq":{"property":"swimming_pool","value":true}},{"or":[{"gt":{"property":"floor","value":5}},{"like":{"property":"material","value":"brick%"}},{"like":{"property":"material","value":"%brick"}}]}]}
-        Filter filter = parse("cqlJsonTest.json", 0);
+    public void convertLikeTest() throws IOException, CQLException {
+        Filter likeFilter = parse("cqlJsonTest.json", 6);
+        Assert.assertEquals("\"eo:instrument\" ILIKE 'OLI%'", ECQL.toCQL(likeFilter));
+    }
+
+    @Test
+    public void convertBinaryEqualsTest() throws IOException, CQLException {
+        Filter equalsFilter = parse("cqlJsonTest.json", 0);
+        Assert.assertEquals("scene_id = 'LC82030282019133LGN00'", ECQL.toCQL(equalsFilter));
+    }
+
+    @Test
+    public void convertBinaryNotEqualsTest() throws IOException, CQLException {
+        Filter equalsFilter = parse("cqlJsonTest.json", 1);
+        Assert.assertEquals("scene_id <> 'LC82030282019133LGN00'", ECQL.toCQL(equalsFilter));
+    }
+
+    @Test
+    public void convertBinaryGreaterThanTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 2);
+        Assert.assertEquals("count > 4", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertBinaryLessThanTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 3);
+        Assert.assertEquals("count < 4", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertBinaryGreaterThanOrEqTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 4);
+        Assert.assertEquals("count >= 4", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertBinaryLessThanOrEqTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 5);
+        Assert.assertEquals("count <= 4", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertBetweenTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 7);
+        Assert.assertEquals("\"eo:cloud_cover\" BETWEEN 0.1 AND 0.2", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertInTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 8);
+        Assert.assertEquals("\"landsat:wrs_path\" IN ('153','154','155')", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertOrTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 10);
+        Assert.assertEquals("\"ro:cloud_cover\" IN (0.1,0.2)", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertAndTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 9);
         Assert.assertEquals(
-                "swimming_pool = true AND (floor > 5 OR material ILIKE 'brick%' OR material ILIKE '%brick')",
-                ECQL.toCQL(filter));
+                "\"ro:cloud_cover\" < 0.1 AND \"landsat:wrs_row\" = 28 AND \"landsat:wrs_path\" = 203",
+                ECQL.toCQL(gtFilter));
     }
 
     @Test
-    public void convertAndIntersects() throws IOException, CQLException {
-        // {"and":[{"eq":{"property":"beamMode","value":"ScanSAR
-        // Narrow"}},{"eq":{"property":"swathDirection","value":"ascending"}},{"eq":{"property":"polarization","value":"HH+VV+HV+VH"}},{"intersects":{"property":"footprint","value":{"type":"Polygon","coordinates":[[[-77.117938,38.93686],[-77.040604,39.995648],[-76.910536,38.892912],[-77.039359,38.791753],[-77.047906,38.841462],[-77.034183,38.840655],[-77.033142,38.85749]]]}}}]}
-        Filter filter = parse("cqlJsonTest.json", 1);
+    public void convertIsNullTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 11);
+        Assert.assertEquals("scene_id IS NULL", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertContainsTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 12);
         Assert.assertEquals(
-                "beamMode = 'ScanSAR Narrow' AND swathDirection = 'ascending' AND polarization = 'HH+VV+HV+VH' AND INTERSECTS(footprint, POLYGON ((-77.117938 38.93686, -77.040604 39.995648, -76.910536 38.892912, -77.039359 38.791753, -77.047906 38.841462, -77.034183 38.840655, -77.033142 38.85749, -77.117938 38.93686)))",
-                ECQL.toCQL(filter));
+                "CONTAINS(location, POLYGON ((-118 33.8, -118 34, -117.9 34, -117.9 33.8, -118 33.8)))",
+                ECQL.toCQL(gtFilter));
     }
 
     @Test
-    public void convertLTE() throws IOException, CQLException {
-        // {"lte":{"property":"taxes","value":500}}
-        Filter filter = parse("cqlJsonTest.json", 2);
-        Assert.assertEquals("taxes <= 500", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertNotLTE() throws IOException, CQLException {
-        // {"not":{"lte":{"property":"taxes","value":500}}}
-        Filter filter = parse("cqlJsonTest.json", 3);
-        Assert.assertEquals("NOT (taxes <= 500)", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertAndLTE() throws IOException, CQLException {
-        // {"and":[{"not":{"lte":{"property":"taxes","value":500}}},{"lte":{"property":"taxes","value":800}}]}
-        Filter filter = parse("cqlJsonTest.json", 4);
-        Assert.assertEquals("NOT (taxes <= 500) AND taxes <= 800", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertCompoundAndLTE() throws IOException, CQLException {
-        // {"and":[{"lte":{"property":"taxes","value":500}},{"and":[{"lte":{"property":"taxes","value":600}},{"lte":{"property":"taxes","value":400}}]}]}
-        Filter filter = parse("cqlJsonTest.json", 5);
-        Assert.assertEquals("taxes <= 500 AND (taxes <= 600 AND taxes <= 400)", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertLike() throws IOException, CQLException {
-        // {"like":{"property":"owner","value":"% Jones %"}}
-        Filter filter = parse("cqlJsonTest.json", 6);
-        Assert.assertEquals("owner ILIKE '% Jones %'", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertStartsWithLike() throws IOException, CQLException {
-        // {"like":{"wildcard":"%","property":"owner","value":"Mike%"}}
-        Filter filter = parse("cqlJsonTest.json", 7);
-        Assert.assertEquals("owner ILIKE 'Mike%'", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertNotLike() throws IOException, CQLException {
-        // {"not":{"like":{"property":"owner","value":"% Mike %"}}}
-        Filter filter = parse("cqlJsonTest.json", 8);
-        Assert.assertEquals("NOT (owner ILIKE '% Mike %')", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertEQ() throws IOException, CQLException {
-        // {"eq":{"property":"swimming_pool","value":true}}
-        Filter filter = parse("cqlJsonTest.json", 9);
-        Assert.assertEquals("swimming_pool = true", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertGTAndEQ() throws IOException, CQLException {
-        // {"and":[{"gt":{"property":"floor","value":5}},{"eq":{"property":"swimming_pool","value":true}}]}
-        Filter filter = parse("cqlJsonTest.json", 10);
-        Assert.assertEquals("floor > 5 AND swimming_pool = true", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertAndOr() throws IOException, CQLException {
-        // {"and":[{"eq":{"property":"swimming_pool","value":true}},{"or":[{"gt":{"property":"floor","value":5}},{"like":{"property":"material","value":"brick%"}},{"like":{"property":"material","value":"%brick"}}]}]}
-        Filter filter = parse("cqlJsonTest.json", 11);
+    public void convertCrossesTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 13);
         Assert.assertEquals(
-                "swimming_pool = true AND (floor > 5 OR material ILIKE 'brick%' OR material ILIKE '%brick')",
-                ECQL.toCQL(filter));
+                "CROSSES(location, POLYGON ((-118 33.8, -118 34, -117.9 34, -117.9 33.8, -118 33.8)))",
+                ECQL.toCQL(gtFilter));
     }
 
     @Test
-    public void convertOrAnd2() throws IOException, CQLException {
-        // {"or":[{"and":[{"gt":{"property":"floors","value":5}},{"eq":{"property":"material","value":"brick"}}]},{"eq":{"property":"swimming_pool","value":true}}]}
-        Filter filter = parse("cqlJsonTest.json", 12);
+    public void convertDisjointTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 14);
         Assert.assertEquals(
-                "(floors > 5 AND material = 'brick') OR swimming_pool = true", ECQL.toCQL(filter));
+                "DISJOINT(location, POLYGON ((-118 33.8, -118 34, -117.9 34, -117.9 33.8, -118 33.8)))",
+                ECQL.toCQL(gtFilter));
     }
 
     @Test
-    public void convertOrNot() throws IOException, CQLException {
-        // {"or":[{"not":{"lt":{"property":"floors","value":5}}},{"eq":{"property":"swimming_pool","value":true}}]}
-        Filter filter = parse("cqlJsonTest.json", 13);
-        Assert.assertEquals("NOT (floors < 5) OR swimming_pool = true", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertLikeAnd() throws IOException, CQLException {
-        // {"and":[{"or":[{"like":{"property":"owner","value":"mike%"}},{"like":{"property":"owner","value":"Mike%"}}]},{"lt":{"property":"floors","value":4}}]}
-        Filter filter = parse("cqlJsonTest.json", 14);
+    public void convertSEqualsTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 15);
         Assert.assertEquals(
-                "(owner ILIKE 'mike%' OR owner ILIKE 'Mike%') AND floors < 4", ECQL.toCQL(filter));
+                "EQUALS(location, POLYGON ((-118 33.8, -118 34, -117.9 34, -117.9 33.8, -118 33.8)))",
+                ECQL.toCQL(gtFilter));
     }
 
     @Test
-    public void convertBefore() throws IOException, CQLException {
-        Filter filter = parse("cqlJsonTest.json", 15);
-        Assert.assertEquals("built BEFORE '2015-01-01'", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertAfter() throws IOException, CQLException {
-        // {"after":{"property":"built","value":"2012-06-05"}}
-        Filter filter = parse("cqlJsonTest.json", 16);
-        Assert.assertEquals("built AFTER '2012-06-05'", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertDuring() throws IOException, CQLException {
-        // {"during":{"property":"updated","value":["2017-06-10T07:30:00","2017-06-11T10:30:00"]}}
-        Filter filter = parse("cqlJsonTest.json", 23);
+    public void convertIntersectsTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 16);
         Assert.assertEquals(
-                "updated DURING '[2017-06-10T07:30:00, 2017-06-11T10:30:00]'", ECQL.toCQL(filter));
+                "INTERSECTS(location, POLYGON ((-118 33.8, -118 34, -117.9 34, -117.9 33.8, -118 33.8)))",
+                ECQL.toCQL(gtFilter));
     }
 
     @Test
-    public void convertWithin() throws IOException, CQLException {
-        // {"within":{"property":"location","value":{"bbox":[33.8,-118,34,-117.9]}}}
-        Filter filter = parse("cqlJsonTest.json", 17);
-        Assert.assertEquals(
-                "WITHIN(location, POLYGON ((33.8 -118, 33.8 -117.9, 34 -117.9, 34 -118, 33.8 -118)))",
-                ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertIntersects() throws IOException, CQLException {
-        // {"intersects":{"property":"location","value":{"type":"Polygon","coordinates":[[[-10,-10],[10,-10],[10,10],[-10,-10]]]}}}
-        Filter filter = parse("cqlJsonTest.json", 18);
+    public void convertIntersectsGeoJsonTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 20);
         Assert.assertEquals(
                 "INTERSECTS(location, POLYGON ((-10 -10, 10 -10, 10 10, -10 -10)))",
-                ECQL.toCQL(filter));
+                ECQL.toCQL(gtFilter));
     }
 
     @Test
-    public void convertAndWithin() throws IOException, CQLException {
-        // {"and":[{"gt":{"property":"floors","value":5}},{"within":{"property":"geometry","value":{"bbox":[33.8,-118,34,-117.9]}}}]}
-        Filter filter = parse("cqlJsonTest.json", 19);
+    public void convertOverlapsTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 17);
         Assert.assertEquals(
-                "floors > 5 AND WITHIN(geometry, POLYGON ((33.8 -118, 33.8 -117.9, 34 -117.9, 34 -118, 33.8 -118)))",
-                ECQL.toCQL(filter));
+                "OVERLAPS(location, POLYGON ((-118 33.8, -118 34, -117.9 34, -117.9 33.8, -118 33.8)))",
+                ECQL.toCQL(gtFilter));
     }
 
     @Test
-    public void convertTouchesLine() throws IOException, CQLException {
-        // {"touches":{"property":"location","value":{"type":"LineString","coordinates":[[100,0],[101,1]]}}}
-        Filter filter = parse("cqlJsonTest.json", 20);
-        Assert.assertEquals("TOUCHES(location, LINESTRING (100 0, 101 1))", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertContainsPoint() throws IOException, CQLException {
-        // {"contains":{"property":"location","value":{"type":"Point","coordinates":[100,0]}}}
-        Filter filter = parse("cqlJsonTest.json", 21);
-        Assert.assertEquals("CONTAINS(location, POINT (100 0))", ECQL.toCQL(filter));
-    }
-
-    @Test
-    public void convertOverlapsMultiPoint() throws IOException, CQLException {
-        // {"overlaps":{"property":"location","value":{"type":"MultiPoint","coordinates":[[100,0],[101,1]]}}}
-        Filter filter = parse("cqlJsonTest.json", 22);
+    public void convertTouchesTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 18);
         Assert.assertEquals(
-                "OVERLAPS(location, MULTIPOINT ((100 0), (101 1)))", ECQL.toCQL(filter));
+                "TOUCHES(location, POLYGON ((-118 33.8, -118 34, -117.9 34, -117.9 33.8, -118 33.8)))",
+                ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertWithinTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 19);
+        Assert.assertEquals(
+                "WITHIN(location, POLYGON ((-118 33.8, -118 34, -117.9 34, -117.9 33.8, -118 33.8)))",
+                ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertNotTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 21);
+        Assert.assertEquals("NOT (floors < 5)", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertAfterTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 23);
+        Assert.assertEquals("built AFTER '2012-06-05'", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertBeforeTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 22);
+        Assert.assertEquals("built BEFORE '2015-01-01'", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertTDisjointTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 24);
+        Assert.assertEquals(
+                "updated BEFORE '[2017-06-10T07:30:00Z, 2017-06-10T07:30:00Z]' AND updated AFTER '[2017-06-10T07:30:00Z, 2017-06-10T07:30:00Z]'",
+                ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertTEqualsTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 26);
+        Assert.assertEquals("built = '2012-06-05'", ECQL.toCQL(gtFilter));
+    }
+
+    @Test
+    public void convertDuringTest() throws IOException, CQLException {
+        Filter gtFilter = parse("cqlJsonTest.json", 25);
+        Assert.assertEquals(
+                "updated DURING '[2017-06-10T07:30:00Z, 2017-06-10T07:30:00Z]'",
+                ECQL.toCQL(gtFilter));
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertFinishedByTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 27);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertFinishingTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 28);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertTIntersectsTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 29);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertMeetsTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 30);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertMetByTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 31);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertTOverlappedByTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 32);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertTOverlapsTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 33);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertStartedByTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 34);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertStartsTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 35);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertAContainedByTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 36);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertAContainsTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 37);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertAEqualsTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 38);
+    }
+
+    @Test(expected = CQLException.class)
+    public void convertAOverlapsTest() throws IOException, CQLException {
+        parse("cqlJsonTest.json", 39);
     }
 
     private Filter parse(String file, Integer lineNumber) throws IOException, CQLException {

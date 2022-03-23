81a44d858629db8a154a71465e5980cb9fc8ef18
Jody Garnett <jody.garnett@gmail.com>
2022-03-23 16:59:05 -0700

    [GEOT-7094] Setup tutorial code as CC0 starting with quickstart example
    This is in keeping with our original public domain policy (that was only useful in the united states).

--- docs/src/main/java/org/geotools/wmts/WMTSMapViewer.java
+++ docs/src/main/java/org/geotools/wmts/WMTSMapViewer.java
@@ -1,11 +1,13 @@
 /*
- *    GeoTools - The Open Source Java GIS Toolkit
- *    http://geotools.org
+ *    GeoTools Sample code and Tutorials by Open Source Geospatial Foundation, and others
+ *    https://docs.geotools.org
  *
- *    (C) 2022, Open Source Geospatial Foundation (OSGeo)
+ *    To the extent possible under law, the author(s) have dedicated all copyright
+ *    and related and neighboring rights to this software to the public domain worldwide.
+ *    This software is distributed without any warranty.
  *
- *    This file is hereby placed into the Public Domain. This means anyone is
- *    free to do whatever they wish with this file. Use it well and enjoy!
+ *    You should have received a copy of the CC0 Public Domain Dedication along with this
+ *    software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
 package org.geotools.wmts;
 

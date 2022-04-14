8ffadd9c222b62d39044a709bd7aa5a78bd4d456
Stefan Uhrig <stefan.uhrig@sap.com>
2022-04-14 14:26:16 +0200

    Fix broken HANA online tests

--- modules/plugin/jdbc/jdbc-hana/src/test/java/org/geotools/data/hana/HanaGroupByVisitorTestSetup.java
+++ modules/plugin/jdbc/jdbc-hana/src/test/java/org/geotools/data/hana/HanaGroupByVisitorTestSetup.java
@@ -41,34 +41,119 @@ public class HanaGroupByVisitorTestSetup extends JDBCGroupByVisitorTestSetup {
                 {"building_type", "VARCHAR(255)"},
                 {"energy_type", "VARCHAR(255)"},
                 {"energy_consumption", "DECIMAL(5, 1)"},
-                {"last_update", "TIMESTAMP"}
+                {"last_update", "TIMESTAMP"},
+                {"last_update_date", "DATE"}
             };
             htu.createTestTable(TABLE, cols);
 
             htu.insertIntoTestTable(
-                    TABLE, 1, "SCHOOL_A", "SCHOOL", "FLOWING_WATER", 50.0, "2016-06-03 12:00:00");
+                    TABLE,
+                    1,
+                    "SCHOOL_A",
+                    "SCHOOL",
+                    "FLOWING_WATER",
+                    50.0,
+                    "2016-06-03 12:00:00",
+                    "2016-06-03");
             htu.insertIntoTestTable(
-                    TABLE, 2, "SCHOOL_A", "SCHOOL", "NUCLEAR", 10.0, "2016-06-03 16:00:00");
+                    TABLE,
+                    2,
+                    "SCHOOL_A",
+                    "SCHOOL",
+                    "NUCLEAR",
+                    10.0,
+                    "2016-06-03 16:00:00",
+                    "2016-06-03");
             htu.insertIntoTestTable(
-                    TABLE, 3, "SCHOOL_A", "SCHOOL", "WIND", 20.0, "2016-06-03 20:00:00");
+                    TABLE,
+                    3,
+                    "SCHOOL_A",
+                    "SCHOOL",
+                    "WIND",
+                    20.0,
+                    "2016-06-03 20:00:00",
+                    "2016-06-03");
             htu.insertIntoTestTable(
-                    TABLE, 4, "SCHOOL_B", "SCHOOL", "SOLAR", 30.0, "2016-06-05 12:00:00");
+                    TABLE,
+                    4,
+                    "SCHOOL_B",
+                    "SCHOOL",
+                    "SOLAR",
+                    30.0,
+                    "2016-06-05 12:00:00",
+                    "2016-06-05");
             htu.insertIntoTestTable(
-                    TABLE, 5, "SCHOOL_B", "SCHOOL", "FUEL", 60.0, "2016-06-06 12:00:00");
+                    TABLE,
+                    5,
+                    "SCHOOL_B",
+                    "SCHOOL",
+                    "FUEL",
+                    60.0,
+                    "2016-06-06 12:00:00",
+                    "2016-06-06");
             htu.insertIntoTestTable(
-                    TABLE, 6, "SCHOOL_B", "SCHOOL", "NUCLEAR", 10.0, "2016-06-06 14:00:00");
+                    TABLE,
+                    6,
+                    "SCHOOL_B",
+                    "SCHOOL",
+                    "NUCLEAR",
+                    10.0,
+                    "2016-06-06 14:00:00",
+                    "2016-06-06");
             htu.insertIntoTestTable(
-                    TABLE, 7, "FABRIC_A", "FABRIC", "FLOWING_WATER", 500.0, "2016-06-07 12:00:00");
+                    TABLE,
+                    7,
+                    "FABRIC_A",
+                    "FABRIC",
+                    "FLOWING_WATER",
+                    500.0,
+                    "2016-06-07 12:00:00",
+                    "2016-06-07");
             htu.insertIntoTestTable(
-                    TABLE, 8, "FABRIC_A", "FABRIC", "NUCLEAR", 150.0, "2016-06-07 18:00:00");
+                    TABLE,
+                    8,
+                    "FABRIC_A",
+                    "FABRIC",
+                    "NUCLEAR",
+                    150.0,
+                    "2016-06-07 18:00:00",
+                    "2016-06-07");
             htu.insertIntoTestTable(
-                    TABLE, 9, "FABRIC_B", "FABRIC", "WIND", 20.0, "2016-06-07 20:00:00");
+                    TABLE,
+                    9,
+                    "FABRIC_B",
+                    "FABRIC",
+                    "WIND",
+                    20.0,
+                    "2016-06-07 20:00:00",
+                    "2016-06-07");
             htu.insertIntoTestTable(
-                    TABLE, 10, "FABRIC_B", "FABRIC", "SOLAR", 30.0, "2016-06-15 12:00:00");
+                    TABLE,
+                    10,
+                    "FABRIC_B",
+                    "FABRIC",
+                    "SOLAR",
+                    30.0,
+                    "2016-06-15 12:00:00",
+                    "2016-06-15");
             htu.insertIntoTestTable(
-                    TABLE, 11, "HOUSE_A", "HOUSE", "FUEL", 6.0, "2016-06-15 19:00:00");
+                    TABLE,
+                    11,
+                    "HOUSE_A",
+                    "HOUSE",
+                    "FUEL",
+                    6.0,
+                    "2016-06-15 19:00:00",
+                    "2016-06-15");
             htu.insertIntoTestTable(
-                    TABLE, 12, "HOUSE_B", "HOUSE", "NUCLEAR", 4.0, "2016-06-15 20:00:00");
+                    TABLE,
+                    12,
+                    "HOUSE_B",
+                    "HOUSE",
+                    "NUCLEAR",
+                    4.0,
+                    "2016-06-15 20:00:00",
+                    "2016-06-15");
         }
     }
 

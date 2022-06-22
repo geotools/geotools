05095c1d5c8f8eb14e374fc94a3eba60fa954eee
Ian Turton <ijturton@gmail.com>
2022-06-22 16:23:43 +0100

    fix qa

--- modules/library/main/src/main/java/org/geotools/geometry/jts/LiteCoordinateSequence.java
+++ modules/library/main/src/main/java/org/geotools/geometry/jts/LiteCoordinateSequence.java
@@ -1,16 +1,20 @@
 /*
- * GeoTools - The Open Source Java GIS Toolkit http://geotools.org
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
  *
- * (C) 2004-2015, Open Source Geospatial Foundation (OSGeo)
+ *    (C) 2004-2022, Open Source Geospatial Foundation (OSGeo)
  *
- * This library is free software; you can redistribute it and/or modify it under the terms of the
- * GNU Lesser General Public License as published by the Free Software Foundation; version 2.1 of
- * the License.
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
+
 package org.geotools.geometry.jts;
 
 import java.util.ArrayList;

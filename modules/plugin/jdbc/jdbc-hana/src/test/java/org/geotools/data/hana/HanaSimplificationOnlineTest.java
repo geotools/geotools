aae880aefad0c26d1171610865c0d5183cd819da
Stefan Uhrig <stefan.uhrig@sap.com>
2022-10-17 15:44:33 +0200

    [GEOT-7242] Simplification fails for geographic shapes in HANA
    Simplification for geographic ("round-earth") shapes is not supported
    by HANA. Only projected ("planar") shapes can be simplified.
    
    The encodeGeometryColumnSimplified() method must verify that the shape
    is planar before applying the ST_Simplify() function on it.

---
+++ modules/plugin/jdbc/jdbc-hana/src/test/java/org/geotools/data/hana/HanaSimplificationOnlineTest.java
@@ -0,0 +1,80 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2002-2022, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.data.hana;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assume.assumeTrue;
+
+import org.geotools.data.Query;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.jdbc.JDBCTestSetup;
+import org.geotools.jdbc.JDBCTestSupport;
+import org.geotools.util.factory.Hints;
+import org.junit.Test;
+import org.locationtech.jts.geom.Geometry;
+
+/** @author Stefan Uhrig, SAP SE */
+public class HanaSimplificationOnlineTest extends JDBCTestSupport {
+
+    @Override
+    protected JDBCTestSetup createTestSetup() {
+        return new HanaSimplificationTestSetup(new HanaTestSetupPSPooling());
+    }
+
+    @Test
+    public void testRoundEarthSimplification() throws Exception {
+        SimpleFeatureSource fs = dataStore.getFeatureSource(tname("roundEarth"));
+
+        assumeTrue(fs.getSupportedHints().contains(Hints.GEOMETRY_SIMPLIFICATION));
+
+        Query query = new Query();
+        Hints hints = new Hints(Hints.GEOMETRY_SIMPLIFICATION, 0.5);
+        query.setHints(hints);
+
+        SimpleFeatureCollection coll = fs.getFeatures(query);
+        Geometry geom = null;
+        try (SimpleFeatureIterator iter = coll.features()) {
+            if (iter.hasNext()) {
+                geom = (Geometry) iter.next().getDefaultGeometry();
+            }
+        }
+        assertNotNull(geom);
+    }
+
+    @Test
+    public void testPlanarSimplification() throws Exception {
+        SimpleFeatureSource fs = dataStore.getFeatureSource(tname("planar"));
+
+        assumeTrue(fs.getSupportedHints().contains(Hints.GEOMETRY_SIMPLIFICATION));
+
+        Query query = new Query();
+        Hints hints = new Hints(Hints.GEOMETRY_SIMPLIFICATION, 0.5);
+        query.setHints(hints);
+
+        SimpleFeatureCollection coll = fs.getFeatures(query);
+        Geometry geom = null;
+        try (SimpleFeatureIterator iter = coll.features()) {
+            if (iter.hasNext()) {
+                geom = (Geometry) iter.next().getDefaultGeometry();
+            }
+        }
+        assertEquals(2, geom.getNumPoints());
+    }
+}

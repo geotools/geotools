fe583f4b4e77c06078747e2872158de69b23c8a3
chengwu <1126651085@qq.com>
2022-10-18 17:13:17 +0800

    ERROR: function postgis_lib_version() does not exist
    If the PostGIS extension is in other schema (not public schema), an exception is thrown. "ERROR: function postgis_lib_version() does not exist"

--- modules/plugin/jdbc/jdbc-postgis/src/main/java/org/geotools/data/postgis/PostGISDialect.java
+++ modules/plugin/jdbc/jdbc-postgis/src/main/java/org/geotools/data/postgis/PostGISDialect.java
@@ -1491,7 +1491,7 @@ public class PostGISDialect extends BasicSQLDialect {
             ResultSet rs = null;
             try {
                 st = conn.createStatement();
-                rs = st.executeQuery("select PostGIS_Lib_Version()");
+                rs = st.executeQuery("select PostGIS_Lib_Version()");// If the PostGIS extension is in other schema (not public schema), an exception is thrown. 
                 if (rs.next()) {
                     version = new Version(rs.getString(1));
                 }

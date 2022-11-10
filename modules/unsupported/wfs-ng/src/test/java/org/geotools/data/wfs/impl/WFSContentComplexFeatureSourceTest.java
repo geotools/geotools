9a807606adaa51a9a56e4f8ed5d1ad94096faf65
Roar Brænden <1826286+roarbra@users.noreply.github.com>
2022-11-10 10:25:00 +0100

    [GEOT-7261] WFSContentComplexFeatureSource.getBounds will end up in StackOverflowException

--- modules/unsupported/wfs-ng/src/test/java/org/geotools/data/wfs/impl/WFSContentComplexFeatureSourceTest.java
+++ modules/unsupported/wfs-ng/src/test/java/org/geotools/data/wfs/impl/WFSContentComplexFeatureSourceTest.java
@@ -19,16 +19,21 @@ import org.geotools.data.Query;
 import org.geotools.data.wfs.TestHttpClient;
 import org.geotools.data.wfs.TestWFSClient;
 import org.geotools.data.wfs.internal.WFSClient;
+import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureIterator;
 import org.geotools.feature.NameImpl;
+import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.http.MockHttpResponse;
+import org.geotools.referencing.CRS;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.opengis.feature.Feature;
 import org.opengis.feature.type.FeatureType;
 import org.opengis.feature.type.Name;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 /**
  * WFS returning complex feature source
@@ -47,6 +52,8 @@ public class WFSContentComplexFeatureSourceTest {
             new NameImpl(
                     "http://skjema.geonorge.no/SOSI/produktspesifikasjon/StedsnavnForVanligBruk/20181115",
                     STED);
+
+    private static final String DEFAULT_SRS = "urn:ogc:def:crs:EPSG::4258";
 
     @Test
     public void testGetFeaturesWithoutArgument() throws Exception {
@@ -77,6 +84,46 @@ public class WFSContentComplexFeatureSourceTest {
         try (FeatureIterator<Feature> features = collection.features()) {
             Assert.assertNotNull(features);
         }
+    }
+
+    @Test
+    public void testGetBounds() throws Exception {
+        final WFSClient client = createWFSClient();
+        final WFSContentDataAccess dataAccess = createDataAccess(client);
+
+        ReferencedEnvelope envelope = dataAccess.getFeatureSource(STED_NAME).getBounds();
+        Assert.assertNotNull(envelope);
+        ReferencedEnvelope actual =
+                new ReferencedEnvelope(
+                        58.111523, 70.671561, 6.034809, 30.528068, CRS.decode(DEFAULT_SRS));
+        assertEnvelope(actual, envelope);
+
+        CoordinateReferenceSystem utm33 = CRS.decode("EPSG:32633");
+        Query reprojectQuery = new Query();
+        reprojectQuery.setCoordinateSystemReproject(utm33);
+
+        ReferencedEnvelope envelopeReprojected =
+                dataAccess.getFeatureSource(STED_NAME).getBounds(reprojectQuery);
+        ReferencedEnvelope actualReprojected = actual.transform(utm33, true);
+        assertEnvelope(actualReprojected, envelopeReprojected);
+
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
+        Query filteredQuery =
+                new Query(STED, ff.bbox("posisjon", 60.0, 20.0, 61.0, 21.0, DEFAULT_SRS));
+        ReferencedEnvelope envelopeFilter =
+                dataAccess.getFeatureSource(STED_NAME).getBounds(filteredQuery);
+        Assert.assertNull("We're assuming a null when using a filter.", envelopeFilter);
+    }
+
+    private void assertEnvelope(ReferencedEnvelope actual, ReferencedEnvelope test) {
+        Assert.assertTrue(
+                CRS.equalsIgnoreMetadata(
+                        actual.getCoordinateReferenceSystem(),
+                        test.getCoordinateReferenceSystem()));
+        Assert.assertEquals(actual.getMinX(), test.getMinX(), 0.1);
+        Assert.assertEquals(actual.getMaxX(), test.getMaxX(), 0.1);
+        Assert.assertEquals(actual.getMinY(), test.getMinY(), 0.1);
+        Assert.assertEquals(actual.getMaxY(), test.getMaxY(), 0.1);
     }
 
     private TestWFSClient createWFSClient() throws Exception {

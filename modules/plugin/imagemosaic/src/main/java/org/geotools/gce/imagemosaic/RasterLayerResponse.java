5a0f6e591cdc8464ed4ae13260bcd5904d24f1c4
aaime <andrea.aime@gmail.com>
2022-11-23 18:04:23 +0100

    [GEOT-7270] GeoTIFFReader should expose GDAL band specific metadata when available

--- modules/plugin/imagemosaic/src/main/java/org/geotools/gce/imagemosaic/RasterLayerResponse.java
+++ modules/plugin/imagemosaic/src/main/java/org/geotools/gce/imagemosaic/RasterLayerResponse.java
@@ -1259,7 +1259,7 @@ public class RasterLayerResponse {
             properties.put(GridCoverage2DReader.SOURCE_URL_PROPERTY, sourceUrl);
         }
         if (mosaicOutput.pamDataset != null) {
-            properties.put(Utils.PAM_DATASET, mosaicOutput.pamDataset);
+            properties.put(GridCoverage2DReader.PAM_DATASET, mosaicOutput.pamDataset);
         }
         // Setting NoData as the NoData for the first Band
         ImageWorker w = new ImageWorker(image);

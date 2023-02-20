39a268ed0974c7a89040d1fd72c269965fd391c1
marthamareal <marthamareal@gmail.com>
2023-02-20 15:58:54 +0000

    updating version numbers and README for 24.7

--- docs/common.py
+++ docs/common.py
@@ -66,7 +66,7 @@ copyright = u'{} Open Source Geospatial Foundation'.format(year)
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '24-SNAPSHOT'
+release = '24.7'
 
 # The short X.Y version.
 version = release

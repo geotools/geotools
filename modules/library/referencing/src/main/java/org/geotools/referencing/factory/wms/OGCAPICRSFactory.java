7aef735903d807e57d7b47882692bd3a6df7a59f
aaime <andrea.aime@gmail.com>
2023-03-02 14:53:56 +0100

    [GEOT-7314] Add OGC authority so that GeoTools can parse URIs such as "http://www.opengis.net/def/crs/OGC/1.3/CRS84"

---
+++ modules/library/referencing/src/main/java/org/geotools/referencing/factory/wms/OGCAPICRSFactory.java
@@ -0,0 +1,33 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2023, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.referencing.factory.wms;
+
+import org.geotools.metadata.iso.citation.Citations;
+import org.opengis.metadata.citation.Citation;
+
+/**
+ * Similar to the OGC CRS factory, but with a different authority name, designed to support URNs
+ * coming from the OGC API - Features - Part 1: Core specification (e.g.,
+ * "http://www.opengis.net/def/crs/OGC/1.3/CRS84").
+ */
+public class OGCAPICRSFactory extends WebCRSFactory {
+
+    @Override
+    public Citation getAuthority() {
+        return Citations.OGC;
+    }
+}

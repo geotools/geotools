47c1b87bc982b0acfd1a9a2c1773721064fb0a5f
aaime <andrea.aime@gmail.com>
2023-03-24 17:03:13 +0100

    [GEOT-6512] Control remote HTTP requests sent by GeoTools

---
+++ modules/library/main/src/main/java/org/geotools/data/ows/URLCheckers.java
@@ -0,0 +1,120 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2023, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.data.ows;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.geotools.util.factory.FactoryCreator;
+import org.geotools.util.factory.FactoryRegistry;
+import org.geotools.util.factory.GeoTools;
+
+/**
+ * Scans for all available URLChecker implementations, allows to register new ones, and provides a
+ * convenient method to apply them on a given location.
+ */
+public class URLCheckers {
+
+    /** The service registry for {@link URLCheckers}. Will be initialized only when first needed. */
+    private static FactoryRegistry registry;
+
+    private static FactoryRegistry getServiceRegistry() {
+        synchronized (URLChecker.class) {
+            if (registry == null) {
+                registry = new FactoryCreator(URLChecker.class);
+            }
+        }
+        return registry;
+    }
+
+    /** Re-initializes all static state, in particular, the {@link URLChecker} service registry */
+    public static synchronized void reset() {
+        if (registry == null) {
+            // nothing to do
+            return;
+        }
+        registry.deregisterAll();
+        registry.scanForPlugins();
+    }
+
+    /**
+     * Dynamically register a new URLChecker. The operation removes all instances of the same
+     * classes already present in the registry, if multiple instances are needed, one can use
+     * subclasses (eventually, anonymous inner classes) so that the instances are not sharing
+     * exactly the same class.
+     */
+    public static void register(URLChecker checker) {
+        getServiceRegistry().registerFactory(checker, URLChecker.class);
+    }
+
+    /**
+     * Dynamically removes a new URLChecker, that might have been registered using the {@link
+     * #register(URLChecker)} method.
+     */
+    public static void deregister(URLChecker checker) {
+        getServiceRegistry().deregisterFactory(checker, URLChecker.class);
+    }
+
+    /** @return enabled list of org.geotools.data.ows.URLChecker implementations */
+    public static List<URLChecker> getEnabledURLCheckers() {
+        return getServiceRegistry()
+                .getFactories(URLChecker.class, null, GeoTools.getDefaultHints())
+                .filter(u -> u.isEnabled())
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Confirm the URL against all enabled URLChecker
+     *
+     * @param url to confirm using all available URLCheckers
+     * @throws URLCheckerException if the URL is not allowed for use
+     */
+    public static void confirm(URL url) throws URLCheckerException {
+        confirm(url.toExternalForm());
+    }
+
+    /**
+     * Confirms URI against all enabled URLChecker
+     *
+     * @param uri to evaluate using all available URLCheckers
+     * @throws URLCheckerException if the URI is not allowed for use
+     */
+    public static void confirm(URI uri) throws URLCheckerException {
+        confirm(uri.toString());
+    }
+
+    /**
+     * Confirm the location against all enabled URLChecker
+     *
+     * @param location String(URI/URI/path) to evaluate using all available URLCheckers
+     * @throws URLCheckerException if the location is not allowed for use
+     */
+    public static void confirm(String location) throws URLCheckerException {
+        // get enabled URLChecker only
+        List<URLChecker> checkers = getEnabledURLCheckers();
+        // no enabled checkers, the system is not configured... don't do anything
+        if (checkers.isEmpty()) return;
+        // evaluate using all available implementations
+        for (URLChecker urlChecker : checkers) {
+            if (urlChecker.confirm(location)) return;
+        }
+        // no URLChecker evaluated the passed URL/URI
+        throw new URLCheckerException(
+                "Evaluation Failure: " + location + " was not accepted by external URL checks");
+    }
+}

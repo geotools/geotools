78be004051e547e7df693029e0cd4343c54a7797
Sebastian Orlowski <orlowski@atesio.de>
2023-05-19 10:27:33 +0200

    [GEOT-7332] Resolve incompatibility between LGPL-2.1 and LGPL-3+
    Before this change, the file GeometrySmoother.java was licensed both
    with "LGPL-2.1 only" and "LGPL-3.0 or any later version", which is
    impossible to fulfill at the same time -- which license could be used
    for this file?
    
    By changing "LGPL-2.1"to "LGPL-2.1 or any later version", it is
    possible to license this file under LGPL-3.0, hereby resolving this
    conflict.

--- modules/library/main/src/main/java/org/geotools/geometry/jts/GeometrySmoother.java
+++ modules/library/main/src/main/java/org/geotools/geometry/jts/GeometrySmoother.java
@@ -7,7 +7,7 @@
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation;
- *    version 2.1 of the License.
+ *    version 2.1 or any later versiun of the License.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of

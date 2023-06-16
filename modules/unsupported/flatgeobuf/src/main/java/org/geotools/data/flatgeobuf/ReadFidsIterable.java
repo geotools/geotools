d7d59ccc1b1e8a0106c1347459b7c0b5afad9988
Björn Harrtell <bjorn@wololo.org>
2023-06-16 23:00:43 +0200

    [GEOT-7381] FlatGeobufDataStore FID filtering

---
+++ modules/unsupported/flatgeobuf/src/main/java/org/geotools/data/flatgeobuf/ReadFidsIterable.java
@@ -0,0 +1,64 @@
+package org.geotools.data.flatgeobuf;
+
+import com.google.common.io.LittleEndianDataInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.opengis.feature.simple.SimpleFeature;
+import org.wololo.flatgeobuf.HeaderMeta;
+import org.wololo.flatgeobuf.PackedRTree;
+
+final class ReadFidsIterable implements Iterable<SimpleFeature> {
+    private final class IteratorImplementation implements Iterator<SimpleFeature> {
+        int i = 0;
+        long pos = 0;
+
+        @Override
+        public boolean hasNext() {
+            return i < featureOffsets.length;
+        }
+
+        @Override
+        public SimpleFeature next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            try {
+                long delta = featureOffsets[i] - pos;
+                FlatGeobufFeatureReader.skipNBytes(data, delta);
+                int featureSize = data.readInt();
+                SimpleFeature feature =
+                        FeatureConversions.deserialize(data, fb, headerMeta, fids[i], featureSize);
+                pos += delta + 4 + featureSize;
+                i++;
+                return feature;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private final SimpleFeatureBuilder fb;
+    private final long[] fids;
+    private final long[] featureOffsets;
+    private final HeaderMeta headerMeta;
+    private final LittleEndianDataInputStream data;
+
+    ReadFidsIterable(
+            SimpleFeatureBuilder fb,
+            long[] fids,
+            HeaderMeta headerMeta,
+            LittleEndianDataInputStream data)
+            throws IOException {
+        this.fb = fb;
+        this.fids = fids;
+        this.featureOffsets = PackedRTree.readFeatureOffsets(data, fids, headerMeta);
+        this.headerMeta = headerMeta;
+        this.data = data;
+    }
+
+    @Override
+    public Iterator<SimpleFeature> iterator() {
+        Iterator<SimpleFeature> it = new IteratorImplementation();
+        return it;
+    }
+}

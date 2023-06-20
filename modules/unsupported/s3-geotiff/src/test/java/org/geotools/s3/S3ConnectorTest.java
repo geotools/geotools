389ea30b17b259923e96df37a0c6d5ea8b119a92
Callum McKenna <contact@callum.id.au>
2023-06-21 08:05:11 +1000

    Fix formatting

--- modules/unsupported/s3-geotiff/src/test/java/org/geotools/s3/S3ConnectorTest.java
+++ modules/unsupported/s3-geotiff/src/test/java/org/geotools/s3/S3ConnectorTest.java
@@ -16,10 +16,10 @@
  */
 package org.geotools.s3;
 
+import static org.junit.Assert.assertEquals;
+
 import com.amazonaws.services.s3.AmazonS3;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class S3ConnectorTest {
     @Test
@@ -32,7 +32,8 @@ public class S3ConnectorTest {
 
     @Test
     public void testRegionOverrideWithDefaultCredentialChain() {
-        S3Connector s3Connector = new S3Connector("s3://bucket/prefix/raster.tif?awsRegion=US_EAST_2");
+        S3Connector s3Connector =
+                new S3Connector("s3://bucket/prefix/raster.tif?awsRegion=US_EAST_2");
 
         AmazonS3 s3Client = s3Connector.getS3Client();
         assertEquals("US_East_2", s3Client.getRegion().name());
@@ -48,7 +49,8 @@ public class S3ConnectorTest {
 
     @Test
     public void testRegionOverrideWithAnonymousCredentialChain() {
-        S3Connector s3Connector = new S3Connector("s3://bucket/prefix/raster.tif?useAnon=true&awsRegion=US_EAST_2");
+        S3Connector s3Connector =
+                new S3Connector("s3://bucket/prefix/raster.tif?useAnon=true&awsRegion=US_EAST_2");
 
         AmazonS3 s3Client = s3Connector.getS3Client();
         assertEquals("US_East_2", s3Client.getRegion().name());

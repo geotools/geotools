e6ca03eb9b0fc0ce6f7a7cbbca990fea749c4f2f
Joseph Miller <millerj@pixia.com>
2023-10-20 13:34:31 -0400

    [GEOT-7480] SumVisitor NPE When Value from Database is Null
    removed stddev because geopackage does not support it
    
    cleanup
    
    more cleanup
    
    cleanup3
    
    comment cleanup

--- modules/plugin/jdbc/jdbc-sqlserver/src/test/java/org/geotools/data/sqlserver/SQLServerGroupByVisitorTestSetup.java
+++ modules/plugin/jdbc/jdbc-sqlserver/src/test/java/org/geotools/data/sqlserver/SQLServerGroupByVisitorTestSetup.java
@@ -30,22 +30,22 @@ public class SQLServerGroupByVisitorTestSetup extends JDBCGroupByVisitorTestSetu
         // against date fields, as opposed to timestamp fields
         run(
                 "CREATE TABLE buildings_group_by_tests (id int PRIMARY KEY, building_id varchar(255), "
-                        + "building_type varchar(255), energy_type varchar(255), "
+                        + "building_type varchar(255), energy_type varchar(255), fuel_consumption float,"
                         + "energy_consumption float, last_update datetime, last_update_date date)");
         run(
                 "INSERT INTO buildings_group_by_tests VALUES "
-                        + "(1, 'SCHOOL_A', 'SCHOOL', 'FLOWING_WATER', 50.0, CAST('2016-06-03 12:00:00' AS DATETIME), CAST('2016-06-03' AS DATE)),"
-                        + "(2, 'SCHOOL_A', 'SCHOOL', 'NUCLEAR', 10.0, CAST('2016-06-03 16:00:00' AS DATETIME), CAST('2016-06-03' AS DATE)),"
-                        + "(3, 'SCHOOL_A', 'SCHOOL', 'WIND', 20.0, CAST('2016-06-03 20:00:00' AS DATETIME), CAST('2016-06-03' AS DATE)),"
-                        + "(4, 'SCHOOL_B', 'SCHOOL', 'SOLAR', 30.0, CAST('2016-06-05 12:00:00' AS DATETIME), CAST('2016-06-05' AS DATE)),"
-                        + "(5, 'SCHOOL_B', 'SCHOOL', 'FUEL', 60.0, CAST('2016-06-06 12:00:00' AS DATETIME), CAST('2016-06-06' AS DATE)),"
-                        + "(6, 'SCHOOL_B', 'SCHOOL', 'NUCLEAR', 10.0, CAST('2016-06-06 14:00:00' AS DATETIME), CAST('2016-06-06' AS DATE)),"
-                        + "(7, 'FABRIC_A', 'FABRIC', 'FLOWING_WATER', 500.0, CAST('2016-06-07 12:00:00' AS DATETIME), CAST('2016-06-07' AS DATE)),"
-                        + "(8, 'FABRIC_A', 'FABRIC', 'NUCLEAR', 150.0, CAST('2016-06-07 18:00:00' AS DATETIME), CAST('2016-06-07' AS DATE)),"
-                        + "(9, 'FABRIC_B', 'FABRIC', 'WIND', 20.0, CAST('2016-06-07 20:00:00' AS DATETIME), CAST('2016-06-07' AS DATE)),"
-                        + "(10, 'FABRIC_B', 'FABRIC', 'SOLAR', 30.0, CAST('2016-06-15 12:00:00' AS DATETIME), CAST('2016-06-15' AS DATE)),"
-                        + "(11, 'HOUSE_A', 'HOUSE', 'FUEL', 6.0, CAST('2016-06-15 19:00:00' AS DATETIME), CAST('2016-06-15' AS DATE)),"
-                        + "(12, 'HOUSE_B', 'HOUSE', 'NUCLEAR', 4.0, CAST('2016-06-15 20:00:00' AS DATETIME), CAST('2016-06-15' AS DATE));");
+                        + "(1, 'SCHOOL_A', 'SCHOOL', 'FLOWING_WATER', NULL, 50.0, CAST('2016-06-03 12:00:00' AS DATETIME), CAST('2016-06-03' AS DATE)),"
+                        + "(2, 'SCHOOL_A', 'SCHOOL', 'NUCLEAR', NULL, 10.0, CAST('2016-06-03 16:00:00' AS DATETIME), CAST('2016-06-03' AS DATE)),"
+                        + "(3, 'SCHOOL_A', 'SCHOOL', 'WIND', NULL, 20.0, CAST('2016-06-03 20:00:00' AS DATETIME), CAST('2016-06-03' AS DATE)),"
+                        + "(4, 'SCHOOL_B', 'SCHOOL', 'SOLAR', NULL, 30.0, CAST('2016-06-05 12:00:00' AS DATETIME), CAST('2016-06-05' AS DATE)),"
+                        + "(5, 'SCHOOL_B', 'SCHOOL', 'FUEL', NULL, 60.0, CAST('2016-06-06 12:00:00' AS DATETIME), CAST('2016-06-06' AS DATE)),"
+                        + "(6, 'SCHOOL_B', 'SCHOOL', 'NUCLEAR', NULL, 10.0, CAST('2016-06-06 14:00:00' AS DATETIME), CAST('2016-06-06' AS DATE)),"
+                        + "(7, 'FABRIC_A', 'FABRIC', 'FLOWING_WATER', NULL, 500.0, CAST('2016-06-07 12:00:00' AS DATETIME), CAST('2016-06-07' AS DATE)),"
+                        + "(8, 'FABRIC_A', 'FABRIC', 'NUCLEAR', NULL, 150.0, CAST('2016-06-07 18:00:00' AS DATETIME), CAST('2016-06-07' AS DATE)),"
+                        + "(9, 'FABRIC_B', 'FABRIC', 'WIND', NULL, 20.0, CAST('2016-06-07 20:00:00' AS DATETIME), CAST('2016-06-07' AS DATE)),"
+                        + "(10, 'FABRIC_B', 'FABRIC', 'SOLAR', NULL, 30.0, CAST('2016-06-15 12:00:00' AS DATETIME), CAST('2016-06-15' AS DATE)),"
+                        + "(11, 'HOUSE_A', 'HOUSE', 'FUEL', NULL, 6.0, CAST('2016-06-15 19:00:00' AS DATETIME), CAST('2016-06-15' AS DATE)),"
+                        + "(12, 'HOUSE_B', 'HOUSE', 'NUCLEAR', NULL, 4.0, CAST('2016-06-15 20:00:00' AS DATETIME), CAST('2016-06-15' AS DATE));");
     }
 
     @Override

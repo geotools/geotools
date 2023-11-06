656137c550d6aeca71ca0e642b7079a06dffcf38
Peter Smythe <peter@afrigis.co.za>
2023-11-06 14:38:52 +0200

    Efficiency improvement from Andrea, to avoid regex

--- modules/library/xml/src/main/java/org/geotools/xml/transform/TransformerBase.java
+++ modules/library/xml/src/main/java/org/geotools/xml/transform/TransformerBase.java
@@ -794,7 +794,10 @@ public abstract class TransformerBase {
             start(element, atts);
 
             if (content != null) {
-                if (content.matches("^\\s+$")) {
+                int length = content.length();
+                if (length > 0
+                        && (Character.isWhitespace(content.charAt(0))
+                                || Character.isWhitespace(content.charAt(length - 1)))) {
                     cdata(content);
                 } else {
                     chars(content);

a87c95747ed9cb37f1d96a10b1016f7475b34c8c
Björn Harrtell <bjorn@wololo.org>
2023-11-20 11:54:37 +0100

    Simplify assert

--- modules/unsupported/flatgeobuf/src/test/java/org/geotools/data/flatgeobuf/FlatGeobufDataStoreTest.java
+++ modules/unsupported/flatgeobuf/src/test/java/org/geotools/data/flatgeobuf/FlatGeobufDataStoreTest.java
@@ -881,7 +881,7 @@ public class FlatGeobufDataStoreTest {
             SimpleFeature f2 = it.next();
             assertEquals("countries.48", f2.getID());
             boolean hasNext = it.hasNext();
-            assertEquals(false, hasNext);
+            assertFalse(hasNext);
         }
     }
 

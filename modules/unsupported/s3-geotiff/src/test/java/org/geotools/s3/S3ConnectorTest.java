7cbc39b886e4c03a722ecdfeaf3257c7ccef3a36
Matthias Loeks <matthias@loeks.net>
2023-12-20 11:38:44 +0100

    Fix updating immutable AmazonS3 client
    When using a custom endpoint (for instance using a mock in
    integration tests), the S3Connector still tried to update
    the immutable client and throw.
    
    A similar issue was already addressed in
    27c6861858fe81979085bf41f2ed7e7a986e8e1d, however, the chain
    which is used for custom endpoints was not covered.

--- modules/unsupported/s3-geotiff/src/test/java/org/geotools/s3/S3ConnectorTest.java
+++ modules/unsupported/s3-geotiff/src/test/java/org/geotools/s3/S3ConnectorTest.java
@@ -17,11 +17,20 @@
 package org.geotools.s3;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import com.amazonaws.services.s3.AmazonS3;
+import org.junit.Before;
 import org.junit.Test;
 
 public class S3ConnectorTest {
+
+    @Before
+    public void before() {
+        System.setProperty(
+                S3Connector.S3_GEOTIFF_CONFIG_PATH, "./src/test/resources/s3.properties");
+    }
+
     @Test
     public void testDefaultRegionWithDefaultCredentialChain() {
         S3Connector s3Connector = new S3Connector("s3://bucket/prefix/raster.tif");
@@ -54,5 +63,38 @@ public class S3ConnectorTest {
 
         AmazonS3 s3Client = s3Connector.getS3Client();
         assertEquals("US_East_2", s3Client.getRegion().name());
+    }
+
+    @Test
+    public void testDefaultRegionWithCustomEndpointChain() {
+        S3Connector s3Connector = new S3Connector("other://bucket/prefix/raster.tif");
+
+        AmazonS3 s3Client = s3Connector.getS3Client();
+        assertEquals("US_Standard", s3Client.getRegion().name());
+        assertEquals(
+                "http://your-other-s3-server/bucket/prefix",
+                s3Client.getUrl("bucket", "prefix").toString());
+    }
+
+    @Test
+    public void testRegionOverrideWithCustomEndpointChain() {
+        S3Connector s3Connector =
+                new S3Connector("other://bucket/prefix/raster.tif?awsRegion=US_EAST_2");
+
+        AmazonS3 s3Client = s3Connector.getS3Client();
+        assertEquals("US_East_2", s3Client.getRegion().name());
+        assertEquals(
+                "http://your-other-s3-server/bucket/prefix",
+                s3Client.getUrl("bucket", "prefix").toString());
+    }
+
+    @Test
+    public void throwsWithInvalidCustomEndpoint() {
+        S3Connector s3Connector = new S3Connector("invalid/url/raster.tif");
+
+        assertThrows(
+                "Following this style: s3Alias://bucket/filename",
+                IllegalArgumentException.class,
+                s3Connector::getS3Client);
     }
 }

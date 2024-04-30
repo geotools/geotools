7383308ba0a4b6839cf0712dfbf89f78a3ffee5b
Peter Smythe <petersmythe@users.noreply.github.com>
2024-04-30 16:16:55 +0200

    [GEOT-7557] Provide the ability for downstream applications (e.g. GeoServer) to append additional querystring parameters e.g. AuthKey to every request made by the HTTPClient (#4727)
    * To support HTTP Headers see github.com/geoserver/geoserver/pull/7552
    
    * Add AuthKey by appending to URL in HTTPClient
    
    * Undo test version in pom files
    
    * Fix SimpleHTTPClient
    
    * Add test cases
    
    * Fix review issues with URL
    
    * URL Host not Authority
    
    * Change AuthKey -> ExtraParams and from String to Map
    
    * URL encode keys and values and build a test that verifies this
    
    * Update modules/extension/wms/src/main/java/org/geotools/ows/wms/WebMapServer.java
    
    Improve grammar
    
    Co-authored-by: Jody Garnett <jody.garnett@gmail.com>
    
    * Update modules/library/http/src/main/java/org/geotools/http/HTTPClient.java
    
    Improve docs
    
    Co-authored-by: Jody Garnett <jody.garnett@gmail.com>
    
    * Petersmythe map string object (#2)
    
    Replace Map<String, Object>
    
    * Change null to emptyMap, add duplicate key check
    
    * Oops, when applying patch
    
    * Fix integer test
    
    * Fix spelling
    
    * To support HTTP Headers see github.com/geoserver/geoserver/pull/7552
    
    * Add AuthKey by appending to URL in HTTPClient
    
    * Undo test version in pom files
    
    * Fix SimpleHTTPClient
    
    * Add test cases
    
    * Fix review issues with URL
    
    * URL Host not Authority
    
    * Change AuthKey -> ExtraParams and from String to Map
    
    * URL encode keys and values and build a test that verifies this
    
    * Update modules/extension/wms/src/main/java/org/geotools/ows/wms/WebMapServer.java
    
    Improve grammar
    
    Co-authored-by: Jody Garnett <jody.garnett@gmail.com>
    
    * Update modules/library/http/src/main/java/org/geotools/http/HTTPClient.java
    
    Improve docs
    
    Co-authored-by: Jody Garnett <jody.garnett@gmail.com>
    
    * Petersmythe map string object (#2)
    
    Replace Map<String, Object>
    
    * Change null to emptyMap, add duplicate key check
    
    * Oops, when applying patch
    
    * Fix integer test
    
    * Fix spelling
    
    ---------
    
    Co-authored-by: Peter Smythe <peter@afrigis.co.za>
    Co-authored-by: Jody Garnett <jody.garnett@gmail.com>

--- modules/library/http/src/main/java/org/geotools/http/HTTPClient.java
+++ modules/library/http/src/main/java/org/geotools/http/HTTPClient.java
@@ -133,6 +133,16 @@ public interface HTTPClient {
     /** @param password the HTTP BASIC Authentication password */
     public void setPassword(String password);
 
+    /**
+     * Optional query parameters.
+     *
+     * @return querystring ExtraParams, or {@code emptyMap()} if not set
+     */
+    public Map<String, String> getExtraParams();
+
+    /** @param extraParams the optional querystring ExtraParams to be appended to finalURL */
+    public void setExtraParams(Map<String, String> extraParams);
+
     /** @return the tcp/ip connect timeout in seconds */
     public int getConnectTimeout();
 

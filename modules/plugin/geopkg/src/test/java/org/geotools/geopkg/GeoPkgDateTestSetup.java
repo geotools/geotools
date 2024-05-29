8d19d8d6dd5966bfea3c9c8bd55b461c5dd1c179
david.blasby <david.blasby@geocat.net>
2024-05-29 20:03:14 +0100

    online tests

--- modules/plugin/geopkg/src/test/java/org/geotools/geopkg/GeoPkgDateTestSetup.java
+++ modules/plugin/geopkg/src/test/java/org/geotools/geopkg/GeoPkgDateTestSetup.java
@@ -11,25 +11,30 @@ public class GeoPkgDateTestSetup extends JDBCDateTestSetup {
 
     @Override
     protected void createDateTable() throws Exception {
-        run("CREATE TABLE dates (id INTEGER PRIMARY KEY, d DATE, dt TIMESTAMP, t TIME)");
+
+        // we are doing direct inserts into the database
+        // dates MUST be in ISO-8601 date/time string in the form YYYY-MM-DDTHH:MM[:SS.SSS]Z
+
+        // NOTE: geopackage does NOT support TIME columns - just DATE and DATETIME
+        run("CREATE TABLE dates (id INTEGER PRIMARY KEY, d DATE, dt DATETIME, t TIME)");
 
         run(
                 "INSERT INTO dates VALUES (1,"
                         + "DATE('2009-06-28','localtime'), "
-                        + "strftime('%Y-%m-%d %H:%M:%S','2009-06-28 15:12:41','localtime' ),"
-                        + "strftime('%H:%M:%S','15:12:41','localtime')  )");
+                        + "'2009-06-28T15:12:41Z',"
+                        + "strftime('%H:%M:%S','15:12:41','utc')  )");
 
         run(
                 "INSERT INTO dates VALUES (2,"
                         + "DATE('2009-01-15','localtime'), "
-                        + "strftime('%Y-%m-%d %H:%M:%S','2009-01-15 13:10:12','localtime'),"
-                        + "strftime('%H:%M:%S','13:10:12','localtime')  )");
+                        + "'2009-01-15T13:10:12Z',"
+                        + "strftime('%H:%M:%S','13:10:12','utc')  )");
 
         run(
                 "INSERT INTO dates VALUES (3,"
                         + "DATE('2009-09-29','localtime'), "
-                        + "strftime('%Y-%m-%d %H:%M:%S','2009-09-29 17:54:23','localtime'),"
-                        + "strftime('%H:%M:%S','17:54:23','localtime')  )");
+                        + "'2009-09-29T17:54:23Z',"
+                        + "strftime('%H:%M:%S','17:54:23','utc')  )");
         String sql =
                 "INSERT INTO gpkg_contents (table_name, data_type, identifier, srs_id) VALUES "
                         + "('dates', 'features', 'dates', 4326)";

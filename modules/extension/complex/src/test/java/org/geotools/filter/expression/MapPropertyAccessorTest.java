fc724cbab0d1ab81367b2e84bb50755fe17304b0
Steve Ikeoka <steve.ikeoka@gdit.com>
2024-05-30 13:22:06 -0700

    [GEOT-7587] Improve handling of XPath expressions

---
+++ modules/extension/complex/src/test/java/org/geotools/filter/expression/MapPropertyAccessorTest.java
@@ -0,0 +1,37 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2024, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.filter.expression;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collections;
+import org.geotools.api.filter.expression.PropertyName;
+import org.geotools.factory.CommonFactoryFinder;
+import org.junit.Test;
+
+public class MapPropertyAccessorTest {
+
+    @Test
+    public void testAccessWithJavaMethod() {
+        PropertyName property =
+                CommonFactoryFinder.getFilterFactory().property("java.lang.Thread.sleep(30000)");
+        long start = System.currentTimeMillis();
+        property.evaluate(Collections.emptyMap());
+        long runtime = System.currentTimeMillis() - start;
+        assertTrue("java.lang.Thread.sleep(30000) was executed", runtime < 30000);
+    }
+}

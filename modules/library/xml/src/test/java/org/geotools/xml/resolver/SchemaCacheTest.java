d3ab583cf5f4cd239cf9fba63834a319905ddfc7
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2024-05-31 11:43:15 +0200

    [GEOT-7576] Use HttpClient in SchemaCache

--- modules/library/xml/src/test/java/org/geotools/xml/resolver/SchemaCacheTest.java
+++ modules/library/xml/src/test/java/org/geotools/xml/resolver/SchemaCacheTest.java
@@ -17,12 +17,20 @@
 
 package org.geotools.xml.resolver;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.Assert.assertTrue;
 
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import java.io.File;
 import java.io.PrintWriter;
 import java.net.URI;
+import org.geotools.http.commons.MultithreadedHttpClientFactory;
 import org.geotools.util.URLs;
+import org.geotools.util.factory.Hints;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -34,6 +42,10 @@ import org.junit.rules.TemporaryFolder;
  * @author Ben Caradoc-Davies (CSIRO Earth Science and Resource Engineering)
  */
 public class SchemaCacheTest {
+
+    public static final String MOCK_SCHEMA_LOCATION = "http://schemacache";
+
+    public static final String MOCK_HTTP_RESPONSE_BODY = "<schemaCacheMockHttpClientResponse>";
 
     @Rule public TemporaryFolder folder = new TemporaryFolder();
 
@@ -92,4 +104,38 @@ public class SchemaCacheTest {
         defaultXmlFile.createNewFile();
         assertTrue(SchemaCache.isSuitableDirectoryToContainCache(dataFolder));
     }
+
+    /** Test download with the HTTP client specified in the GeoTools hints */
+    @Test
+    public void downloadWithHttpClient() {
+        Hints.putSystemDefault(Hints.HTTP_CLIENT_FACTORY, SchemaCacheMockHttpClientFactory.class);
+        byte[] responseBody = SchemaCache.download(MOCK_SCHEMA_LOCATION);
+        Assert.assertArrayEquals(MOCK_HTTP_RESPONSE_BODY.getBytes(), responseBody);
+        Hints.removeSystemDefault(Hints.HTTP_CLIENT_FACTORY);
+    }
+
+    /**
+     * Test that a circular redirect is not followed indefinitely when using the multithreaded HTTP
+     * client
+     */
+    @Test
+    public void circularRedirectMultithreadedHttpClient() {
+        Hints.putSystemDefault(Hints.HTTP_CLIENT_FACTORY, MultithreadedHttpClientFactory.class);
+        String redirectUrl = "http://localhost:" + wireMockRule.port() + "/test";
+        wireMockRule.addStubMapping(
+                stubFor(
+                        get(urlEqualTo("/test"))
+                                .willReturn(
+                                        aResponse()
+                                                .withStatus(301)
+                                                .withHeader("Location", redirectUrl))));
+        byte[] responseBody = SchemaCache.download(redirectUrl);
+        Assert.assertNull(responseBody);
+        Hints.removeSystemDefault(Hints.HTTP_CLIENT_FACTORY);
+    }
+
+    // use a dynamic http port to avoid conflicts
+    @Rule
+    public WireMockRule wireMockRule =
+            new WireMockRule(WireMockConfiguration.options().dynamicPort());
 }

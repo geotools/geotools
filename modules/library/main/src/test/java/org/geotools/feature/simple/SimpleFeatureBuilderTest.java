f34cd5120de999908c2e335c9c918b076e6a0951
Jody Garnett <jody.garnett@gmail.com>
2024-07-04 19:46:43 -0700

    [GEOT-7612] ComplexAttribute validation of attribute values

--- modules/library/main/src/test/java/org/geotools/feature/simple/SimpleFeatureBuilderTest.java
+++ modules/library/main/src/test/java/org/geotools/feature/simple/SimpleFeatureBuilderTest.java
@@ -19,13 +19,17 @@ package org.geotools.feature.simple;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.geotools.api.feature.Attribute;
+import org.geotools.api.feature.IllegalAttributeException;
 import org.geotools.api.feature.simple.SimpleFeature;
 import org.geotools.api.feature.simple.SimpleFeatureType;
+import org.geotools.api.filter.Filter;
 import org.geotools.api.filter.FilterFactory;
 import org.geotools.api.filter.PropertyIsEqualTo;
 import org.geotools.api.referencing.crs.CoordinateReferenceSystem;
 import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.feature.AttributeTypeBuilder;
+import org.geotools.feature.type.Types;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -42,9 +46,19 @@ public class SimpleFeatureBuilderTest {
     @Before
     public void setUp() throws Exception {
         SimpleFeatureTypeBuilder typeBuilder = new SimpleFeatureTypeBuilder();
+        AttributeTypeBuilder attributeBuilder = new AttributeTypeBuilder();
+        FilterFactory ff = CommonFactoryFinder.getFilterFactory();
+
         typeBuilder.setName("test");
         typeBuilder.add("point", Point.class, (CoordinateReferenceSystem) null);
-        typeBuilder.add("integer", Integer.class);
+
+        // integer value is required, and must be non-negative
+        Filter nonNegative = ff.greaterOrEqual(ff.property("."), ff.literal(0));
+        attributeBuilder.setBinding(Integer.class);
+        attributeBuilder.setNillable(false);
+        attributeBuilder.addRestriction(nonNegative);
+        typeBuilder.add(attributeBuilder.buildDescriptor("integer"));
+
         typeBuilder.add("float", Float.class);
 
         featureType = typeBuilder.buildFeatureType();
@@ -71,14 +85,57 @@ public class SimpleFeatureBuilderTest {
     }
 
     @Test
-    public void testTooFewAttributes() throws Exception {
+    public void testValidation() {
+        GeometryFactory gf = new GeometryFactory();
+        Point point = gf.createPoint(new Coordinate(0, 0));
+
+        builder.setValidating(false);
+        builder.add(point);
+        builder.add(-1);
+        builder.add(0.0f);
+        SimpleFeature invalidFeature = builder.buildFeature("invalid");
+
+        Assert.assertTrue("valid", Types.isValid((Attribute) invalidFeature.getProperty("point")));
+        Assert.assertFalse(
+                "valid", Types.isValid((Attribute) invalidFeature.getProperty("integer")));
+        Assert.assertTrue("valid", Types.isValid((Attribute) invalidFeature.getProperty("float")));
+        Assert.assertFalse("invalid", Types.isValid(invalidFeature));
+
+        builder.setValidating(true);
+        builder.add(point);
+        try {
+            builder.add(-1);
+            Assert.fail(
+                    "Builder with validation enabled should not be able to add an invalid value");
+        } catch (IllegalAttributeException huh) {
+            // expected error
+        }
+
+        builder.reset();
+        builder.setValidating(false);
+        builder.add(point);
+        builder.add(-1);
+        builder.add(0.0f);
+
+        builder.setValidating(true);
+        try {
+            SimpleFeature brokenFeature = builder.buildFeature("broken");
+            Assert.assertNotNull(brokenFeature);
+            Assert.fail("Builder with validation should not produce invalid feature");
+        } catch (IllegalAttributeException huh) {
+            // expected error
+        }
+    }
+
+    @Test
+    public void testBeRelaxedAboutNumberOfAttributes() throws Exception {
         GeometryFactory gf = new GeometryFactory();
         Point point = gf.createPoint(new Coordinate(0, 0));
         builder.add(point);
         builder.add(Integer.valueOf(1));
 
-        // build feature uses default values for anything missing
-        SimpleFeature feature = builder.buildFeature("fid");
+        // be forgiving if too few values provided (use default values for anything missing)
+        SimpleFeature feature = builder.buildFeature("fid.1");
         Assert.assertNotNull(feature);
 
         Assert.assertEquals(3, feature.getAttributeCount());
@@ -90,13 +147,20 @@ public class SimpleFeatureBuilderTest {
         List<Object> tooFew = new ArrayList<>();
         tooFew.add(point);
 
-        // static build method requires values in the order specified
-        try {
-            SimpleFeature feature2 = SimpleFeatureBuilder.build(featureType, tooFew, "fid");
-            Assert.assertNotNull(feature2);
-        } catch (IllegalArgumentException expected) {
-            Assert.assertTrue(expected.getMessage().contains("count does not match"));
-        }
+        SimpleFeature feature2 = SimpleFeatureBuilder.build(featureType, tooFew, "fid.2");
+        Assert.assertNotNull(feature2);
+
+        Assert.assertSame("provided point", point, feature.getAttribute(0));
+        Assert.assertNotNull("default count", feature.getAttribute(1));
+
+        // be forgiving if too many values provided
+        List<Object> tooMany = new ArrayList<>();
+        tooMany.add(point);
+        tooMany.add(1);
+        tooMany.add(3.0f);
+        tooMany.add("Sasquatch");
+        SimpleFeature feature3 = SimpleFeatureBuilder.build(featureType, tooMany, "fid.3");
+        Assert.assertNotNull(feature3);
     }
 
     @Test

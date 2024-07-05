9ae47f40d8ff18a5a8222a15666c185a49617fdf
Jody Garnett <jody.garnett@gmail.com>
2024-07-04 19:46:43 -0700

    [GEOT-7612] ComplexAttribute validation of attribute values

--- modules/library/main/src/test/java/org/geotools/feature/type/TypesTest.java
+++ modules/library/main/src/test/java/org/geotools/feature/type/TypesTest.java
@@ -85,13 +85,17 @@ public class TypesTest {
 
         SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder(); // $NON-NLS-1$
         builder.setName("test");
-        builder.restriction(filter).add(attributeName, String.class);
+        builder.restriction(filter).minOccurs(1).nillable(false).add(attributeName, String.class);
         SimpleFeatureType featureType = builder.buildFeatureType();
 
         SimpleFeature feature =
                 SimpleFeatureBuilder.build(featureType, new Object[] {"Value"}, null);
 
         Assert.assertNotNull(feature);
+        Assert.assertTrue("valid", Types.isValid(feature));
+
+        feature.setAttribute("string", null);
+        Assert.assertFalse("invalid", Types.isValid(feature));
     }
 
     @Test

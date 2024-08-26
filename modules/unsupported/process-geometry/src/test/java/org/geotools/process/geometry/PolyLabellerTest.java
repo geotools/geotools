085bc036e81edca9915d08cb85a5ae540a3892b9
Jody Garnett <jody.garnett@gmail.com>
2024-08-25 23:16:41 -0700

    [GEOS-11532] Update to JTS 1.20.
    Update jts.version to 1.20.0
    
    Skeletonize code getting similar, but not exactly the same results
    
    MaximumInscribedCircle now starts from interior point, rather than centroid

--- modules/unsupported/process-geometry/src/test/java/org/geotools/process/geometry/PolyLabellerTest.java
+++ modules/unsupported/process-geometry/src/test/java/org/geotools/process/geometry/PolyLabellerTest.java
@@ -36,26 +36,9 @@ public class PolyLabellerTest {
         GeometryBuilder gb = new GeometryBuilder();
 
         Polygon p = (Polygon) reader.read("Polygon(( 0 0, 10 0, 10 10, 0 10, 0 0))");
-        Point point = (Point) PolyLabeller.getPolylabel(p, 1d);
+        Point point = PolyLabeller.getPolylabel(p, 1d);
 
         Point expected = gb.point(5, 5);
-        Assert.assertEquals(expected, point);
-    }
-
-    /**
-     * This one has infinite potential maximum inscribed circles
-     *
-     * @throws ParseException
-     */
-    @Test
-    public void testRectangle() throws ParseException {
-        WKTReader reader = new WKTReader();
-        GeometryBuilder gb = new GeometryBuilder();
-
-        Polygon p = (Polygon) reader.read("Polygon(( 0 0, 20 0, 20 10, 0 10, 0 0))");
-        Point point = (Point) PolyLabeller.getPolylabel(p, 1d);
-
-        Point expected = gb.point(10, 5);
         Assert.assertEquals(expected, point);
     }
 
@@ -69,15 +52,17 @@ public class PolyLabellerTest {
                 (Polygon)
                         reader.read(
                                 "POLYGON((0 5, 5 10, 10 6, 15 10, 20 5, 15 0, 10 4, 5 0, 0 5))");
-        Point point = (Point) PolyLabeller.getPolylabel(p, 1d);
 
+        Point point = PolyLabeller.getPolylabel(p, 1d);
+
+        // technically either expected 1 or expected 2 are good locations
         Point expected1 = gb.point(5, 5);
+        Point expected2 = gb.point(15, 5);
 
-        // This is also a valid result, but the algorithm at this point prefers the
-        // first.
-        // Point expected2 = gb.point(15, 5);
-
-        Assert.assertEquals(expected1, point);
+        // JTS 1.20.0 switched MaximumInscribedCircle grid start from centroid to interior point
+        double delta1 = point.distance(expected1);
+        double delta2 = point.distance(expected2);
+        Assert.assertTrue("close to expected label position", delta1 < 1.0 || delta2 < 1.0);
     }
 
     @Test
@@ -90,10 +75,12 @@ public class PolyLabellerTest {
                 (Polygon)
                         reader.read(
                                 "Polygon ((0 5, 5 10, 10 6, 15 10, 20 5, 15 0, 10 4, 5 0, 0 5),(5.4267578125 6.68164062499999822, 3.7451171875 5.30761718749999822, 5.365234375 3.21582031249999822, 8.3388671875 5.08203124999999822, 5.4267578125 6.68164062499999822))");
-        Point point = (Point) PolyLabeller.getPolylabel(p, 1d);
-
+        Point point = PolyLabeller.getPolylabel(p, 1d);
         Point expected = gb.point(15, 5);
-        Assert.assertEquals(expected, point);
+
+        // JTS 1.20.0 switched MaximumInscribedCircle grid start from centroid to interior point
+        double delta = point.distance(expected);
+        Assert.assertTrue("close to expected label position: " + delta, delta < 1.0);
     }
 
     @Test
@@ -106,14 +93,17 @@ public class PolyLabellerTest {
                 (MultiPolygon)
                         reader.read(
                                 "MultiPolygon (((0 5, 5 10, 10 5, 5 0, 0 5)),((11.74609375 1.6357421875, 11.7255859375 3.24609375, 13.9306640625 3.3076171875, 13.951171875 1.73828125, 11.74609375 1.6357421875)))");
-        Point point = (Point) PolyLabeller.getPolylabel(p, 1d);
+        Point point = PolyLabeller.getPolylabel(p, 1d);
+        Point expected = gb.point(5.0, 5.0);
 
-        Point expected = gb.point(5, 5);
-        Assert.assertEquals(expected, point);
+        // delta < 0.5 is good enough for precision 1 above
+        double delta = point.distance(expected);
+        Assert.assertTrue("close enough: " + delta, delta < 0.5);
     }
 
     @Test
     public void testBadInput() {
+
         GeometryBuilder gb = new GeometryBuilder();
         Polygon p = gb.polygon();
 

a29b382043a97868b29640483d08f33d448cba3d
aaime <andrea.aime@gmail.com>
2024-11-27 12:49:33 +0100

    [GEOT-7689] Logic filter equality checks incorrectly implemented

--- modules/library/main/src/main/java/org/geotools/filter/BinaryLogicAbstract.java
+++ modules/library/main/src/main/java/org/geotools/filter/BinaryLogicAbstract.java
@@ -16,6 +16,8 @@
  */
 package org.geotools.filter;
 
+import static java.util.Collections.frequency;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -27,6 +29,7 @@ public abstract class BinaryLogicAbstract extends AbstractFilter {
     protected BinaryLogicAbstract(List<org.geotools.api.filter.Filter> children) {
         this.children = children;
     }
+
     /** Returned list is unmodifieable. For a cheaper access option use visitor */
     public List<org.geotools.api.filter.Filter> getChildren() {
         return Collections.unmodifiableList(children);
@@ -43,7 +46,15 @@ public abstract class BinaryLogicAbstract extends AbstractFilter {
         BinaryLogicAbstract that = (BinaryLogicAbstract) o;
         if (children == null) return that.children == null;
         if (children.size() != that.children.size()) return false;
-        return children.containsAll(that.children);
+        // order is not important, but the values must be the same, and the comparison
+        // should take into account duplicates ("a AND a" is not the same as "a AND a AND a"
+        // even if the actual result is the same, the filter is different)
+
+        // this method takes all the elements from the first list and checks if they are present
+        // in the second list the same number of times. Couple with the size comparison above
+        // it should do the trick
+        return children.stream()
+                .allMatch(e -> frequency(children, e) == frequency(that.children, e));
     }
 
     @Override

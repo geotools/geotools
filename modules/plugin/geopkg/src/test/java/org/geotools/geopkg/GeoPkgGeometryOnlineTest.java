95afde23e10ebdfc84da3ff3ddb1f4d3251f8e89
aaime <andrea.aime@gmail.com>
2025-01-12 12:02:26 +0100

    [GEOT-7700] GeoPackage geometry type name is case sensitive

--- modules/plugin/geopkg/src/test/java/org/geotools/geopkg/GeoPkgGeometryOnlineTest.java
+++ modules/plugin/geopkg/src/test/java/org/geotools/geopkg/GeoPkgGeometryOnlineTest.java
@@ -16,6 +16,12 @@
  */
 package org.geotools.geopkg;
 
+import static org.geotools.geopkg.GeoPackage.GEOMETRY_COLUMNS;
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import org.geotools.jdbc.JDBCGeometryOnlineTest;
 import org.geotools.jdbc.JDBCGeometryTestSetup;
 
@@ -29,5 +35,24 @@ public class GeoPkgGeometryOnlineTest extends JDBCGeometryOnlineTest {
     @Override
     public void testLinearRing() throws Exception {
         // JD: GeoPkg does not do linear rings
+    }
+
+    @Override
+    protected Class checkGeometryType(Class geomClass) throws Exception {
+        Class result = super.checkGeometryType(geomClass);
+
+        // check the geometry column entry matches the expected contents
+        String tableName = tname("t" + geomClass.getSimpleName());
+        String sql = String.format("SELECT * FROM %s WHERE table_name = '%s'", GEOMETRY_COLUMNS, tableName);
+        try (Connection cx = dataStore.getDataSource().getConnection();
+                Statement st = cx.createStatement();
+                ResultSet rs = st.executeQuery(sql)) {
+            rs.next();
+            assertEquals(geomClass.getSimpleName().toUpperCase(), rs.getString("geometry_type_name"));
+            assertEquals("geom", rs.getString("column_name"));
+            assertEquals(4326, rs.getInt("srs_id"));
+        }
+
+        return result;
     }
 }

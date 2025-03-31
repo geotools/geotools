202347a64ff812796e814d858ce333fc62feb31b
skalesse <soeren.kalesse@dwd.de>
2025-03-31 09:17:49 +0200

    [GEOT-7632] WMS capabilities parsing should not fail if 'units' for a dimension is empty   - cleanup the javadoc with proper code block and citations   - improve argument validation of Dimension.java

--- modules/extension/wms/src/main/java/org/geotools/ows/wms/xml/Dimension.java
+++ modules/extension/wms/src/main/java/org/geotools/ows/wms/xml/Dimension.java
@@ -20,21 +20,49 @@ package org.geotools.ows.wms.xml;
  * Property class for holding and handling of property values declared in Dimension-element of a layer. In WMS 1.3.0
  * this is expanded to include Extent information documenting the valid data values for this range.
  *
- * <p>http://schemas.opengis.net/wms/1.1.1/WMS_MS_Capabilities.dtd
- * <!-- The Dimension element declares
- * the _existence_ of a dimension. -->
- * <!ELEMENT Dimension EMPTY > <!ATTLIST Dimension name CDATA #REQUIRED units CDATA #REQUIRED unitSymbol CDATA #IMPLIED>
+ * <p><a href="http://schemas.opengis.net/wms/1.1.1/WMS_MS_Capabilities.dtd">
+ * http://schemas.opengis.net/wms/1.1.1/WMS_MS_Capabilities.dtd</a>:
  *
- * <p>http://schemas.opengis.net/wms/1.3.0/capabilities_1_3_0.xsd <element name="Dimension"> <complexType>
- * <simpleContent> <extension base="string"> <attribute name="name" type="string" use="required"/> <attribute
- * name="units" type="string" use="required"/> <attribute name="unitSymbol" type="string"/> <attribute name="default"
- * type="string"/> <attribute name="multipleValues" type="boolean"/> <attribute name="nearestValue" type="boolean"/>
- * <attribute name="current" type="boolean"/> </extension> </simpleContent> </complexType> </element>
+ * <pre>{@code
+ * <!-- The Dimension element declares the _existence_ of a dimension. -->
+ * <!ELEMENT Dimension EMPTY >
+ * <!ATTLIST Dimension
+ *           name CDATA #REQUIRED
+ *           units CDATA #REQUIRED
+ *           unitSymbol CDATA #IMPLIED>
+ * }</pre>
+ *
+ * <p><a href="http://schemas.opengis.net/wms/1.3.0/capabilities_1_3_0.xsd">
+ * http://schemas.opengis.net/wms/1.3.0/capabilities_1_3_0.xsd</a>:
+ *
+ * <pre>{@code
+ * <element name="Dimension">
+ *   <complexType>
+ *     <simpleContent>
+ *       <extension base="string">
+ *         <attribute name="name" type="string" use="required"/>
+ *         <attribute name="units" type="string" use="required"/>
+ *         <attribute name="unitSymbol" type="string"/>
+ *         <attribute name="default" type="string"/>
+ *         <attribute name="multipleValues" type="boolean"/>
+ *         <attribute name="nearestValue" type="boolean"/>
+ *         <attribute name="current" type="boolean"/>
+ *       </extension>
+ *     </simpleContent>
+ *   </complexType>
+ * </element>
+ * }</pre>
  *
  * <p>According to <a href="https://portal.ogc.org/files/?artifact_id=14416">OpenGIS Web Map Service WMS Implementation
- * Specification - C.2 Declaring dimensions and their allowed value (page 52)</a>, units must not be missing, but their
- * values are allowed to be empty: <i>"If the dimensional quantity has no units (e.g. band number in a multi-wavelength
- * sensor), use the null string: units=""."</i>
+ * Specification - C.2 Declaring dimensions and their allowed value (page 52)</a>, the 'units' attribute must not be
+ * missing, but its value is allowed to be empty:
+ *
+ * <blockquote cite="https://portal.ogc.org/files/?artifact_id=14416">
+ *
+ * If the dimensional quantity has no units (e.g. band number in a multi-wavelength sensor), use the null string:
+ * units="".
+ *
+ * </blockquote>
  *
  * @version SVN $Id$
  * @author Per Engstrom, Curalia AB, pereng@gmail.com
@@ -54,30 +82,44 @@ public class Dimension {
     protected Extent extent = null;
 
     public Dimension(String name, String units, String unitSymbol) {
-        if (name == null || name.length() == 0) {
-            throw new IllegalArgumentException("Error creating Extent: parameter name must not be null!");
-        }
-        // unit must not be null, but can be empty according to WMS spec 1.3, page 52
-        if (units == null) {
-            throw new IllegalArgumentException("Error creating Extent: parameter units must not be null!");
-        }
-
+        // check arguments and throw IllegalArgumentException if not valid
+        validateArguments(name, units);
         this.name = name;
         this.units = units;
         this.unitSymbol = unitSymbol;
     }
 
     public Dimension(String name, String units) {
-        if (name == null || name.length() == 0) {
-            throw new IllegalArgumentException("Error creating Extent: parameter name must not be null!");
-        }
-        // unit must not be null, but can be empty according to WMS spec 1.3, page 52
-        if (units == null) {
-            throw new IllegalArgumentException("Error creating Extent: parameter units must not be null!");
-        }
-
+        // check arguments and throw IllegalArgumentException if not valid
+        validateArguments(name, units);
         this.name = name;
         this.units = units;
+    }
+
+    /**
+     * Check the two input arguments 'name' and 'units' for validity.
+     *
+     * <ul>
+     *   <li>'name': must neither be {@code null} nor empty
+     *   <li>'units': must not be {@code null}
+     * </ul>
+     *
+     * @param name the name attribute
+     * @param units the units attribute
+     */
+    private static void validateArguments(String name, String units) throws IllegalArgumentException {
+        // 'name' must not be null
+        if (name == null) {
+            throw new IllegalArgumentException("Error creating Dimension: parameter 'name' must not be null!");
+        }
+        // 'name' must not be empty
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Error creating Dimension: parameter 'name' must not be empty!");
+        }
+        // 'units' must not be null, but can be empty according to WMS spec 1.3, page 52
+        if (units == null) {
+            throw new IllegalArgumentException("Error creating Dimension: parameter 'units' must not be null!");
+        }
     }
 
     public String getName() {

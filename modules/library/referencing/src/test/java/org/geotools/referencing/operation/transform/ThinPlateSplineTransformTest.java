3fd98aff69bcb8274e6976fb2937539ed62b509b
murrayk <murrayhking@gmail.com>
2025-07-05 20:45:12 +0100

    okay mvn spotless:apply

--- modules/library/referencing/src/test/java/org/geotools/referencing/operation/transform/ThinPlateSplineTransformTest.java
+++ modules/library/referencing/src/test/java/org/geotools/referencing/operation/transform/ThinPlateSplineTransformTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;

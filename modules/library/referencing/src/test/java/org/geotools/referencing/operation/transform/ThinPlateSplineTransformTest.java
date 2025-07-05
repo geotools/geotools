31890e06835871d42fe20bc35e16b3c95fea8f48
murrayk <murrayhking@gmail.com>
2025-07-05 21:25:03 +0100

    checkstyle missing gnu licence

--- modules/library/referencing/src/test/java/org/geotools/referencing/operation/transform/ThinPlateSplineTransformTest.java
+++ modules/library/referencing/src/test/java/org/geotools/referencing/operation/transform/ThinPlateSplineTransformTest.java
@@ -14,7 +14,6 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
-
 package org.geotools.referencing.operation.transform;
 
 import static org.junit.Assert.assertEquals;

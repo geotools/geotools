ff60b73f691ea70b583272d143b93ae97fafb2c2
Daniele Romagnoli <daniele.romagnoli@geosolutionsgroup.com>
2025-11-21 17:46:56 +0100

    [GEOT-7850] vectormosaic: supporting common params; delegate params on DGGSDatastore

---
+++ modules/unsupported/dggs/dggs-h3/src/test/java/org/geotools/dggs/h3/H3DGGSDelegateDatastoreTest.java
@@ -0,0 +1,96 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2025, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.dggs.h3;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeFalse;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import org.geotools.api.data.DataStore;
+import org.geotools.api.data.Query;
+import org.geotools.api.filter.Filter;
+import org.geotools.api.filter.FilterFactory;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.dggs.datastore.DGGSDataStore;
+import org.geotools.dggs.datastore.DGGSStoreFactory;
+import org.geotools.dggs.gstore.DGGSFeatureSource;
+import org.geotools.factory.CommonFactoryFinder;
+import org.junit.Before;
+import org.junit.Test;
+
+public class H3DGGSDelegateDatastoreTest extends H3DGGSInstanceTest {
+
+    /** Flag to detect Windows OS */
+    private static final boolean IS_WINDOWS =
+            System.getProperty("os.name").toLowerCase().contains("windows");
+
+    String dataUri;
+
+    private static final FilterFactory FF = CommonFactoryFinder.getFilterFactory();
+
+    @Before
+    public void setUp() throws URISyntaxException, IOException {
+        // Skip test on Windows due to path handling differences
+        assumeFalse("Test disabled on Windows platform", IS_WINDOWS);
+        URL resource = getClass().getResource("/org/geotools/dggs/h3/localsample.parquet");
+        File file = new File(resource.toURI());
+        dataUri = file.toURI().toASCIIString();
+    }
+
+    @Test
+    public void testDelegateDatastore() throws IOException {
+        DGGSStoreFactory factory = new DGGSStoreFactory();
+        Map<String, Object> params = new HashMap<>();
+        // DGGSDatastore params
+        params.put(DGGSStoreFactory.ZONE_ID_COLUMN_NAME.key, "h3indexstr");
+        params.put(DGGSStoreFactory.RESOLUTION.key, "10");
+        params.put(DGGSStoreFactory.DGGS_FACTORY_ID.key, "H3");
+
+        // Delegate DataStore params
+        params.put(DGGSStoreFactory.DELEGATE_PREFIX + "dbtype", "geoparquet");
+        params.put(DGGSStoreFactory.DELEGATE_PREFIX + "uri", dataUri);
+        DGGSDataStore datastore = null;
+        try {
+            datastore = (DGGSDataStore) factory.createDataStore(params);
+            DataStore delegateDatastore = datastore.getDelegate();
+            assertEquals(
+                    "class org.geotools.data.geoparquet.GeoparquetDataStore",
+                    delegateDatastore.getClass().toString());
+            String[] typeNames = datastore.getTypeNames();
+            assertEquals(1, typeNames.length);
+            String typeName = typeNames[0];
+            assertEquals("localsample", typeName);
+            DGGSFeatureSource source = datastore.getFeatureSource(typeName);
+            int count = source.getFeatures().size();
+            assertEquals(548, count);
+            Filter filter = FF.greater(FF.property("landcover"), FF.literal("20"));
+            Query landcoverQuery = new Query(typeName, filter);
+            SimpleFeatureCollection above20 = source.getFeatures(landcoverQuery);
+            assertEquals(25, above20.size());
+
+        } finally {
+            if (datastore != null) {
+                datastore.dispose();
+            }
+        }
+    }
+}

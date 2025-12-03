4e8e47c36ade6e93ab2fc47c8ab3214cedae1300
Mikhail Lukyaniuk <lukyaniuk.mikhail@gmail.com>
2025-12-03 18:20:52 +0000

    [GEOT-7848] fix test failures after H2 removal

--- modules/plugin/geopkg/src/main/java/org/geotools/geopkg/GeoPkgDataStoreFactory.java
+++ modules/plugin/geopkg/src/main/java/org/geotools/geopkg/GeoPkgDataStoreFactory.java
@@ -218,7 +218,7 @@ public class GeoPkgDataStoreFactory extends JDBCDataStoreFactory {
 
     private static SQLiteConfig setupSQLiteConfig(Map<String, ?> params) throws IOException {
         SQLiteConfig config = new SQLiteConfig();
-        config.setSharedCache(true);
+        config.setSharedCache(false);
         config.enableLoadExtension(true);
         Object readOnly = READ_ONLY.lookUp(params);
         if (Boolean.TRUE.equals(readOnly)) {

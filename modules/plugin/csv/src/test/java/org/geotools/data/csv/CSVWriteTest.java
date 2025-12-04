d57a02eb14f0fd9e2aeee54dea7c060866c3f2c6
Alessandro Ricchiuti <alessandro.ricchiuti@geosolutionsgroup.com>
2025-12-04 14:03:23 +0100

    [GEOT-7849] Fixed indentation.

--- modules/plugin/csv/src/test/java/org/geotools/data/csv/CSVWriteTest.java
+++ modules/plugin/csv/src/test/java/org/geotools/data/csv/CSVWriteTest.java
@@ -522,8 +522,8 @@ public class CSVWriteTest {
         SimpleFeature feature, newFeature;
 
         try (FeatureReader<SimpleFeatureType, SimpleFeature> reader = store.getFeatureReader();
-             FeatureWriter<SimpleFeatureType, SimpleFeature> writer =
-                     duplicate.getFeatureWriterAppend(duplicate.getTypeNames()[0], Transaction.AUTO_COMMIT)) {
+                FeatureWriter<SimpleFeatureType, SimpleFeature> writer =
+                        duplicate.getFeatureWriterAppend(duplicate.getTypeNames()[0], Transaction.AUTO_COMMIT)) {
             while (reader.hasNext()) {
                 feature = reader.next();
                 newFeature = writer.next();

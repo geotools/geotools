9c84b3abbe5fc405c9096e6fef6dba375dfd06c4
Daniele Romagnoli <daniele.romagnoli@geosolutionsgroup.com>
2026-01-28 12:32:23 +0100

    [GEOT-7865]: ImageMosaic supporting PAMDataset from tiff headers/imagemetadata (#5511)

--- modules/plugin/imagemosaic/src/test/java/org/geotools/gce/imagemosaic/ImageMosaicCogMaskTest.java
+++ modules/plugin/imagemosaic/src/test/java/org/geotools/gce/imagemosaic/ImageMosaicCogMaskTest.java
@@ -21,6 +21,8 @@ import static org.junit.Assert.assertTrue;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import it.geosolutions.imageio.pam.PAMDataset;
+import it.geosolutions.imageio.pam.PAMParser;
 import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
 import java.io.File;
@@ -31,6 +33,7 @@ import org.geotools.api.parameter.GeneralParameterValue;
 import org.geotools.api.parameter.ParameterValue;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
+import org.geotools.coverage.grid.io.GridCoverage2DReader;
 import org.geotools.coverage.grid.io.HarvestedSource;
 import org.geotools.coverage.grid.io.footprint.FootprintBehavior;
 import org.geotools.test.TestData;
@@ -40,6 +43,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ImageMosaicCogMaskTest {
+
+    private static final double DELTA = 1E-4;
     private static WireMockServer wireMockServer;
 
     @BeforeClass
@@ -123,6 +128,22 @@ public class ImageMosaicCogMaskTest {
             raster.getPixel(190, 190, pixel);
             assertArrayEquals(new int[] {255, 255, 68, 255}, pixel);
             assertTrue(roi.contains(190, 190));
+
+            Object pamDataset = coverage.getProperty(GridCoverage2DReader.PAM_DATASET);
+            PAMDataset pam = (PAMDataset) pamDataset;
+            assertNotNull(pam);
+            PAMParser parser = PAMParser.getInstance();
+            PAMDataset.PAMRasterBand b0 = pam.getPAMRasterBand().get(0);
+            assertEquals(255, Double.parseDouble(parser.getMetadataValue(b0, "STATISTICS_MAXIMUM")), DELTA);
+            assertEquals(132.20475, Double.parseDouble(parser.getMetadataValue(b0, "STATISTICS_MEAN")), DELTA);
+            assertEquals(0, Double.parseDouble(parser.getMetadataValue(b0, "STATISTICS_MINIMUM")), DELTA);
+            assertEquals(127.4131677, Double.parseDouble(parser.getMetadataValue(b0, "STATISTICS_STDDEV")), DELTA);
+            PAMDataset.PAMRasterBand b2 = pam.getPAMRasterBand().get(2);
+            assertEquals(68, Double.parseDouble(parser.getMetadataValue(b2, "STATISTICS_MAXIMUM")), DELTA);
+            assertEquals(35.2546, Double.parseDouble(parser.getMetadataValue(b2, "STATISTICS_MEAN")), DELTA);
+            assertEquals(0, Double.parseDouble(parser.getMetadataValue(b2, "STATISTICS_MINIMUM")), DELTA);
+            assertEquals(33.976844, Double.parseDouble(parser.getMetadataValue(b2, "STATISTICS_STDDEV")), DELTA);
+
         } finally {
             if (reader != null) reader.dispose();
             if (coverage != null) coverage.dispose(true);

62b8e151a70898ba1b19e66de0504557bbf24572
Mark Prins <1165786+mprins@users.noreply.github.com>
2026-04-20 17:33:16 +0200

    Fix compiler warnings for AssignmentExpression, PatternMatchingInstanceof, StatementSwitchToExpressionSwitch, ignore compiler warnings for EffectivelyPrivate

--- modules/library/metadata/src/main/java/org/geotools/util/Base64.java
+++ modules/library/metadata/src/main/java/org/geotools/util/Base64.java
@@ -335,30 +335,31 @@ public class Base64 {
                 | (numSigBytes > 2 ? ((source[srcOffset + 2] << 24) >>> 24) : 0);
 
         switch (numSigBytes) {
-            case 3:
+            case 3 -> {
                 destination[destOffset] = ALPHABET[(inBuff >>> 18)];
                 destination[destOffset + 1] = ALPHABET[(inBuff >>> 12) & 0x3f];
                 destination[destOffset + 2] = ALPHABET[(inBuff >>> 6) & 0x3f];
                 destination[destOffset + 3] = ALPHABET[(inBuff) & 0x3f];
                 return destination;
-
-            case 2:
+            }
+            case 2 -> {
                 destination[destOffset] = ALPHABET[(inBuff >>> 18)];
                 destination[destOffset + 1] = ALPHABET[(inBuff >>> 12) & 0x3f];
                 destination[destOffset + 2] = ALPHABET[(inBuff >>> 6) & 0x3f];
                 destination[destOffset + 3] = EQUALS_SIGN;
                 return destination;
-
-            case 1:
+            }
+            case 1 -> {
                 destination[destOffset] = ALPHABET[(inBuff >>> 18)];
                 destination[destOffset + 1] = ALPHABET[(inBuff >>> 12) & 0x3f];
                 destination[destOffset + 2] = EQUALS_SIGN;
                 destination[destOffset + 3] = EQUALS_SIGN;
                 return destination;
-
-            default:
+            }
+            default -> {
                 return destination;
-        } // end switch
+            }
+        }
     } // end encode3to4
 
     /**
